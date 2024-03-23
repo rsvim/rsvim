4529f764aa1bbc867d5bd9f5c5d65b3a7776aade
linrongbin16 <linrongbin16@outlook.com>
2024-03-23 23:30:03 +0800

    chore

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,5 @@
-use crossterm::{cursor, execute, terminal};
-use std::io::stdout;
-
-use crossterm::style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor};
-
-pub fn example() -> std::io::Result<()> {
-  if !terminal::is_raw_mode_enabled()? {
-    terminal::enable_raw_mode()?;
-  }
-  let (cols, rows) = terminal::size()?;
-
-  execute!(
-    stdout(),
-    terminal::Clear(terminal::ClearType::All),
-    cursor::SetCursorStyle::BlinkingBar,
-    cursor::Show,
-    SetForegroundColor(Color::Yellow),
-    SetBackgroundColor(Color::DarkGrey),
-    Print(format!(
-      "Hello Rsvim! This is a {cols} rows, {rows} columns terminal!"
-    )),
-    ResetColor
-  )?;
-
-  Ok(())
-}
-
 #[cfg(test)]
 mod tests {
-  use super::*;
-
   #[test]
-  fn test_example() {
-    let actual = example();
-    println!("actual:{:?}", actual);
-    assert!(actual.is_ok());
-  }
+  fn test_example() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use crossterm::style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor};
+use crossterm::{cursor, execute, terminal};
+use std::io::stdout;
+
+pub fn hello() -> std::io::Result<()> {
+  if !terminal::is_raw_mode_enabled()? {
+    terminal::enable_raw_mode()?;
+  }
+  let (cols, rows) = terminal::size()?;
+
+  execute!(
+    stdout(),
+    terminal::Clear(terminal::ClearType::All),
+    cursor::SetCursorStyle::BlinkingBar,
+    cursor::Show,
+    SetForegroundColor(Color::Yellow),
+    SetBackgroundColor(Color::DarkGrey),
+    Print(format!(
+      "Hello Rsvim! This is a {cols} rows, {rows} columns terminal!"
+    )),
+    ResetColor
+  )?;
+
+  Ok(())
+}
+
 fn main() {
-  let _ = rsvim::example();
+  let _ = hello();
 }
