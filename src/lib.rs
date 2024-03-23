a9489b4e814988889177d19ca9816b914379e100
linrongbin16 <linrongbin16@outlook.com>
2024-03-23 23:34:20 +0800

    chore(deps): add more dependencies (#11)

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,5 @@
-use std::io::stdout;
-
-use crossterm::{
-  style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-  ExecutableCommand,
-};
-
-pub fn example() -> std::io::Result<()> {
-  stdout()
-    .execute(SetForegroundColor(Color::Yellow))?
-    .execute(SetBackgroundColor(Color::DarkGrey))?
-    .execute(Print("Hello Rsvim!"))?
-    .execute(ResetColor)?;
-
-  Ok(())
-}
-
 #[cfg(test)]
 mod tests {
-  use super::*;
-
   #[test]
-  fn test_example() {
-    let actual = example();
-    assert!(actual.is_ok());
-  }
+  fn test_example() {}
 }
