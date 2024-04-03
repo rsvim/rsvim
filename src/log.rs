98069ebae432af6c1dc41270be96a4b9023bfc96
linrongbin16 <linrongbin16@outlook.com>
2024-04-03 21:17:13 +0800

    chore

--- src/log.rs
+++ src/log.rs
@@ -4,35 +4,66 @@ use tracing::{self, debug, Level};
 use tracing_appender;
 use tracing_subscriber::{self, fmt, EnvFilter};
 
+enum LogMode {
+  ERROR,
+  INFO,
+  DEBUG,
+}
+
 pub fn init(cli: &Cli) {
-  let mut log_level = Level::WARN;
-  let mut use_file_appender = false;
-  let mut use_console_appender = true;
+  let mut mode = LogMode::ERROR;
   if cli.debug() {
-    log_level = Level::DEBUG;
-    use_file_appender = true;
-    use_console_appender = true;
+    mode = LogMode::DEBUG;
   } else if cli.verbose() {
-    log_level = Level::INFO;
-    use_console_appender = true;
+    mode = LogMode::INFO;
   }
 
-  let subscriber = tracing_subscriber::FmtSubscriber::builder()
-    .with_file(true)
-    .with_line_number(true)
-    .with_thread_ids(true)
-    .with_thread_names(true)
-    .with_level(true)
-    .with_ansi(false)
-    .with_env_filter(EnvFilter::from_default_env())
-    .with_max_level(log_level);
-  if use_file_appender {
-    subscriber.with_writer(tracing_appender::rolling::never(".", "rsvim.log"));
-  }
-  if use_console_appender {
-    subscriber.with_writer(io::stderr);
-  }
-  let subscriber = subscriber.finish();
-  tracing::subscriber::set_global_default(subscriber).expect("Failed to initialize tracing log");
-  debug!("Initialize tracing log");
+  match mode {
+    LogMode::DEBUG => {
+      let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .with_level(true)
+        .with_ansi(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_max_level(Level::TRACE)
+        .with_writer(tracing_appender::rolling::never(".", "rsvim.log"))
+        .with_writer(io::stderr)
+        .finish();
+      tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed to initialize tracing log");
+    }
+    LogMode::INFO => {
+      let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .with_level(true)
+        .with_ansi(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_max_level(Level::INFO)
+        .with_writer(io::stderr)
+        .finish();
+      tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed to initialize tracing log");
+    }
+    _ => {
+      let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .with_level(true)
+        .with_ansi(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_max_level(Level::ERROR)
+        .with_writer(io::stderr)
+        .finish();
+      tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed to initialize tracing log");
+    }
+  };
 }
