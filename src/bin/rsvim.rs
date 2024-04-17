660d776a4bc7e143e85d3f95698a01d5320438fd
linrongbin16 <linrongbin16@outlook.com>
2024-04-17 13:02:41 +0800

    chore(demo): use select for input loop (#22)

--- src/bin/rsvim.rs
+++ src/bin/rsvim.rs
@@ -1,17 +1,26 @@
+#![allow(unused_imports)]
+
 use clap::Parser;
+use crossterm::event::{
+  DisableFocusChange, DisableMouseCapture, EnableFocusChange, EnableMouseCapture, Event,
+  EventStream, KeyCode,
+};
 use crossterm::style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor};
 use crossterm::{cursor, execute, terminal};
+use futures::StreamExt;
 use rsvim::cli::Cli;
 use rsvim::log;
 use std::io::stdout;
+use std::time::Duration;
 use std::{thread, time};
-use tracing::{self, debug};
+use tracing::{debug, error};
 
-pub fn hello() -> std::io::Result<()> {
-  if !terminal::is_raw_mode_enabled()? {
-    terminal::enable_raw_mode()?;
-  }
+async fn input_loop() -> std::io::Result<()> {
+  terminal::enable_raw_mode()?;
   let (cols, rows) = terminal::size()?;
+
+  execute!(stdout(), EnableMouseCapture)?;
+  execute!(stdout(), EnableFocusChange)?;
 
   let msg = format!("Hello Rsvim! This is a {rows} row, {cols} column terminal!");
   execute!(
@@ -27,25 +36,32 @@ pub fn hello() -> std::io::Result<()> {
     ResetColor,
   )?;
 
-  let corners: Vec<(u16, u16)> = vec![(0, 0), (cols, 0), (0, rows), (cols, rows)];
-  for corner in corners {
-    let msg = format!("Here's column:{}, row:{}!", corner.0, corner.1);
-    let (mut c, r) = corner;
-    if c > 0 {
-      c -= msg.len() as u16;
+  let mut reader = EventStream::new();
+  loop {
+    tokio::select! {
+      event_result = reader.next() => match event_result {
+        Some(Ok(event)) => {
+          println!("Event::{:?}\r", event);
+          debug!("Event::{:?}", event);
+
+          if event == Event::Key(KeyCode::Char('c').into()) {
+            println!("Curosr position: {:?}\r", cursor::position());
+          }
+
+          if event == Event::Key(KeyCode::Esc.into()) {
+            break;
+          }
+        }
+        Some(Err(e)) => println!("Error: {:?}\r", e),
+        None => break,
+      }
     }
-    execute!(
-      stdout(),
-      cursor::MoveTo(c, r),
-      SetForegroundColor(Color::Yellow),
-      SetBackgroundColor(Color::DarkGrey),
-      Print(msg),
-      ResetColor,
-    )?;
-    thread::sleep(time::Duration::from_secs(1));
   }
 
   execute!(stdout(), terminal::LeaveAlternateScreen)?;
+
+  execute!(stdout(), DisableMouseCapture)?;
+  execute!(stdout(), DisableFocusChange)?;
 
   if terminal::is_raw_mode_enabled()? {
     terminal::disable_raw_mode()?;
@@ -55,9 +71,10 @@ pub fn hello() -> std::io::Result<()> {
   Ok(())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
   let cli = Cli::parse();
   log::init(&cli);
   debug!("cli: {:?}", cli);
-  let _ = hello();
+  input_loop().await
 }
