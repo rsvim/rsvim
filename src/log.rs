836b6ca20412763f633e031e6ac81cca221e7edd
linrongbin16 <linrongbin16@outlook.com>
2024-04-18 16:08:54 +0800

    chore

--- src/log.rs
+++ src/log.rs
@@ -1,112 +1,11 @@
 #![allow(dead_code)]
 
 use crate::cli::Cli;
-use std::io::{self, Stderr, StderrLock, Stdout, StdoutLock};
 use time::{format_description, Date, Month, OffsetDateTime, Time, UtcOffset};
 use tracing::{self, Level};
 use tracing_appender;
-use tracing_core;
-use tracing_subscriber::fmt::writer::MakeWriter;
 use tracing_subscriber::{self, EnvFilter};
 use tzdb;
-
-struct ConsoleMakeWriter {
-  stdout: Stdout,
-  stderr: Stderr,
-}
-
-enum ConsoleLock<'a> {
-  Stdout(StdoutLock<'a>),
-  Stderr(StderrLock<'a>),
-}
-
-impl<'a> io::Write for ConsoleLock<'a> {
-  fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-    match self {
-      ConsoleLock::Stdout(lock) => lock.write(buf),
-      ConsoleLock::Stderr(lock) => lock.write(buf),
-    }
-  }
-
-  fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
-    match self {
-      ConsoleLock::Stdout(lock) => lock.write_all(buf),
-      ConsoleLock::Stderr(lock) => lock.write_all(buf),
-    }
-  }
-
-  fn flush(&mut self) -> io::Result<()> {
-    match self {
-      ConsoleLock::Stdout(lock) => lock.flush(),
-      ConsoleLock::Stderr(lock) => lock.flush(),
-    }
-  }
-}
-
-impl<'a> MakeWriter<'a> for ConsoleMakeWriter {
-  type Writer = ConsoleLock<'a>;
-
-  fn make_writer(&'a self) -> Self::Writer {
-    ConsoleLock::Stdout(self.stdout.lock())
-  }
-
-  fn make_writer_for(&'a self, meta: &tracing_core::Metadata<'_>) -> Self::Writer {
-    if meta.level() <= &tracing_core::Level::WARN {
-      ConsoleLock::Stderr(self.stderr.lock())
-    } else {
-      ConsoleLock::Stdout(self.stdout.lock())
-    }
-  }
-}
-
-struct DebugConsoleMakeWriter {
-  stdout: Stdout,
-  stderr: Stderr,
-}
-
-enum DebugConsoleLock<'a> {
-  Stdout(StdoutLock<'a>),
-  Stderr(StderrLock<'a>),
-}
-
-impl<'a> io::Write for DebugConsoleLock<'a> {
-  fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-    match self {
-      DebugConsoleLock::Stdout(_) => Ok(0),
-      DebugConsoleLock::Stderr(lock) => lock.write(buf),
-    }
-  }
-
-  fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
-    match self {
-      DebugConsoleLock::Stdout(_) => Ok(()),
-      DebugConsoleLock::Stderr(lock) => lock.write_all(buf),
-    }
-  }
-
-  fn flush(&mut self) -> io::Result<()> {
-    match self {
-      DebugConsoleLock::Stdout(_) => Ok(()),
-      DebugConsoleLock::Stderr(lock) => lock.flush(),
-    }
-  }
-}
-
-impl<'a> MakeWriter<'a> for DebugConsoleMakeWriter {
-  type Writer = DebugConsoleLock<'a>;
-
-  fn make_writer(&'a self) -> Self::Writer {
-    DebugConsoleLock::Stdout(self.stdout.lock())
-  }
-
-  fn make_writer_for(&'a self, meta: &tracing_core::Metadata<'_>) -> Self::Writer {
-    if meta.level() <= &tracing_core::Level::WARN {
-      DebugConsoleLock::Stderr(self.stderr.lock())
-    } else {
-      DebugConsoleLock::Stdout(self.stdout.lock())
-    }
-  }
-}
 
 pub fn init(cli: &Cli) {
   if cli.debug() {
@@ -136,10 +35,7 @@ pub fn init(cli: &Cli) {
       .with_env_filter(EnvFilter::from_default_env())
       .with_max_level(Level::TRACE)
       .with_writer(tracing_appender::rolling::never(".", log_name))
-      .with_writer(DebugConsoleMakeWriter {
-        stdout: io::stdout(),
-        stderr: io::stderr(),
-      })
+      .with_writer(std::io::stderr)
       .finish();
     tracing::subscriber::set_global_default(subscriber).unwrap();
   } else {
@@ -154,10 +50,7 @@ pub fn init(cli: &Cli) {
       .with_level(true)
       .with_env_filter(EnvFilter::from_default_env())
       .with_max_level(log_level)
-      .with_writer(ConsoleMakeWriter {
-        stdout: io::stdout(),
-        stderr: io::stderr(),
-      })
+      .with_writer(std::io::stderr)
       .pretty()
       .finish();
     tracing::subscriber::set_global_default(subscriber).unwrap();
