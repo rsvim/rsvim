815c66aa04d6efe5abaf9db0a2ff49a8e761e8df
linrongbin16 <linrongbin16@outlook.com>
2024-04-22 17:17:14 +0800

    chore(poc): add ui module

--- src/ui.rs
+++ src/ui.rs
@@ -1,3 +1,5 @@
+pub mod pos;
+
 #[cfg(test)]
 mod tests {
   #[test]
---
+++ src/ui/pos.rs
@@ -0,0 +1,52 @@
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Pos<T> {
+  pub x: T,
+  pub y: T,
+}
+
+impl<T> Pos<T> {
+  fn new(x: T, y: T) -> Self {
+    Pos { x, y }
+  }
+
+  fn swap(self) -> Self {
+    Pos::new(self.y, self.x)
+  }
+}
+
+// Relative position.
+type RelPos = Pos<isize>;
+
+// Absolute position.
+type AbsPos = Pos<usize>;
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn should_all_zero_on_relpos_default() {
+    let p1: RelPos = Default::default();
+    let p2 = RelPos::new(0, 0);
+    assert_eq!(p1, p2);
+  }
+
+  #[test]
+  fn should_reverse_after_relpos_swap() {
+    let p1 = RelPos::new(1, 2);
+    assert_eq!(p1.swap(), RelPos::new(2, 1));
+  }
+
+  #[test]
+  fn should_all_zero_on_abspos_default() {
+    let p1: AbsPos = Default::default();
+    let p2 = AbsPos::new(0, 0);
+    assert_eq!(p1, p2);
+  }
+
+  #[test]
+  fn should_reverse_after_abspos_swap() {
+    let p1 = AbsPos::new(1, 2);
+    assert_eq!(p1.swap(), AbsPos::new(2, 1));
+  }
+}
