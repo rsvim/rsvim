8bc9f5487344d6dac3f1d904c21b119164729824
linrongbin16 <linrongbin16@outlook.com>
2024-04-22 18:02:45 +0800

    chore

--- src/ui/tree/node.rs
+++ src/ui/tree/node.rs
@@ -6,9 +6,4 @@ use std::rc::Rc;
 pub struct Node {
   pub parent: Option<Rc<RefCell<Node>>>,
   pub children: LinkedList<Rc<RefCell<Node>>>,
-  pub view: Rc<RefCell<View>>,
-}
-
-lazy_static! {
-  static ref ROOT: Node = {};
 }
