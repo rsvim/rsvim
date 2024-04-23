e74eed069643b01d61a24d4f947f5bdf496cfd38
linrongbin16 <linrongbin16@outlook.com>
2024-04-23 10:19:45 +0800

    chore

--- src/ui/vec/vec2d.rs
+++ src/ui/vec/vec2d.rs
@@ -1,5 +1,33 @@
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Vec2d<T> {
-  pub x: T,
-  pub y: T,
+pub struct Vec2d {
+  pub x: i32,
+  pub y: i32,
+}
+
+impl Vec2d {
+  pub fn new(x: i32, y: i32) -> Self {
+    Vec2d { x, y }
+  }
+
+  pub fn swap(self) -> Self {
+    Vec2d::new(self.y, self.x)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn should_all_zero_on_vec2d_default() {
+    let p1: Vec2d = Default::default();
+    let p2 = Vec2d::new(0, 0);
+    assert_eq!(p1, p2);
+  }
+
+  #[test]
+  fn should_reverse_after_vec2d_swap() {
+    let p1 = Vec2d::new(1, 2);
+    assert_eq!(p1.swap(), Vec2d::new(2, 1));
+  }
 }
