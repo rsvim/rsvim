6f6de6d5a2ff06aae869d455c0aa823cba342265
linrongbin16 <linrongbin16@outlook.com>
2024-04-23 10:15:18 +0800

    chore

--- src/ui/position.rs
+++ src/ui/position.rs
@@ -1,24 +1,24 @@
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Pos<T> {
+pub struct Position<T> {
   pub x: T,
   pub y: T,
 }
 
-impl<T> Pos<T> {
+impl<T> Position<T> {
   pub fn new(x: T, y: T) -> Self {
-    Pos { x, y }
+    Position { x, y }
   }
 
   pub fn swap(self) -> Self {
-    Pos::new(self.y, self.x)
+    Position::new(self.y, self.x)
   }
 }
 
 // Relative position.
-pub type RelPos = Pos<i32>;
+pub type RelPosition = Position<i32>;
 
 // Absolute position.
-pub type AbsPos = Pos<u32>;
+pub type AbsPosition = Position<u32>;
 
 #[cfg(test)]
 mod tests {
@@ -26,27 +26,27 @@ mod tests {
 
   #[test]
   fn should_all_zero_on_relpos_default() {
-    let p1: RelPos = Default::default();
-    let p2 = RelPos::new(0, 0);
+    let p1: RelPosition = Default::default();
+    let p2 = RelPosition::new(0, 0);
     assert_eq!(p1, p2);
   }
 
   #[test]
   fn should_reverse_after_relpos_swap() {
-    let p1 = RelPos::new(1, 2);
-    assert_eq!(p1.swap(), RelPos::new(2, 1));
+    let p1 = RelPosition::new(1, 2);
+    assert_eq!(p1.swap(), RelPosition::new(2, 1));
   }
 
   #[test]
   fn should_all_zero_on_abspos_default() {
-    let p1: AbsPos = Default::default();
-    let p2 = AbsPos::new(0, 0);
+    let p1: AbsPosition = Default::default();
+    let p2 = AbsPosition::new(0, 0);
     assert_eq!(p1, p2);
   }
 
   #[test]
   fn should_reverse_after_abspos_swap() {
-    let p1 = AbsPos::new(1, 2);
-    assert_eq!(p1.swap(), AbsPos::new(2, 1));
+    let p1 = AbsPosition::new(1, 2);
+    assert_eq!(p1.swap(), AbsPosition::new(2, 1));
   }
 }
