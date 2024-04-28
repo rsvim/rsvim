d83f5f96b53bcd8e0add2bb3c90fb0b5f8f5b913
linrongbin16 <linrongbin16@outlook.com>
2024-04-28 16:06:28 +0800

    chore

--- src/ui/view.rs
+++ src/ui/view.rs
@@ -9,15 +9,13 @@ use crate::ui::vec::vec2d::AbsVec2d;
 pub trait View {
   fn draw(&self, dvc: &Device);
 
-  fn offset(&self) -> RelPos;
-
-  fn abs_offset(&self) -> AbsPos;
+  fn offset(&self) -> AbsPos;
 
   fn size(&self) -> AbsVec2d;
+
+  fn relative_offset(&self) -> RelPos;
 
   fn parent(&self) -> Option<Rc<RefCell<dyn View>>>;
 
   fn children(&self) -> LinkedList<Rc<RefCell<dyn View>>>;
-
-  fn add_child(&self);
 }
