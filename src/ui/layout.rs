b1f0f097c4e4fdd2f1df9ae8bb967a6e09972edb
linrongbin16 <linrongbin16@outlook.com>
2024-04-29 10:17:34 +0800

    chore

--- src/ui/layout.rs
+++ src/ui/layout.rs
@@ -2,8 +2,26 @@ use std::cell::RefCell;
 use std::collections::LinkedList;
 use std::rc::Rc;
 
-use crate::ui::view::View;
+use crate::ui::view::{View, ViewRc};
 
 pub trait Layout {
-  fn children(&self) -> LinkedList<Rc<RefCell<dyn View>>>;
+  fn children(&self) -> LinkedList<ViewRc>;
+}
+
+pub type LayoutRc = Rc<RefCell<dyn Layout>>;
+
+pub struct HorizontalLayout {}
+
+impl Layout for HorizontalLayout {
+  fn children(&self) -> LinkedList<Rc<RefCell<dyn View>>> {
+    LinkedList::new()
+  }
+}
+
+pub struct VerticalLayout {}
+
+impl Layout for VerticalLayout {
+  fn children(&self) -> LinkedList<Rc<RefCell<dyn View>>> {
+    LinkedList::new()
+  }
 }
--- src/ui/view.rs
+++ src/ui/view.rs
@@ -2,7 +2,7 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 use crate::ui::device::Device;
-use crate::ui::layout::Layout;
+use crate::ui::layout::LayoutRc;
 use crate::ui::rect::{AbsPos, RelPos, Size};
 
 pub trait View {
@@ -14,7 +14,9 @@ pub trait View {
 
   fn abs_offset(&self) -> AbsPos;
 
-  fn parent(&self) -> Option<Rc<RefCell<dyn View>>>;
+  fn parent(&self) -> Option<ViewRc>;
 
-  fn layout(&self) -> Rc<RefCell<dyn Layout>>;
+  fn layout(&self) -> LayoutRc;
 }
+
+pub type ViewRc = Rc<RefCell<dyn View>>;
