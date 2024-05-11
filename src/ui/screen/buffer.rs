f29e8b97214a2655ca512066e1858512e3f9b4ae
linrongbin16 <linrongbin16@outlook.com>
2024-05-11 09:04:23 +0800

    chore

--- src/ui/screen/buffer.rs
+++ src/ui/screen/buffer.rs
@@ -1,7 +1,10 @@
 use crate::ui::rect::Size;
 use crate::ui::screen::cell::Cell;
 
-/// Screen buffer.
+/// Rendering buffer for all UI components, they will first write symbols/grapheme/characters to
+/// this buffer, then flushed to terminal screen. Terminal screen will save the buffer after flush,
+/// and use it to diff with next buffer, find out the difference and reduce the bytes flushed to
+/// backend terminal device, i.e. the crossterm library.
 ///
 /// * `size`: Buffer size.
 /// * `cells`: Buffer cells.
