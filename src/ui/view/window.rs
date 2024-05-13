588127a67fe1e7aaaf66a0f383644a4707d2e9ab
linrongbin16 <linrongbin16@outlook.com>
2024-05-13 15:14:15 +0800

    chore

--- src/ui/view/window.rs
+++ src/ui/view/window.rs
@@ -2,7 +2,6 @@ use crate::ui::layout::LayoutRc;
 use crate::ui::rect::{IPos, Size, UPos};
 use crate::ui::screen::Screen;
 use crate::ui::view::{View, ViewWk};
-use std::rc::{Rc, Weak};
 use tracing::debug;
 
 pub struct Window {
