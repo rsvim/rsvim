f83066165a66604f15a440f238c623dede16ba9b
linrongbin16 <linrongbin16@outlook.com>
2024-05-13 12:15:13 +0800

    chore

--- src/ui/screen.rs
+++ src/ui/screen.rs
@@ -57,8 +57,8 @@ pub async fn shutdown() -> IoResult<()> {
 }
 
 pub struct Screen {
-  prev_buf: Buffer,
   buf: Buffer,
+  prev_buf: Buffer,
 }
 
 impl Screen {
--- src/ui/view/root_window.rs
+++ src/ui/view/root_window.rs
@@ -1,3 +1,47 @@
-pub struct RootWindow {}
+use crate::ui::layout::LayoutRc;
+use crate::ui::rect::{IPos, Size, UPos};
+use crate::ui::screen::Screen;
+use crate::ui::view::{View, ViewWk};
+use tracing::debug;
 
-impl View for RootWindow {}
+pub struct RootWindow {
+  size: Size,
+}
+
+impl View for RootWindow {
+  fn offset(&self) -> IPos {
+    IPos::new(0, 0)
+  }
+
+  fn abs_offset(&self) -> UPos {
+    UPos::new(0, 0)
+  }
+
+  fn size(&self) -> Size {
+    self.size
+  }
+
+  fn zindex(&self) -> usize {
+    0
+  }
+
+  fn parent(&self) -> Option<ViewWk> {
+    None
+  }
+
+  fn layout(&self) -> Option<LayoutRc> {
+    None
+  }
+
+  fn draw(&self, screen: &Screen) {
+    debug!("draw");
+  }
+}
+
+impl RootWindow {
+  fn new(&screen: Screen) -> Self {
+    RootWindow {
+      size: screen.size(),
+    }
+  }
+}
