36c5a872271a7c34a5f3b668da34f483b88d2417
linrongbin16 <linrongbin16@outlook.com>
2024-05-13 15:05:45 +0800

    chore

--- src/ui/view/window.rs
+++ src/ui/view/window.rs
@@ -1,3 +1,43 @@
-pub struct Window {}
+use crate::ui::layout::LayoutRc;
+use crate::ui::rect::{IPos, Size, UPos};
+use crate::ui::screen::Screen;
+use crate::ui::view::{View, ViewWk};
+use tracing::debug;
 
-impl View for Window {}
+pub struct Window {
+  offset: IPos,
+  abs_offset: UPos,
+  size: Size,
+  zindex: usize,
+  parent: Option<ViewWk>,
+}
+
+impl View for Window {
+  fn offset(&self) -> IPos {
+    self.offset
+  }
+
+  fn abs_offset(&self) -> UPos {
+    self.abs_offset
+  }
+
+  fn size(&self) -> Size {
+    self.size
+  }
+
+  fn zindex(&self) -> usize {
+    self.zindex
+  }
+
+  fn parent(&self) -> Option<ViewWk> {
+    self.parent
+  }
+
+  fn layout(&self) -> Option<LayoutRc> {
+    None
+  }
+
+  fn draw(&self, screen: &Screen) {
+    debug!("draw");
+  }
+}
