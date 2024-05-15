3fba4833dbd5f058c681fe515b9be18632aa577b
linrongbin16 <linrongbin16@outlook.com>
2024-05-15 17:53:44 +0800

    chore

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-//! The core library for [RSVIM](https://github.com/rsvim/rsvim), the [VIM](https://www.vim.org/)
-//! editor reinvented in Rust+Typescript.
+//! The core library for [RSVIM](https://github.com/rsvim/rsvim), the VIM editor reinvented in
+//! Rust+Typescript.
 
 pub mod cli;
 pub mod log;
