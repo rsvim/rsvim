c0f42fe2bb715e7926192f9a9aa263093009e4b2
linrongbin16 <linrongbin16@outlook.com>
2024-05-16 10:46:42 +0800

    chore

--- src/ui/term/buffer.rs
+++ src/ui/term/buffer.rs
@@ -1,4 +1,4 @@
-use crate::ui::rect::Size;
+use crate::ui::rect::{Rect, Size, UPos};
 use crate::ui::term::cell::Cell;
 
 #[derive(Debug, Clone)]
@@ -20,6 +20,37 @@ impl Buffer {
       size,
       cells: vec![Cell::default(); size.area()],
     }
+  }
+
+  pub fn get_cell(&self, pos: UPos) -> &Cell {
+    &self.cells[pos.x * pos.y]
+  }
+
+  pub fn mut_get_cell(&mut self, pos: UPos) -> &mut Cell {
+    &mut self.cells[pos.x * pos.y]
+  }
+
+  pub fn set_cell(&mut self, pos: UPos, cell: Cell) -> &mut Self {
+    self.cells[pos.x * pos.y] = cell;
+    self
+  }
+
+  pub fn get_cells(&self, pos: UPos, count: usize) -> &[Cell] {
+    let start_at = pos.x * pos.y;
+    let end_at = start_at + count;
+    &self.cells[start_at..end_at]
+  }
+
+  pub fn mut_get_cells(&mut self, pos: UPos, count: usize) -> &mut [Cell] {
+    let start_at = pos.x * pos.y;
+    let end_at = start_at + count;
+    &mut self.cells[start_at..end_at]
+  }
+
+  pub fn set_cells(&mut self, pos: UPos, cells: Vec<Cell>) -> Vec<Cell> {
+    let start_at = pos.x * pos.y;
+    let end_at = start_at + cells.len();
+    self.cells.splice(start_at..end_at, cells).collect()
   }
 }
 
