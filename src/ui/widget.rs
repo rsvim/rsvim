443a1243afcf64ae8332c6c2f3cbc9fbdd13d878
linrongbin16 <linrongbin16@outlook.com>
2024-05-17 16:27:53 +0800

    chore

--- src/ui/widget.rs
+++ src/ui/widget.rs
@@ -11,7 +11,7 @@ pub mod root;
 
 /// Widget is the base trait of all UI components, it provide a common layer for receiving user
 /// inputs, keyboard/mouse events, and rendering itself on terminal.
-/// It is more of a logical data rather than a visible UI component.
+/// It is more of a logical container rather than a visible UI component.
 pub trait Widget {
   /// (Relative) offset based on parent widget.
   fn offset(&self) -> IPos;
