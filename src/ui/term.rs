7bc335ab9d05b2b11db5f37177551d506fb3c996
linrongbin16 <linrongbin16@outlook.com>
2024-05-20 11:49:34 +0800

    chore

--- src/ui/term.rs
+++ src/ui/term.rs
@@ -14,6 +14,79 @@ use tracing::debug;
 pub mod buffer;
 pub mod cell;
 
+pub async fn init() -> Result<Terminal> {
+  if !terminal::is_raw_mode_enabled()? {
+    terminal::enable_raw_mode()?;
+  }
+
+  let (cols, rows) = terminal::size()?;
+  let size = Size::new(rows as usize, cols as usize);
+  let mut out = std::io::stdout();
+
+  queue!(out, EnableMouseCapture)?;
+  queue!(out, EnableFocusChange)?;
+
+  queue!(
+    out,
+    terminal::EnterAlternateScreen,
+    terminal::Clear(terminal::ClearType::All),
+    cursor::SetCursorStyle::BlinkingBlock,
+    cursor::MoveTo(0, 0),
+    cursor::Show,
+  )?;
+
+  out.flush()?;
+
+  let t = Terminal {
+    prev_buf: Buffer::new(size),
+    buf: Buffer::new(size),
+  };
+  Ok(t)
+}
+
+pub async fn shutdown() -> Result<()> {
+  let mut out = std::io::stdout();
+  queue!(
+    out,
+    DisableMouseCapture,
+    DisableFocusChange,
+    terminal::LeaveAlternateScreen,
+  )?;
+
+  out.flush()?;
+
+  if terminal::is_raw_mode_enabled()? {
+    terminal::disable_raw_mode()?;
+  }
+
+  Ok(())
+}
+
+pub async fn run(_term: &mut Terminal) -> Result<()> {
+  let mut reader = EventStream::new();
+  loop {
+    tokio::select! {
+      maybe_event = reader.next() => match maybe_event {
+        Some(Ok(event)) => {
+          println!("Event::{:?}\r", event);
+          debug!("Event::{:?}", event);
+
+          if event == Event::Key(KeyCode::Char('c').into()) {
+            println!("Curosr position: {:?}\r", cursor::position());
+          }
+
+          if event == Event::Key(KeyCode::Esc.into()) {
+            break;
+          }
+        }
+        Some(Err(e)) => println!("Error: {:?}\r", e),
+        None => break,
+      }
+    }
+  }
+  Ok(())
+}
+
 /// Backend terminal
 pub struct Terminal {
   buf: Buffer,
@@ -21,77 +94,11 @@ pub struct Terminal {
 }
 
 impl Terminal {
-  pub async fn init() -> Result<Self> {
-    if !terminal::is_raw_mode_enabled()? {
-      terminal::enable_raw_mode()?;
-    }
-
-    let (cols, rows) = terminal::size()?;
-    let size = Size::new(rows as usize, cols as usize);
-    let mut out = std::io::stdout();
-
-    queue!(out, EnableMouseCapture)?;
-    queue!(out, EnableFocusChange)?;
-
-    queue!(
-      out,
-      terminal::EnterAlternateScreen,
-      terminal::Clear(terminal::ClearType::All),
-      cursor::SetCursorStyle::BlinkingBlock,
-      cursor::MoveTo(0, 0),
-      cursor::Show,
-    )?;
-
-    out.flush()?;
-
-    let t = Terminal {
+  pub fn new(size: Size) -> Self {
+    Terminal {
       prev_buf: Buffer::new(size),
       buf: Buffer::new(size),
-    };
-    Ok(t)
-  }
-
-  pub async fn shutdown(&self) -> Result<()> {
-    let mut out = self.out();
-    queue!(
-      out,
-      DisableMouseCapture,
-      DisableFocusChange,
-      terminal::LeaveAlternateScreen,
-    )?;
-
-    out.flush()?;
-
-    if terminal::is_raw_mode_enabled()? {
-      terminal::disable_raw_mode()?;
     }
-
-    Ok(())
-  }
-
-  pub async fn run(&mut self) -> Result<()> {
-    let mut reader = EventStream::new();
-    loop {
-      tokio::select! {
-        maybe_event = reader.next() => match maybe_event {
-          Some(Ok(event)) => {
-            println!("Event::{:?}\r", event);
-            debug!("Event::{:?}", event);
-
-            if event == Event::Key(KeyCode::Char('c').into()) {
-              println!("Curosr position: {:?}\r", cursor::position());
-            }
-
-            if event == Event::Key(KeyCode::Esc.into()) {
-              break;
-            }
-          }
-          Some(Err(e)) => println!("Error: {:?}\r", e),
-          None => break,
-        }
-      }
-    }
-    Ok(())
   }
 
   pub fn size(&self) -> Size {
@@ -105,22 +112,6 @@ impl Terminal {
   pub fn flush(&mut self) {
     self.prev_buf = self.buf.clone();
   }
-
-  pub fn out(&self) -> std::io::Stdout {
-    std::io::stdout()
-  }
-
-  pub fn err(&self) -> std::io::Stderr {
-    std::io::stderr()
-  }
-
-  #[cfg(test)]
-  pub fn new_from_size(size: Size) -> Self {
-    Terminal {
-      prev_buf: Buffer::new(size),
-      buf: Buffer::new(size),
-    }
-  }
 }
 
 #[cfg(test)]
@@ -130,7 +121,7 @@ mod tests {
   #[test]
   fn should_equal_on_terminal_new() {
     let sz = Size::new(1, 2);
-    let c1 = Terminal::new_from_size(sz);
+    let c1 = Terminal::new(sz);
     assert_eq!(c1.size(), c1.prev_size());
   }
 }
