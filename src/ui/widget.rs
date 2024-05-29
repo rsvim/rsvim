08b24137ab914dfb20fa61e1c9ccc248a9d20275
linrongbin16 <linrongbin16@outlook.com>
2024-05-29 10:10:28 +0800

    chore

--- src/ui/widget.rs
+++ src/ui/widget.rs
@@ -13,33 +13,35 @@ pub mod root;
 /// Widget is the base trait for all UI components, it provide a common layer for receiving user
 /// events (keyboard/mouse), and rendering itself on terminal. It is more of a logical container
 /// rather than a visible entity.
+///
 /// All widgets are maintained in a tree structure, i.e. the whole terminal is a root widget,
 /// everything inside it is children widgets, and nested recurse infinitely downwards.
 ///
 /// Here we have several terms:
+///
 /// * Parent: the direct upper side node in the tree structure.
 /// * Child: the direct down side node in the tree structure.
 /// * Ancestor: the indirect upper side node in the tree structure, i.e. parent of parent.
 /// * Offspring: the indirect down side node in the tree structure, i.e. child of child.
 ///
 /// The widget tree structure guarantee:
-/// 1. Children will be destroyed when their parent is, and are also displayed inside their parent's
-///    coordinate system, clipped by boundaries.
-/// 2. Children always cover their parent's display, for children who cover each other, higher
-///    [z-index](Widget::zindex()) will cover others.
-/// 2. Each widget can bind event handlers to handle the user events happening inside it & update
-///    the content.
-/// 3. The parent widget will be responsible for dispatching user events to the corresponding child
-///    widget, based on whether user event is happening within the range of the widget geometric
-///    shape.
-/// 4. Children's attributes are by default inherited from their parent, if not explicitly set.
+///
+/// 1. Children will be destroyed when their parent is, and are also displayed inside their
+///    parent's coordinate system, clipped by boundaries. Children are always on top of the canvas
+///    layer over their parent. For those who overlap each other, higher [z-index](Widget::zindex())
+///    wins.
+/// 2. Parent will dispatch user events to corresponding child if an event happens within the range
+///    of the parent widget geometric shape. Each widget can bind handlers to process & update the
+///    data.
+/// 3. Children's attributes are by default inherited from their parent, if not explicitly set.
 pub trait Widget {
   // { Life cycle
 
   /// Delete the widget itself (later), and remove it from parent.
-  /// Note: The widget cannot be just deleted right now, right here, due to some life cycle
-  /// management issue. For logic level, user should never use it after been deleted, for system
-  /// level, the memory will be released after all references on the smart pointer are removed.
+  ///
+  /// The widget cannot be just deleted right now, right here, due to life cycle management. For
+  /// logic level, user cannot use it after deleted, for system level, the memory will be released
+  /// after all references are removed.
   fn delete(&self);
 
   /// Create new widget based on the parent, with all default settings.
@@ -53,23 +55,27 @@ pub trait Widget {
   fn id(&self) -> usize;
 
   /// Get (relative) offset based on parent widget.
-  /// Note: The anchor is always NW (North-West).
+  ///
+  /// The anchor is always NW (North-West).
   fn offset(&self) -> IPos;
 
   /// Set (relative) offset.
   fn set_offset(&mut self, value: IPos);
 
   /// Get absolute offset based on whole [terminal](crate::ui::term::Terminal).
-  /// Note: The anchor is always north-west.
+  ///
+  /// The anchor is always NW (North-West).
   fn abs_offset(&self) -> UPos;
 
   /// Get size.
   fn size(&self) -> Size;
 
   /// Control arrange content stack when multiple children conflict on each other.
+  ///
   /// A widget that has a higher z-index will be put to the top of the parent's widget's stack,
   /// which has higher priority to be displayed.
-  /// Note: z-index only works for the children stack under the same parent, a child widget will
+  ///
+  /// Z-index only works for the children stack under the same parent, a child widget will
   /// always cover/override its parent. To change the visibility priority between children and
   /// parent, you need to directly set another parent for the children, or even switch the
   /// relationship between children and parent, i.e. make child the parent, make parent the child.
@@ -78,12 +84,19 @@ pub trait Widget {
   /// Set z-index value.
   fn set_zindex(&mut self, value: usize);
 
-  /// Whether the widget is visible. When a widget is invisible, user event will no longer been
-  /// received or processed, not be rendered to terminal, just like it's been deleted.
+  /// Whether the widget is visible.
+  ///
+  /// When a widget is invisible, user event will no longer been received or processed, and not
+  /// rendered to terminal, just like it's been deleted.
   fn visible(&self) -> bool;
 
   /// Set the widget visible/invisible.
   ///
+  /// Make a widget invisible also implicitly invisibles all its children and offsprings, unless
+  /// they have been explicitly been visibled.
+  ///
+  /// Make a widget visible also implicitly visibles all its children and offsprings, unless they
+  /// have been explicitly been invisibled.
   fn set_visible(&mut self, value: bool);
 
   /// Whether the widget is enabled. When a widget is disabled, user event will no longer been
@@ -102,7 +115,8 @@ pub trait Widget {
   // { Parent-child relationship
 
   /// Parent widget.
-  /// Note: root widget doesn't have a parent.
+  ///
+  /// Root widget doesn't have a parent.
   fn parent(&self) -> Option<WidgetWk>;
 
   /// Change parent widget.
@@ -122,17 +136,22 @@ pub trait Widget {
   // { Event
 
   /// Process a user keyboard/mouse event.
+  ///
   /// Parent widget will first try to dispatch the event to its children based on their geometric
   /// shape, i.e. if an event happens inside one of its child, the parent will first dispatch the
   /// event to that child.
+  ///
   /// If there're multiple children can handle an event, the one who has a higher z-index value
   /// wins. If there's no children can handle the event, the parent will then try to handle it by
   /// itself.
+  ///
   /// When the child returns `true`, the event is been handled, and the parent doesn't need to
-  /// handle it. Note: if the child want its parent to also handle the event (again), it has to
+  /// handle it. If the child want its parent to also handle the event (again), it has to
   /// explicitly call the parent's `event` method.
+  ///
   /// When the child returns `false`, the event is been ignored, thus the parent will then try to
   /// find the next child to handle it.
+  ///
   /// When a child is invisible, the event is been ignored, thus next child or the parent will
   /// handle it.
   /// When a child is disabled, the event is been ignored, but
