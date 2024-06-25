182849bff466a05a17c12a01731d162606b06be2
linrongbin16 <linrongbin16@outlook.com>
2024-06-25 13:32:52 +0800

    chore

--- src/ui/widget/cursive.rs
+++ src/ui/widget/cursive.rs
@@ -1,17 +1,22 @@
 //! Cursor widget.
 //! Note: To avoid naming confliction with `crossterm::cursor`, here name it `cursive`.
 
+use crate::geo::pos::U16Pos;
 use crate::geo::size::Size;
 use crate::ui::widget::{ChildWidgetsRw, Widget, WidgetRw};
 use crate::uuid;
 
 pub struct Cursive {
   id: usize,
+  pos: U16Pos,
 }
 
 impl Cursive {
-  pub fn new() -> Self {
-    Cursive { id: uuid::next() }
+  pub fn new(pos: U16Pos) -> Self {
+    Cursive {
+      id: uuid::next(),
+      pos,
+    }
   }
 }
 
