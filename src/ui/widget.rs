e642261e20a8f80ddb790daf2314cd40b5bbbe41
linrongbin16 <linrongbin16@outlook.com>
2024-07-10 17:41:13 +0800

    chore

--- src/ui/widget.rs
+++ src/ui/widget.rs
@@ -104,7 +104,7 @@ pub trait Widget {
   }
 
   /// Calculate absolute position, based on relative position and parent's absolute position.
-  /// If there's no parent widget, the relative position is already absolute itself.
+  /// If there's no parent widget, the relative position itself is already absolute.
   fn get_absolute_pos(&self, terminal_size: U16Size) -> UPos {
     match self.parent() {
       Some(parent) => {
