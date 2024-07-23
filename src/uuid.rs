1cd0d1dbbb14206b8e1e7265fcb1562659c85fb3
linrongbin16 <linrongbin16@outlook.com>
2024-07-23 11:14:51 +0800

    chore

--- src/uuid.rs
+++ src/uuid.rs
@@ -2,7 +2,7 @@
 
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-/// Get the next global unique ID.
+/// Returns the next global unique ID.
 pub fn next() -> usize {
   static GLOBAL: AtomicUsize = AtomicUsize::new(0usize);
   GLOBAL.fetch_add(0, Ordering::SeqCst)
