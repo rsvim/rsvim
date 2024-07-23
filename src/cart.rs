15d00bb4ec980e6797db7c329139659aa21fed18
linrongbin16 <linrongbin16@outlook.com>
2024-07-23 14:57:04 +0800

    chore

--- src/cart.rs
+++ src/cart.rs
@@ -208,22 +208,18 @@ mod tests {
     assert_eq!(mem::size_of_val(&actual1_h), mem::size_of_val(&1_u8));
     assert_eq!(mem::size_of_val(&actual1_w), mem::size_of_val(&2_u8));
 
-    let s2: U16Rect = U16Rect::new((15_u16, 25_u16), (35_u16, 45_u16));
-    let actual2 = geo_rect_as!(s2, i32);
-    let actual2_min = actual2.min();
-    let actual2_max = actual2.max();
-    assert_eq!(mem::size_of_val(&actual2_min.x), mem::size_of_val(&15_i32));
-    assert_eq!(mem::size_of_val(&actual2_min.y), mem::size_of_val(&25_i32));
-    assert_eq!(mem::size_of_val(&actual2_max.x), mem::size_of_val(&35_i32));
-    assert_eq!(mem::size_of_val(&actual2_max.y), mem::size_of_val(&45_i32));
+    let s2: U16Size = U16Size::new(15_u16, 25_u16);
+    let actual2 = geo_size_as!(s2, i32);
+    let actual2_w = actual2.width();
+    let actual2_h = actual2.height();
+    assert_eq!(mem::size_of_val(&actual2_h), mem::size_of_val(&15_i32));
+    assert_eq!(mem::size_of_val(&actual2_w), mem::size_of_val(&25_i32));
 
-    let s3: Rect<u32> = Rect::new((78_u32, 88_u32), (99_u32, 100_u32));
-    let actual3 = geo_rect_as!(s3, i16);
-    let actual3_min = actual3.min();
-    let actual3_max = actual3.max();
-    assert_eq!(mem::size_of_val(&actual3_min.x), mem::size_of_val(&78_i16));
-    assert_eq!(mem::size_of_val(&actual3_min.y), mem::size_of_val(&88_i16));
-    assert_eq!(mem::size_of_val(&actual3_max.x), mem::size_of_val(&99_i16));
-    assert_eq!(mem::size_of_val(&actual3_max.y), mem::size_of_val(&100_i16));
+    let s3: Size<u32> = Size::new(78_u32, 88_u32);
+    let actual3 = geo_size_as!(s3, i16);
+    let actual3_h = actual3.height();
+    let actual3_w = actual3.width();
+    assert_eq!(mem::size_of_val(&actual3_h), mem::size_of_val(&78_i16));
+    assert_eq!(mem::size_of_val(&actual3_w), mem::size_of_val(&88_i16));
   }
 }
