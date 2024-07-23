e4dc0286e5fd33e4828aad959e946dab7ff7073d
linrongbin16 <linrongbin16@outlook.com>
2024-07-23 16:55:01 +0800

    chore

--- src/uuid.rs
+++ src/uuid.rs
@@ -4,6 +4,25 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 
 /// Returns the next global unique ID.
 pub fn next() -> usize {
-  static GLOBAL: AtomicUsize = AtomicUsize::new(0usize);
-  GLOBAL.fetch_add(0, Ordering::SeqCst)
+  static GLOBAL: AtomicUsize = AtomicUsize::new(0_usize);
+  GLOBAL.fetch_add(1_usize, Ordering::SeqCst)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn incremental_ids() {
+    let start = next();
+    for i in 0..10 {
+      let actual = next();
+      println!(
+        "uuid::incremental_ids i+start+1:{},next:{}",
+        i + start + 1,
+        actual
+      );
+      assert_eq!(actual, i + start + 1);
+    }
+  }
 }
