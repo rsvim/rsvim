62a830521f6e8e2b7a7e1b870044987523c031c1
linrongbin16 <linrongbin16@outlook.com>
2024-07-23 14:18:33 +0800

    chore(poc): add unit tests

--- src/cart.rs
+++ src/cart.rs
@@ -146,7 +146,7 @@ mod tests {
   use std::mem;
 
   #[test]
-  fn point_should_cast_types() {
+  fn cast_geo_points() {
     let p1: IPos = point!(x: 1, y: 2);
     let actual1 = geo_point_as!(p1, usize);
     let actual1_x = actual1.x();
@@ -170,7 +170,7 @@ mod tests {
   }
 
   #[test]
-  fn rect_should_cast_types() {
+  fn cast_geo_rects() {
     let r1: IRect = IRect::new((1, 2), (3, 4));
     let actual1 = geo_rect_as!(r1, u8);
     let actual1_min = actual1.min();
@@ -191,6 +191,34 @@ mod tests {
 
     let r3: Rect<u32> = Rect::new((78_u32, 88_u32), (99_u32, 100_u32));
     let actual3 = geo_rect_as!(r3, i16);
+    let actual3_min = actual3.min();
+    let actual3_max = actual3.max();
+    assert_eq!(mem::size_of_val(&actual3_min.x), mem::size_of_val(&78_i16));
+    assert_eq!(mem::size_of_val(&actual3_min.y), mem::size_of_val(&88_i16));
+    assert_eq!(mem::size_of_val(&actual3_max.x), mem::size_of_val(&99_i16));
+    assert_eq!(mem::size_of_val(&actual3_max.y), mem::size_of_val(&100_i16));
+  }
+
+  #[test]
+  fn cast_geo_sizes() {
+    let s1: ISize = ISize::new(1, 2);
+    let actual1 = geo_size_as!(s1, u8);
+    let actual1_w = actual1.width();
+    let actual1_h = actual1.height();
+    assert_eq!(mem::size_of_val(&actual1_h), mem::size_of_val(&1_u8));
+    assert_eq!(mem::size_of_val(&actual1_w), mem::size_of_val(&2_u8));
+
+    let s2: U16Rect = U16Rect::new((15_u16, 25_u16), (35_u16, 45_u16));
+    let actual2 = geo_rect_as!(s2, i32);
+    let actual2_min = actual2.min();
+    let actual2_max = actual2.max();
+    assert_eq!(mem::size_of_val(&actual2_min.x), mem::size_of_val(&15_i32));
+    assert_eq!(mem::size_of_val(&actual2_min.y), mem::size_of_val(&25_i32));
+    assert_eq!(mem::size_of_val(&actual2_max.x), mem::size_of_val(&35_i32));
+    assert_eq!(mem::size_of_val(&actual2_max.y), mem::size_of_val(&45_i32));
+
+    let s3: Rect<u32> = Rect::new((78_u32, 88_u32), (99_u32, 100_u32));
+    let actual3 = geo_rect_as!(s3, i16);
     let actual3_min = actual3.min();
     let actual3_max = actual3.max();
     assert_eq!(mem::size_of_val(&actual3_min.x), mem::size_of_val(&78_i16));
