5a3c5771bd4f38b49c96636d7f1994ab5aa0e5f1
linrongbin16 <linrongbin16@outlook.com>
2024-07-24 17:18:56 +0800

    chore

--- src/test.rs
+++ src/test.rs
@@ -2,4 +2,20 @@
 //!
 //! Note: This module should only be used in unit tests, not some where else.
 
-pub mod log;
+#[cfg(test)]
+mod log {
+  pub fn init() {
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+      .with_file(true)
+      .with_line_number(true)
+      .with_thread_ids(true)
+      .with_thread_names(true)
+      .with_level(true)
+      .with_ansi(true)
+      .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+      .with_max_level(tracing::Level::TRACE)
+      .with_writer(std::io::stderr)
+      .finish();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+  }
+}
