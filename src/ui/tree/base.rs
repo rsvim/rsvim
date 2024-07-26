17e67aea141a6e6f589cf4cb38bc466331417d06
linrongbin16 <linrongbin16@outlook.com>
2024-07-26 21:49:32 +0800

    chore

--- src/ui/tree/base.rs
+++ src/ui/tree/base.rs
@@ -5,21 +5,19 @@ use std::collections::VecDeque;
 use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
 
 use crate::ui::tree::edge::Edge;
-use crate::ui::tree::node::{make_node_ptr, NodeAttribute, NodeId, NodePtr};
+use crate::ui::tree::node::{make_node_ptr, Node, NodeAttribute, NodeId, NodePtr};
 use crate::ui::widget::layout::root::RootLayout;
 use crate::ui::widget::Widget;
 use crate::uuid;
 
-use super::Node;
-
 /// The base of a widget tree.
 ///
-/// Note: The tree itself is also a node, i.e. the root node exists along with the tree.
+/// Note: The tree itself is also a node, i.e. the root node is created along with the tree.
 pub struct TreeBase {
   // Root {
 
-  // The root node is a dummy node, and is always created along with the tree.
-  // All the other node related collections don't contain the root node.
+  // The tree itself is the root node.
+  // All the other node collections don't contain the root node.
   //
   // Root node id.
   root_node_id: NodeId,
@@ -31,10 +29,12 @@ pub struct TreeBase {
 
   // Node {
 
-  // A collection of all nodes, maps from node ID to node struct.
+  // A collection of all nodes.
+  // Maps from node ID to node struct.
   nodes: BTreeMap<NodeId, NodePtr>,
 
-  // A collection of all node attributes, maps from node ID to node attribute.
+  // A collection of all node attributes.
+  // Maps from node ID to node attribute.
   attributes: HashMap<NodeId, NodeAttribute>,
 
   // Node }
@@ -52,9 +52,10 @@ pub struct TreeBase {
   // Edge }
 }
 
-pub struct RemovedSubTree {}
-
 impl TreeBase {
+  /// Make a widget tree.
+  ///
+  /// Note: The root node is created along with the tree.
   pub fn new() -> Self {
     let root_node = RootLayout::new();
     let root_node_id = root_node.id();
@@ -67,5 +68,24 @@ impl TreeBase {
       children_ids: HashMap::new(),
       parent_ids: HashMap::new(),
     }
+  }
+
+  /// Whether the tree is empty (except the root node).
+  pub fn is_empty(&self) -> bool {
+    self.nodes.is_empty()
+      && self.attributes.is_empty()
+      && self.edges.is_empty()
+      && self.children_ids.is_empty()
+      && self.parent_ids.is_empty()
+  }
+
+  /// Get the collection of all nodes.
+  pub fn nodes(&self) -> &BTreeMap<NodeId, NodePtr> {
+    &self.nodes
+  }
+
+  /// Get the collection of all node attributes.
+  pub fn attributes(&self) -> &HashMap<NodeId, NodeAttribute> {
+    &self.attributes
   }
 }
