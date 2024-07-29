b3e684bfd886ae330de258a611bc84a893e76de0
linrongbin16 <linrongbin16@outlook.com>
2024-07-29 12:15:40 +0800

    chore

--- src/ui/tree/internal/inode.rs
+++ src/ui/tree/internal/inode.rs
@@ -1,9 +1,11 @@
 //! Internal tree structure implementation: the `Inode` structure.
 
 use std::collections::VecDeque;
+use std::fs::write;
+use std::ptr::write;
 use std::sync::{Arc, RwLock, Weak};
 
-use crate::cart::{IRect, U16Rect};
+use crate::cart::{shapes, IRect, U16Rect};
 use crate::uuid;
 
 #[derive(Debug, Clone)]
@@ -50,10 +52,6 @@ impl<T> Inode<T> {
     }
   }
 
-  pub fn parent(&self) -> Option<InodeWk<T>> {
-    self.parent
-  }
-
   pub fn id(&self) -> usize {
     self.id
   }
@@ -66,18 +64,57 @@ impl<T> Inode<T> {
     &self.value
   }
 
+  // Parent {
+
+  pub fn parent(&self) -> Option<InodeWk<T>> {
+    self.parent
+  }
+
+  // Parent }
+
   // Children {
 
   pub fn children(&self) -> Option<&Vec<InodePtr<T>>> {
     self.children
   }
 
-  pub fn children_mut(&mut self) -> Option<&mut Vec<InodePtr<T>>> {
-    &mut self.children
+  /// Push a child node.
+  /// This operation also calculates and updates the actual shape for the pushed node and all its
+  /// descendant children.
+  pub fn push(&mut self, child: InodePtr<T>) {
+    if self.children.is_none() {
+      self.children = Some(vec![]);
+    }
+    self.children.unwrap().push(child);
+
+    let mut child1 = child.write().unwrap();
+    child1.attr.actual_shape =
+      shapes::convert_to_actual_shape(child1.attr.shape, self.attr.actual_shape);
+
+    let mut q: VecDeque<(InodePtr<T>, InodePtr<T>)> = match child1.children {
+      Some(children) => children.iter().map(|child| (child1, child)).collect(),
+      None => vec![].iter().collect(),
+    };
+    while let Some(parent_child_pair) = q.pop_front() {
+      let parent2 = parent_child_pair.0;
+      let child2 = parent_child_pair.1;
+      let shape = child2.read().unwrap().attr.shape;
+      let parent_actual_shape = parent2.read().unwrap().attr.actual_shape;
+      child2.write().unwrap().attr.actual_shape =
+        shapes::convert_to_actual_shape(shape, parent_actual_shape);
+      match child2.read().unwrap().children {
+        Some(children) => {
+          for c in children.iter() {
+            q.push_back(c);
+          }
+        }
+        None => { /* Do nothing */ }
+      }
+    }
   }
 
-  /// Get descendant child by its ID, i.e. all nested children under the sub-tree.
-  pub fn get_child(&self, id: usize) -> Option<InodePtr<T>> {
+  /// Get descendant child by its ID, i.e. search in all children nodes in the sub-tree.
+  pub fn get_descendant_child(&self, id: usize) -> Option<InodePtr<T>> {
     let mut q: VecDeque<InodePtr<T>> = match self.children {
       Some(c) => c.iter().collect(),
       None => vec![].iter().collect(),
