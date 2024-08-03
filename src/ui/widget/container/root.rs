63426cda1d6488521b42e7effadf85aba0061f1a
linrongbin16 <linrongbin16@outlook.com>
2024-08-04 07:39:55 +0800

    chore

--- src/ui/widget/container/root.rs
+++ src/ui/widget/container/root.rs
@@ -1,15 +1,22 @@
 //! Root container is the root node in the widget tree.
 
-use crate::ui::widget::Widget;
+use crate::ui::widget::{Widget, WidgetId};
+use crate::uuid;
 
 /// Root container.
 #[derive(Debug, Clone, Copy, Default)]
-pub struct RootContainer {}
+pub struct RootContainer {
+  id: WidgetId,
+}
 
 impl RootContainer {
   pub fn new() -> Self {
-    RootContainer {}
+    RootContainer { id: uuid::next() }
   }
 }
 
-impl Widget for RootContainer {}
+impl Widget for RootContainer {
+  fn id(&self) -> WidgetId {
+    self.id
+  }
+}
