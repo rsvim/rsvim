1d22b3c051ece2d77588d77827d748cb1da2eb8f
linrongbin16 <linrongbin16@outlook.com>
2024-08-09 22:20:16 +0800

    chore

--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,9 @@
 //! The global editing state of the editor.
 
+use std::string::ToString;
+
 /// Editing mode enums.
-pub enum EditingMode {
+pub enum Mode {
   /// Normal mode.
   Normal,
   /// Visual mode.
@@ -16,6 +18,35 @@ pub enum EditingMode {
   CommandLine,
   /// Terminal mode.
   Terminal,
+}
+
+impl ToString for Mode {
+  fn to_string(&self) -> String {
+    match self {
+      Mode::Normal => "Normal".to_string(),
+      Mode::Visual => "Visual".to_string(),
+      Mode::Select => "Select".to_string(),
+      Mode::OperatorPending => "Operator-pending".to_string(),
+      Mode::Insert => "Insert".to_string(),
+      Mode::CommandLine => "Command-line".to_string(),
+      Mode::Terminal => "Terminal".to_string(),
+    }
+  }
+}
+
+impl From<&str> for Mode {
+  fn from(name: &str) -> Self {
+    match name {
+      "Normal" => Mode::Normal,
+      "Visual" => Mode::Visual,
+      "Select" => Mode::Visual,
+      "Operator-pending" => Mode::OperatorPending,
+      "Insert" => Mode::Insert,
+      "Command-line" => Mode::CommandLine,
+      "Terminal" => Mode::Terminal,
+      _ => unreachable!("Invalid Mode name"),
+    }
+  }
 }
 
 /// The global editing state.
