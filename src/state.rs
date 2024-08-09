0e84a258b1da8da756d08fa3182b14f1a2aeaad1
linrongbin16 <linrongbin16@outlook.com>
2024-08-09 22:12:03 +0800

    chore

--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,22 @@
 //! The global editing state of the editor.
 
+/// Editing mode enums.
+pub enum EditingMode {
+  /// Normal mode.
+  Normal,
+  /// Visual mode.
+  Visual,
+  /// Select mode.
+  Select,
+  /// Operator-pending mode.
+  OperatorPending,
+  /// Insert mode.
+  Insert,
+  /// Command-line mode.
+  CommandLine,
+  /// Terminal mode.
+  Terminal,
+}
+
 /// The global editing state.
 pub struct State {}
