04c3b354a88a2ad3420b8dde36b323d4af024049
linrongbin16 <linrongbin16@outlook.com>
2024-08-12 18:10:05 +0800

    chore

--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,6 @@
 //! User interfaces, inputs & events.
 
 pub mod frame;
-pub mod movement;
 pub mod term;
 pub mod tree;
 pub mod widget;
--- src/ui/movement.rs
+++
@@ -1,9 +0,0 @@
-//! Widget movement.
-
-#[derive(Debug, Default, Copy, Clone)]
-/// Bounded move, the movement will not take effect if it's already on the edge.
-pub struct BoundedMove {}
-
-#[derive(Debug, Default, Copy, Clone)]
-/// Unbounded move, the movement is logically always effective.
-pub struct UnboundedMove {}
