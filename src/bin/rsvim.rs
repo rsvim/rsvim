bed8f227d1008b521a9b1ac592c2744a58dbf994
linrongbin16 <linrongbin16@outlook.com>
2024-08-15 09:51:44 +0800

    chore

--- src/bin/rsvim.rs
+++ src/bin/rsvim.rs
@@ -63,26 +63,26 @@ async fn main() -> IoResult<()> {
 
   init().await?;
 
-  // // V8 engine
-  // let v8_platform = v8::new_default_platform(0, false).make_shared();
-  // v8::V8::initialize_platform(v8_platform);
-  // v8::V8::initialize();
-  // let v8_isolate = &mut v8::Isolate::new(Default::default());
-  // let v8_handle_scope = &mut v8::HandleScope::new(v8_isolate);
-  // let v8_context = v8::Context::new(v8_handle_scope, Default::default());
-  // let v8_context_scope = &mut v8::ContextScope::new(v8_handle_scope, v8_context);
-  // let js_code = v8::String::new(v8_context_scope, "'Hello' + ' World!'").unwrap();
-  // debug!(
-  //   "javascript code: {}",
-  //   js_code.to_rust_string_lossy(v8_context_scope)
-  // );
-  // let v8_script = v8::Script::compile(v8_context_scope, js_code, None).unwrap();
-  // let js_result = v8_script.run(v8_context_scope).unwrap();
-  // let js_result = js_result.to_string(v8_context_scope).unwrap();
-  // debug!(
-  //   "javascript result: {}",
-  //   js_result.to_rust_string_lossy(v8_context_scope)
-  // );
+  // V8 engine
+  let v8_platform = v8::new_default_platform(0, false).make_shared();
+  v8::V8::initialize_platform(v8_platform);
+  v8::V8::initialize();
+  let v8_isolate = &mut v8::Isolate::new(Default::default());
+  let v8_handle_scope = &mut v8::HandleScope::new(v8_isolate);
+  let v8_context = v8::Context::new(v8_handle_scope, Default::default());
+  let v8_context_scope = &mut v8::ContextScope::new(v8_handle_scope, v8_context);
+  let js_code = v8::String::new(v8_context_scope, "'Hello' + ' World!'").unwrap();
+  debug!(
+    "javascript code: {}",
+    js_code.to_rust_string_lossy(v8_context_scope)
+  );
+  let v8_script = v8::Script::compile(v8_context_scope, js_code, None).unwrap();
+  let js_result = v8_script.run(v8_context_scope).unwrap();
+  let js_result = js_result.to_string(v8_context_scope).unwrap();
+  debug!(
+    "javascript result: {}",
+    js_result.to_rust_string_lossy(v8_context_scope)
+  );
 
   // Event loop
   let mut event_loop = EventLoop::new().await?;
