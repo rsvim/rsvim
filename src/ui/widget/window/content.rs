5f9c90643ca2b90870122fc12541b96e83bdb871
linrongbin16 <linrongbin16@outlook.com>
2024-08-20 09:52:34 +0800

    chore

--- src/ui/widget/window/content.rs
+++ src/ui/widget/window/content.rs
@@ -3,6 +3,7 @@
 #![allow(unused_imports, dead_code)]
 
 use compact_str::CompactString;
+use std::collections::VecDeque;
 use std::convert::From;
 use tracing::debug;
 
@@ -15,37 +16,81 @@ use crate::uuid;
 /// The VIM window content.
 pub struct WindowContent {
   id: WidgetId,
-  lines: Vec<CompactString>,
+  lines: VecDeque<CompactString>,
+  dirty_lines: VecDeque<bool>,
   line_wrap: bool,
   word_wrap: bool,
-  dirty: bool,
+  dirty_option: bool,
 }
 
 impl WindowContent {
   pub fn new() -> Self {
     WindowContent {
       id: uuid::next(),
-      lines: vec![],
+      lines: VecDeque::new(),
+      dirty_lines: VecDeque::new(),
       line_wrap: false,
       word_wrap: false,
-      dirty: false,
+      dirty_option: false,
     }
   }
 
-  pub fn lines(&self) -> &Vec<CompactString> {
+  pub fn lines(&self) -> &VecDeque<CompactString> {
     &self.lines
-  }
-
-  pub fn lines_mut(&mut self) -> &mut Vec<CompactString> {
-    &mut self.lines
   }
 
   pub fn line(&self, index: usize) -> &CompactString {
     &self.lines[index]
   }
 
-  pub fn line_mut(&mut self, index: usize) -> &mut CompactString {
-    &mut self.lines[index]
+  pub fn set_line(&mut self, index: usize, line: CompactString) {
+    self.lines[index] = line;
+    self.dirty_lines[index] = true;
+  }
+
+  /// Get the first line.
+  pub fn front_line(&self) -> Option<&CompactString> {
+    self.lines.front()
+  }
+
+  /// Get the last line.
+  pub fn back_line(&self) -> Option<&CompactString> {
+    self.lines.back()
+  }
+
+  pub fn front_line_mut(&mut self) -> Option<&mut CompactString> {
+    self.lines.front_mut()
+  }
+
+  pub fn back_line_mut(&mut self) -> Option<&mut CompactString> {
+    self.lines.back_mut()
+  }
+
+  pub fn resize(&mut self, new_len: usize) {
+    self.lines.resize(new_len, CompactString::const_new(""));
+    self.dirty_lines.resize(new_len, false);
+  }
+
+  pub fn push_back_line(&mut self, line: CompactString) {
+    self.lines.push_back(line);
+    self.dirty_lines.push_back(false);
+  }
+
+  pub fn pop_back_line(&mut self) -> Option<CompactString> {
+    let result = self.lines.pop_back();
+    self.dirty_lines.pop_back();
+    result
+  }
+
+  pub fn push_front_line(&mut self, line: CompactString) {
+    self.lines.push_front(line);
+    self.dirty_lines.push_front(false);
+  }
+
+  pub fn pop_front_line(&mut self) -> Option<CompactString> {
+    let result = self.lines.pop_front();
+    self.dirty_lines.pop_front();
+    result
   }
 
   pub fn line_wrap(&self) -> bool {
@@ -56,7 +101,7 @@ impl WindowContent {
     if self.line_wrap != line_wrap {
       let old_value = self.line_wrap;
       self.line_wrap = line_wrap;
-      self.dirty = true;
+      self.dirty_option = true;
       old_value
     } else {
       self.line_wrap
@@ -71,7 +116,7 @@ impl WindowContent {
     if self.word_wrap != word_wrap {
       let old_value = self.word_wrap;
       self.word_wrap = word_wrap;
-      self.dirty = true;
+      self.dirty_option = true;
       old_value
     } else {
       self.word_wrap
@@ -87,12 +132,14 @@ impl Default for WindowContent {
 
 impl From<Vec<CompactString>> for WindowContent {
   fn from(lines: Vec<CompactString>) -> Self {
+    let dirty_lines = lines.iter().map(|_| false).collect();
     WindowContent {
       id: uuid::next(),
       lines,
+      dirty_lines,
       line_wrap: false,
       word_wrap: false,
-      dirty: false,
+      dirty_option: true,
     }
   }
 }
