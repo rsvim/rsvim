a4a1484a743590e540257a7b2485e5e35ddffa9a
linrongbin16 <linrongbin16@outlook.com>
2024-09-14 17:20:05 +0800

    chore

--- src/rt.rs
+++ src/rt.rs
@@ -21,6 +21,7 @@ fn into_str(buf: &[u8], bufsize: usize) -> String {
   String::from_utf8_lossy(&buf[0..bufsize]).into_owned()
 }
 
+/// Initialize V8 platform.
 pub fn init_v8_platform() {
   let platform = v8::new_default_platform(0, false).make_shared();
   v8::V8::initialize_platform(platform);
