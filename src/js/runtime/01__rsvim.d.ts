6e299325fb12bbe5aa6f46bc1a068592e1533e86
linrongbin16 <linrongbin16@outlook.com>
2024-09-29 11:20:19 +0800

    chore

--- src/js/runtime/01__rsvim.d.ts
+++ src/js/runtime/01__rsvim.d.ts
@@ -1,33 +1,21 @@
 //! Js runtime type declarations for `Rsvim` namespace.
 
-export {};
-
-// The `__InternalRsvimGlobalObject.opt` exposed in rust side.
-type __InternalRsvimOptionGlobalObject = {
-  line_wrap: () => boolean;
-  set_line_wrap: (value: boolean) => void;
-};
-
-// The `__InternalRsvimGlobalObject` exposed in rust side.
-type __InternalRsvimGlobalObject = {
-  opt: __InternalRsvimOptionGlobalObject;
-};
-
 // The `Rsvim.opt` namespace.
-type RsvimOption = {
+declare class RsvimOption {
   // Get `line_wrap` option.
-  lineWrap: () => boolean;
+  lineWrap(): boolean;
   // Set `line_wrap` option.
-  setLineWrap: (value: boolean) => void;
-};
+  setLineWrap(value: boolean): void;
+}
 
 // The `Rsvim` namespace.
-type Rsvim = {
+declare class Rsvim {
   opt: RsvimOption;
-};
+}
 
 // Declare the `Rsvim` global object in `globalThis`.
 declare global {
   const Rsvim: Rsvim;
-  const __InternalRsvimGlobalObject: __InternalRsvimGlobalObject;
 }
+
+export {};
--- src/js/runtime/01__rsvim.js
+++ src/js/runtime/01__rsvim.js
@@ -1,28 +1,21 @@
 "use strict";
 //! Js runtimes for `Rsvim` namespace.
 (function (globalThis) {
-    // const { $$queueMicrotask, reportError } = globalThis;
-    //
-    // // Note: We wrap `queueMicrotask` and manually emit the exception because
-    // // v8 doesn't provide any mechanism to handle callback exceptions during
-    // // the microtask_checkpoint phase.
-    // function queueMicrotask(callback) {
-    //   // Check if the callback argument is a valid type.
-    //   if (typeof callback !== "function") {
-    //     throw new TypeError(`The "callback" argument must be a function.`);
-    //   }
-    //
-    //   $$queueMicrotask(() => {
-    //     try {
-    //       callback();
-    //     } catch (err) {
-    //       reportError(err);
-    //     }
-    //   });
-    // }
-    // `Rsvim`
-    globalThis.Rsvim = {
-        // `Rsvim.opt`
-        opt: opt,
-    };
+  // `Rsvim`
+  globalThis.Rsvim = {
+    // `Rsvim.opt`
+    opt: {
+      lineWrap: function () {
+        return __InternalRsvimGlobalObject.line_wrap;
+      },
+      setLineWrap: function (value) {
+        if (typeof value !== "boolean") {
+          throw new Error(
+            `Value (${value}) must be boolean type, but found ${typeof value}`,
+          );
+        }
+        __InternalRsvimGlobalObject.set_line_wrap(value);
+      },
+    },
+  };
 })(globalThis);
