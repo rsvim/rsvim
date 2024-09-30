f75798cadcb8ee1682dc128d04064739ab15f12b
linrongbin16 <linrongbin16@outlook.com>
2024-09-30 12:51:36 +0800

    chore

--- src/js/runtime/01__rsvim.js
+++ src/js/runtime/01__rsvim.js
@@ -1,14 +1,14 @@
 "use strict";
-function optLineWrap() {
-    return __InternalRsvimGlobalObject.opt_line_wrap();
-}
-function optSetLineWrap(value) {
-    if (typeof value !== "boolean") {
-        throw new Error("Value (".concat(value, ") must be boolean type, but found ").concat(typeof value));
-    }
-    __InternalRsvimGlobalObject.opt_set_line_wrap(value);
-}
 (function (globalThis) {
+    function optLineWrap() {
+        return __InternalRsvimGlobalObject.opt_line_wrap();
+    }
+    function optSetLineWrap(value) {
+        if (typeof value !== "boolean") {
+            throw new Error("Value (".concat(value, ") must be boolean type, but found ").concat(typeof value));
+        }
+        __InternalRsvimGlobalObject.opt_set_line_wrap(value);
+    }
     globalThis.Rsvim = {
         opt: {
             lineWrap: optLineWrap,
--- src/js/runtime/01__rsvim.ts
+++ src/js/runtime/01__rsvim.ts
@@ -11,28 +11,28 @@ interface RsvimType {
   opt: RsvimOptionType;
 }
 
-// `Rsvim.opt.lineWrap`
-//
-// Get `line_wrap` option.
-function optLineWrap(): boolean {
-  // @ts-ignore
-  return __InternalRsvimGlobalObject.opt_line_wrap();
-}
-
-// `Rsvim.opt.setLineWrap`
-//
-// Set `line_wrap` option.
-function optSetLineWrap(value: boolean): void {
-  if (typeof value !== "boolean") {
-    throw new Error(
-      `Value (${value}) must be boolean type, but found ${typeof value}`,
-    );
-  }
-  // @ts-ignore
-  __InternalRsvimGlobalObject.opt_set_line_wrap(value);
-}
-
 (function (globalThis: { Rsvim: RsvimType }) {
+  // `Rsvim.opt.lineWrap`
+  //
+  // Get `line_wrap` option.
+  function optLineWrap(): boolean {
+    // @ts-ignore
+    return __InternalRsvimGlobalObject.opt_line_wrap();
+  }
+
+  // `Rsvim.opt.setLineWrap`
+  //
+  // Set `line_wrap` option.
+  function optSetLineWrap(value: boolean): void {
+    if (typeof value !== "boolean") {
+      throw new Error(
+        `Value (${value}) must be boolean type, but found ${typeof value}`,
+      );
+    }
+    // @ts-ignore
+    __InternalRsvimGlobalObject.opt_set_line_wrap(value);
+  }
+
   globalThis.Rsvim = {
     opt: {
       lineWrap: optLineWrap,
