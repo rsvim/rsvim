e05c5d964d82a4ec6f83c03d1ed32d85f80537e1
linrongbin16 <linrongbin16@outlook.com>
2024-09-30 12:41:36 +0800

    chore

--- src/js/runtime/01__rsvim.js
+++ src/js/runtime/01__rsvim.js
@@ -1,19 +1,14 @@
 "use strict";
-//! Js runtimes for `Rsvim` namespace.
 (function (globalThis) {
-    // `Rsvim`
     globalThis.Rsvim = {
-        // `Rsvim.opt`
         opt: {
             lineWrap: function () {
-                // @ts-ignore
                 return __InternalRsvimGlobalObject.opt_line_wrap();
             },
             setLineWrap: function (value) {
                 if (typeof value !== "boolean") {
                     throw new Error("Value (".concat(value, ") must be boolean type, but found ").concat(typeof value));
                 }
-                // @ts-ignore
                 __InternalRsvimGlobalObject.opt_set_line_wrap(value);
             },
         },
