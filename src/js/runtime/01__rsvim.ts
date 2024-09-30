dc56a05438c699da8d139b8b80ba33bde2592a7d
linrongbin16 <linrongbin16@outlook.com>
2024-09-30 12:47:33 +0800

    chore

--- src/js/runtime/01__rsvim.ts
+++ src/js/runtime/01__rsvim.ts
@@ -11,22 +11,32 @@ interface RsvimType {
   opt: RsvimOptionType;
 }
 
+// `Rsvim.opt.lineWrap`
+//
+// Get `line_wrap` option.
+function optLineWrap(): boolean {
+  // @ts-ignore
+  return __InternalRsvimGlobalObject.opt_line_wrap();
+}
+
+// `Rsvim.opt.setLineWrap`
+//
+// Set `line_wrap` option.
+function optSetLineWrap(value: boolean): void {
+  if (typeof value !== "boolean") {
+    throw new Error(
+      `Value (${value}) must be boolean type, but found ${typeof value}`,
+    );
+  }
+  // @ts-ignore
+  __InternalRsvimGlobalObject.opt_set_line_wrap(value);
+}
+
 (function (globalThis: { Rsvim: RsvimType }) {
   globalThis.Rsvim = {
     opt: {
-      lineWrap: function (): boolean {
-        // @ts-ignore
-        return __InternalRsvimGlobalObject.opt_line_wrap();
-      },
-      setLineWrap: function (value: boolean): void {
-        if (typeof value !== "boolean") {
-          throw new Error(
-            `Value (${value}) must be boolean type, but found ${typeof value}`,
-          );
-        }
-        // @ts-ignore
-        __InternalRsvimGlobalObject.opt_set_line_wrap(value);
-      },
+      lineWrap: optLineWrap,
+      setLineWrap: optSetLineWrap,
     } as RsvimOptionType,
   } as RsvimType;
 })(globalThis as unknown as { Rsvim: RsvimType });
