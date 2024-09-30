8179152e938f1d3cb2ac9b42072ad234f44d1e7d
linrongbin16 <linrongbin16@outlook.com>
2024-09-30 12:47:48 +0800

    chore

--- src/js/runtime/01__rsvim.js
+++ src/js/runtime/01__rsvim.js
@@ -1,16 +1,18 @@
 "use strict";
+function optLineWrap() {
+    return __InternalRsvimGlobalObject.opt_line_wrap();
+}
+function optSetLineWrap(value) {
+    if (typeof value !== "boolean") {
+        throw new Error("Value (".concat(value, ") must be boolean type, but found ").concat(typeof value));
+    }
+    __InternalRsvimGlobalObject.opt_set_line_wrap(value);
+}
 (function (globalThis) {
     globalThis.Rsvim = {
         opt: {
-            lineWrap: function () {
-                return __InternalRsvimGlobalObject.opt_line_wrap();
-            },
-            setLineWrap: function (value) {
-                if (typeof value !== "boolean") {
-                    throw new Error("Value (".concat(value, ") must be boolean type, but found ").concat(typeof value));
-                }
-                __InternalRsvimGlobalObject.opt_set_line_wrap(value);
-            },
+            lineWrap: optLineWrap,
+            setLineWrap: optSetLineWrap,
         },
     };
 })(globalThis);
