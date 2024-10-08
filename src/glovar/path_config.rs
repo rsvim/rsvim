4714cee1af25cb8c314539fd230dd5e70c3bdea2
linrongbin16 <linrongbin16@outlook.com>
2024-10-08 15:22:55 +0800

    chore

--- src/glovar/path_config.rs
+++ src/glovar/path_config.rs
@@ -130,3 +130,41 @@ impl Default for PathConfig {
     PathConfig::new()
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::test::log::init as test_log_init;
+
+  use std::sync::Once;
+  use tracing::info;
+
+  static INIT: Once = Once::new();
+
+  #[cfg(target_os = "windows")]
+  #[test]
+  fn config_file_windows() {
+    INIT.call_once(test_log_init);
+    let cfg = PathConfig::default();
+    let actual = cfg.config_file().as_ref().unwrap();
+    info!("config_file (windows): ${:?}", actual);
+    assert!(actual.to_str().unwrap().ends_with("AppData\\Local"));
+  }
+
+  #[cfg(not(target_os = "windows"))]
+  #[test]
+  fn config_file_unix() {
+    INIT.call_once(test_log_init);
+    let cfg = PathConfig::default();
+    match cfg.config_file().as_ref() {
+      Some(actual) => {
+        info!("config_file (unix): ${:?}", actual);
+        assert!(
+          actual.to_str().unwrap().ends_with(".rsvim.js")
+            || actual.to_str().unwrap().ends_with(".rsvim.ts")
+        );
+      }
+      None => { /* Skip */ }
+    }
+  }
+}
