fe46d971717952900b6a47513126db189af8287b
linrongbin16 <linrongbin16@outlook.com>
2024-10-16 11:29:00 +0800

    ci(security): add cargo audit (#193)
    refactor(js): generate declarations (.d.ts files) (#193)

---
+++ rsvim_core/src/js/runtime/00__infra.d.ts
@@ -0,0 +1,5 @@
+export declare function stringify(value: any): string;
+declare const _default: {
+    stringify: typeof stringify;
+};
+export default _default;
--- rsvim_core/src/js/runtime/50__rsvim.d.ts
+++ rsvim_core/src/js/runtime/50__rsvim.d.ts
@@ -1,16 +1,9 @@
-//! Js runtime type declarations for `Rsvim` namespace.
-
-interface __InternalRsvimGlobalObjectType {
-  opt_get_wrap(): boolean;
-  opt_set_wrap(value: boolean): void;
-  opt_get_line_break(): boolean;
-  opt_set_line_break(value: boolean): void;
-  opt_get_break_at(): string;
-  opt_set_break_at(value: string): void;
+export declare class Rsvim {
+    readonly opt: RsvimOpt;
 }
-
-declare global {
-  const __InternalRsvimGlobalObject: __InternalRsvimGlobalObjectType;
+export declare class RsvimOpt {
+    get wrap(): boolean;
+    set wrap(value: boolean);
+    get lineBreak(): boolean;
+    set lineBreak(value: boolean);
 }
-
-export {};
