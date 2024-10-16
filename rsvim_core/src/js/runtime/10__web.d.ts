f38fce6e7e63ca0fca62293c40eb553e977f2a7b
linrongbin16 <linrongbin16@outlook.com>
2024-10-16 10:13:48 +0800

    chore

--- rsvim_core/src/js/runtime/10__web.d.ts
+++ rsvim_core/src/js/runtime/10__web.d.ts
@@ -1,12 +1,4 @@
-//! Js runtime type declarations for `Rsvim` namespace.
-
-interface __InternalRsvimGlobalObjectType {
-  global_set_timeout(callback: (...args: any[]) => void, delay: number): number;
-  global_clear_timeout(id: number): void;
+export interface GlobalThis {
+    setTimeout(callback: (...args: any[]) => void, delay: number, ...args: any[]): number;
+    clearTimeout(id: number): void;
 }
-
-declare global {
-  const __InternalRsvimGlobalObject: __InternalRsvimGlobalObjectType;
-}
-
-export {};
