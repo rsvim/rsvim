46a87ea4f13b2d81e3d275b446b9ee1defccff33
linrongbin16 <linrongbin16@outlook.com>
2024-10-18 23:15:12 +0800

    chore

--- rsvim_cli/build.rs
+++ rsvim_cli/build.rs
@@ -3,7 +3,17 @@ use std::path::Path;
 
 fn main() {
   let js_runtime = JsRuntimeForSnapshot::new();
-  let snapshot = js_runtime.create_snapshot();
-  println!("snapshot is empty: {}", snapshot.is_empty());
+  let snapshot = {
+    let snapshot = js_runtime.create_snapshot();
+    let snapshot = Box::from(&snapshot);
+    let mut vec = Vec::with_capacity(snapshot.len());
+    vec.extend((snapshot.len() as u32).to_le_bytes());
+    let max_compress_level: i32 = *zstd::compression_level_range().end();
+    vec.extend_from_slice(
+      &zstd::bulk::compress(&snapshot, max_compress_level)
+        .expect("Failed to compress snapshot with zstd"),
+    );
+    vec.into_boxed_slice()
+  };
   std::fs::write(Path::new("RSVIM_SNAPSHOT.BIN"), &snapshot).unwrap();
 }
