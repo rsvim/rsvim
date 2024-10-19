4e4138a1736feed90fef9daf329f827d61c8d930
linrongbin16 <linrongbin16@outlook.com>
2024-10-19 18:02:04 +0800

    chore(release): fix tag and changelog generate (#209)

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# set -x
+set -x
 
 if [ $# -lt 1 ]; then
 	echo "usage: ./release.sh [LEVEL] (--execute --no-verify)"
@@ -13,4 +13,5 @@ export GIT_CLIFF_WORKDIR=$PWD
 export GIT_CLIFF_REPOSITORY=$PWD
 export GIT_CLIFF_OUTPUT=$PWD/CHANGELOG.md
 
+# git-cliff will create "CHANGELOG.md" for each packages, but we only need the one from rsvim_cli.
 cargo release "$@"
