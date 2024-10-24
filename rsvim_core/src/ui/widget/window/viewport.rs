5e0e32f32c94b7b9e25b54080f0e0df683267036
linrongbin16 <linrongbin16@outlook.com>
2024-10-24 12:00:49 +0800

    chore

--- rsvim_core/src/ui/widget/window/viewport.rs
+++ rsvim_core/src/ui/widget/window/viewport.rs
@@ -673,4 +673,31 @@ mod tests {
 
     _test_collect_from_top_left_for_nowrap(U16Size::new(27, 15), buffer, &expect);
   }
+
+  #[test]
+  fn collect_from_top_left_for_nowrap3() {
+    INIT.call_once(test_log_init);
+
+    let buffer = make_buffer_from_lines(vec![
+      "Hello, RSVIM!\n",
+      "This is a quite simple and small test lines.\n",
+      "But still it contains several things we want to test:\n",
+      "  1. When the line is small enough to completely put inside a row of the window content widget, then the line-wrap and word-wrap doesn't affect the rendering.\n",
+      "  2. When the line is too long to be completely put in a row of the window content widget, there're multiple cases:\n",
+      "     * The extra parts are been truncated if both line-wrap and word-wrap options are not set.\n",
+      "     * The extra parts are split into the next row, if either line-wrap or word-wrap options are been set. If the extra parts are still too long to put in the next row, repeat this operation again and again. This operation also eats more rows in the window, thus it may contains less lines in the buffer.\n",
+    ]);
+    let expect = vec![
+      "Hello, RSVIM!",
+      "This is a quite simple and smal",
+      "But still it contains several t",
+      "  1. When the line is small eno",
+      "  2. When the line is too long ",
+      "     * The extra parts are been",
+      "     * The extra parts are spli",
+      "",
+    ];
+
+    _test_collect_from_top_left_for_nowrap(U16Size::new(31, 19), buffer, &expect);
+  }
 }
