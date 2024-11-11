4ab2cd9c237cd9f2a29fad570c2e3ed255c27b5c
linrongbin16 <linrongbin16@outlook.com>
2024-11-11 16:08:10 +0800

    chore

--- rsvim_core/src/test/log.rs
+++ rsvim_core/src/test/log.rs
@@ -10,14 +10,15 @@ pub fn init() {
   static INITIALIZED: Once = Once::new();
   INITIALIZED.call_once(|| {
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
-      .with_file(true)
+      // .with_file(true)
       .with_line_number(true)
-      .with_thread_ids(true)
-      .with_thread_names(true)
+      // .with_thread_ids(true)
+      // .with_thread_names(true)
       .with_level(true)
       .with_ansi(true)
       .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
       .with_writer(std::io::stderr)
+      .without_time()
       .finish();
     tracing::subscriber::set_global_default(subscriber).unwrap();
   });
