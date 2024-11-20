7ff05299e44f43dcb05d04d1e974924aba979c7b
linrongbin16 <linrongbin16@outlook.com>
2024-11-20 14:56:12 +0800

    chore

--- rsvim_core/src/locks.rs
+++ rsvim_core/src/locks.rs
@@ -1,6 +1,6 @@
 //! Lock utils
 
-/// Alias to `id`.try_read_for(envar::MUTEX_TIMEOUT()).unwrap()
+/// Alias to `($id).try_read_for(envar::MUTEX_TIMEOUT()).unwrap()`.
 #[macro_export]
 macro_rules! rlock {
   ($id:expr) => {
@@ -8,7 +8,7 @@ macro_rules! rlock {
   };
 }
 
-/// Alias to `id`.try_write_for(envar::MUTEX_TIMEOUT()).unwrap()
+/// Alias to `($id).try_write_for(envar::MUTEX_TIMEOUT()).unwrap()`.
 #[macro_export]
 macro_rules! wlock {
   ($id:expr) => {
