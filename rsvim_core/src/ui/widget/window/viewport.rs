e16560a3dac8c29f3300afa32df0563c65380c0c
linrongbin16 <linrongbin16@outlook.com>
2024-11-20 13:21:08 +0800

    chore

--- rsvim_core/src/ui/widget/window/viewport.rs
+++ rsvim_core/src/ui/widget/window/viewport.rs
@@ -1182,7 +1182,7 @@ fn _sync_from_top_left_wrap_linebreak(
               width - tmp_wcol
             };
 
-            wrow += 1;
+            // wrow += 1;
             wcol = 0_u16;
             start_bcol = end_bcol;
             start_c_idx = bchars;
@@ -2745,6 +2745,79 @@ mod tests {
       &expect,
       0,
       3,
+      &expect_start_fills,
+      &expect_end_fills,
+    );
+  }
+
+  #[test]
+  fn sync_from_top_left_wrap_linebreak11() {
+    test_log_init();
+
+    let buffer = make_buffer_from_lines(vec![
+      "Hello, RSVIM!\n",
+      "This is a quite simple andsmalltestlineswithoutevenanewlinebreakbecausewewanttotesthowitwillhappensifthereisaverylongwordthatcannotbeenpplaceinsidearowofthewindowcontent.\n",
+      "But still it contains several things we want to test:\n",
+      "  1. When the line is small enough to completely put inside a row of the window content widget, 那么行换行和单词换行选项都不会影响最终的渲染效果。\n",
+      "  2. When the line is too long to be completely put in a row of the window content widget, there're multiple cases:\n",
+      "     * The extra parts are been truncated if both line-wrap and word-wrap options are not set.\n",
+      "     * The extra parts are split into the next row, if either line-wrap or word-wrap options are been set. If the extra parts are still too long to put in the next row, repeat this operation again and again. This operation also eats more rows in the window, thus it may contains less lines in the buffer.\n",
+    ]);
+    let expect = vec![
+      "Hello, RSVIM!",
+      "\n",
+      "This is a ",
+      "quite simple ",
+      "andsmalltestl",
+      "ineswithoutev",
+      "enanewlinebre",
+      "akbecausewewa",
+      "nttotesthowit",
+      "willhappensif",
+      "thereisaveryl",
+      "ongwordthatca",
+      "nnotbeenpplac",
+      "einsidearowof",
+      "thewindowcont",
+      "ent.\n",
+      "But still it ",
+      "contains ",
+      "several ",
+      "things we ",
+      "want to test:",
+      "  1. When the",
+      " line is ",
+      "small enough ",
+      "to completely",
+      " put inside a",
+      " row of the  ",
+      "window ",
+      "content ",
+      "widget, 那么 ",
+      "行换行和单词",
+      "换行选项都不",
+    ];
+
+    let size = U16Size::new(13, 31);
+    let options = WindowLocalOptions::builder()
+      .wrap(true)
+      .line_break(true)
+      .build();
+    let actual = make_viewport_from_size(size, buffer.clone(), &options);
+    let expect_start_fills: BTreeMap<usize, usize> =
+      vec![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0)]
+        .into_iter()
+        .collect();
+    let expect_end_fills: BTreeMap<usize, usize> =
+      vec![(0, 0), (1, 0), (2, 0), (3, 1), (4, 0), (5, 0)]
+        .into_iter()
+        .collect();
+    do_test_sync_from_top_left(
+      buffer,
+      &actual,
+      &expect,
+      0,
+      6,
       &expect_start_fills,
       &expect_end_fills,
     );
