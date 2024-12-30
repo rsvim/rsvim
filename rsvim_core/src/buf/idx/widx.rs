f1cfe926886d2001681adf2f690d421955c764bd
linrongbin16 <linrongbin16@outlook.com>
2024-12-30 11:14:25 +0800

    chore

--- rsvim_core/src/buf/idx/widx.rs
+++ rsvim_core/src/buf/idx/widx.rs
@@ -4,12 +4,13 @@ use crate::buf::unicode;
 use crate::buf::Buffer;
 //use ropey::{Rope, RopeBuilder, RopeSlice};
 
+use std::collections::BTreeMap;
+
 #[derive(Debug, Clone)]
 /// Display width index (line-wise) for each unicode char in vim buffer. For each line, the
 /// char/column index starts from 0.
 ///
-/// NOTE: This structure is wrapped on a [`Vec`], all APIs follow the standard [`Vec`]'s API
-/// design.
+/// This structure is mostly like a prefix-sum tree structure.
 pub struct BufWindex {
   // Char index maps to its accumulate display width, i.e. from the first char/column (0) to
   // current char/column, not just the current char's display width.
@@ -83,11 +84,58 @@ impl BufWindex {
     self.char2width[c_start] - self.char2width[c_end]
   }
 
-  pub fn update(&mut self) {}
+  /// Update a specific char's width, and re-calculate all display width since this char.
+  ///
+  /// NOTE: This operation is `O(N)`, where `N` is the chars count of current line.
+  pub fn update(&mut self, char_idx: usize, width: usize) {
+    self._internal_check();
+    assert!(char_idx < self.char2width.len());
+    if width > self.char2width[char_idx] {
+      let diff = width - self.char2width[char_idx];
+      for (_i, w) in self.char2width.iter_mut().skip(char_idx).enumerate() {
+        *w += diff;
+      }
+    } else if width < self.char2width[char_idx] {
+      let diff = self.char2width[char_idx] - width;
+      for (_i, w) in self.char2width.iter_mut().skip(char_idx).enumerate() {
+        *w -= diff;
+      }
+    }
+  }
 
-  pub fn update_between(&mut self) {}
+  /// Update a range of chars and their width, and re-calculate all display width since the first
+  /// char in the range.
+  ///
+  /// NOTE: This operation is `O(N)`, where `N` is the chars count of current line.
+  pub fn update_between(&mut self, char2width: &BTreeMap<usize, usize>) {}
 
+  /// Push (append) a specific char's width.
+  ///
+  /// NOTE: This operation is `O(1)`.
+  pub fn push(&mut self, width: usize) {}
+
+  /// Extend (append) multiple chars and their display width, and re-calculate all display width
+  /// for the extended chars.
+  ///
+  /// NOTE: This operation is `O(M)`, where `M` is the chars count of the extended chars.
+  pub fn extend(&mut self, char2width: &Vec<usize>) {}
+
+  /// Replace a range of chars and their display width, with a new range, and re-calculate all
+  /// display width since the first char in the newly added range of chars.
+  ///
+  /// NOTE: This operation is `O(N+M)`, where `N` is the chars count of current line, `M` is the
+  /// chars count of the new range.
   pub fn splice(&mut self) {}
 
-  pub fn truncate(&self) {}
+  /// Shorten (remove/truncate) the chars since a specific char index. This operation doesn't need
+  /// to trigger re-calculation.
+  ///
+  /// NOTE: This operation is `O(1)`.
+  pub fn truncate(&mut self) {}
+
+  /// Remove a specific range of chars, and re-calculate all display width since the start index in
+  /// the removed range.
+  ///
+  /// NOTE: This operation is `O(N)`, where `N` is the chars count of current line.
+  pub fn drain(&mut self) {}
 }
