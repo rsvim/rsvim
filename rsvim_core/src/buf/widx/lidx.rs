1619ea33e2c4f612b0fb3e0c300e040db1921427
linrongbin16 <linrongbin16@outlook.com>
2025-02-05 14:24:32 +0800

    chore

--- rsvim_core/src/buf/widx/lidx.rs
+++ rsvim_core/src/buf/widx/lidx.rs
@@ -22,20 +22,6 @@ impl LineIndex {
     }
   }
 
-  /// Get the prefix display width on line `line_idx`, in char index range `[0,char_idx)`,
-  /// left-inclusive and right-exclusive.
-  ///
-  /// NOTE: This is equivalent to `width_until(line_idx, char_idx-1)`.
-  ///
-  /// # Return
-  ///
-  /// 1. It returns 0 if `char_idx <= 0`.
-  /// 2. It returns the prefix display width if `char_idx` is inside the line.
-  /// 3. It returns the whole display width of the line if `char_idx` is greater than the line
-  ///    length.
-  ///
-  /// # Panics
-  ///
   /// It panics if the `line_idx` doesn't exist in rope.
   pub fn width_before(
     &mut self,
@@ -53,20 +39,6 @@ impl LineIndex {
       .width_before(options, &rope_line, char_idx)
   }
 
-  /// Get the prefix display width on line `line_idx`, char index range `[0,char_idx]`, both sides
-  /// are inclusive.
-  ///
-  /// NOTE: This is equivalent to `width_before(line_idx, char_idx+1)`.
-  ///
-  /// # Return
-  ///
-  /// 1. It returns 0 if the line length is 0, i.e. the line itself is empty.
-  /// 2. It returns the prefix display width if `char_idx` is inside the line.
-  /// 3. It returns the whole display width of the line if `char_idx` is greater than or equal to
-  ///    the line length.
-  ///
-  /// # Panics
-  ///
   /// It panics if the `line_idx` doesn't exist in rope.
   pub fn width_until(
     &mut self,
@@ -84,24 +56,6 @@ impl LineIndex {
       .width_until(options, &rope_line, char_idx)
   }
 
-  /// Get the right-most char index which the width is less than the specified width, on line
-  /// `line_idx`.
-  ///
-  /// Note:
-  /// 1. The specified width is exclusive, i.e. the returned char index's width is always less than
-  ///    the specified width, but cannot be greater than or equal to it.
-  /// 2. For all the char indexes which the width is less, it returns the right-most char index.
-  ///
-  /// # Return
-  ///
-  /// 1. It returns None if the line length is 0, i.e. the line itself is empty, or there's no such
-  ///    char.
-  /// 2. It returns the right-most char index if `width` is inside the line.
-  /// 3. It returns the last char index of the line if `width` is greater than or equal to
-  ///    the line's whole display width.
-  ///
-  /// # Panics
-  ///
   /// It panics if the `line_idx` doesn't exist in rope.
   pub fn char_before(
     &mut self,
@@ -119,25 +73,6 @@ impl LineIndex {
       .char_before(options, &rope_line, width)
   }
 
-  /// Get the right-most char index which the width is greater than or equal to the specified
-  /// width, on line `line_idx`.
-  ///
-  /// Note:
-  /// 1. The specified width is inclusive, i.e. the returned char index's width is greater than or
-  ///    equal to the specified width, but cannot be less than it.
-  /// 2. For all the char indexes which the width is greater or equal, it returns the right-most
-  ///    char index.
-  ///
-  /// # Return
-  ///
-  /// 1. It returns None if the line length is 0, i.e. the line itself is empty, or there's no such
-  ///    char.
-  /// 2. It returns the right-most char index if `width` is inside the line.
-  /// 3. It returns the last char index of the line if `width` is greater than or equal to
-  ///    the line's whole display width.
-  ///
-  /// # Panics
-  ///
   /// It panics if the `line_idx` doesn't exist in rope.
   pub fn char_until(
     &mut self,
