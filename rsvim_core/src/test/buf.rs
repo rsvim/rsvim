a0910dd6b8e4d34d9ea46624c0c4d8f9dfb541e1
linrongbin16 <linrongbin16@outlook.com>
2025-02-08 16:25:11 +0800

    chore

--- rsvim_core/src/test/buf.rs
+++ rsvim_core/src/test/buf.rs
@@ -75,104 +75,125 @@ pub fn print_buffer_line_details(buf: BufferArc, line_idx: usize, msg: &str) {
     } else {
       info!("line");
     }
-    let mut builder = String::new();
+
+    let mut payload = String::new();
     for c in line.chars() {
       let (cs, _cw) = buf.char_symbol(c);
-      builder.push_str(cs.as_ref());
+      payload.push_str(cs.as_ref());
     }
-    info!("-{}-", builder);
+    info!("-{}-", payload);
 
-    let mut builder = String::new();
-    let mut n = 0_usize;
-    let mut w = 0_usize;
-    let mut zero_width_chars: Vec<String> = vec![];
-    let mut big_width_chars: Vec<String> = vec![];
-    for (i, c) in line.chars().enumerate() {
-      let (_cs, cw) = buf.char_symbol(c);
-      w += cw;
-      n += 1;
-      if cw == 0 {
-        zero_width_chars.push(format!("{}", i));
+    {
+      let mut builder = String::new();
+      let mut n = 0_usize;
+      let mut w = 0_usize;
+      let mut zero_width_chars: Vec<String> = vec![];
+      let mut big_width_chars: Vec<String> = vec![];
+      for (i, c) in line.chars().enumerate() {
+        let (_cs, cw) = buf.char_symbol(c);
+        w += cw;
+        n += 1;
+        if cw == 0 {
+          zero_width_chars.push(format!("{}", i));
+        }
+        if cw > 1 {
+          big_width_chars.push(format!("{}", i));
+        }
+        if i % 10 == 0 {
+          builder.push_str(&format!("{}", i));
+        }
+        if builder.len() < w {
+          let diff = w - builder.len();
+          builder.push_str(&" ".repeat(diff));
+        }
       }
-      if cw > 1 {
-        big_width_chars.push(format!("{}", i));
-      }
-      if i % 10 == 0 {
-        builder.push_str(&format!("{}", i));
-      }
-      if builder.len() < w {
-        let diff = w - builder.len();
-        builder.push_str(&" ".repeat(diff));
-      }
-    }
-    info!("-{}- char index, total:{} (width = 0 chars: count:{} indexes:{}, width > 1 chars: count:{} indexes:{})", builder, n,
+      info!("-{}- char index, total:{} (width = 0 chars: count:{} indexes:{}, width > 1 chars: count:{} indexes:{})", builder, n,
       zero_width_chars.len(),
       zero_width_chars.join(","),
       big_width_chars.len(),
       big_width_chars.join(",")
     );
+    }
 
-    let mut builder = String::new();
-    let mut builder2 = String::new();
-    let mut builder3 = String::new();
-    let mut show2 = false;
-    let mut show3 = false;
-    let mut w = 0_usize;
-    for (_i, c) in line.chars().enumerate() {
-      let (_cs, cw) = buf.char_symbol(c);
-      w += cw;
-      if w == 1 || w % 10 == 0 {
-        if builder.is_empty() || builder.ends_with(" ") {
-          builder.push_str(&format!("{}", w));
-        } else if cw > 0 {
-          builder2.push_str(&format!("{}", w));
-          show2 = true;
-        } else {
-          builder3.push_str(&format!("{}", w));
-          show3 = true;
+    {
+      let mut builder1 = String::new();
+      let mut builder2 = String::new();
+      let mut builder3 = String::new();
+      let mut show2 = false;
+      let mut show3 = false;
+      let mut w = 0_usize;
+      for (_i, c) in line.chars().enumerate() {
+        let (_cs, cw) = buf.char_symbol(c);
+        w += cw;
+        if w == 1 || w % 10 == 0 {
+          if builder1.is_empty() || builder1.ends_with(" ") {
+            builder1.push_str(&format!("{}", w));
+          } else if cw > 0 {
+            builder2.push_str(&format!("{}", w));
+            show2 = true;
+          } else {
+            builder3.push_str(&format!("{}", w));
+            show3 = true;
+          }
         }
-      } else if builder.len() < w {
-        let diff = w - builder.len();
-        builder.push_str(&" ".repeat(diff));
+
+        if builder1.len() < w {
+          let diff = w - builder1.len();
+          builder1.push_str(&" ".repeat(diff));
+        }
+        if builder2.len() < w {
+          let diff = w - builder2.len();
+          builder2.push_str(&" ".repeat(diff));
+        }
+        if builder3.len() < w {
+          let diff = w - builder3.len();
+          builder3.push_str(&" ".repeat(diff));
+        }
       }
-      if builder2.len() < w {
-        let diff = w - builder2.len();
-        builder2.push_str(&" ".repeat(diff));
+      info!("-{}- display width, total width:{}", builder1, w);
+      if show2 {
+        info!(
+          "-{}- display width (extra, conflicted with the above one)",
+          builder2
+        );
       }
-      if builder3.len() < w {
-        let diff = w - builder3.len();
-        builder3.push_str(&" ".repeat(diff));
+      if show3 {
+        info!("-{}- display width for width = 0 chars", builder3);
       }
-    }
-    info!("-{}- display width, total width:{}", builder, w);
-    if show2 {
-      info!(
-        "-{}- display width (extra, conflicted with the above one)",
-        builder2
-      );
-    }
-    if show3 {
-      info!("-{}- display width for width = 0 chars", builder3);
     }
 
-    let mut builder = String::new();
-    let mut w = 0_usize;
-    let mut show = false;
-    for (_i, c) in line.chars().enumerate() {
-      let (_cs, cw) = buf.char_symbol(c);
-      w += cw;
-      if cw > 1 {
-        if builder.is_empty() || builder.ends_with(" ") {
+    {
+      let mut builder = String::new();
+      let mut w = 0_usize;
+      let mut show = false;
+      let mut last_pos: Option<usize> = None;
+      for (i, c) in line.chars().enumerate() {
+        let (_cs, cw) = buf.char_symbol(c);
+        w += cw;
+        if cw > 1 && (builder.is_empty() || builder.ends_with(" ")) {
+          builder.push_str(&format!("{}", w));
+          last_pos = Some(i);
+          show = true;
+        }
+
+        if cw == 1
+          && builder.ends_with(" ")
+          && i > 0
+          && last_pos.is_some()
+          && last_pos.unwrap() == i - 1
+        {
           builder.push_str(&format!("{}", w));
           show = true;
         }
-      } else if builder.len() < w {
-        let diff = w - builder.len();
-        builder.push_str(&" ".repeat(diff));
+
+        if builder.len() < w {
+          let diff = w - builder.len();
+          builder.push_str(&" ".repeat(diff));
+        }
       }
-    }
-    if show {
-      info!("-{}- display width for width > 1 chars", builder);
+      if show {
+        info!("-{}- display width for width > 1 chars", builder);
+      }
     }
   });
 }
