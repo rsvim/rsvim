a10ece0d806f4689d132866ddc9bfac9f819ca7e
linrongbin16 <linrongbin16@outlook.com>
2025-02-11 10:43:39 +0800

    chore

--- dev.sh
+++ dev.sh
@@ -6,8 +6,8 @@
 usage() {
 	echo "Usage: $0 [-h] [-c] [-t TARGET]"
 	echo " -h           Display help message and quit."
-	echo " -c           Run 'cargo clippy' with 'RUSTFLAGS=-Dwarnings', use 'bacon' if exists."
-	echo " -t [TARGET]  Run 'cargo test' on [TARGET] with 'RUST_LOG=trace', use 'cargo-nextest' if exists."
+	echo " -c           Run 'cargo clippy' (with 'bacon' if exists) with 'RUSTFLAGS=-Dwarnings'."
+	echo " -t [TARGET]  Run 'cargo test' (with 'cargo-nextest' if exists) on [TARGET] with 'RUST_LOG=trace'."
 	echo "              Note: use '--all' to run all tests."
 }
 
