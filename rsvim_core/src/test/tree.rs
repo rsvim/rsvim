c46edf903e2ce24989ac51e096343a391d4f6808
linrongbin16 <linrongbin16@outlook.com>
2025-03-07 15:52:27 +0800

    chore

--- rsvim_core/src/test/tree.rs
+++ rsvim_core/src/test/tree.rs
@@ -18,13 +18,40 @@ use std::sync::Arc;
 use tracing::{self, info};
 
 /// Create tree with 1 window and 1 buffer, the buffer is loaded from the filename.
-pub fn make_tree_from_file(filename: String) -> TreeArc {
-  // Canvas
-  let (cols, rows) = crossterm::terminal::size().unwrap();
-  let canvas_size = U16Size::new(cols, rows);
-  let canvas = Canvas::new(canvas_size);
-  let canvas = Canvas::to_arc(canvas);
+pub fn make_tree_from_file(canvas_size: U16Size, filename: &str) -> TreeArc {
+  // UI Tree
+  let tree = Tree::to_arc(Tree::new(canvas_size));
+  // Buffers
+  let buffers_manager = BuffersManager::to_arc(BuffersManager::new());
 
+  let mut buffers = wlock!(buffers_manager);
+  let buf_id = buffers.new_file_buffer(Path::new(filename)).unwrap();
+
+  let mut tree_mut = wlock!(tree);
+  let tree_root_id = tree_mut.root_id();
+  let window_shape = IRect::new(
+    (0, 0),
+    (canvas_size.width() as isize, canvas_size.height() as isize),
+  );
+  let window = {
+    let (buf_id, buf) = buffers.first_key_value().unwrap();
+    Window::new(window_shape, Arc::downgrade(buf), tree_mut.local_options())
+  };
+  let window_id = window.id();
+  let window_node = TreeNode::Window(window);
+  tree_mut.bounded_insert(&tree_root_id, window_node);
+
+  // Initialize cursor.
+  let cursor_shape = IRect::new((0, 0), (1, 1));
+  let cursor = Cursor::new(cursor_shape);
+  let cursor_node = TreeNode::Cursor(cursor);
+  tree_mut.bounded_insert(&window_id, cursor_node);
+
+  tree
+}
+
+/// Create tree with 1 window and 1 buffer, the buffer is filled with strings list.
+pub fn make_tree_from_lines(canvas_size: U16Size, lines: Vec<&str>) -> TreeArc {
   // UI Tree
   let tree = Tree::to_arc(Tree::new(canvas_size));
   // Buffers
