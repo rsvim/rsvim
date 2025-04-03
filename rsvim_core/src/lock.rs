ce5a8c139d8bec1c304c3777d0ee8c445b578c6e
linrongbin16 <linrongbin16@outlook.com>
2025-04-03 17:42:49 +0800

    chore

--- rsvim_core/src/lock.rs
+++ rsvim_core/src/lock.rs
@@ -1,5 +1,39 @@
 //! Mutex utility.
 
+/// Generate Arc pointers.
+#[macro_export]
+macro_rules! arc_impl {
+  ($name:ident) => {
+    paste! {
+      pub type [<$name Arc>] = std::sync::Arc<parking_lot::ReentrantMutex<$name>>;
+      pub type [<$name Wk>] = std::sync::Weak<parking_lot::ReentrantMutex<$name>>;
+      pub type [<$name MutexGuard>]<'a> = parking_lot::ReentrantMutexGuard<'a, $name>>;
+
+      impl $name {
+        pub fn to_arc(value: $name) -> [<$name Arc>] {
+          std::sync::Arc::new(parking_lot::ReentrantMutex::new(value))
+        }
+      }
+    }
+  };
+}
+
+/// Generate Rc pointers.
+#[macro_export]
+macro_rules! rc_impl {
+  ($name:ident) => {
+    paste! {
+      pub type [<$name Rc>] = std::rc::Rc<std::cell::RefCell<$name>>;
+
+      impl $name {
+        pub fn to_rc(value: $name) -> [<$name Rc>] {
+          std::rc::Rc::new(std::cell::RefCell::new(value))
+        }
+      }
+    }
+  };
+}
+
 /// Alias to `($id).try_read_for(envar::MUTEX_TIMEOUT()).unwrap()`.
 #[macro_export]
 macro_rules! rlock {
