4029d1a45bfcdb1792d88128b908244a2bf862e9
linrongbin16 <linrongbin16@outlook.com>
2025-04-23 11:25:41 +0800

    chore

--- rsvim_core/src/ui/widget/window/viewport.rs
+++ rsvim_core/src/ui/widget/window/viewport.rs
@@ -1227,7 +1227,7 @@ mod tests_downward_nowrap {
 
 #[allow(unused_imports)]
 #[cfg(test)]
-mod tests_downward_nowrap_start_column {
+mod tests_downward_nowrap_startcol {
   use super::tests_util::*;
   use super::*;
 
@@ -2460,6 +2460,68 @@ mod tests_downward_wrap_nolinebreak {
       &expect_fills,
       &expect_fills,
     );
+  }
+}
+
+#[allow(unused_imports)]
+#[cfg(test)]
+mod tests_downward_wrap_nolinebreak_startcol {
+  use super::tests_util::*;
+  use super::*;
+
+  use crate::buf::BufferLocalOptionsBuilder;
+  use crate::lock;
+  use crate::prelude::*;
+  use crate::test::buf::{make_buffer_from_lines, make_empty_buffer};
+  #[allow(dead_code)]
+  use crate::test::log::init as test_log_init;
+  use crate::ui::tree::*;
+
+  #[test]
+  fn new1() {
+    test_log_init();
+
+    let terminal_size = U16Size::new(10, 10);
+    let buf_opts = BufferLocalOptionsBuilder::default().build().unwrap();
+    let win_opts = make_wrap_nolinebreak();
+
+    let buf = make_buffer_from_lines(
+      terminal_size.height(),
+      buf_opts,
+      vec![
+        "Hello, RSVIM!\n",
+        "This is a quite simple and small test lines.\n",
+        "But still it contains several things we want to test:\n",
+        "  1. When the line is small enough to completely put inside a row of the window content widget, then the line-wrap and word-wrap doesn't affect the rendering.\n",
+        "  2. When the line is too long to be completely put in a row of the window content widget, there're multiple cases:\n",
+        "     * The extra parts are been truncated if both line-wrap and word-wrap options are not set.\n",
+        "     * The extra parts are split into the next row, if either line-wrap or word-wrap options are been set. If the extra parts are still too long to put in the next row, repeat this operation again and again. This operation also eats more rows in the window, thus it may contains less lines in the buffer.\n",
+      ],
+    );
+    let expect = vec![
+      "lo, RSVIM!",
+      "\n",
+      "s is a qui",
+      "te simple ",
+      "and small ",
+      "test lines",
+      ".\n",
+      " still it ",
+      "contains s",
+      "everal thi",
+    ];
+
+    let mut window = make_window(terminal_size, buf.clone(), &win_opts);
+    let actual = {
+      let buf = lock!(buf);
+      let window_actual_shape = window.actual_shape();
+      let window_local_options = window.options();
+      let viewport = Viewport::downward(&buf, window_actual_shape, window_local_options, 0, 3);
+      window.set_viewport(Viewport::to_arc(viewport));
+      lock!(window.viewport()).clone()
+    };
+    let expect_fills: BTreeMap<usize, usize> = vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
+    assert_viewport(buf, &actual, &expect, 0, 3, &expect_fills, &expect_fills);
   }
 }
 
