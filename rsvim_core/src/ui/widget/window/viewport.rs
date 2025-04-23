aa4226743bcdf83c6f69b1700f28d5ac9bedd11c
linrongbin16 <linrongbin16@outlook.com>
2025-04-23 10:00:14 +0800

    chore

--- rsvim_core/src/ui/widget/window/viewport.rs
+++ rsvim_core/src/ui/widget/window/viewport.rs
@@ -505,7 +505,7 @@ impl Viewport {
   /// NOTE: By default the viewport should starts from (0, 0), i.e. when first open buffer in a
   /// window.
   pub fn downward(
-    buffer: &mut Buffer,
+    buffer: &Buffer,
     window_actual_shape: &U16Rect,
     window_local_options: &WindowLocalOptions,
     start_line_idx: usize,
@@ -1237,9 +1237,72 @@ mod tests_downward_nowrap_start_column {
   use crate::test::buf::{make_buffer_from_lines, make_empty_buffer};
   #[allow(dead_code)]
   use crate::test::log::init as test_log_init;
+  use crate::ui::tree::Inodeable;
 
   #[test]
-  fn new1() {}
+  fn new1() {
+    test_log_init();
+
+    let terminal_size = U16Size::new(10, 10);
+    let buf_opts = BufferLocalOptionsBuilder::default().build().unwrap();
+    let win_opts = make_nowrap();
+
+    let buf = make_buffer_from_lines(
+      terminal_size.height(),
+      buf_opts,
+      vec![
+        "Hello, RSVIM!\n",
+        "This is a quite simple and small test lines.\n",
+        "But still it contains several things we want to test:\n",
+        "  1. When the line is small enough to completely put inside a row of the window content widget, then the line-wrap and word-wrap doesn't affect the rendering.\n",
+        "  2. When the line is too long to be completely put in a row of the window content widget, there're multiple cases:\n",
+        "     * The extra parts are been truncated if both line-wrap and word-wrap options are not set.\n",
+        "     * The extra parts are split into the next row, if either line-wrap or word-wrap options are been set. If the extra parts are still too long to put in the next row, repeat this operation again and again. This operation also eats more rows in the window, thus it may contains less lines in the buffer.\n",
+      ],
+    );
+
+    let expect = vec![
+      "lo, RSVIM!",
+      "s is a qui",
+      " still it ",
+      ". When the",
+      ". When the",
+      "  * The ex",
+      "  * The ex",
+      "",
+    ];
+
+    let mut window = make_window(terminal_size, buf.clone(), &win_opts);
+    let actual = {
+      let buf = lock!(buf);
+      let window_actual_shape = window.actual_shape();
+      let window_local_options = window.options();
+      let viewport = Viewport::downward(&buf, window_actual_shape, window_local_options, 0, 3);
+      window.set_viewport(Viewport::to_arc(viewport));
+      lock!(window.viewport()).clone()
+    };
+    let expect_fills: BTreeMap<usize, usize> = vec![
+      (0, 0),
+      (1, 0),
+      (2, 0),
+      (3, 0),
+      (4, 0),
+      (5, 0),
+      (6, 0),
+      (7, 0),
+    ]
+    .into_iter()
+    .collect();
+    assert_viewport(
+      buf.clone(),
+      &actual,
+      &expect,
+      0,
+      8,
+      &expect_fills,
+      &expect_fills,
+    );
+  }
 }
 
 // #[allow(unused_imports)]
@@ -1997,8 +2060,8 @@ mod tests_downward_wrap_nolinebreak {
       "ndering.\n",
     ];
     let actual = {
-      let mut buf = lock!(buf);
-      Viewport::downward(&mut buf, window.actual_shape(), &win_opts, 2, 0)
+      let buf = lock!(buf);
+      Viewport::downward(&buf, window.actual_shape(), &win_opts, 2, 0)
     };
     let expect_fills: BTreeMap<usize, usize> = vec![(2, 0), (3, 0)].into_iter().collect();
     assert_viewport(
@@ -2081,8 +2144,8 @@ mod tests_downward_wrap_nolinebreak {
       "is operation al",
     ];
     let actual = {
-      let mut buf = lock!(buf);
-      Viewport::downward(&mut buf, window.actual_shape(), &win_opts, 6, 0)
+      let buf = lock!(buf);
+      Viewport::downward(&buf, window.actual_shape(), &win_opts, 6, 0)
     };
     let expect_fills: BTreeMap<usize, usize> = vec![(6, 0)].into_iter().collect();
     assert_viewport(
@@ -2133,8 +2196,8 @@ mod tests_downward_wrap_nolinebreak {
 
     let expect = vec!["This is a quite", " simple and sma", "ll test lines.\n", ""];
     let actual = {
-      let mut buf = lock!(buf);
-      Viewport::downward(&mut buf, window.actual_shape(), &win_opts, 1, 0)
+      let buf = lock!(buf);
+      Viewport::downward(&buf, window.actual_shape(), &win_opts, 1, 0)
     };
     let expect_fills: BTreeMap<usize, usize> = vec![(1, 0), (2, 0)].into_iter().collect();
     assert_viewport(
