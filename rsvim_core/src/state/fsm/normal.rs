813d1f503924e328f04748c90d549a78140cb5f2
linrongbin16 <linrongbin16@outlook.com>
2025-04-27 16:04:31 +0800

    chore

--- rsvim_core/src/state/fsm/normal.rs
+++ rsvim_core/src/state/fsm/normal.rs
@@ -1685,7 +1685,7 @@ mod tests_cursor_move_to {
   }
 
   #[test]
-  fn _wrap1() {
+  fn wrap1() {
     test_log_init();
 
     let lines = vec![
@@ -1718,7 +1718,7 @@ mod tests_cursor_move_to {
 
     let data_access = StatefulDataAccess::new(state, tree, bufs, Event::Key(key_event));
     let stateful = NormalStateful::default();
-    let next_stateful = stateful.cursor_move(&data_access, Command::CursorMoveBy((0, 10)));
+    let next_stateful = stateful.cursor_move(&data_access, Command::CursorMoveTo((7, 2)));
     assert!(matches!(next_stateful, StatefulValue::NormalMode(_)));
 
     let stateful = match next_stateful {
@@ -1729,9 +1729,9 @@ mod tests_cursor_move_to {
     let tree = data_access.tree.clone();
     let actual1 = get_cursor_viewport(tree);
     assert_eq!(actual1.line_idx(), 2);
-    assert_eq!(actual1.char_idx(), 0);
+    assert_eq!(actual1.char_idx(), 7);
 
-    let next_stateful = stateful.cursor_move(&data_access, Command::CursorMoveBy((0, -1)));
+    let next_stateful = stateful.cursor_move(&data_access, Command::CursorMoveTo((0, 1)));
     assert!(matches!(next_stateful, StatefulValue::NormalMode(_)));
 
     let tree = data_access.tree.clone();
