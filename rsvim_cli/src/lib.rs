ac31fa01f47a1faa6feb601deab145ec0a794adf
linrongbin16 <linrongbin16@outlook.com>
2025-04-30 22:14:50 +0800

    chore

---
+++ rsvim_cli/src/lib.rs
@@ -0,0 +1,14 @@
+//! Common utils for rsvim executables.
+
+#[cfg(all(
+  not(target_os = "windows"),
+  not(target_os = "openbsd"),
+  not(target_os = "freebsd"),
+  any(
+    target_arch = "x86_64",
+    target_arch = "aarch64",
+    target_arch = "powerpc64"
+  )
+))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
