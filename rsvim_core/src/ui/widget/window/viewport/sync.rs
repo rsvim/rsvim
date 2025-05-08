d9a759b092d66d61653cf62e0ca1952b398737bc
linrongbin16 <linrongbin16@outlook.com>
2025-05-08 14:35:54 +0800

    chore

--- rsvim_core/src/ui/widget/window/viewport/sync.rs
+++ rsvim_core/src/ui/widget/window/viewport/sync.rs
@@ -754,6 +754,7 @@ fn sync_wrap_linebreak(
   }
 }
 
+#[allow(clippy::too_many_arguments)]
 // Returns `start_line`, `start_column` for a viewport.
 pub fn search_anchor_downward(
   buffer: &Buffer,
