588aafd209ccbdbe9284e8a3406b72bc8dbf22a8
linrongbin16 <linrongbin16@outlook.com>
2025-05-14 11:04:29 +0800

    chroe

--- rsvim_core/src/ui/widget/window/viewport.rs
+++ rsvim_core/src/ui/widget/window/viewport.rs
@@ -6525,7 +6525,7 @@ mod tests_search_anchor_upward_nowrap {
 
     // Search-1
     {
-      let expect = vec!["\t1. When", "\t2. When", "\t\t3", "\t\t4", ""];
+      let expect = vec!["is\tsmall", "long", "runcated if", "into the\tn", ""];
 
       let actual = {
         let target_cursor_line = 6;
@@ -6556,10 +6556,10 @@ mod tests_search_anchor_upward_nowrap {
         lock!(window.viewport()).clone()
       };
 
-      let expect_start_fills: BTreeMap<usize, usize> = vec![(3, 0), (4, 0), (5, 0), (6, 0), (7, 0)]
+      let expect_start_fills: BTreeMap<usize, usize> = vec![(3, 1), (4, 7), (5, 0), (6, 0), (7, 0)]
         .into_iter()
         .collect();
-      let expect_end_fills: BTreeMap<usize, usize> = vec![(3, 2), (4, 2), (5, 0), (6, 0), (7, 0)]
+      let expect_end_fills: BTreeMap<usize, usize> = vec![(3, 1), (4, 6), (5, 6), (6, 0), (7, 0)]
         .into_iter()
         .collect();
       assert_viewport(
@@ -6573,7 +6573,7 @@ mod tests_search_anchor_upward_nowrap {
       );
     }
 
-    // Search-3
+    // Search-2
     {
       let expect = vec!["ut still it conta", "1. When", "2. When", "\t3.", "\t4."];
 
@@ -6623,7 +6623,7 @@ mod tests_search_anchor_upward_nowrap {
       );
     }
 
-    // Search-4
+    // Search-3
     {
       let expect = vec!["\t1. When", "\t2. When", "\t\t3", "\t\t4", ""];
 
