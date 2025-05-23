7e735ad7c2471a6c38a4391f4df0bfddbf475932
linrongbin16 <linrongbin16@outlook.com>
2025-05-23 10:01:53 +0800

    chore

--- rsvim_core/src/state/fsm/normal.rs
+++ rsvim_core/src/state/fsm/normal.rs
@@ -18,7 +18,6 @@ pub struct NormalStateful {}
 
 impl Stateful for NormalStateful {
   fn handle(&self, data_access: StatefulDataAccess) -> StatefulValue {
-    debug_assert!(false);
     let event = data_access.event.clone();
 
     match event {
