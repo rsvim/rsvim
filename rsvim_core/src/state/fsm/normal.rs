7c254510bfe31440949f7728af51cafea9db4283
linrongbin16 <linrongbin16@outlook.com>
2025-06-01 12:08:58 +0800

    chroe

--- rsvim_core/src/state/fsm/normal.rs
+++ rsvim_core/src/state/fsm/normal.rs
@@ -5467,7 +5467,7 @@ mod tests_cursor_move_and_scroll {
       let tree = data_access.tree.clone();
       let actual = get_cursor_viewport(tree.clone());
       assert_eq!(actual.line_idx(), 0);
-      assert_eq!(actual.char_idx(), 0);
+      assert_eq!(actual.char_idx(), 12);
 
       let viewport = get_viewport(tree.clone());
       let expect = vec![
