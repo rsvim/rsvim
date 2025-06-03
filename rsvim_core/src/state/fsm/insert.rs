b1225e8efd50ec41e0e31d4bbff4790dee1fd81c
linrongbin16 <linrongbin16@outlook.com>
2025-06-03 16:35:59 +0800

    fix(cursor): limit cursor position by last visible char when back to normal mode (#438)

--- rsvim_core/src/state/fsm/insert.rs
+++ rsvim_core/src/state/fsm/insert.rs
@@ -66,16 +66,38 @@ impl Stateful for InsertStateful {
   }
 }
 
+#[derive(Debug, Copy, Clone)]
+struct CursorMoveImplOptions {
+  pub include_empty_eol: bool,
+}
+
+impl CursorMoveImplOptions {
+  pub fn include_empty_eol() -> Self {
+    Self {
+      include_empty_eol: true,
+    }
+  }
+
+  pub fn exclude_empty_eol() -> Self {
+    Self {
+      include_empty_eol: false,
+    }
+  }
+}
+
 impl InsertStateful {
-  fn goto_normal_mode(
-    &self,
-    data_access: &StatefulDataAccess,
-    _command: Operation,
-  ) -> StatefulValue {
-    debug_assert!(matches!(_command, Operation::GotoNormalMode));
+  fn goto_normal_mode(&self, data_access: &StatefulDataAccess, _op: Operation) -> StatefulValue {
+    debug_assert!(matches!(_op, Operation::GotoNormalMode));
 
     let tree = data_access.tree.clone();
     let mut tree = lock!(tree);
+
+    self._cursor_move_impl(
+      &mut tree,
+      Operation::CursorMoveBy((0, 0)),
+      CursorMoveImplOptions::exclude_empty_eol(),
+    );
+
     let cursor_id = tree.cursor_id().unwrap();
     if let Some(TreeNode::Cursor(cursor)) = tree.node_mut(cursor_id) {
       cursor.set_style(&CursorStyle::SteadyBlock);
@@ -92,6 +114,12 @@ impl InsertStateful {
     let tree = data_access.tree.clone();
     let mut tree = lock!(tree);
 
+    self._cursor_move_impl(&mut tree, op, CursorMoveImplOptions::include_empty_eol());
+
+    StatefulValue::InsertMode(InsertStateful::default())
+  }
+
+  fn _cursor_move_impl(&self, tree: &mut Tree, op: Operation, opts: CursorMoveImplOptions) {
     if let Some(current_window_id) = tree.current_window_id() {
       if let Some(TreeNode::Window(current_window)) = tree.node_mut(current_window_id) {
         let buffer = current_window.buffer().upgrade().unwrap();
@@ -102,7 +130,7 @@ impl InsertStateful {
 
         // Only move cursor when it is different from current cursor.
         if let Some((target_cursor_char, target_cursor_line, search_direction)) =
-          self._target_cursor_include_empty_eol(&cursor_viewport, &buffer, op)
+          self._target_cursor_considering_empty_eol(opts, &cursor_viewport, &buffer, op)
         {
           let new_viewport: Option<ViewportArc> = {
             let viewport = lock!(viewport);
@@ -164,31 +192,32 @@ impl InsertStateful {
     } else {
       unreachable!()
     }
-
-    StatefulValue::InsertMode(InsertStateful::default())
   }
 
   // Returns `(target_cursor_char, target_cursor_line, viewport_search_direction)`.
-  fn _target_cursor_include_empty_eol(
+  fn _target_cursor_considering_empty_eol(
     &self,
+    opts: CursorMoveImplOptions,
     cursor_viewport: &CursorViewport,
     buffer: &Buffer,
     op: Operation,
   ) -> Option<(usize, usize, ViewportSearchAnchorDirection)> {
-    let (target_cursor_char, target_cursor_line, move_direction) =
-      cursor_ops::normalize_as_cursor_move_to(
+    let (target_cursor_char, target_cursor_line, move_direction) = if opts.include_empty_eol {
+      cursor_ops::normalize_as_cursor_move_to_include_empty_eol(
+        buffer,
         op,
         cursor_viewport.char_idx(),
         cursor_viewport.line_idx(),
-      );
-    let target_cursor_line = std::cmp::min(
-      target_cursor_line,
-      buffer.get_rope().len_lines().saturating_sub(1),
-    );
-    let target_cursor_char = match buffer.last_char_on_line(target_cursor_line) {
-      Some(last_visible_char) => std::cmp::min(target_cursor_char, last_visible_char),
-      None => target_cursor_char,
+      )
+    } else {
+      cursor_ops::normalize_as_cursor_move_to_exclude_empty_eol(
+        buffer,
+        op,
+        cursor_viewport.char_idx(),
+        cursor_viewport.line_idx(),
+      )
     };
+
     if target_cursor_char != cursor_viewport.char_idx()
       || target_cursor_line != cursor_viewport.line_idx()
     {
