87fc0213527a0388ab7305b2ae2d885679419d04
linrongbin16 <linrongbin16@outlook.com>
2025-06-05 14:36:22 +0800

    chore

--- rsvim_core/src/buf/opt/file_format.rs
+++ rsvim_core/src/buf/opt/file_format.rs
@@ -7,10 +7,19 @@ use std::string::ToString;
 pub enum FileFormatOption {
   /// CRLF (<CR><NL>)
   Dos,
+
   /// LF (<NL>)
   Unix,
-  /// CR (<CR>)
+
+  /// LF (<NL>)
+  ///
+  /// NOTE: CR (<CR>) is deprecated in macos.
   Mac,
+
+  /// CR (<CR>)
+  ///
+  /// NOTE: CR (<CR>) is deprecated in macos.
+  ClassicMac,
 }
 
 impl Display for FileFormatOption {
@@ -19,6 +28,7 @@ impl Display for FileFormatOption {
       FileFormatOption::Dos => write!(f, "dos"),
       FileFormatOption::Unix => write!(f, "unix"),
       FileFormatOption::Mac => write!(f, "mac"),
+      FileFormatOption::ClassicMac => write!(f, "classic_mac"),
     }
   }
 }
@@ -32,7 +42,65 @@ impl TryFrom<&str> for FileFormatOption {
       "dos" => Ok(FileFormatOption::Dos),
       "unix" => Ok(FileFormatOption::Unix),
       "mac" => Ok(FileFormatOption::Mac),
+      "classic_mac" => Ok(FileFormatOption::ClassicMac),
       _ => Err("Unknown FileFormat value".to_string()),
+    }
+  }
+}
+
+impl From<EndOfLineOption> for FileFormatOption {
+  fn from(value: EndOfLineOption) -> Self {
+    match value {
+      EndOfLineOption::CRLF => FileFormatOption::Dos,
+      EndOfLineOption::LF => FileFormatOption::Unix,
+      EndOfLineOption::CR => FileFormatOption::ClassicMac,
+    }
+  }
+}
+
+impl Into<EndOfLineOption> for FileFormatOption {
+  fn into(self) -> EndOfLineOption {
+    match self {
+      FileFormatOption::Dos => EndOfLineOption::CRLF,
+      FileFormatOption::Unix => EndOfLineOption::LF,
+      FileFormatOption::Mac => EndOfLineOption::LF,
+      FileFormatOption::ClassicMac => EndOfLineOption::CR,
+    }
+  }
+}
+
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+pub enum EndOfLineOption {
+  /// Windows
+  CRLF,
+
+  /// Unix
+  LF,
+
+  /// Classic Mac, not used today
+  CR,
+}
+
+impl Display for EndOfLineOption {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      EndOfLineOption::CRLF => write!(f, "CRLF"),
+      EndOfLineOption::LF => write!(f, "LF"),
+      EndOfLineOption::CR => write!(f, "CR"),
+    }
+  }
+}
+
+impl TryFrom<&str> for EndOfLineOption {
+  type Error = String;
+
+  fn try_from(value: &str) -> Result<Self, Self::Error> {
+    let lower_value = value.to_lowercase();
+    match lower_value.as_str() {
+      "CRLF" => Ok(EndOfLineOption::CRLF),
+      "LF" => Ok(EndOfLineOption::LF),
+      "CR" => Ok(EndOfLineOption::CR),
+      _ => Err("Unknown EndOfLine value".to_string()),
     }
   }
 }
--- rsvim_core/src/defaults/buf.rs
+++ rsvim_core/src/defaults/buf.rs
@@ -3,7 +3,17 @@
 //! See: [`crate::buf::BufferLocalOptions`].
 
 use crate::buf::FileEncodingOption;
+use crate::buf::FileFormatOption;
 
 pub const TAB_STOP: u16 = 8;
 
 pub const FILE_ENCODING: FileEncodingOption = FileEncodingOption::Utf8;
+
+#[cfg(target_os = "windows")]
+pub const FILE_FORMAT: FileFormatOption = FileFormatOption::Dos;
+
+#[cfg(target_os = "macos")]
+pub const FILE_FORMAT: FileFormatOption = FileFormatOption::Mac;
+
+#[cfg(not(any(target_os = "macos", target_os = "windows")))]
+pub const FILE_FORMAT: FileFormatOption = FileFormatOption::Dos;
