a2e40d73ca87af834821a0e8f0a7b459191ee610
linrongbin16 <linrongbin16@outlook.com>
2025-06-06 16:22:51 +0800

    chore

--- rsvim_core/src/state/fsm/insert.rs
+++ rsvim_core/src/state/fsm/insert.rs
@@ -195,6 +195,105 @@ impl InsertStateful {
       _ => unreachable!(),
     };
 
+    let tree = data_access.tree.clone();
+    let mut tree = lock!(tree);
+    let buffer = self._current_buffer(&mut tree);
+    let buffer = buffer.upgrade().unwrap();
+    let mut buffer = lock!(buffer);
+
+    // Delete text.
+    let (cursor_line_idx_after_deleted, cursor_char_idx_after_deleted) = {
+      if let Some(current_window_id) = tree.current_window_id() {
+        if let Some(TreeNode::Window(current_window)) = tree.node_mut(current_window_id) {
+          let cursor_viewport = current_window.cursor_viewport();
+          let cursor_line_idx = cursor_viewport.line_idx();
+          let cursor_char_idx = cursor_viewport.char_idx();
+          debug_assert!(buffer.get_rope().get_line(cursor_line_idx).is_some());
+          let start_char_pos_of_line = buffer.get_rope().line_to_char(cursor_line_idx);
+          let before_insert_char_idx = start_char_pos_of_line + cursor_char_idx;
+
+          _dbg_print_details(
+            &buffer,
+            cursor_line_idx,
+            before_insert_char_idx,
+            "Before delete(left)",
+          );
+
+          buffer.get_rope_mut().insert(before_insert_char_idx, "");
+          buffer
+            .truncate_cached_line_since_char(cursor_line_idx, cursor_char_idx.saturating_sub(1));
+          let after_inserted_char_idx = cursor_char_idx + n;
+
+          self._append_eol_if_not_exists_at_file_end(&mut buffer, cursor_line_idx);
+
+          _dbg_print_details_on_line(
+            &buffer,
+            cursor_line_idx,
+            after_inserted_char_idx,
+            "After inserted",
+          );
+
+          (cursor_line_idx, after_inserted_char_idx)
+        } else {
+          unreachable!()
+        }
+      } else {
+        unreachable!()
+      }
+    };
+
+    // Update viewport since the buffer doesn't match the viewport.
+    if let Some(current_window_id) = tree.current_window_id() {
+      if let Some(TreeNode::Window(current_window)) = tree.node_mut(current_window_id) {
+        let viewport = current_window.viewport();
+        let cursor_viewport = current_window.cursor_viewport();
+        trace!("before viewport:{:?}", viewport);
+        trace!("before cursor_viewport:{:?}", cursor_viewport);
+
+        let start_line = std::cmp::min(
+          viewport.start_line_idx(),
+          buffer.get_rope().len_lines().saturating_sub(1),
+        );
+        debug_assert!(buffer.get_rope().get_line(start_line).is_some());
+        let bufline_len_chars = buffer.get_rope().line(start_line).len_chars();
+        let start_column = std::cmp::min(
+          viewport.start_column_idx(),
+          buffer.width_before(start_line, bufline_len_chars),
+        );
+
+        let updated_viewport = Viewport::to_arc(Viewport::view(
+          &buffer,
+          current_window.actual_shape(),
+          current_window.options(),
+          start_line,
+          start_column,
+        ));
+        trace!("after updated_viewport:{:?}", updated_viewport);
+
+        current_window.set_viewport(updated_viewport.clone());
+        if let Some(updated_cursor_viewport) = cursor_ops::cursor_move_to(
+          &updated_viewport,
+          &cursor_viewport,
+          &buffer,
+          Operation::CursorMoveTo((cursor_viewport.char_idx(), cursor_viewport.line_idx())),
+        ) {
+          trace!(
+            "after updated_cursor_viewport:{:?}",
+            updated_cursor_viewport
+          );
+          current_window.set_cursor_viewport(updated_cursor_viewport);
+        }
+      } else {
+        unreachable!();
+      }
+    } else {
+      unreachable!();
+    }
+
+    trace!(
+      "Move to inserted pos, line:{cursor_line_idx_after_deleted}, char:{cursor_char_idx_after_deleted}"
+    );
+
     StatefulValue::InsertMode(InsertStateful::default())
   }
 
@@ -256,70 +355,72 @@ impl InsertStateful {
             .truncate_cached_line_since_char(cursor_line_idx, cursor_char_idx.saturating_sub(1));
           let after_inserted_char_idx = cursor_char_idx + text.len();
 
-          // For text mode (different from the 'binary' mode, i.e. bin/hex mode), the editor have
-          // to always keep an eol (end-of-line) at the end of text file. It helps the cursor
-          // motion.
-          if cursor_line_idx == buffer.get_rope().len_lines().saturating_sub(1) {
-            use crate::defaults::ascii::end_of_line as eol;
+          self._append_eol_if_not_exists_at_file_end(&mut buffer, cursor_line_idx);
 
-            let buf_eol = buffer.options().end_of_line();
-            let bufline = buffer.get_rope().line(cursor_line_idx);
-            let bufline_len_chars = bufline.len_chars();
-
-            if bufline_len_chars == 0 {
-              buffer
-                .get_rope_mut()
-                .insert(0_usize, buf_eol.to_compact_string().as_str());
-              buffer.remove_cached_line(cursor_line_idx);
-
-              _dbg_print_details(
-                &buffer,
-                cursor_line_idx,
-                before_insert_char_idx,
-                "Eol inserted(line=0)",
-              );
-            } else {
-              let bufline_start_char_pos = buffer.get_rope().line_to_char(cursor_line_idx);
-              let bufline_insert_char_pos = bufline_start_char_pos + bufline_len_chars;
-
-              let last1 = bufline.char(bufline_len_chars - 1);
-              if last1.to_compact_string() != eol::CR || last1.to_compact_string() != eol::LF {
-                buffer.get_rope_mut().insert(
-                  bufline_insert_char_pos,
-                  buf_eol.to_compact_string().as_str(),
-                );
-                buffer.truncate_cached_line_since_char(
-                  cursor_line_idx,
-                  bufline_len_chars.saturating_sub(1),
-                );
-
-                _dbg_print_details(
-                  &buffer,
-                  cursor_line_idx,
-                  before_insert_char_idx,
-                  "Eol inserted(last=1)",
-                );
-              } else if bufline_len_chars >= 2 {
-                let last2 = format!("{}{}", bufline.char(bufline_len_chars - 2), last1);
-                if last2 != eol::CRLF {
-                  buffer.get_rope_mut().insert(
-                    bufline_insert_char_pos,
-                    buf_eol.to_compact_string().as_str(),
-                  );
-                  buffer.truncate_cached_line_since_char(
-                    cursor_line_idx,
-                    bufline_len_chars.saturating_sub(1),
-                  );
-                  _dbg_print_details(
-                    &buffer,
-                    cursor_line_idx,
-                    before_insert_char_idx,
-                    "Eol inserted(last=2)",
-                  );
-                }
-              }
-            }
-          }
+          // // For text mode (different from the 'binary' mode, i.e. bin/hex mode), the editor have
+          // // to always keep an eol (end-of-line) at the end of text file. It helps the cursor
+          // // motion.
+          // if cursor_line_idx == buffer.get_rope().len_lines().saturating_sub(1) {
+          //   use crate::defaults::ascii::end_of_line as eol;
+          //
+          //   let buf_eol = buffer.options().end_of_line();
+          //   let bufline = buffer.get_rope().line(cursor_line_idx);
+          //   let bufline_len_chars = bufline.len_chars();
+          //
+          //   if bufline_len_chars == 0 {
+          //     buffer
+          //       .get_rope_mut()
+          //       .insert(0_usize, buf_eol.to_compact_string().as_str());
+          //     buffer.remove_cached_line(cursor_line_idx);
+          //
+          //     _dbg_print_details(
+          //       &buffer,
+          //       cursor_line_idx,
+          //       before_insert_char_idx,
+          //       "Eol inserted(line=0)",
+          //     );
+          //   } else {
+          //     let bufline_start_char_pos = buffer.get_rope().line_to_char(cursor_line_idx);
+          //     let bufline_insert_char_pos = bufline_start_char_pos + bufline_len_chars;
+          //
+          //     let last1 = bufline.char(bufline_len_chars - 1);
+          //     if last1.to_compact_string() != eol::CR || last1.to_compact_string() != eol::LF {
+          //       buffer.get_rope_mut().insert(
+          //         bufline_insert_char_pos,
+          //         buf_eol.to_compact_string().as_str(),
+          //       );
+          //       buffer.truncate_cached_line_since_char(
+          //         cursor_line_idx,
+          //         bufline_len_chars.saturating_sub(1),
+          //       );
+          //
+          //       _dbg_print_details(
+          //         &buffer,
+          //         cursor_line_idx,
+          //         before_insert_char_idx,
+          //         "Eol inserted(last=1)",
+          //       );
+          //     } else if bufline_len_chars >= 2 {
+          //       let last2 = format!("{}{}", bufline.char(bufline_len_chars - 2), last1);
+          //       if last2 != eol::CRLF {
+          //         buffer.get_rope_mut().insert(
+          //           bufline_insert_char_pos,
+          //           buf_eol.to_compact_string().as_str(),
+          //         );
+          //         buffer.truncate_cached_line_since_char(
+          //           cursor_line_idx,
+          //           bufline_len_chars.saturating_sub(1),
+          //         );
+          //         _dbg_print_details(
+          //           &buffer,
+          //           cursor_line_idx,
+          //           before_insert_char_idx,
+          //           "Eol inserted(last=2)",
+          //         );
+          //       }
+          //     }
+          //   }
+          // }
 
           _dbg_print_details_on_line(
             &buffer,
@@ -410,6 +511,66 @@ impl InsertStateful {
       }
     } else {
       unreachable!()
+    }
+  }
+
+  // For text mode (different from the 'binary' mode, i.e. bin/hex mode), the editor have
+  // to always keep an eol (end-of-line) at the end of text file. It helps the cursor
+  // motion.
+  fn _append_eol_if_not_exists_at_file_end(&self, buffer: &mut Buffer, cursor_line_idx: usize) {
+    if cursor_line_idx == buffer.get_rope().len_lines().saturating_sub(1) {
+      use crate::defaults::ascii::end_of_line as eol;
+
+      let buf_eol = buffer.options().end_of_line();
+      let bufline = buffer.get_rope().line(cursor_line_idx);
+      let bufline_len_chars = bufline.len_chars();
+
+      if bufline_len_chars == 0 {
+        buffer
+          .get_rope_mut()
+          .insert(0_usize, buf_eol.to_compact_string().as_str());
+        buffer.remove_cached_line(cursor_line_idx);
+
+        _dbg_print_details_on_line(&buffer, cursor_line_idx, 0_usize, "Eol appended(line=0)");
+      } else {
+        let bufline_start_char_pos = buffer.get_rope().line_to_char(cursor_line_idx);
+        let bufline_insert_char_pos = bufline_start_char_pos + bufline_len_chars;
+
+        let last1 = bufline.char(bufline_len_chars - 1);
+        if last1.to_compact_string() != eol::CR || last1.to_compact_string() != eol::LF {
+          buffer.get_rope_mut().insert(
+            bufline_insert_char_pos,
+            buf_eol.to_compact_string().as_str(),
+          );
+          buffer
+            .truncate_cached_line_since_char(cursor_line_idx, bufline_len_chars.saturating_sub(1));
+
+          _dbg_print_details(
+            &buffer,
+            cursor_line_idx,
+            bufline_insert_char_pos,
+            "Eol appended(last=1)",
+          );
+        } else if bufline_len_chars >= 2 {
+          let last2 = format!("{}{}", bufline.char(bufline_len_chars - 2), last1);
+          if last2 != eol::CRLF {
+            buffer.get_rope_mut().insert(
+              bufline_insert_char_pos,
+              buf_eol.to_compact_string().as_str(),
+            );
+            buffer.truncate_cached_line_since_char(
+              cursor_line_idx,
+              bufline_len_chars.saturating_sub(1),
+            );
+            _dbg_print_details(
+              &buffer,
+              cursor_line_idx,
+              bufline_insert_char_pos,
+              "Eol appended(last=2)",
+            );
+          }
+        }
+      }
     }
   }
 }
