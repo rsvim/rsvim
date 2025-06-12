34d06a9feb32fe49f8b4d3912046e32cbd6050ad
linrongbin16 <linrongbin16@outlook.com>
2025-06-12 14:50:15 +0800

    chore

--- rsvim_core/src/ui/widget/window/content.rs
+++ rsvim_core/src/ui/widget/window/content.rs
@@ -77,7 +77,7 @@ impl Widgetable for WindowContent {
     let mut row_idx = 0_u16;
     let mut line_idx = viewport.start_line_idx();
 
-    let mut buflines = buffer.text().rope().get_lines_at(line_idx).unwrap();
+    let mut buflines = buffer.text().rope().lines_at(line_idx);
 
     while line_idx < viewport.end_line_idx() {
       debug_assert!(row_idx < height);
