6b469d499f44987dc08689bd7eb1877ba452ad58
linrongbin16 <linrongbin16@outlook.com>
2025-06-17 23:13:10 +0800

    chore

--- rsvim_core/src/ui/widget/cmdline.rs
+++ rsvim_core/src/ui/widget/cmdline.rs
@@ -4,11 +4,16 @@
 
 use crate::content::TemporaryContentsWk;
 use crate::prelude::*;
+use crate::ui::canvas::{Canvas, Cell};
 use crate::ui::tree::*;
 use crate::ui::viewport::ViewportWk;
 use crate::ui::widget::Widgetable;
 use crate::ui::widget::window::opt::WindowLocalOptions;
 use crate::{inode_impl, lock};
+
+use geo::point;
+use std::convert::From;
+use tracing::trace;
 
 #[derive(Debug, Clone)]
 /// The Vim cmdline.
@@ -25,13 +30,19 @@ pub struct Cmdline {
 }
 
 impl Cmdline {
-  pub fn new(opts: &WindowLocalOptions, shape: IRect, contents: TemporaryContentsWk) -> Self {
+  pub fn new(
+    opts: &WindowLocalOptions,
+    shape: IRect,
+    contents: TemporaryContentsWk,
+    viewport: ViewportWk,
+  ) -> Self {
     let options = *opts;
     let base = InodeBase::new(shape);
     Self {
       base,
       contents,
       options,
+      viewport,
     }
   }
 }
@@ -55,25 +66,17 @@ impl Widgetable for Cmdline {
 
     // If viewport has no lines.
     if viewport.end_line_idx() <= viewport.start_line_idx() {
-      trace!("Draw window content, viewport is empty");
+      trace!("Draw cmdline, viewport is empty");
       return;
     }
 
-    // trace!(
-    //   "Draw window content, actual shape min pos:{:?}, height/width:{}/{}",
-    //   upos,
-    //   height,
-    //   width
-    // );
-    // trace!("Draw window content, viewport:{:?}", viewport);
-
-    let buffer = self.buffer.upgrade().unwrap();
-    let buffer = lock!(buffer);
+    let contents = self.contents.upgrade().unwrap();
+    let contents = lock!(contents);
 
     let mut row_idx = 0_u16;
     let mut line_idx = viewport.start_line_idx();
 
-    let mut buflines = buffer.text().rope().lines_at(line_idx);
+    let mut buflines = contents.cmdline_content().rope().lines_at(line_idx);
 
     while line_idx < viewport.end_line_idx() {
       debug_assert!(row_idx < height);
@@ -128,14 +131,6 @@ impl Widgetable for Cmdline {
             let cells_upos = point!(x: col_idx + upos.x(), y: row_idx + upos.y());
             canvas.frame_mut().set_cells_at(cells_upos, cells);
             col_idx += start_fills;
-            // trace!(
-            //   "1-line_idx:{}, row_idx:{}, col_idx:{}, line_viewport:{:?}, r:{:?}",
-            //   line_idx,
-            //   row_idx,
-            //   col_idx,
-            //   line_viewport,
-            //   r_viewport
-            // );
           }
 
           // Render line content.
@@ -145,7 +140,7 @@ impl Widgetable for Cmdline {
             let mut chars_iter = bline.get_chars_at(r_viewport.start_char_idx()).unwrap();
             while char_idx < r_viewport.end_char_idx() {
               let c = chars_iter.next().unwrap();
-              let (unicode_symbol, unicode_width) = buffer.text().char_symbol(c);
+              let (unicode_symbol, unicode_width) = contents.text().char_symbol(c);
 
               let cell = Cell::with_symbol(unicode_symbol);
               let cell_upos = point!(x: col_idx + upos.x(), y: row_idx + upos.y());
@@ -153,25 +148,15 @@ impl Widgetable for Cmdline {
 
               col_idx += unicode_width as u16;
               char_idx += 1;
-              // total_width += unicode_width;
             }
-            // trace!(
-            //   "2-line_idx:{}, row_idx:{}, col_idx:{}, total_width:{}, line_viewport:{:?}, r:{:?}",
-            //   line_idx,
-            //   row_idx,
-            //   col_idx,
-            //   total_width,
-            //   line_viewport,
-            //   r_viewport
-            // );
           }
 
           // Render left empty parts.
-          let end_dcol_idx = buffer
-            .text()
+          let end_dcol_idx = contents
+            .cmdline_content()
             .width_before(line_idx, r_viewport.end_char_idx());
-          let start_dcol_idx = buffer
-            .text()
+          let start_dcol_idx = contents
+            .cmdline_content()
             .width_before(line_idx, r_viewport.start_char_idx());
           let occupied_length = (end_dcol_idx - start_dcol_idx) as u16 + start_fills + end_fills;
 
@@ -183,15 +168,6 @@ impl Widgetable for Cmdline {
             let cells_upos = point!(x: col_idx + upos.x(), y: row_idx + upos.y());
             canvas.frame_mut().set_cells_at(cells_upos, cells);
             col_idx += left_length;
-            // trace!(
-            //   "3-line_idx:{}, row_idx:{}, col_idx:{}, left_length:{}, line_viewport:{:?}, r:{:?}",
-            //   line_idx,
-            //   row_idx,
-            //   col_idx,
-            //   left_length,
-            //   line_viewport,
-            //   r_viewport
-            // );
           }
 
           // Render end fills.
@@ -203,14 +179,6 @@ impl Widgetable for Cmdline {
             canvas.frame_mut().set_cells_at(cells_upos, cells);
 
             col_idx += end_fills;
-            // trace!(
-            //   "4-line_idx:{}, row_idx:{}, col_idx:{}, line_viewport:{:?}, r:{:?}",
-            //   line_idx,
-            //   row_idx,
-            //   col_idx,
-            //   line_viewport,
-            //   r_viewport
-            // );
           }
           debug_assert_eq!(width, col_idx);
 
