1ed8cb5efe8050d0e60c2c9824e1af618ae9cd6a
linrongbin16 <linrongbin16@outlook.com>
2025-06-19 11:07:26 +0800

    chore

--- rsvim_core/src/state/mode.rs
+++ rsvim_core/src/state/mode.rs
@@ -35,7 +35,7 @@ impl Display for Mode {
       Mode::Select => write!(f, "Select"),
       Mode::OperatorPending => write!(f, "Operator-pending"),
       Mode::Insert => write!(f, "Insert"),
-      Mode::CommandLineEx => write!(f, "Command-line"),
+      Mode::CommandLineEx => write!(f, "Command-line (ex)"),
       Mode::CommandLineSearchForward => write!(f, "Command-line (search forward)"),
       Mode::CommandLineSearchBackward => write!(f, "Command-line (search backward)"),
       Mode::Terminal => write!(f, "Terminal"),
