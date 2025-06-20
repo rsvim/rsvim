753da6d784a8b2b3d30aca73dc7a18b837498821
linrongbin16 <linrongbin16@outlook.com>
2025-06-20 14:36:15 +0800

    chore

--- rsvim_core/src/state/fsm/command_line_ex.rs
+++ rsvim_core/src/state/fsm/command_line_ex.rs
@@ -1,7 +1,10 @@
 //! The command-line mode, ex-command variant.
 
+use crate::lock;
 use crate::state::fsm::{Stateful, StatefulDataAccess, StatefulValue};
 use crate::state::ops::Operation;
+use crate::ui::canvas::CursorStyle;
+use crate::ui::tree::*;
 
 use compact_str::{CompactString, ToCompactString};
 use crossterm::event::{Event, KeyCode, KeyEventKind};
@@ -73,7 +76,51 @@ impl Stateful for CommandLineExStateful {
 }
 
 impl CommandLineExStateful {
-  fn goto_normal_mode(&self, _data_access: &StatefulDataAccess) -> StatefulValue {
+  fn goto_normal_mode(&self, data_access: &StatefulDataAccess) -> StatefulValue {
+    let tree = data_access.tree.clone();
+    let mut tree = lock!(tree);
+
+    debug_assert!(tree.current_window_id().is_some());
+    let current_window_id = tree.current_window_id().unwrap();
+    debug_assert!(tree.node(current_window_id).is_some());
+    debug_assert!(matches!(
+      tree.node(current_window_id).unwrap(),
+      TreeNode::Window(_)
+    ));
+
+    debug_assert!(tree.cursor_id().is_some());
+    let cursor_id = tree.cursor_id().unwrap();
+
+    // Remove from current parent
+    debug_assert!(tree.command_line_id().is_some());
+    let cmdline_id = tree.command_line_id().unwrap();
+    debug_assert!(tree.parent_id(cursor_id).is_some());
+    debug_assert_eq!(tree.parent_id(cursor_id).unwrap(), cmdline_id);
+    debug_assert!(tree.node(cmdline_id).is_some());
+    debug_assert!(matches!(
+      tree.node(cmdline_id).unwrap(),
+      TreeNode::CommandLine(_)
+    ));
+    let cursor_node = tree.remove(cursor_id);
+    debug_assert!(cursor_node.is_some());
+    let cursor_node = cursor_node.unwrap();
+    debug_assert!(matches!(cursor_node, TreeNode::Cursor(_)));
+    debug_assert!(!tree.children_ids(cmdline_id).contains(&cursor_id));
+    match cursor_node {
+      TreeNode::Cursor(mut cursor) => cursor.set_style(&CursorStyle::SteadyBlock),
+      _ => unreachable!(),
+    }
+
+    // Insert to new parent
+    let _inserted = tree.bounded_insert(current_window_id, cursor_node);
+    debug_assert!(_inserted.is_none());
+
+    // Clear command-line contents.
+    let contents = data_access.contents.clone();
+    let mut contents = lock!(contents);
+    contents.command_line_content_mut().rope_mut().remove(0..);
+    contents.command_line_content_mut().clear_cached_lines();
+
     StatefulValue::NormalMode(super::NormalStateful::default())
   }
 }
