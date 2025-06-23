593feae28f5618a253c72ae3cd7b48f3b12eaf04
linrongbin16 <linrongbin16@outlook.com>
2025-06-23 10:20:54 +0800

    chore

--- rsvim_core/src/ui/viewport.rs
+++ rsvim_core/src/ui/viewport.rs
@@ -4543,41 +4543,7 @@ mod tests_search_anchor_downward_nowrap {
     {
       let expect = vec!["", "s.\n", "nt to test:\n", "tely put inside.\n"];
 
-      let actual = {
-        let target_cursor_line = 3;
-        let target_cursor_char = 60;
-
-        let mut window = window.borrow_mut();
-        let old = window.viewport();
-        let buf = lock!(buf);
-        let opts = *window.options();
-        let (start_line, start_column) = old.search_anchor(
-          ViewportSearchDirection::Down,
-          &opts,
-          buf.text(),
-          window.actual_shape(),
-          target_cursor_line,
-          target_cursor_char,
-        );
-        assert_eq!(start_line, 0);
-        assert_eq!(start_column, 42);
-
-        let viewport = Viewport::view(
-          &opts,
-          buf.text(),
-          window.actual_shape(),
-          start_line,
-          start_column,
-        );
-        window.set_cursor_viewport(CursorViewport::to_arc(CursorViewport::from_position(
-          &viewport,
-          buf.text(),
-          target_cursor_line,
-          target_cursor_char,
-        )));
-        window.set_viewport(Viewport::to_arc(viewport));
-        window.viewport()
-      };
+      let actual = search_and_update_viewport(window.clone(), buf.clone(), 3, 60, 0, 42);
 
       let expect_start_fills: BTreeMap<usize, usize> =
         vec![(0, 0), (1, 0), (2, 0), (3, 0)].into_iter().collect();
