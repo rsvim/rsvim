7c8361ac814828c964f626c576db8a5624929e99
linrongbin16 <linrongbin16@outlook.com>
2025-06-27 22:04:47 +0800

    chore

--- rsvim_cli/build.rs
+++ rsvim_cli/build.rs
@@ -4,10 +4,8 @@ use std::path::Path;
 
 fn version() {
   let output_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("RSVIM_VERSION.TXT");
-  eprintln!(
-    "[RSVIM] Writing version into {:?}...",
-    output_path.as_path()
-  );
+  let output_path1 = output_path.as_path();
+  eprintln!("[RSVIM] Writing version into {output_path1:?}...");
   let pkg_version = env!("CARGO_PKG_VERSION");
   let mut f = std::fs::File::create(output_path).unwrap();
   write!(&mut f, "rsvim {} (v8 {})", pkg_version, v8_version()).unwrap();
@@ -19,33 +17,24 @@ fn snapshot() {
   let snapshot = {
     let snapshot = js_runtime.create_snapshot();
     let snapshot = Box::from(&snapshot);
-    eprintln!(
-      "[RSVIM] Snapshot blob size is {} before compress...",
-      snapshot.len()
-    );
+    let snapshot_len = snapshot.len();
+    eprintln!("[RSVIM] Snapshot blob size is {snapshot_len} before compress...");
     let mut vec = Vec::with_capacity(snapshot.len());
     vec.extend((snapshot.len() as u32).to_le_bytes());
     let max_compress_level: i32 = *zstd::compression_level_range().end();
-    eprintln!(
-      "[RSVIM] Compress snapshot with zstd-level={}...",
-      max_compress_level
-    );
+    eprintln!("[RSVIM] Compress snapshot with zstd-level={max_compress_level}...");
     vec.extend_from_slice(
       &zstd::bulk::compress(&snapshot, max_compress_level)
         .expect("Failed to compress snapshot with zstd"),
     );
     let snapshot = vec.into_boxed_slice();
-    eprintln!(
-      "[RSVIM] Snapshot blob size is {} after compress...",
-      snapshot.len()
-    );
+    let snapshot_len = snapshot.len();
+    eprintln!("[RSVIM] Snapshot blob size is {snapshot_len} after compress...");
     snapshot
   };
   let output_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("RSVIM_SNAPSHOT.BIN");
-  eprintln!(
-    "[RSVIM] Writing snapshot into {:?}...",
-    output_path.as_path()
-  );
+  let output_path1 = output_path.as_path();
+  eprintln!("[RSVIM] Writing snapshot into {output_path1:?}...");
   std::fs::write(output_path.as_path(), &snapshot).unwrap();
 }
 
