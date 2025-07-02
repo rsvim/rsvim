834d2e09e334ed441cd27adb82f4c7ff44232041
linrongbin16 <linrongbin16@outlook.com>
2025-07-02 14:22:47 +0800

    chore

--- rsvim_core/src/excommand.rs
+++ rsvim_core/src/excommand.rs
@@ -7,19 +7,19 @@ use compact_str::CompactString;
 #[derive(Debug)]
 /// Ex-command definition.
 pub struct ExCommand {
-  name: CompactString,
+  _name: CompactString,
   // backend: JsCallbackFunction,
 }
 
 impl ExCommand {
   pub fn new(name: CompactString) -> Self {
-    Self { name }
+    Self { _name: name }
   }
 }
 
 #[derive(Debug)]
 pub struct ExCommandsManager {
-  ex_commands: HashMap<CompactString, ExCommand>,
+  _ex_commands: HashMap<CompactString, ExCommand>,
 }
 
 arc_mutex_ptr!(ExCommandsManager);
@@ -27,19 +27,19 @@ arc_mutex_ptr!(ExCommandsManager);
 impl ExCommandsManager {
   pub fn new() -> Self {
     Self {
-      ex_commands: HashMap::new(),
+      _ex_commands: HashMap::new(),
     }
   }
 
-  pub fn insert(&mut self, cmd: ExCommand) -> Option<ExCommand> {
+  pub fn insert(&mut self, _cmd: ExCommand) -> Option<ExCommand> {
     None
   }
 
-  pub fn remove(&mut self, name: CompactString) -> Option<ExCommand> {
+  pub fn remove(&mut self, _name: CompactString) -> Option<ExCommand> {
     None
   }
 
-  pub fn get(&self, command_line_content: CompactString) -> Option<ExCommand> {
+  pub fn get(&self, _command_line_content: CompactString) -> Option<ExCommand> {
     None
   }
 }
