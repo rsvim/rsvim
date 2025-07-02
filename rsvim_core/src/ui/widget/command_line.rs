101a43b01f1cb2f3291a5b9ca85a11e59d923f48
linrongbin16 <linrongbin16@outlook.com>
2025-07-02 09:47:22 +0800

    chore

--- rsvim_core/src/ui/widget/command_line.rs
+++ rsvim_core/src/ui/widget/command_line.rs
@@ -160,7 +160,10 @@ impl CommandLine {
 
   /// Set viewport.
   pub fn set_viewport(&mut self, viewport: ViewportArc) {
-    self.viewport = viewport;
+    self.viewport = viewport.clone();
+    if let Some(CommandLineNode::CommandLineContent(content)) = self.base.node_mut(self.content_id) {
+      content.set_viewport(Arc::downgrade(&viewport));
+    }
   }
 
   /// Get cursor viewport.
