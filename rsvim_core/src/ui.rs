a757156722b22ac1d9f881024e656d779a2a1297
linrongbin16 <linrongbin16@outlook.com>
2025-07-07 11:17:10 +0800

    chore

--- rsvim_core/src/ui.rs
+++ rsvim_core/src/ui.rs
@@ -6,4 +6,8 @@ pub mod viewport;
 pub mod widget;
 
 #[cfg(test)]
+mod canvas_tests;
+#[cfg(test)]
+mod tree_tests;
+#[cfg(test)]
 mod viewport_tests;
---
+++ rsvim_core/src/ui/tree_tests.rs
@@ -0,0 +1,11 @@
+use super::tree::*;
+
+use crate::prelude::*;
+
+#[test]
+fn new() {
+  let terminal_size = U16Size::new(18, 10);
+  let tree = Tree::new(terminal_size);
+  assert!(tree.is_empty());
+  assert!(tree.len() == 1);
+}
