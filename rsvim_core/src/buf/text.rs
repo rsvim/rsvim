594b7eb8dc7ec432ed42b3241d8b572f154c22f5
linrongbin16 <linrongbin16@outlook.com>
2025-07-08 10:03:11 +0800

    chore

--- rsvim_core/src/buf/text.rs
+++ rsvim_core/src/buf/text.rs
@@ -193,16 +193,7 @@ impl Text {
   /// Whether the `line_idx`/`char_idx` is eol (end-of-line).
   pub fn is_eol(&self, line_idx: usize, char_idx: usize) -> bool {
     match self.rope.get_line(line_idx) {
-      Some(line) => {
-        if char_idx == line.len_chars().saturating_sub(1) {
-          match line.get_char(char_idx) {
-            Some(c) => self.char_width(c) == 0,
-            None => false,
-          }
-        } else {
-          false
-        }
-      }
+      Some(line) => self._is_eol_impl(&line, char_idx),
       None => false,
     }
   }
