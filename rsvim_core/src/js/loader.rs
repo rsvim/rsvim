c2e0cdfae8737d2203eea6f92356b594eaec08ed
linrongbin16 <linrongbin16@outlook.com>
2025-07-16 16:33:26 +0800

    chore

--- rsvim_core/src/js/loader.rs
+++ rsvim_core/src/js/loader.rs
@@ -21,9 +21,28 @@ pub mod fs_loader;
 #[cfg(test)]
 mod fs_loader_tests;
 
-/// Defines the interface of a module loader.
+/// Module loader.
+///
+/// There are 3 kinds of module loaders:
+/// 1. Core module loader, provide builtin modules.
+/// 2. Fs module loader, provide modules on local filesystem.
+/// 3. URL module loader, provide remote modules on network.
+///
+/// TODO:
+/// For now we only implement the fs module loader, in the future we may want to implement other
+/// loaders.
 pub trait ModuleLoader {
-  /// Resolve the module's path by its specifier.
+  /// Resolve module path by its specifier.
+  ///
+  /// - For core module loader, the module path is always same with its specifier (core modules
+  ///   don't have a file path on local filesystem). But core modules use a specific pattern to
+  ///   indicate, such as a `"rsvim:ext/"` prefix.
+  /// - For fs module loader, it returns the full file path on local filesystem.
+  /// - For url module loader, the specifier is a url (mostly http protocol) which indicates a
+  ///   remote location that can download the resource. Url module loader will first download the
+  ///   resource to local filesystem as a local file cache, then returns the cached full file path.
+  ///
+  /// NOTE: This API (and all 3 loaders) are synchronized.
   fn resolve(&self, base: Option<&str>, specifier: &str) -> AnyResult<ModulePath>;
 
   /// Load the module path by its specifier.
