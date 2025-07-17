1d34ae1a2725fbb00936b29fba78579cf5c19b78
linrongbin16 <linrongbin16@outlook.com>
2025-07-17 12:44:00 +0800

    chore

--- rsvim_core/src/js/loader/fs_loader.rs
+++ rsvim_core/src/js/loader/fs_loader.rs
@@ -126,7 +126,7 @@ impl ModuleLoader for FsModuleLoader {
         let single_path = single_specifier.absolutize()?;
 
         // If single file exists, resolve it.
-        if single_path.exists() {
+        if single_path.exists() && single_path.is_file() {
           return Ok(self.transform(single_path.to_path_buf()));
         }
 
