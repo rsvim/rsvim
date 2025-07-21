56d6b3c23ce98a6daf512ddfa4a24264b455f87e
linrongbin16 <linrongbin16@outlook.com>
2025-07-21 16:18:48 +0800

    chore

--- rsvim_core/src/js/module.rs
+++ rsvim_core/src/js/module.rs
@@ -240,7 +240,7 @@ pub fn fetch_module_tree<'a>(
     let specifier = request.get_specifier().to_rust_string_lossy(scope);
     let specifier = resolve_import(Some(filename), &specifier, None).unwrap();
     trace!(
-      "Resolved dependency modules, base filename: {:?}, dependency specifier: {:?}",
+      "Resolved dependency modules, filename: {:?}, specifier: {:?}",
       filename,
       specifier.as_str(),
     );
