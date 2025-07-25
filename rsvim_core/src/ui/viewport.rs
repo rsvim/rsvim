3a1f9adfecf5ff31a669ebca622f70153f7836e4
linrongbin16 <linrongbin16@outlook.com>
2025-07-26 07:18:05 +0800

    chore

--- rsvim_core/src/ui/viewport.rs
+++ rsvim_core/src/ui/viewport.rs
@@ -249,17 +249,15 @@ impl CursorViewport {
       debug_assert_eq!(cursor_row.len(), 1);
       let (row_idx, row_viewport) = cursor_row[0];
 
+      let mut row_start_width =
+        text.width_before(line_idx, row_viewport.start_char_idx());
+
+      // Subtract `start_filled_cols` if the row is the first row in the line.
       let (first_row_idx, _first_row_viewport) =
         line_viewport.rows.first().unwrap();
-
-      let row_start_width = if first_row_idx == row_idx {
-        // Subtract `start_filled_cols` if the row is the first row in the line.
-        let start_width =
-          text.width_before(line_idx, row_viewport.start_char_idx());
-        debug_assert!(start_width >= line_viewport.start_filled_cols());
-        start_width - line_viewport.start_filled_cols()
-      } else {
-        text.width_before(line_idx, row_viewport.start_char_idx())
+      if first_row_idx == row_idx {
+        debug_assert!(row_start_width >= line_viewport.start_filled_cols());
+        row_start_width -= line_viewport.start_filled_cols();
       };
 
       let char_start_width = text.width_before(line_idx, char_idx);
