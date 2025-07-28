f4db8ee9d9acdf7e102cdd9093262091a949cda0
linrongbin16 <linrongbin16@outlook.com>
2025-07-28 16:48:01 +0800

    chore

--- rsvim_core/src/js/module_tests.rs
+++ rsvim_core/src/js/module_tests.rs
@@ -1,6 +1,5 @@
 use super::module::*;
 
-use crate::js::JsRuntime;
 use crate::test::js::make_js_runtime;
 use crate::test::log::init as test_log_init;
 
@@ -8,6 +7,6 @@ use crate::test::log::init as test_log_init;
 fn test_fetch1() {
   test_log_init();
   let mut jsrt = make_js_runtime();
-  let scope = jsrt.handle_scope();
-  // let module = fetch_module(&mut scope, filename, source);
+  let mut scope = jsrt.handle_scope();
+  let module = fetch_module(&mut scope, "", None);
 }
