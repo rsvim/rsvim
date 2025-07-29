2583bfce3e9b895ed9c193369dd7fa8286caf20c
linrongbin16 <linrongbin16@outlook.com>
2025-07-29 10:28:20 +0800

    chore

--- rsvim_core/src/js/module_tests.rs
+++ rsvim_core/src/js/module_tests.rs
@@ -78,3 +78,67 @@ fn fetch2() {
     fetch_module(&mut scope, fetch2.as_os_str().to_str().unwrap(), None);
   assert!(actual2.is_none());
 }
+
+#[test]
+fn fetch_tree3() {
+  let _guard = acquire_sequential_guard();
+
+  let tmpdir = TempDir::new().unwrap();
+
+  let src1: &str = r#"
+  export const PI = 3.14159;
+  "#;
+
+  let src2: &str = r#"
+  import { PI } from "./pi.js";
+
+  function addPI(a:number, b:number) :number {
+    return PI+a+b;
+  }
+
+  export { addPI };
+  "#;
+
+  let src3: &str = r#"
+  import * as pi from "./util/pi.js";
+  import addUtil from "./util/add";
+
+  const result = addUtil.addPI(1.0, 2.5);
+  "#;
+
+  let tmp_util_dir = tmpdir.join("util");
+  let fetch1 = tmp_util_dir.join("pi.js");
+  let fetch2 = tmp_util_dir.join("add.ts");
+  let fetch3 = tmpdir.join("fetch3.js");
+
+  {
+    std::fs::create_dir_all(tmp_util_dir).unwrap();
+
+    let mut fp1 = std::fs::File::create(&fetch1).unwrap();
+    fp1.write_all(src1.as_bytes()).unwrap();
+    fp1.flush().unwrap();
+
+    let mut fp2 = std::fs::File::create(&fetch2).unwrap();
+    fp2.write_all(src2.as_bytes()).unwrap();
+    fp2.flush().unwrap();
+
+    let mut fp3 = std::fs::File::create(&fetch3).unwrap();
+    fp3.write_all(src3.as_bytes()).unwrap();
+    fp3.flush().unwrap();
+  }
+
+  test_log_init();
+  let mut jsrt = make_js_runtime();
+  let mut scope = jsrt.handle_scope();
+  let actual1 =
+    fetch_module(&mut scope, fetch1.as_os_str().to_str().unwrap(), None);
+  assert!(actual1.is_some());
+  let actual1 = actual1.unwrap();
+  info!(
+    "fetch1 actual1:{:?}, script_id:{:?}",
+    actual1,
+    actual1.script_id()
+  );
+  assert!(actual1.script_id().is_some());
+  assert!(actual1.script_id().unwrap() > 0);
+}
