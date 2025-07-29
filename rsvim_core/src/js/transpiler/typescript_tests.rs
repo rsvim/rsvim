d0e89cdfe3af0e42ea0c5608ae04a67f7b24bcc2
linrongbin16 <linrongbin16@outlook.com>
2025-07-29 16:21:52 +0800

    choreq

--- rsvim_core/src/js/transpiler/typescript_tests.rs
+++ rsvim_core/src/js/transpiler/typescript_tests.rs
@@ -9,15 +9,25 @@ fn test1() {
   test_log_init();
 
   let m1 = "./runtime/00__web.ts";
-  let actual1 =
-    TypeScript::compile(Some(m1), include_str!("./../runtime/00__web.ts"));
+  let actual1 = TypeScript::compile(
+    Some(m1),
+    include_str!(concat!(
+      env!("CARGO_MANIFEST_DIR"),
+      "/src/js/runtime/00__web.ts"
+    )),
+  );
   assert!(actual1.is_ok());
   let actual1 = actual1.unwrap();
   info!("{m1}:\n{actual1}");
 
   let m2 = "./runtime/01__rsvim.ts";
-  let actual2 =
-    TypeScript::compile(Some(m2), include_str!("./../runtime/01__rsvim.ts"));
+  let actual2 = TypeScript::compile(
+    Some(m2),
+    include_str!(concat!(
+      env!("CARGO_MANIFEST_DIR"),
+      "/src/js/runtime/01__rsvim.ts"
+    )),
+  );
   assert!(actual2.is_ok());
   let actual2 = actual2.unwrap();
   info!("{m2}:\n{actual2}");
