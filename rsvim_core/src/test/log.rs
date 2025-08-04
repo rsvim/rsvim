34af4362e79dd3f0cbae5046ddea20f3fa91bac5
linrongbin16 <linrongbin16@outlook.com>
2025-08-04 15:15:38 +0800

    chore

--- rsvim_core/src/test/log.rs
+++ rsvim_core/src/test/log.rs
@@ -2,10 +2,7 @@
 //!
 //! NOTE: This module should only be used in unit tests, not some where else.
 
-use crate::log::FORMATTER;
-
 use env_filter::Builder;
-use jiff::Zoned;
 
 /// Initialize the logging prints to `stderr`.
 pub fn init() {
@@ -19,8 +16,7 @@ pub fn init() {
       .filter(move |metadata| env_filter.enabled(metadata))
       .format(|out, message, record| {
         out.finish(format_args!(
-          "[{} {} {}:{}] {}",
-          Zoned::now().strftime(FORMATTER),
+          "[{} {}:{}] {}",
           record.level(),
           record.target(),
           record.line().unwrap_or(0),
