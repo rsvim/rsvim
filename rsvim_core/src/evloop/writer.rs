fe0796ad7c40d0b693e2f46666e91a2047fc5c30
linrongbin16 <linrongbin16@outlook.com>
2025-08-07 11:31:53 +0800

    chore

--- rsvim_core/src/evloop/writer.rs
+++ rsvim_core/src/evloop/writer.rs
@@ -23,9 +23,12 @@ pub trait StdoutWriter {
   /// Initialize STDOUT.
   fn init(&self) -> IoResult<()>;
 
+  /// Initialize STDOUT complete.
   fn init_complete(&mut self, canvas: &mut Canvas) -> IoResult<()>;
 
+  /// Shutdown STDOUT.
   fn shutdown(&self) -> IoResult<()>;
 
+  /// Write logical UI to STDOUT.
   fn write(&mut self, canvas: &mut Canvas) -> IoResult<()>;
 }
