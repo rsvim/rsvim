20454f8dd04b9c0253329639816a56e8ff30a081
linrongbin16 <linrongbin16@outlook.com>
2025-08-09 07:18:47 +0800

    chore

--- rsvim_core/src/evloop/reader/mock_reader.rs
+++ rsvim_core/src/evloop/reader/mock_reader.rs
@@ -1,11 +1,14 @@
 //! Mocked event reader.
 
+use crate::prelude::*;
+
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use futures::stream::Stream;
-use jiff::Zoned;
+use jiff::{SignedDuration, Span, ToSpan, Zoned};
 use std::pin::Pin;
 use std::sync::mpsc::channel;
 use std::task::{Context, Poll};
+use std::thread;
 use std::time::Duration;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -22,27 +25,40 @@ pub enum MockEvent {
   SleepUntil(Zoned),
 }
 
-#[derive(Debug)]
-pub struct MockReader {
-  idx: usize,
-  events: Vec<MockEvent>,
-}
-
-impl MockReader {
-  pub fn new(events: Vec<MockEvent>) -> Self {
-    let (tx, rx) = channel();
-    Self {
-      idx: 0_usize,
-      events: events,
-    }
-  }
-}
-
 const CTRL_C_EVENT: Event = Event::Key(KeyEvent::new_with_kind(
   KeyCode::Char('c'),
   KeyModifiers::CONTROL,
   KeyEventKind::Press,
 ));
+
+#[derive(Debug)]
+pub struct MockReader {}
+
+impl MockReader {
+  pub fn new(events: Vec<MockEvent>) -> Self {
+    let (tx, rx) = channel::<Event>();
+
+    thread::spawn(move || {
+      for (i, event) in events.iter().enumerate() {
+        match event {
+          MockEvent::Event(e) => tx.send(e.clone()).unwrap(),
+          MockEvent::ExitEvent => tx.send(CTRL_C_EVENT.clone()).unwrap(),
+          MockEvent::SleepFor(d) => thread::sleep(*d),
+          MockEvent::SleepUntil(ts) => {
+            let now = Zoned::now();
+            let diff = ts.duration_since(&now);
+            let diff = diff.as_millis();
+            if diff > 0 {
+              let diff = Duration::from_millis(diff as u64);
+              thread::sleep(diff)
+            }
+          }
+        }
+      }
+      Ok(())
+    });
+  }
+}
 
 impl Stream for MockReader {
   type Item = std::io::Result<Event>;
