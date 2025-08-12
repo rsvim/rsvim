528e5f9e80eb118f8d3e41f9e89698f0542cea7a
linrongbin16 <linrongbin16@outlook.com>
2025-08-12 13:21:28 +0800

    chore

--- rsvim_core/src/tests/constant.rs
+++ rsvim_core/src/tests/constant.rs
@@ -3,13 +3,7 @@ use std::ffi::OsStr;
 
 use parking_lot::{Mutex, MutexGuard};
 
-static GLOBAL_SEQUENTIAL_LOCK: Mutex<()> = Mutex::new(());
-
-pub fn acquire_sequential_guard() -> MutexGuard<'static, ()> {
-  GLOBAL_SEQUENTIAL_LOCK.lock()
-}
-
-pub fn set_env_var<K: AsRef<OsStr>, V: AsRef<OsStr>>(
+fn set_env_var<K: AsRef<OsStr>, V: AsRef<OsStr>>(
   name: K,
   value: V,
 ) -> Result<String, VarError> {
@@ -20,11 +14,70 @@ pub fn set_env_var<K: AsRef<OsStr>, V: AsRef<OsStr>>(
   saved
 }
 
-pub fn restore_env_var(name: &str, saved_value: Result<String, VarError>) {
+fn restore_env_var(name: &str, saved_value: Result<String, VarError>) {
   match saved_value {
     Ok(saved) => unsafe {
       std::env::set_var(name, saved);
     },
     Err(_) => { /* */ }
+  }
+}
+
+pub struct TempPathCfg {
+  pub home_dir: assert_fs::TempDir,
+  pub xdg_config_home: assert_fs::TempDir,
+  pub xdg_cache_home: assert_fs::TempDir,
+  pub xdg_data_home: assert_fs::TempDir,
+
+  saved_home_dir: Result<String, VarError>,
+  saved_xdg_config_home: Result<String, VarError>,
+  saved_xdg_cache_home: Result<String, VarError>,
+  saved_xdg_data_home: Result<String, VarError>,
+
+  sequential_guard: MutexGuard<'static, ()>,
+}
+
+static GLOBAL_SEQUENTIAL_LOCK: Mutex<()> = Mutex::new(());
+
+impl TempPathCfg {
+  pub fn create() -> Self {
+    use crate::constant::path_config::{
+      HOME, XDG_CACHE_HOME, XDG_CONFIG_HOME, XDG_DATA_HOME,
+    };
+
+    let home_dir = assert_fs::TempDir::new().unwrap();
+    let xdg_config_home = assert_fs::TempDir::new().unwrap();
+    let xdg_cache_home = assert_fs::TempDir::new().unwrap();
+    let xdg_data_home = assert_fs::TempDir::new().unwrap();
+
+    let saved_home_dir = set_env_var(HOME, home_dir.path());
+    let saved_xdg_config_home =
+      set_env_var(XDG_CONFIG_HOME, xdg_config_home.path());
+    let saved_xdg_cache_home =
+      set_env_var(XDG_CACHE_HOME, xdg_cache_home.path());
+    let saved_xdg_data_home = set_env_var(XDG_DATA_HOME, xdg_data_home.path());
+
+    Self {
+      home_dir,
+      xdg_config_home,
+      xdg_cache_home,
+      xdg_data_home,
+      saved_home_dir,
+      saved_xdg_config_home,
+      saved_xdg_cache_home,
+      saved_xdg_data_home,
+      sequential_guard: GLOBAL_SEQUENTIAL_LOCK.lock(),
+    }
+  }
+
+  pub fn restore(&self) {
+    use crate::constant::path_config::{
+      HOME, XDG_CACHE_HOME, XDG_CONFIG_HOME, XDG_DATA_HOME,
+    };
+
+    restore_env_var(HOME, self.saved_home_dir);
+    restore_env_var(XDG_CONFIG_HOME, self.saved_xdg_config_home);
+    restore_env_var(XDG_CACHE_HOME, self.saved_xdg_cache_home);
+    restore_env_var(XDG_DATA_HOME, self.saved_xdg_data_home);
   }
 }
