80411cb9462f5afa927d0016725e3f18440367b3
linrongbin16 <linrongbin16@outlook.com>
2025-08-12 13:27:48 +0800

    chore

--- rsvim_core/src/js/binding/global_this/timeout_tests.rs
+++ rsvim_core/src/js/binding/global_this/timeout_tests.rs
@@ -2,32 +2,21 @@ use super::timeout::*;
 
 use crate::constant::path_config::*;
 use crate::js::loader::ModuleLoader;
-use crate::prelude::*;
-use crate::tests::constant::{
-  acquire_sequential_guard, restore_env_var, set_env_var,
-};
+use crate::tests::constant::TempPathCfg;
 use crate::tests::evloop::*;
 use crate::tests::log::init as test_log_init;
+use crate::{evloop, prelude::*};
 
 use assert_fs::prelude::*;
 use std::fs;
 use std::path::Path;
 use std::time::Duration;
 
-#[test]
-fn test_timeout1() {
-  let _guard = acquire_sequential_guard();
+#[tokio::test]
+async fn test_timeout1() -> IoResult<()> {
   test_log_init();
 
-  let tmp_home_dir = assert_fs::TempDir::new().unwrap();
-  let tmp_config_dir = assert_fs::TempDir::new().unwrap();
-  let tmp_cache_dir = assert_fs::TempDir::new().unwrap();
-  let tmp_data_dir = assert_fs::TempDir::new().unwrap();
-
-  let saved_home = set_env_var(HOME, tmp_home_dir.path());
-  let saved_conf = set_env_var(XDG_CONFIG_HOME, tmp_config_dir.path());
-  let saved_cache = set_env_var(XDG_CACHE_HOME, tmp_cache_dir.path());
-  let saved_data = set_env_var(XDG_DATA_HOME, tmp_data_dir.path());
+  let tp = TempPathCfg::create();
 
   let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(200))];
   let src: &str = r#"
@@ -38,5 +27,12 @@ fn test_timeout1() {
   }, 100);
 "#;
 
-  let mut evloop = make_event_loop();
+  let mut event_loop = make_event_loop();
+  event_loop.initialize()?;
+  event_loop.mock_run(MockReader::new(&mocked_events)).await?;
+  event_loop.shutdown()?;
+
+  tp.restore();
+
+  Ok(())
 }
