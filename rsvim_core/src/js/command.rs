a229317325427c91d0cb9fcb52bebfcc560c0930
linrongbin16 <linrongbin16@outlook.com>
2025-08-14 23:47:10 +0800

    chore

---
+++ rsvim_core/src/js/command.rs
@@ -0,0 +1,14 @@
+//! The vim command.
+
+use crate::js::msg::{self as jsmsg, JsRuntimeToEventLoopMessage};
+use crate::js::{self, JsFuture, JsFutureId, JsRuntime};
+use crate::prelude::*;
+
+use std::rc::Rc;
+use std::time::Duration;
+
+struct CommandFuture {
+  future_id: JsFutureId,
+  cb: Rc<v8::Global<v8::Function>>,
+  params: Rc<Vec<v8::Global<v8::Value>>>,
+}
