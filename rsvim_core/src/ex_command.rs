4cd1813bb04a48bd3031659cc807c104710c3ecb
linrongbin16 <linrongbin16@outlook.com>
2025-08-15 11:35:57 +0800

    chore

--- rsvim_core/src/ex_command.rs
+++ rsvim_core/src/ex_command.rs
@@ -5,20 +5,29 @@
 //! command-line mode and input commands. The "ex" word is used to distinguish
 //! it from searching forward/backward in command line mode.
 
+use crate::js::{JsFuture, JsFutureId, JsRuntime, next_future_id};
 use crate::prelude::*;
 
 use compact_str::CompactString;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 /// Ex-command definition.
 pub struct ExCommand {
-  _name: CompactString,
-  // backend: JsCallbackFunction,
+  // Command name, case sensitive
+  name: CompactString,
+
+  // For all commands, they are implemented/registered with a javascript
+  // callback function, managed by js runtime.
+  js_callback_id: JsFutureId,
 }
 
 impl ExCommand {
   pub fn new(name: CompactString) -> Self {
-    Self { _name: name }
+    let id = next_future_id();
+    Self {
+      name,
+      js_callback_id: id,
+    }
   }
 }
 
