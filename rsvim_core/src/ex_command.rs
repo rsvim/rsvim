c5727d1a151a9276158887244cdd1d797004e317
linrongbin16 <linrongbin16@outlook.com>
2025-08-18 14:09:09 +0800

    chore

--- rsvim_core/src/ex_command.rs
+++ rsvim_core/src/ex_command.rs
@@ -7,6 +7,38 @@ use compact_str::{CompactString, ToCompactString};
 
 pub mod parser;
 
+const JS_COMMAND_NAME: &str = "js";
+const JS_COMMAND_HANDLE_ID: JsHandleId = -1;
+
+#[derive(Debug, Clone)]
+/// Vim ex command instance.
+pub struct ExCommand {
+  name: CompactString,
+  payload: CompactString,
+  handle_id: JsHandleId,
+  is_js: bool,
+}
+
+arc_mutex_ptr!(ExCommand);
+
+impl ExCommand {
+  pub fn name(&self) -> &str {
+    &self.name
+  }
+
+  pub fn payload(&self) -> &str {
+    &self.payload
+  }
+
+  pub fn js_handle(&self) -> JsHandleId {
+    self.handle_id
+  }
+
+  pub fn is_js(&self) -> bool {
+    self.is_js
+  }
+}
+
 #[derive(Debug)]
 pub struct ExCommandsManager {
   command_ids: HashMap<CompactString, JsHandleId>,
@@ -25,14 +57,23 @@ impl ExCommandsManager {
     self.command_ids.get(name)
   }
 
-  pub fn parse(
-    &self,
-    command_line: &str,
-  ) -> Option<(&CompactString, &JsHandleId)> {
+  pub fn parse(&self, command_line: &str) -> Option<ExCommand> {
     match command_line.find(char::is_whitespace) {
       Some(pos) => {
         let name = command_line.get(0..pos).unwrap().trim();
-        self.command_ids.get_key_value(name)
+        match self.command_ids.get(name) {
+          Some(handle_id) => {
+            let payload = command_line.get(pos..).unwrap().trim();
+            let is_js = name == JS_COMMAND_NAME;
+            Some(ExCommand {
+              name: name.to_compact_string(),
+              payload: payload.to_compact_string(),
+              is_js,
+              handle_id: *handle_id,
+            })
+          }
+          None => None,
+        }
       }
       None => None,
     }
