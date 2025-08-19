6841f187333d64841649e2e0b83e9fb97a10c35a
linrongbin16 <linrongbin16@outlook.com>
2025-08-19 23:23:17 +0800

    c

--- rsvim_core/src/state/ops/cmdline_ops.rs
+++ rsvim_core/src/state/ops/cmdline_ops.rs
@@ -27,3 +27,22 @@ pub fn set_cmdline_message(
 
   cmdline.set_message_viewport(new_message_viewport);
 }
+
+pub fn clear_cmdline_message(
+  tree: &mut Tree,
+  text_contents: &mut TextContents,
+) {
+  debug_assert!(tree.command_line().is_some());
+
+  let message_text = text_contents.command_line_message_mut();
+  message_text.clear();
+
+  let cmdline = tree.command_line_mut().unwrap();
+  let opts = *cmdline.options();
+  let actual_shape = *cmdline.message().actual_shape();
+
+  let new_message_viewport =
+    Viewport::to_arc(Viewport::view(&opts, message_text, &actual_shape, 0, 0));
+
+  cmdline.set_message_viewport(new_message_viewport);
+}
