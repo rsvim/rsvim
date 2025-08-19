58b55bdfbcad99b84e6c755295a4815918bf53ad
linrongbin16 <linrongbin16@outlook.com>
2025-08-19 13:02:03 +0800

    chore

--- rsvim_core/src/state/ops/message_ops.rs
+++ rsvim_core/src/state/ops/message_ops.rs
@@ -23,29 +23,19 @@ pub fn set_message_visible(command_line: &mut CommandLine, visible: bool) {
 /// Refresh the command line view to have no content in both content and message widgets.
 pub fn refresh_view(command_line: &mut CommandLine) {
   let input_shape = command_line.input_mut().shape();
-  let cmdline_content_shape = geo_rect_as!(input_shape, u16);
+  let input_actual_shape = geo_rect_as!(input_shape, u16);
   let text_contents = command_line.text_contents().upgrade().unwrap();
   let mut text_contents = lock!(text_contents);
   let content = text_contents.command_line_input_mut();
   content.clear();
-  let content_viewport = Viewport::view(
-    command_line.options(),
-    content,
-    &cmdline_content_shape,
-    0,
-    0,
-  );
-  let content_viewport_arc = Viewport::to_arc(content_viewport);
+  let input_viewport =
+    Viewport::view(command_line.options(), content, &input_actual_shape, 0, 0);
+  let input_viewport_arc = Viewport::to_arc(input_viewport);
   let message = text_contents.command_line_message_mut();
   message.clear();
-  command_line.set_input_viewport(content_viewport_arc.clone());
-  let message_viewport = Viewport::view(
-    command_line.options(),
-    message,
-    &cmdline_content_shape,
-    0,
-    0,
-  );
+  command_line.set_input_viewport(input_viewport_arc.clone());
+  let message_viewport =
+    Viewport::view(command_line.options(), message, &input_actual_shape, 0, 0);
   let message_viewport_arc = Viewport::to_arc(message_viewport);
   command_line.set_message_viewport(message_viewport_arc.clone());
 }
