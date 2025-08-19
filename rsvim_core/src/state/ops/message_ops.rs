58598ff8b6c1428182a32012001376765a4350b4
linrongbin16 <linrongbin16@outlook.com>
2025-08-19 13:30:04 +0800

    chroe

--- rsvim_core/src/state/ops/message_ops.rs
+++ rsvim_core/src/state/ops/message_ops.rs
@@ -3,23 +3,6 @@ use crate::ui::viewport::Viewport;
 use crate::ui::widget::command_line::CommandLine;
 use crate::{geo_rect_as, lock};
 
-/// Toggles visibility between the command-line content and its message.
-///
-/// - If `visible` is true: show the message and hide the content.
-/// - If `visible` is false: hide the message and show the content.
-pub fn set_message_visible(command_line: &mut CommandLine, visible: bool) {
-  command_line.indicator_mut().set_visible(!visible);
-  command_line.input_mut().set_visible(!visible);
-  command_line.message_mut().set_visible(visible);
-  let text_contents = command_line
-    .message_mut()
-    .get_text_contents_mut()
-    .upgrade()
-    .unwrap();
-  let mut text_contents = lock!(text_contents);
-  text_contents.command_line_input_mut().clear();
-}
-
 /// Refresh the command line view to have no content in both content and message widgets.
 pub fn refresh_view(command_line: &mut CommandLine) {
   let input_shape = command_line.input_mut().shape();
