rsvim/rsvim

commit 50e08ca3ec1f2d4c9b61f1a2763c2849408df1f7

feat(js/evloop/fsm/ui): Add "echo" command and command_line message widget (#559)
This PR completes several functions:
1. Add new "Rsvim.cmd.echo" API for js
2. Implements the "message" component in "command line" widget

rsvim_core/src/evloop.rs
@@ -12,13 +12,13 @@ use crate::state::fsm::{Stateful, StatefulDataAccess, StatefulValue};
 use crate::state::{State, StateArc};
 use crate::ui::canvas::{Canvas, CanvasArc};
 use crate::ui::tree::*;
-use crate::ui::widget::command_line::CommandLine;
 use crate::ui::widget::cursor::Cursor;
 use crate::ui::widget::window::Window;
 
 use msg::WorkerToMasterMessage;
 use writer::{StdoutWritable, StdoutWriterValue};
 
+use crate::ui::widget::command_line::CommandLine;
 use crossterm::event::{Event, EventStream};
 use futures::StreamExt;
 use std::sync::Arc;
@@ -27,6 +27,7 @@ use tokio::sync::mpsc::{Receiver, Sender, channel};
 use tokio_util::sync::CancellationToken;
 use tokio_util::task::TaskTracker;
 
+use crate::state::ops::cursor_ops::_update_viewport_after_text_changed;
 #[cfg(test)]
 use crate::tests::evloop::MockReader;
 #[cfg(test)]
@@ -500,7 +501,6 @@ impl EventLoop {
     );
     let cmdline =
       CommandLine::new(cmdline_shape, Arc::downgrade(&self.contents));
-    let _cmdline_id = cmdline.id();
 
     tree.bounded_insert(tree_root_id, TreeNode::CommandLine(cmdline));
 
@@ -565,6 +565,24 @@ impl EventLoop {
   ) {
     if let Some(message) = message {
       match message {
+        JsRuntimeToEventLoopMessage::EchoReq(req) => {
+          trace!("Receive req echo_req:{:?}", req.message);
+          let mut tree = lock!(self.tree);
+          let mut tree_clone = tree.clone();
+          debug_assert!(tree.command_line().is_some());
+          let command_line = tree_clone.command_line_mut().unwrap();
+          let command_line_message = command_line.message_mut();
+          command_line_message.set_message(req.message.clone());
+          let command_line_content =
+            command_line_message.get_text_contents().upgrade().unwrap();
+          let command_line_content = lock!(command_line_content);
+          _update_viewport_after_text_changed(
+            &mut tree,
+            command_line.id(),
+            command_line_content.command_line_message(),
+          );
+          trace!("Receive req echo_req:{:?} - done", req.message);
+        }
         JsRuntimeToEventLoopMessage::TimeoutReq(req) => {
           trace!("Receive req timeout_req:{:?}", req.future_id);
           let jsrt_tick_dispatcher = self.jsrt_tick_dispatcher.clone();

rsvim_core/src/js/binding/global_rsvim.rs
@@ -1,3 +1,7 @@
 //! APIs for `Rsvim` namespace.
 
+pub mod cmd;
 pub mod opt;
+
+#[cfg(test)]
+mod cmd_test;

rsvim_core/src/js/binding/global_rsvim/cmd.rs
@@ -1,0 +1,28 @@
+use crate::js::JsRuntime;
+use crate::js::msg::JsRuntimeToEventLoopMessage;
+use compact_str::CompactString;
+use log::trace;
+
+/// Javascript `echo` API.
+pub fn echo(
+  scope: &mut v8::HandleScope,
+  args: v8::FunctionCallbackArguments,
+  _: v8::ReturnValue,
+) {
+  assert!(args.length() == 1);
+  let message = args.get(0).to_rust_string_lossy(scope).to_string();
+  trace!("echo: {:?}", message);
+
+  let state_rc = JsRuntime::state(scope);
+  let state = state_rc.borrow_mut();
+  let jsrt_to_mstr = state.jsrt_to_master.clone();
+  let current_handle = tokio::runtime::Handle::current();
+  current_handle.spawn_blocking(move || {
+    let message = CompactString::from(message);
+    jsrt_to_mstr
+      .blocking_send(JsRuntimeToEventLoopMessage::EchoReq(
+        crate::js::msg::EchoReq::new(message),
+      ))
+      .unwrap();
+  });
+}

rsvim_core/src/js/msg.rs
@@ -12,6 +12,7 @@ use std::time::Duration;
 /// Message between [`EventLoop`](crate::evloop::EventLoop) and
 /// [`JsRuntime`](crate::js::JsRuntime).
 pub enum JsRuntimeToEventLoopMessage {
+  EchoReq(EchoReq),
   TimeoutReq(TimeoutReq),
 }
 
@@ -44,6 +45,17 @@ impl TimeoutResp {
       future_id,
       duration,
     }
+  }
+}
+
+#[derive(Debug)]
+pub struct EchoReq {
+  pub message: CompactString,
+}
+
+impl EchoReq {
+  pub fn new(message: CompactString) -> Self {
+    EchoReq { message }
   }
 }
 

commit ab3d004ac6fe3209b14a4b31c5e5eab94b5f72b0

fix(cmdline): simplify/fix the command-line input/message switching logic (#590)
Since inside command-line widget, it needs to switch between command-line message and command-line input widget (i.e. the widget receives user's input command). The logic is a little confused when switching between normal mode and command-line-ex mode. This PR refactors the switching logic and make it more clear.

Also fixes some issues introduced in #559 :
- <https://github.com/rsvim/rsvim/pull/559/files#r2284144188>
- <https://github.com/rsvim/rsvim/pull/559/files#r2284150710>
- Simplify processing js request logic: <https://github.com/rsvim/rsvim/blob/63188e536d5487bbc307a92d15dea8dfb3da2388/rsvim_core/src/evloop.rs?plain=1#L568-L585>
- Verify test case results when calling `Rsvim.cmd.echo` here: <https://github.com/rsvim/rsvim/blob/63188e536d5487bbc307a92d15dea8dfb3da2388/rsvim_core/src/js/binding/global_rsvim/cmd_test.rs?plain=1#L67-L97>

dev.py
@@ -79,6 +79,7 @@ def test(name, miri, jobs):
     else:
         jobs = f" -j {jobs[0]}"
 
+    set_env("RUST_BACKTRACE", "full")
     if miri is not None:
         set_env(
             "MIRIFLAGS",

rsvim_core/src/buf/text.rs
@@ -567,7 +567,7 @@ impl Text {
     char_idx: usize,
     payload: CompactString,
   ) -> (usize, usize) {
-    debug_assert!(!payload.is_empty());
+    // debug_assert!(!payload.is_empty());
     debug_assert!(self.rope.get_line(line_idx).is_some());
     debug_assert!(char_idx <= self.rope.line(line_idx).len_chars());
 

rsvim_core/src/evloop.rs
@@ -9,6 +9,7 @@ use crate::js::msg::{
 use crate::js::{JsRuntime, JsRuntimeOptions, SnapshotData};
 use crate::prelude::*;
 use crate::state::fsm::{Stateful, StatefulDataAccess, StatefulValue};
+use crate::state::ops::cmdline_ops;
 use crate::state::{State, StateArc};
 use crate::ui::canvas::{Canvas, CanvasArc};
 use crate::ui::tree::*;
@@ -27,7 +28,6 @@ use tokio::sync::mpsc::{Receiver, Sender, channel};
 use tokio_util::sync::CancellationToken;
 use tokio_util::task::TaskTracker;
 
-use crate::state::ops::cursor_ops::_update_viewport_after_text_changed;
 #[cfg(test)]
 use crate::tests::evloop::MockReader;
 #[cfg(test)]
@@ -565,26 +565,18 @@ impl EventLoop {
   ) {
     if let Some(message) = message {
       match message {
-        JsRuntimeToEventLoopMessage::EchoReq(req) => {
-          trace!("Receive req echo_req:{:?}", req.message);
+        JsRuntimeToEventLoopMessage::PrintReq(req) => {
+          trace!("Receive PrintReq:{:?}", req.future_id);
           let mut tree = lock!(self.tree);
-          let mut tree_clone = tree.clone();
-          debug_assert!(tree.command_line().is_some());
-          let command_line = tree_clone.command_line_mut().unwrap();
-          let command_line_message = command_line.message_mut();
-          command_line_message.set_message(req.message.clone());
-          let command_line_content =
-            command_line_message.get_text_contents().upgrade().unwrap();
-          let command_line_content = lock!(command_line_content);
-          _update_viewport_after_text_changed(
+          let mut contents = lock!(self.contents);
+          cmdline_ops::set_cmdline_message(
             &mut tree,
-            command_line.id(),
-            command_line_content.command_line_message(),
+            &mut contents,
+            req.payload,
           );
-          trace!("Receive req echo_req:{:?} - done", req.message);
         }
         JsRuntimeToEventLoopMessage::TimeoutReq(req) => {
-          trace!("Receive req timeout_req:{:?}", req.future_id);
+          trace!("Receive TimeoutReq:{:?}", req.future_id);
           let jsrt_tick_dispatcher = self.jsrt_tick_dispatcher.clone();
           self.detached_tracker.spawn(async move {
             tokio::time::sleep(req.duration).await;
@@ -593,7 +585,6 @@ impl EventLoop {
                 jsmsg::TimeoutResp::new(req.future_id, req.duration),
               ))
               .await;
-            trace!("Receive req timeout_req:{:?} - done", req.future_id);
           });
         }
       }

rsvim_core/src/js/binding/global_rsvim.rs
@@ -4,4 +4,4 @@ pub mod cmd;
 pub mod opt;
 
 #[cfg(test)]
-mod cmd_test;
+mod cmd_tests;

rsvim_core/src/js/binding/global_rsvim/cmd.rs
@@ -1,7 +1,8 @@
-use crate::js::JsRuntime;
-use crate::js::msg::JsRuntimeToEventLoopMessage;
+use crate::js::msg::{self as jsmsg, JsRuntimeToEventLoopMessage};
+use crate::js::{self, JsRuntime};
+use crate::prelude::*;
+
 use compact_str::CompactString;
-use log::trace;
 
 /// Javascript `echo` API.
 pub fn echo(
@@ -13,6 +14,8 @@ pub fn echo(
   let message = args.get(0).to_rust_string_lossy(scope).to_string();
   trace!("echo: {:?}", message);
 
+  let message_id = js::next_future_id();
+
   let state_rc = JsRuntime::state(scope);
   let state = state_rc.borrow_mut();
   let jsrt_to_mstr = state.jsrt_to_master.clone();
@@ -20,8 +23,8 @@ pub fn echo(
   current_handle.spawn_blocking(move || {
     let message = CompactString::from(message);
     jsrt_to_mstr
-      .blocking_send(JsRuntimeToEventLoopMessage::EchoReq(
-        crate::js::msg::EchoReq::new(message),
+      .blocking_send(JsRuntimeToEventLoopMessage::PrintReq(
+        jsmsg::PrintReq::new(message_id, message),
       ))
       .unwrap();
   });

rsvim_core/src/js/msg.rs
@@ -12,7 +12,7 @@ use std::time::Duration;
 /// Message between [`EventLoop`](crate::evloop::EventLoop) and
 /// [`JsRuntime`](crate::js::JsRuntime).
 pub enum JsRuntimeToEventLoopMessage {
-  EchoReq(EchoReq),
+  PrintReq(PrintReq),
   TimeoutReq(TimeoutReq),
 }
 
@@ -49,13 +49,14 @@ impl TimeoutResp {
 }
 
 #[derive(Debug)]
-pub struct EchoReq {
-  pub message: CompactString,
+pub struct PrintReq {
+  pub future_id: JsFutureId,
+  pub payload: CompactString,
 }
 
-impl EchoReq {
-  pub fn new(message: CompactString) -> Self {
-    EchoReq { message }
+impl PrintReq {
+  pub fn new(future_id: JsFutureId, payload: CompactString) -> Self {
+    PrintReq { future_id, payload }
   }
 }
 

rsvim_core/src/js/runtime/01__rsvim.ts
@@ -19,7 +19,8 @@
 /**
  * The `Rsvim` global object, it contains multiple sub fields:
  *
- * - `Rsvim.opt`: Global editor options.
+ * - `Rsvim.opt`: Global options.
+ * - `Rsvim.cmd`: Ex commands.
  *
  * @example
  * ```javascript
@@ -36,7 +37,13 @@ export class Rsvim {
 }
 
 /**
- * The `Rsvim.cmd` global object for rsvim core commands.
+ * The `Rsvim.cmd` global object for ex commands.
+ *
+ * :::tip
+ * The "ex command" mostly describes the product function, i.e. when user types ":" in normal mode,
+ * user can move cursor to command-line and input commands. Rather than referring to the
+ * ["ex commands"](https://vimhelp.org/intro.txt.html#Ex-mode) in Vim editor.
+ * :::
  *
  * @example
  * ```javascript
@@ -48,7 +55,7 @@ export class Rsvim {
  */
 export class RsvimCmd {
   /**
-   * Echo message to the command line widget.
+   * Echo message to the command-line.
    *
    * @example
    * ```javascript

rsvim_core/src/state/fsm/command_line_ex.rs
@@ -10,7 +10,6 @@ use crate::ui::tree::*;
 use crate::ui::widget::command_line::CommandLineNode;
 use crate::ui::widget::command_line::indicator::IndicatorSymbol;
 
-use crate::state::ops::message_ops::{refresh_view, set_message_visible};
 use compact_str::{CompactString, ToCompactString};
 use crossterm::event::{Event, KeyCode, KeyEventKind};
 
@@ -97,7 +96,7 @@ impl CommandLineExStateful {
     &self,
     data_access: &StatefulDataAccess,
   ) -> StatefulValue {
-    let cmdline_content = self._goto_normal_mode_impl(data_access);
+    let cmdline_input_content = self._goto_normal_mode_impl(data_access);
     let state = data_access.state.clone();
     let jsrt_tick_dispatcher = lock!(state).jsrt_tick_dispatcher().clone();
 
@@ -105,7 +104,7 @@ impl CommandLineExStateful {
     current_handle.spawn_blocking(move || {
       jsrt_tick_dispatcher
         .blocking_send(EventLoopToJsRuntimeMessage::ExCommandReq(
-          ExCommandReq::new(next_future_id(), cmdline_content),
+          ExCommandReq::new(next_future_id(), cmdline_input_content),
         ))
         .unwrap();
     });
@@ -125,8 +124,9 @@ impl CommandLineExStateful {
     debug_assert!(tree.command_line_id().is_some());
     let cmdline_id = tree.command_line_id().unwrap();
     let cmdline = tree.command_line_mut().unwrap();
-    set_message_visible(cmdline, true);
-    refresh_view(cmdline);
+
+    cmdline.show_message();
+
     debug_assert!(cmdline.cursor_id().is_some());
 
     // Remove from current parent
@@ -152,26 +152,27 @@ impl CommandLineExStateful {
       cursor_viewport.row_idx() as isize,
     );
 
-    // Clear command-line contents.
+    // Clear command-line both input content and message.
     let contents = data_access.contents.clone();
     let mut contents = lock!(contents);
-    let cmdline_content =
+    let cmdline_input_content =
       contents.command_line_input().rope().to_compact_string();
 
+    contents.command_line_message_mut().clear();
     cursor_ops::cursor_clear(
       &mut tree,
       cmdline_id,
       contents.command_line_input_mut(),
     );
 
-    let cmdline_content = cmdline_content.trim();
+    let cmdline_input_content = cmdline_input_content.trim();
     tree
       .command_line_mut()
       .unwrap()
       .indicator_mut()
       .set_symbol(IndicatorSymbol::Empty);
 
-    cmdline_content.to_compact_string()
+    cmdline_input_content.to_compact_string()
   }
 
   pub fn goto_normal_mode(

rsvim_core/src/state/fsm/normal.rs
@@ -130,8 +130,9 @@ impl NormalStateful {
     // Insert to command-line
     debug_assert!(tree.command_line_mut().is_some());
     let cmdline = tree.command_line_mut().unwrap();
-    set_message_visible(cmdline, false);
-    refresh_view(cmdline);
+
+    cmdline.show_input();
+
     let _previous_cursor = cmdline.insert_cursor(cursor);
     debug_assert!(_previous_cursor.is_none());
     cmdline.move_cursor_to(0, 0);
@@ -247,7 +248,6 @@ impl NormalStateful {
 
 #[cfg(test)]
 use crate::buf::text::Text;
-use crate::state::ops::message_ops::{refresh_view, set_message_visible};
 #[cfg(test)]
 use crate::ui::viewport::{CursorViewport, ViewportSearchDirection};
 

rsvim_core/src/state/ops.rs
@@ -2,8 +2,8 @@
 
 use compact_str::CompactString;
 
+pub mod cmdline_ops;
 pub mod cursor_ops;
-pub mod message_ops;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 /// A set of low-level editor operations between terminal keyboard/mouse events and editor

rsvim_core/src/state/ops/cmdline_ops.rs
@@ -1,0 +1,29 @@
+//! Command-line operations.
+
+use crate::content::TextContents;
+use crate::ui::tree::Inodeable;
+use crate::ui::tree::Tree;
+use crate::ui::viewport::Viewport;
+
+use compact_str::CompactString;
+
+pub fn set_cmdline_message(
+  tree: &mut Tree,
+  text_contents: &mut TextContents,
+  payload: CompactString,
+) {
+  debug_assert!(tree.command_line().is_some());
+
+  let message_text = text_contents.command_line_message_mut();
+  message_text.clear();
+  message_text.insert_at(0, 0, payload);
+
+  let cmdline = tree.command_line_mut().unwrap();
+  let opts = *cmdline.options();
+  let actual_shape = *cmdline.message().actual_shape();
+
+  let new_message_viewport =
+    Viewport::to_arc(Viewport::view(&opts, message_text, &actual_shape, 0, 0));
+
+  cmdline.set_message_viewport(new_message_viewport);
+}

rsvim_core/src/state/ops/cursor_ops.rs
@@ -362,7 +362,7 @@ fn _max_len_chars_since_line(
   max_len_chars
 }
 
-pub fn _update_viewport_after_text_changed(
+fn _update_viewport_after_text_changed(
   tree: &mut Tree,
   id: TreeNodeId,
   text: &Text,
@@ -414,7 +414,6 @@ pub fn _update_viewport_after_text_changed(
   match node {
     TreeNode::Window(window) => window.set_viewport(updated_viewport.clone()),
     TreeNode::CommandLine(cmdline) => {
-      cmdline.set_message_viewport(updated_viewport.clone());
       cmdline.set_input_viewport(updated_viewport.clone())
     }
     _ => unreachable!(),

rsvim_core/src/state/ops/message_ops.rs
@@ -1,41 +1,0 @@
-use crate::ui::tree::Inodeable;
-use crate::ui::viewport::Viewport;
-use crate::ui::widget::command_line::CommandLine;
-use crate::{geo_rect_as, lock};
-
-/// Toggles visibility between the command-line content and its message.
-///
-/// - If `visible` is true: show the message and hide the content.
-/// - If `visible` is false: hide the message and show the content.
-pub fn set_message_visible(command_line: &mut CommandLine, visible: bool) {
-  command_line.indicator_mut().set_visible(!visible);
-  command_line.input_mut().set_visible(!visible);
-  command_line.message_mut().set_visible(visible);
-  let text_contents = command_line
-    .message_mut()
-    .get_text_contents_mut()
-    .upgrade()
-    .unwrap();
-  let mut text_contents = lock!(text_contents);
-  text_contents.command_line_input_mut().clear();
-}
-
-/// Refresh the command line view to have no content in both content and message widgets.
-pub fn refresh_view(command_line: &mut CommandLine) {
-  let input_shape = command_line.input_mut().shape();
-  let input_actual_shape = geo_rect_as!(input_shape, u16);
-  let text_contents = command_line.text_contents().upgrade().unwrap();
-  let mut text_contents = lock!(text_contents);
-  let content = text_contents.command_line_input_mut();
-  content.clear();
-  let input_viewport =
-    Viewport::view(command_line.options(), content, &input_actual_shape, 0, 0);
-  let input_viewport_arc = Viewport::to_arc(input_viewport);
-  let message = text_contents.command_line_message_mut();
-  message.clear();
-  command_line.set_input_viewport(input_viewport_arc.clone());
-  let message_viewport =
-    Viewport::view(command_line.options(), message, &input_actual_shape, 0, 0);
-  let message_viewport_arc = Viewport::to_arc(message_viewport);
-  command_line.set_message_viewport(message_viewport_arc.clone());
-}

rsvim_core/src/ui/widget/command_line.rs
@@ -67,8 +67,6 @@ pub struct CommandLine {
   cursor_id: Option<TreeNodeId>,
   message_id: TreeNodeId,
 
-  text_contents: TextContentsWk,
-
   input_viewport: ViewportArc,
   input_cursor_viewport: CursorViewportArc,
   message_viewport: ViewportArc,
@@ -160,7 +158,6 @@ impl CommandLine {
       input_id,
       message_id,
       cursor_id: None,
-      text_contents,
       input_viewport,
       input_cursor_viewport,
       message_viewport,
@@ -236,11 +233,6 @@ impl CommandLine {
     self.input_cursor_viewport = cursor_viewport;
   }
 
-  /// Get binded global text contents.
-  pub fn text_contents(&self) -> TextContentsWk {
-    self.text_contents.clone()
-  }
-
   /// Cursor widget ID.
   pub fn cursor_id(&self) -> Option<TreeNodeId> {
     self.cursor_id
@@ -261,6 +253,22 @@ impl CommandLine {
     self.message_id
   }
 }
+
+// Show/Hide switch {
+impl CommandLine {
+  pub fn show_message(&mut self) {
+    self.indicator_mut().set_visible(false);
+    self.input_mut().set_visible(false);
+    self.message_mut().set_visible(true);
+  }
+
+  pub fn show_input(&mut self) {
+    self.indicator_mut().set_visible(true);
+    self.input_mut().set_visible(true);
+    self.message_mut().set_visible(false);
+  }
+}
+// Show/Hide switch }
 
 // Widgets {
 impl CommandLine {

rsvim_core/src/ui/widget/command_line/message.rs
@@ -7,7 +7,6 @@ use crate::ui::tree::*;
 use crate::ui::viewport::ViewportWk;
 use crate::ui::widget::Widgetable;
 use crate::{inode_impl, lock};
-use compact_str::CompactString;
 
 #[derive(Debug, Clone)]
 /// Commandline message.
@@ -33,22 +32,6 @@ impl Message {
 
   pub fn set_viewport(&mut self, viewport: ViewportWk) {
     self.viewport = viewport;
-  }
-
-  pub fn get_text_contents(&self) -> &TextContentsWk {
-    &self.text_contents
-  }
-
-  pub fn get_text_contents_mut(&mut self) -> &mut TextContentsWk {
-    &mut self.text_contents
-  }
-
-  pub fn set_message(&mut self, text: CompactString) {
-    let message_content = self.get_text_contents_mut().upgrade().unwrap();
-    let mut message_content = lock!(message_content);
-    let message_content = message_content.command_line_message_mut();
-    message_content.clear();
-    message_content.insert_at(0, 0, text);
   }
 }
 

rsvim_core/src/js/binding/global_rsvim/cmd_tests.rs
@@ -1,8 +1,11 @@
+use crate::prelude::*;
 use crate::results::IoResult;
 use crate::tests::constant::TempPathCfg;
-use crate::tests::evloop::make_event_loop;
+use crate::tests::evloop::*;
 use crate::tests::log::init as test_log_init;
+
 use std::io::Write;
+use std::time::Duration;
 
 #[tokio::test]
 #[should_panic(
@@ -30,6 +33,18 @@ async fn test_echo1_should_panic_with_missing_param() {
   }
 
   let mut event_loop = make_event_loop(terminal_cols, terminal_rows);
+
+  // Before running
+  {
+    let contents = lock!(event_loop.contents);
+    assert!(
+      contents
+        .command_line_message()
+        .rope()
+        .to_string()
+        .is_empty()
+    );
+  }
 
   event_loop.initialize().unwrap();
 }
@@ -61,6 +76,18 @@ async fn test_echo2_should_panic_with_null_param() {
 
   let mut event_loop = make_event_loop(terminal_cols, terminal_rows);
 
+  // Before running
+  {
+    let contents = lock!(event_loop.contents);
+    assert!(
+      contents
+        .command_line_message()
+        .rope()
+        .to_string()
+        .is_empty()
+    );
+  }
+
   event_loop.initialize().unwrap();
 }
 
@@ -70,6 +97,7 @@ async fn test_echo3() -> IoResult<()> {
 
   let terminal_cols = 10_u16;
   let terminal_rows = 10_u16;
+  let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(30))];
   let tp = TempPathCfg::create();
 
   let src: &str = r#"
@@ -90,8 +118,34 @@ async fn test_echo3() -> IoResult<()> {
 
   let mut event_loop = make_event_loop(terminal_cols, terminal_rows);
 
+  // Before running
+  {
+    let contents = lock!(event_loop.contents);
+    assert!(
+      contents
+        .command_line_message()
+        .rope()
+        .to_string()
+        .is_empty()
+    );
+  }
+
   event_loop.initialize()?;
+  event_loop.mock_run(MockReader::new(mocked_events)).await?;
   event_loop.shutdown()?;
+
+  // After running
+  {
+    let contents = lock!(event_loop.contents);
+    let payload = contents.command_line_message().rope().to_string();
+    let payload = payload.trim();
+    assert!(
+      payload.is_empty()
+        || payload == "Test echo"
+        || payload == "123"
+        || payload == "true"
+    );
+  }
 
   Ok(())
 }

rsvim_core/src/state/fsm/command_line_ex_tests.rs
@@ -771,10 +771,12 @@ mod tests_confirm_ex_command_and_goto_normal_mode {
 
     // Goto Normal-2
     {
-      let cmdline_content = stateful._goto_normal_mode_impl(&data_access);
-      info!("cmdline content:{cmdline_content:?}");
-      // After go to normal mode, content is cleared
-      assert_eq!("", cmdline_content.as_str());
+      let cmdline_input_content = stateful._goto_normal_mode_impl(&data_access);
+      info!("cmdline content:{cmdline_input_content:?}");
+      assert_eq!(
+        "Bye1 Bye2 Bye3 Bye4 Bye5 Bye6 Bye7",
+        cmdline_input_content.as_str()
+      );
     }
   }
 }

rsvim_core/src/state/fsm/normal_tests.rs
@@ -8,7 +8,7 @@ use crate::content::{TextContents, TextContentsArc};
 use crate::prelude::*;
 use crate::state::fsm::{Stateful, StatefulDataAccess, StatefulValue};
 use crate::state::ops::Operation;
-use crate::state::ops::cursor_ops;
+use crate::state::ops::{cmdline_ops, cursor_ops};
 use crate::state::{State, StateArc};
 use crate::tests::buf::{make_buffer_from_lines, make_buffers_manager};
 use crate::tests::log::init as test_log_init;
@@ -8534,6 +8534,64 @@ mod tests_goto_command_line_ex_mode {
     let actual_canvas = lock!(actual_canvas);
     assert_canvas(&actual_canvas, &expect_canvas);
   }
+
+  #[test]
+  fn nowrap_with_cmdline_message1() {
+    test_log_init();
+
+    let terminal_size = U16Size::new(60, 3);
+    let (tree, state, bufs, _buf, contents) = make_tree_with_cmdline(
+      terminal_size,
+      WindowOptionsBuilder::default().wrap(false).build().unwrap(),
+      vec!["Should go to insert mode with message command line\n"],
+    );
+
+    // Prepare
+    {
+      let tree = tree.clone();
+      let contents = contents.clone();
+      let mut tree = lock!(tree);
+      let mut contents = lock!(contents);
+      cmdline_ops::set_cmdline_message(
+        &mut tree,
+        &mut contents,
+        CompactString::new("Test echo"),
+      );
+    }
+
+    let key_event = KeyEvent::new_with_kind(
+      KeyCode::Char(':'),
+      KeyModifiers::empty(),
+      KeyEventKind::Press,
+    );
+
+    let prev_cursor_viewport = get_cursor_viewport(tree.clone());
+    assert_eq!(prev_cursor_viewport.line_idx(), 0);
+    assert_eq!(prev_cursor_viewport.char_idx(), 0);
+
+    let data_access = StatefulDataAccess::new(
+      state,
+      tree,
+      bufs,
+      contents,
+      Event::Key(key_event),
+    );
+    let stateful = NormalStateful::default();
+
+    stateful.goto_command_line_ex_mode(&data_access);
+
+    {
+      let tree = data_access.tree.clone();
+      let expect_canvas = vec![
+        "Should go to insert mode with message command line          ",
+        "                                                            ",
+        ":                                                           ",
+      ];
+      let actual_canvas = make_canvas(tree.clone(), terminal_size);
+      let actual_canvas = lock!(actual_canvas);
+      assert_canvas(&actual_canvas, &expect_canvas);
+    }
+  }
 }
 
 #[cfg(test)]
@@ -8555,8 +8613,6 @@ mod tests_goto_insert_mode {
     ViewportSearchDirection,
   };
 
-  use crate::state::ops::cursor_ops::_update_viewport_after_text_changed;
-  use crate::state::ops::message_ops::{refresh_view, set_message_visible};
   use crate::ui::widget::command_line::CommandLineNode::Message;
   use compact_str::ToCompactString;
   use crossterm::event::{
@@ -8965,7 +9021,7 @@ mod tests_goto_insert_mode {
   }
 
   #[test]
-  fn nowrap_goto_insert_with_message_command_line1() {
+  fn nowrap_with_cmdline_message1() {
     test_log_init();
 
     let terminal_size = U16Size::new(60, 3);
@@ -8975,30 +9031,17 @@ mod tests_goto_insert_mode {
       vec!["Should go to insert mode with message command line\n"],
     );
 
+    // Prepare
     {
-      let tree_clone = tree.clone();
-      let mut tree_lock = lock!(tree_clone);
-      let cmd_line = tree_lock.command_line_mut().unwrap();
-      refresh_view(cmd_line);
-      set_message_visible(cmd_line, true);
-      let message = cmd_line.message_mut();
-      message.set_message(CompactString::new("Test echo"));
-    }
-
-    {
+      let tree = tree.clone();
+      let contents = contents.clone();
       let mut tree = lock!(tree);
-      let mut tree_clone = tree.clone();
-      let cmd_line = tree_clone.command_line_mut().unwrap();
-      let cmd_line_message = cmd_line.message();
-      let message_content =
-        cmd_line_message.get_text_contents().upgrade().unwrap();
-      let message_content = lock!(message_content);
-      let message_text = message_content.command_line_message();
-      _update_viewport_after_text_changed(
+      let mut contents = lock!(contents);
+      cmdline_ops::set_cmdline_message(
         &mut tree,
-        cmd_line.id(),
-        message_text,
-      )
+        &mut contents,
+        CompactString::new("Test echo"),
+      );
     }
 
     let key_event = KeyEvent::new_with_kind(
@@ -9040,60 +9083,6 @@ mod tests_goto_insert_mode {
         "Should go to insert mode with message command line          ",
         "                                                            ",
         "Test echo                                                   ",
-      ];
-      let actual_canvas = make_canvas(tree.clone(), terminal_size);
-      let actual_canvas = lock!(actual_canvas);
-      assert_canvas(&actual_canvas, &expect_canvas);
-    }
-  }
-  #[test]
-  fn nowrap_goto_command_line_ex_with_message_command_line1() {
-    test_log_init();
-
-    let terminal_size = U16Size::new(60, 3);
-    let (tree, state, bufs, _buf, contents) = make_tree_with_cmdline(
-      terminal_size,
-      WindowOptionsBuilder::default().wrap(false).build().unwrap(),
-      vec!["Should go to insert mode with message command line\n"],
-    );
-
-    {
-      let tree_clone = tree.clone();
-      let mut tree_lock = lock!(tree_clone);
-      let cmd_line = tree_lock.command_line_mut().unwrap();
-      refresh_view(cmd_line);
-      set_message_visible(cmd_line, true);
-      let message = cmd_line.message_mut();
-      message.set_message(CompactString::new("Test echo"));
-    }
-
-    let key_event = KeyEvent::new_with_kind(
-      KeyCode::Char(':'),
-      KeyModifiers::empty(),
-      KeyEventKind::Press,
-    );
-
-    let prev_cursor_viewport = get_cursor_viewport(tree.clone());
-    assert_eq!(prev_cursor_viewport.line_idx(), 0);
-    assert_eq!(prev_cursor_viewport.char_idx(), 0);
-
-    let data_access = StatefulDataAccess::new(
-      state,
-      tree,
-      bufs,
-      contents,
-      Event::Key(key_event),
-    );
-    let stateful = NormalStateful::default();
-
-    stateful.goto_command_line_ex_mode(&data_access);
-
-    {
-      let tree = data_access.tree.clone();
-      let expect_canvas = vec![
-        "Should go to insert mode with message command line          ",
-        "                                                            ",
-        ":                                                           ",
       ];
       let actual_canvas = make_canvas(tree.clone(), terminal_size);
       let actual_canvas = lock!(actual_canvas);