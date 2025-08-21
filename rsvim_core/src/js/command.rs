1e03344f68571a91c889f36f6c8299bfc0cbbc36
linrongbin16 <linrongbin16@outlook.com>
2025-08-21 17:10:28 +0800

    c

--- rsvim_core/src/js/command.rs
+++ rsvim_core/src/js/command.rs
@@ -1,5 +1,6 @@
 //! Vim ex commands.
 
+use crate::js::binding;
 use crate::js::{self, JsFuture, JsFutureId, JsRuntime, execute_module_impl};
 use crate::prelude::*;
 
@@ -40,31 +41,15 @@ impl JsFuture for ExCommand {
     debug_assert!(self.is_builtin_js());
     let filename = format!("<ExCommand{}>", self.future_id);
 
-    let tc_scope = &mut v8::TryCatch::new(scope);
-
-    match execute_module_impl(tc_scope, &filename, Some(self.body().trim())) {
+    match execute_module_impl(scope, &filename, Some(self.body().trim())) {
       Ok(_) => { /* do nothing */ }
-      Err(e) => {
-        // Report if there's any error while loading/evaluating module.
-        let exception = tc_scope.exception().unwrap();
-        let exception = v8::Global::new(tc_scope, exception);
-        let state_rc = JsRuntime::state(tc_scope);
-        state_rc
-          .borrow_mut()
-          .exceptions
-          .capture_exception(exception);
+      Err(exception) => {
+        // Throw exception if there's any error while loading/evaluating module.
+        trace!(
+          "Failed to execute module, filename:{filename:?}, exception:{exception:?}"
+        );
+        binding::throw_exception(scope, &exception);
       }
-    }
-
-    // Report if callback threw an exception.
-    if tc_scope.has_caught() {
-      let exception = tc_scope.exception().unwrap();
-      let exception = v8::Global::new(tc_scope, exception);
-      let state_rc = JsRuntime::state(tc_scope);
-      state_rc
-        .borrow_mut()
-        .exceptions
-        .capture_exception(exception);
     }
   }
 }
