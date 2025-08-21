34940af8cef54b65138245fd6facbfb67813d454
linrongbin16 <linrongbin16@outlook.com>
2025-08-21 13:05:24 +0800

    c

--- rsvim_core/src/js/runtime/00__web.js
+++ rsvim_core/src/js/runtime/00__web.js
@@ -2,6 +2,15 @@
     const TIMEOUT_MAX = Math.pow(2, 31) - 1;
     let nextTimerId = 1;
     const activeTimers = new Map();
+    function clearTimeout(id) {
+        if (!Number.isInteger(id)) {
+            throw new Error(`"clearTimeout" id parameter must be an integer value, but found ${id} (${typeof id})`);
+        }
+        if (activeTimers.has(id)) {
+            __InternalRsvimGlobalObject.global_clear_timeout(activeTimers.get(id));
+            activeTimers.delete(id);
+        }
+    }
     function setTimeout(callback, delay, ...args) {
         delay *= 1;
         if (!(delay >= 1 && delay <= TIMEOUT_MAX)) {
@@ -18,16 +27,7 @@
         activeTimers.set(id, timer);
         return id;
     }
-    function clearTimeout(id) {
-        if (!Number.isInteger(id)) {
-            throw new Error(`"clearTimeout" id parameter must be an integer value, but found ${id} (${typeof id})`);
-        }
-        if (activeTimers.has(id)) {
-            __InternalRsvimGlobalObject.global_clear_timeout(activeTimers.get(id));
-            activeTimers.delete(id);
-        }
-    }
-    globalThis.setTimeout = setTimeout;
     globalThis.clearTimeout = clearTimeout;
+    globalThis.setTimeout = setTimeout;
 })(globalThis);
 export {};
