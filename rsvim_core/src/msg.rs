c6a3536b3569df0b417db7e5f0d47fc0a7352d5f
linrongbin16 <linrongbin16@outlook.com>
2025-08-21 10:12:27 +0800

    c

--- rsvim_core/src/msg.rs
+++ rsvim_core/src/msg.rs
@@ -1,89 +1,9 @@
 //! Messages between [`EventLoop`](crate::evloop::EventLoop) and
 //! [`JsRuntime`](crate::js::JsRuntime).
 
-use crate::command::ExCommand;
-use crate::js::JsFutureId;
+// Re-export
+pub use jsrt_sink::*;
+pub use master_sink::*;
 
-use compact_str::CompactString;
-use std::time::Duration;
-
-// The message JsRuntime send to EventLoop {
-
-#[derive(Debug)]
-/// Message between [`EventLoop`](crate::evloop::EventLoop) and
-/// [`JsRuntime`](crate::js::JsRuntime).
-pub enum JsRuntimeToEventLoopMessage {
-  PrintReq(PrintReq),
-  TimeoutReq(TimeoutReq),
-}
-
-// The message JsRuntime send to EventLoop }
-
-// The message JsRuntime receive from EventLoop {
-
-#[derive(Debug)]
-/// Message between [`EventLoop`](crate::evloop::EventLoop) and
-/// [`JsRuntime`](crate::js::JsRuntime).
-pub enum EventLoopToJsRuntimeMessage {
-  /// Event loop notify Js runtime to shutdown this thread.
-  TimeoutResp(TimeoutResp),
-
-  /// Event loop send ex command to js runtime to run.
-  ExCommandReq(ExCommandReq),
-}
-
-// The message JsRuntime receive from EventLoop }
-
-#[derive(Debug)]
-pub struct TimeoutResp {
-  pub future_id: JsFutureId,
-  pub duration: Duration,
-}
-
-impl TimeoutResp {
-  pub fn new(future_id: JsFutureId, duration: Duration) -> Self {
-    TimeoutResp {
-      future_id,
-      duration,
-    }
-  }
-}
-
-#[derive(Debug)]
-pub struct PrintReq {
-  pub future_id: JsFutureId,
-  pub payload: CompactString,
-}
-
-impl PrintReq {
-  pub fn new(future_id: JsFutureId, payload: CompactString) -> Self {
-    PrintReq { future_id, payload }
-  }
-}
-
-#[derive(Debug)]
-pub struct TimeoutReq {
-  pub future_id: JsFutureId,
-  pub duration: Duration,
-}
-
-impl TimeoutReq {
-  pub fn new(future_id: JsFutureId, duration: Duration) -> Self {
-    TimeoutReq {
-      future_id,
-      duration,
-    }
-  }
-}
-
-#[derive(Debug)]
-pub struct ExCommandReq {
-  pub future_id: JsFutureId,
-  pub command: ExCommand,
-}
-
-impl ExCommandReq {
-  pub fn new(future_id: JsFutureId, command: ExCommand) -> Self {
-    ExCommandReq { future_id, command }
-  }
-}
+pub mod jsrt_sink;
+pub mod master_sink;
---
+++ rsvim_core/src/msg/master_sink.rs
@@ -0,0 +1,41 @@
+//! Messages that are sent to [`EventLoop`](crate::evloop::EventLoop).
+
+use crate::js::JsFutureId;
+
+use compact_str::CompactString;
+use std::time::Duration;
+
+#[derive(Debug)]
+/// Message between [`EventLoop`](crate::evloop::EventLoop) and
+/// [`JsRuntime`](crate::js::JsRuntime).
+pub enum JsRuntimeToEventLoopMessage {
+  PrintReq(PrintReq),
+  TimeoutReq(TimeoutReq),
+}
+
+#[derive(Debug)]
+pub struct PrintReq {
+  pub future_id: JsFutureId,
+  pub payload: CompactString,
+}
+
+impl PrintReq {
+  pub fn new(future_id: JsFutureId, payload: CompactString) -> Self {
+    PrintReq { future_id, payload }
+  }
+}
+
+#[derive(Debug)]
+pub struct TimeoutReq {
+  pub future_id: JsFutureId,
+  pub duration: Duration,
+}
+
+impl TimeoutReq {
+  pub fn new(future_id: JsFutureId, duration: Duration) -> Self {
+    TimeoutReq {
+      future_id,
+      duration,
+    }
+  }
+}
