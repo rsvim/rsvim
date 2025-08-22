390b849d6c53016663c2f6ff71e26cc85cb88fe4
linrongbin16 <linrongbin16@outlook.com>
2025-08-22 15:31:17 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/buf.rs
+++ rsvim_core/src/js/binding/global_rsvim/buf.rs
@@ -19,24 +19,34 @@ pub fn write(
   let buffers = state.buffers.clone();
   let buffers = lock!(buffers);
 
+  let message_id = js::next_future_id();
   match buffers.write_buffer(buf_id) {
-    Some(buf) => {
-      let mut buf = lock!(buf);
-      buf.write();
-      msg::sync_send_to_master(
-        state.master_tx.clone(),
-        MasterMessage::PrintReq(msg::PrintReq::new(
-          js::next_future_id(),
-          message.to_compact_string(),
-        )),
+    Ok(n) => {
+      let buf = buffers.get(&buf_id).unwrap();
+      let buf = lock!(buf);
+      let message = format!(
+        "Buffer {:?} ({buf_id}) has been saved ({n} bytes written).",
+        buf
+          .filename()
+          .as_ref()
+          .map(|p| String::from(p.as_path().to_string_lossy()))
+          .unwrap_or("<unknown>".to_string())
+          .to_owned()
       );
-    }
-    None => {
       msg::sync_send_to_master(
         state.master_tx.clone(),
         MasterMessage::PrintReq(msg::PrintReq::new(
           message_id,
           message.to_compact_string(),
+        )),
+      );
+    }
+    Err(e) => {
+      msg::sync_send_to_master(
+        state.master_tx.clone(),
+        MasterMessage::PrintReq(msg::PrintReq::new(
+          message_id,
+          e.to_compact_string(),
         )),
       );
     }
