c4f27c18d2a136c372d771df9fdfdc5b69fbd6d7
linrongbin16 <linrongbin16@outlook.com>
2025-08-23 09:46:18 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -57,6 +57,10 @@ export class RsvimBuf {
    * The "current" buffer is the buffer that the window where your cursor is
    * located is binded to. See {@link RsvimWin}.
    *
+   * NOTE: This API returns `undefined` if been called before the editor is initialized,
+   * at that time, no buffer/window is been created. Once editor is initialized, this
+   * API always returns a valid buffer ID since there will always a valid buffer binded
+   * to the current window where your cursor is.
    *
    * @returns {number | undefined}
    *
