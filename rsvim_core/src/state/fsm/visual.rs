8b88afe828b5f1f4228be0a23a3af3aff8812d4d
linrongbin16 <linrongbin16@outlook.com>
2025-08-24 14:24:40 +0800

    c

--- rsvim_core/src/state/fsm/visual.rs
+++ rsvim_core/src/state/fsm/visual.rs
@@ -3,12 +3,18 @@
 use crate::state::fsm::{StateDataAccess, StateMachine, Stateful};
 use crate::state::ops::Operation;
 
+use crossterm::event::Event;
+
 #[derive(Debug, Copy, Clone, Default, PartialEq, Eq, Hash)]
 /// The visual editing mode.
 pub struct VisualStateful {}
 
 impl Stateful for VisualStateful {
-  fn handle(&self, _data_access: StateDataAccess) -> StateMachine {
+  fn handle(
+    &self,
+    _data_access: StateDataAccess,
+    _event: Event,
+  ) -> StateMachine {
     StateMachine::VisualMode(VisualStateful::default())
   }
   fn handle_op(
