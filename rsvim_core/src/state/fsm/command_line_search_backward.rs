24d5e18896bd474894beaf175365bf11b2082cd2
linrongbin16 <linrongbin16@outlook.com>
2025-08-24 14:11:56 +0800

    c

--- rsvim_core/src/state/fsm/command_line_search_backward.rs
+++ rsvim_core/src/state/fsm/command_line_search_backward.rs
@@ -3,12 +3,18 @@
 use crate::state::fsm::{StateDataAccess, StateMachine, Stateful};
 use crate::state::ops::Operation;
 
+use crossterm::event::Event;
+
 #[derive(Debug, Copy, Clone, Default, PartialEq, Eq, Hash)]
 /// The command-line search backward mode.
 pub struct CommandLineSearchBackwardStateful {}
 
 impl Stateful for CommandLineSearchBackwardStateful {
-  fn handle(&self, _data_access: StateDataAccess) -> StateMachine {
+  fn handle(
+    &self,
+    _data_access: StateDataAccess,
+    _event: Event,
+  ) -> StateMachine {
     StateMachine::CommandLineSearchBackwardMode(
       CommandLineSearchBackwardStateful::default(),
     )
