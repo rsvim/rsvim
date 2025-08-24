459fe3e6b483a9ccfcf63fff0ba1ca9903283da4
linrongbin16 <linrongbin16@outlook.com>
2025-08-24 14:24:17 +0800

    c

--- rsvim_core/src/state/fsm/terminal.rs
+++ rsvim_core/src/state/fsm/terminal.rs
@@ -3,12 +3,18 @@
 use crate::state::fsm::{StateDataAccess, StateMachine, Stateful};
 use crate::state::ops::Operation;
 
+use crossterm::event::Event;
+
 #[derive(Debug, Copy, Clone, Default, PartialEq, Eq, Hash)]
 /// The terminal editing mode.
 pub struct TerminalStateful {}
 
 impl Stateful for TerminalStateful {
-  fn handle(&self, _data_access: StateDataAccess) -> StateMachine {
+  fn handle(
+    &self,
+    _data_access: StateDataAccess,
+    _event: Event,
+  ) -> StateMachine {
     StateMachine::TerminalMode(TerminalStateful::default())
   }
   fn handle_op(
