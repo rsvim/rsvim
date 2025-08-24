35852e78a3c97a8d6a0616031dc59b7b76888b99
linrongbin16 <linrongbin16@outlook.com>
2025-08-24 14:12:18 +0800

    c

--- rsvim_core/src/state/fsm/command_line_search_forward.rs
+++ rsvim_core/src/state/fsm/command_line_search_forward.rs
@@ -3,12 +3,18 @@
 use crate::state::fsm::{StateDataAccess, StateMachine, Stateful};
 use crate::state::ops::Operation;
 
+use crossterm::event::Event;
+
 #[derive(Debug, Copy, Clone, Default, PartialEq, Eq, Hash)]
 /// The command-line search forward mode.
 pub struct CommandLineSearchForwardStateful {}
 
 impl Stateful for CommandLineSearchForwardStateful {
-  fn handle(&self, _data_access: StateDataAccess) -> StateMachine {
+  fn handle(
+    &self,
+    _data_access: StateDataAccess,
+    _event: Event,
+  ) -> StateMachine {
     StateMachine::CommandLineSearchForwardMode(
       CommandLineSearchForwardStateful::default(),
     )
