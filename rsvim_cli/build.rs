680ec9f4490d0ecbf3b7565c31bb52b5ef69cb60
linrongbin16 <linrongbin16@outlook.com>
2025-08-25 11:43:48 +0800

    c

--- rsvim_cli/build.rs
+++ rsvim_cli/build.rs
@@ -5,11 +5,10 @@ use std::path::Path;
 fn version() {
   let profile = std::env::var("PROFILE").unwrap();
   let opt_level = std::env::var("OPT_LEVEL").unwrap();
-  let debug = std::env::var("DEBUG").unwrap();
 
   let profile = if profile == "release" && opt_level == "z" {
     "release"
-  } else if profile == "release" && debug == "true" {
+  } else if profile == "release" && opt_level == "s" {
     "nightly"
   } else {
     "dev"
