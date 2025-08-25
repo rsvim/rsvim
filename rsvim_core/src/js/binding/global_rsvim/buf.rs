0a0b6e7c4d20850dbcee39d007e87bd051d20a25
linrongbin16 <linrongbin16@outlook.com>
2025-08-25 17:32:42 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/buf.rs
+++ rsvim_core/src/js/binding/global_rsvim/buf.rs
@@ -46,7 +46,7 @@ pub fn list(
   let buf_ids_array = v8::Array::new(scope, buf_ids.len() as i32);
   for (i, buf_id) in buf_ids.iter().enumerate() {
     let v = v8::Integer::new(scope, *buf_id);
-    let v = v8::Local::new(scope, v);
+    // let v = v8::Local::new(scope, v);
     buf_ids_array.set_index(scope, i as u32, v.into());
   }
   rv.set(v8::Local::new(scope, buf_ids_array).into());
