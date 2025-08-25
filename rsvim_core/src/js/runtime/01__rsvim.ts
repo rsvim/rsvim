a3f14dc2817514978fde5557145be4887730345f
linrongbin16 <linrongbin16@outlook.com>
2025-08-25 23:30:22 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -80,6 +80,13 @@ export class RsvimBuf {
   /**
    * List all buffers' IDs.
    *
+   * :::warning
+   * When the editor is not initialized, i.e. there's no buffer/window created. It
+   * will return an empty array. Once the editor is initialized, there will have at least 1
+   * buffer binded to the "current" window (where your cursor is). It will return all the
+   * buffer IDs as an array.
+   * :::
+   *
    * @returns {number[]} All the buffers' IDs as an array. If there's no
    * buffer (i.e. the editor is not initialized), it returns an empty array.
    *
