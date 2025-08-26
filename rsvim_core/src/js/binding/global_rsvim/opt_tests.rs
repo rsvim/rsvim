11be0bc7ba4cab4077368cefd4d0be5a0a79af2c
linrongbin16 <linrongbin16@outlook.com>
2025-08-26 17:41:28 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/opt_tests.rs
+++ rsvim_core/src/js/binding/global_rsvim/opt_tests.rs
@@ -53,17 +53,21 @@ async fn test_wrap1() -> IoResult<()> {
   Ok(())
 }
 
-#[tokio::test]
-#[cfg_attr(miri, ignore)]
-async fn test_buf_options1() -> IoResult<()> {
-  test_log_init();
+#[cfg(test)]
+mod tests_buffer_options {
+  use super::*;
 
-  let terminal_cols = 10_u16;
-  let terminal_rows = 10_u16;
-  let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(30))];
-  let tp = TempPathCfg::create();
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn success1() -> IoResult<()> {
+    test_log_init();
 
-  let src: &str = r#"
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(30))];
+    let tp = TempPathCfg::create();
+
+    let src: &str = r#"
   const v1 = Rsvim.opt.tabStop;
   Rsvim.opt.tabStop = 4;
   const v2 = Rsvim.opt.fileEncoding;
@@ -72,48 +76,105 @@ async fn test_buf_options1() -> IoResult<()> {
   Rsvim.opt.fileFormat = "mac";
     "#;
 
-  // Prepare $RSVIM_CONFIG/rsvim.js
-  make_configs(&tp, src);
+    // Prepare $RSVIM_CONFIG/rsvim.js
+    make_configs(&tp, src);
 
-  let mut event_loop =
-    make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+    let mut event_loop =
+      make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
 
-  // Before running
-  {
-    use crate::defaults;
+    // Before running
+    {
+      use crate::defaults;
 
-    let buffers = lock!(event_loop.buffers);
-    let global_local_options = buffers.global_local_options();
-    assert_eq!(global_local_options.tab_stop(), defaults::buf::TAB_STOP);
-    assert_eq!(
-      global_local_options.file_encoding(),
-      defaults::buf::FILE_ENCODING
-    );
-    assert_eq!(
-      global_local_options.file_format(),
-      defaults::buf::FILE_FORMAT
-    );
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert_eq!(global_local_options.tab_stop(), defaults::buf::TAB_STOP);
+      assert_eq!(
+        global_local_options.file_encoding(),
+        defaults::buf::FILE_ENCODING
+      );
+      assert_eq!(
+        global_local_options.file_format(),
+        defaults::buf::FILE_FORMAT
+      );
+    }
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      use crate::buf::opt::{FileEncodingOption, FileFormatOption};
+
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert_eq!(global_local_options.tab_stop(), 4);
+      assert_eq!(
+        global_local_options.file_encoding(),
+        FileEncodingOption::Utf8,
+      );
+      assert_eq!(global_local_options.file_format(), FileFormatOption::Mac);
+
+      let contents = lock!(event_loop.contents);
+      let actual = contents.command_line_message().rope().to_string();
+      assert!(actual.trim().is_empty());
+    }
+
+    Ok(())
   }
 
-  event_loop.initialize()?;
-  event_loop
-    .run_with_mock_operations(MockOperationReader::new(mocked_ops))
-    .await?;
-  event_loop.shutdown()?;
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn failed1() -> IoResult<()> {
+    test_log_init();
 
-  // After running
-  {
-    use crate::buf::opt::{FileEncodingOption, FileFormatOption};
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(30))];
+    let tp = TempPathCfg::create();
 
-    let buffers = lock!(event_loop.buffers);
-    let global_local_options = buffers.global_local_options();
-    assert_eq!(global_local_options.tab_stop(), 4);
-    assert_eq!(
-      global_local_options.file_encoding(),
-      FileEncodingOption::Utf8,
-    );
-    assert_eq!(global_local_options.file_format(), FileFormatOption::Mac);
+    let src: &str = r#"
+  const v1 = Rsvim.opt.tabStop;
+  Rsvim.opt.tabStop = -1;
+    "#;
+
+    // Prepare $RSVIM_CONFIG/rsvim.js
+    make_configs(&tp, src);
+
+    let mut event_loop =
+      make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+    // Before running
+    {
+      use crate::defaults;
+
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert_eq!(global_local_options.tab_stop(), defaults::buf::TAB_STOP);
+    }
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      use crate::defaults;
+
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert_eq!(global_local_options.tab_stop(), defaults::buf::TAB_STOP);
+
+      let contents = lock!(event_loop.contents);
+      let actual = contents.command_line_message().rope().to_string();
+      assert!(actual.trim().is_empty());
+    }
+
+    Ok(())
   }
-
-  Ok(())
 }
