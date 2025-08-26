6f3be144fa3d96a074ec5f204d601bcb6040eb0d
linrongbin16 <linrongbin16@outlook.com>
2025-08-26 11:35:39 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/rt.rs
+++ rsvim_core/src/js/binding/global_rsvim/rt.rs
@@ -1,23 +1,24 @@
 //! APIs for `Rsvim.rt` namespace.
 
-use crate::js::JsRuntime;
+use crate::js::{self, JsRuntime};
+use crate::msg::{self, MasterMessage};
 use crate::prelude::*;
 
-/// Get the _wrap_ option.
-/// See: <https://vimhelp.org/options.txt.html#%27wrap%27>
-/// Also known as _line-wrap_, see: <https://en.wikipedia.org/wiki/Line_wrap_and_word_wrap>.
+/// Exit editor.
 pub fn exit(
   scope: &mut v8::HandleScope,
   args: v8::FunctionCallbackArguments,
-  mut rv: v8::ReturnValue,
+  mut _rv: v8::ReturnValue,
 ) {
   debug_assert!(args.length() == 1);
-  let exit_code = args.get(0).int32_value(scope);
+  let exit_code = args.get(0).int32_value(scope).unwrap();
 
   let state_rc = JsRuntime::state(scope);
-  let tree = state_rc.borrow().tree.clone();
-  let tree = lock!(tree);
-  let value = tree.global_local_options().wrap();
-  trace!("get_wrap: {:?}", value);
-  rv.set_bool(value);
+  let state = state_rc.borrow();
+  trace!("exit: {:?}", exit_code);
+
+  msg::sync_send_to_master(
+    state.master_tx.clone(),
+    MasterMessage::ExitReq(msg::ExitReq::new(js::next_future_id(), exit_code)),
+  );
 }
