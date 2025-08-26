80957ae9b9499c83fec56fdb87db80781008554c
linrongbin16 <linrongbin16@outlook.com>
2025-08-26 17:58:44 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -254,7 +254,7 @@ export class RsvimOpt {
    * Sets the [line end](https://en.wikipedia.org/wiki/Newline) for the file of this buffer. There are 3 kinds of line end:
    * - `CRLF`: used by [Windows](https://www.microsoft.com/windows).
    * - `LF`: used by [Linux](https://en.wikipedia.org/wiki/Linux) and [Unix](https://en.wikipedia.org/wiki/Unix) (include [MacOS](https://www.apple.com/macos/)).
-   * - `CR`: used by [classic MacOS](https://en.wikipedia.org/wiki/Classic_Mac_OS). Today's Mac also uses `LF` as line end, you would never use `CR` in most of today's operating systems.
+   * - `CR`: used by [classic MacOS](https://en.wikipedia.org/wiki/Classic_Mac_OS). Today's Mac also uses `LF` as line end, you would never use `CR` any more.
    *
    * :::note
    * In fact it should be named to "line-end", it is called "file-format" just to be consistent
