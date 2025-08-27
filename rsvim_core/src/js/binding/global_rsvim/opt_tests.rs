92b7ef9a3e5dba6b965d937cc1065f9e183e4678
linrongbin16 <linrongbin16@outlook.com>
2025-08-27 11:48:57 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/opt_tests.rs
+++ rsvim_core/src/js/binding/global_rsvim/opt_tests.rs
@@ -440,3 +440,230 @@ mod tests_file_format {
     Ok(())
   }
 }
+
+#[cfg(test)]
+mod tests_expand_tab {
+  use super::*;
+
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn success1() -> IoResult<()> {
+    test_log_init();
+
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(30))];
+    let tp = TempPathCfg::create();
+
+    let src: &str = r#"
+  const value = Rsvim.opt.expandTab;
+  Rsvim.opt.expandTab = true;
+    "#;
+
+    // Prepare $RSVIM_CONFIG/rsvim.js
+    make_configs(&tp, src);
+
+    let mut event_loop =
+      make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+    // Before running
+    {
+      use crate::defaults;
+
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert_eq!(global_local_options.expand_tab(), defaults::buf::EXPAND_TAB);
+    }
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert!(global_local_options.expand_tab());
+
+      let contents = lock!(event_loop.contents);
+      let actual = contents.command_line_message().rope().to_string();
+      assert!(actual.trim().is_empty());
+    }
+
+    Ok(())
+  }
+
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn failed1() -> IoResult<()> {
+    test_log_init();
+
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(30))];
+    let tp = TempPathCfg::create();
+
+    let src: &str = r#"
+  Rsvim.opt.expandTab = null;
+    "#;
+
+    // Prepare $RSVIM_CONFIG/rsvim.js
+    make_configs(&tp, src);
+
+    let mut event_loop =
+      make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+    // Before running
+    {
+      use crate::defaults;
+
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert_eq!(global_local_options.expand_tab(), defaults::buf::EXPAND_TAB);
+    }
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      use crate::defaults;
+
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert_eq!(global_local_options.expand_tab(), defaults::buf::EXPAND_TAB);
+
+      let contents = lock!(event_loop.contents);
+      let actual = contents.command_line_message().rope().to_string();
+      let actual = actual.trim();
+      info!("actual:{actual}");
+      let expect = r####"Uncaught Error: "Rsvim.opt.fileFormat" parameter must be a valid option, but found"####;
+      assert!(actual.starts_with(expect));
+    }
+
+    Ok(())
+  }
+}
+
+#[cfg(test)]
+mod tests_shift_width {
+  use super::*;
+
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn success1() -> IoResult<()> {
+    test_log_init();
+
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(30))];
+    let tp = TempPathCfg::create();
+
+    let src: &str = r#"
+  const value = Rsvim.opt.shiftWidth;
+  Rsvim.opt.shiftWidth = 4;
+    "#;
+
+    // Prepare $RSVIM_CONFIG/rsvim.js
+    make_configs(&tp, src);
+
+    let mut event_loop =
+      make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+    // Before running
+    {
+      use crate::defaults;
+
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert_eq!(
+        global_local_options.shift_width(),
+        defaults::buf::SHIFT_WIDTH
+      );
+    }
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert_eq!(global_local_options.shift_width(), 4);
+
+      let contents = lock!(event_loop.contents);
+      let actual = contents.command_line_message().rope().to_string();
+      assert!(actual.trim().is_empty());
+    }
+
+    Ok(())
+  }
+
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn failed1() -> IoResult<()> {
+    test_log_init();
+
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(30))];
+    let tp = TempPathCfg::create();
+
+    let src: &str = r#"
+  Rsvim.opt.shiftWidth = 10000;
+    "#;
+
+    // Prepare $RSVIM_CONFIG/rsvim.js
+    make_configs(&tp, src);
+
+    let mut event_loop =
+      make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+    // Before running
+    {
+      use crate::defaults;
+
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert_eq!(
+        global_local_options.shift_width(),
+        defaults::buf::SHIFT_WIDTH
+      );
+    }
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      use crate::defaults;
+
+      let buffers = lock!(event_loop.buffers);
+      let global_local_options = buffers.global_local_options();
+      assert_eq!(
+        global_local_options.shift_width(),
+        defaults::buf::SHIFT_WIDTH
+      );
+
+      let contents = lock!(event_loop.contents);
+      let actual = contents.command_line_message().rope().to_string();
+      let actual = actual.trim();
+      info!("actual:{actual}");
+      let expect = r####"Uncaught Error: "Rsvim.opt.fileFormat" parameter must be a valid option, but found"####;
+      assert!(actual.starts_with(expect));
+    }
+
+    Ok(())
+  }
+}
