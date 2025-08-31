69ac76ef43e081daef258330d90f18a2c43795ea
linrongbin16 <linrongbin16@outlook.com>
2025-08-31 14:05:00 +0800

    c

--- rsvim_cli/build.rs
+++ rsvim_cli/build.rs
@@ -4,10 +4,12 @@ use std::path::Path;
 
 fn version() {
   let profile = std::env::var("PROFILE").unwrap_or("debug".to_string());
+  let opt_level = std::env::var("OPT_LEVEL").unwrap_or("0".to_string());
 
   let version = if profile == "release" {
     format!("{} (v8 {})", env!("CARGO_PKG_VERSION"), v8_version())
   } else {
+    let profile = if opt_level == "z" { "nightly" } else { profile };
     let repo_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("..");
     let maybe_git_commit = match Repository::open(repo_path) {
       Ok(repo) => {
