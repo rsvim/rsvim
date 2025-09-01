4d8ea21b18777173065127f3fce3e4f429b174b0
linrongbin16 <linrongbin16@outlook.com>
2025-09-01 11:06:49 +0800

    c

--- rsvim_core/src/defaults/ascii/end_of_line.rs
+++ rsvim_core/src/defaults/ascii/end_of_line.rs
@@ -1,15 +1,20 @@
 //! End of line.
 
-// use std::str::FromStr;
+use std::str::FromStr;
 // use std::string::ToString;
 // use strum_macros::{Display, EnumString};
 
-#[derive(Debug, PartialEq, Eq, strum_macros::Display)]
+#[derive(
+  Debug, PartialEq, Eq, strum_macros::Display, strum_macros::EnumString,
+)]
 pub enum EndOfLine {
+  #[strum(serialize = "\r\n")]
   #[strum(to_string = "\r\n")]
   CRLF,
+  #[strum(serialize = "\n")]
   #[strum(to_string = "\n")]
   LF,
+  #[strum(serialize = "\r")]
   #[strum(to_string = "\r")]
   CR,
 }
