8c56d1d58bebf386dc6327a865473966f895204b
linrongbin16 <linrongbin16@outlook.com>
2025-09-01 11:03:08 +0800

    c

--- rsvim_core/src/defaults/ascii/end_of_line.rs
+++ rsvim_core/src/defaults/ascii/end_of_line.rs
@@ -1,5 +1,19 @@
 //! End of line.
 
-pub const CRLF: &str = "\r\n";
-pub const LF: &str = "\n";
-pub const CR: &str = "\r";
+// use std::str::FromStr;
+// use std::string::ToString;
+// use strum_macros::{Display, EnumString};
+
+#[derive(Debug, PartialEq, Eq, strum_macros::Display)]
+pub enum EndOfLine {
+  #[strum(to_string = "\r\n")]
+  CRLF,
+  #[strum(to_string = "\n")]
+  LF,
+  #[strum(to_string = "\r")]
+  CR,
+}
+
+// pub const CRLF: &str = "\r\n";
+// pub const LF: &str = "\n";
+// pub const CR: &str = "\r";
