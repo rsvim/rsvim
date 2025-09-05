6e8fa7b2d48aa5755cd3885ee7a64022bb823166
linrongbin16 <linrongbin16@outlook.com>
2025-09-05 21:48:35 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/cmd.rs
+++ rsvim_core/src/js/binding/global_rsvim/cmd.rs
@@ -1,28 +1,11 @@
 //! APIs for `Rsvim.cmd` namespace.
 
-use crate::js::{self, JsRuntime, JsRuntimeState};
+use crate::js::{self, JsRuntime};
 use crate::msg::{self, MasterMessage};
 use crate::prelude::*;
 use crate::state::ops::cmdline_ops;
 
-use compact_str::{CompactString, ToCompactString};
-
-/// `Rsvim.cmd.echo` API.
-pub fn echo_impl(state: &JsRuntimeState, message: CompactString) {
-  let mut tree = lock!(state.tree);
-  let mut contents = lock!(state.contents);
-  if tree.command_line_id().is_some() {
-    cmdline_ops::cmdline_set_message(&mut tree, &mut contents, message);
-  } else {
-    msg::sync_send_to_master(
-      state.master_tx.clone(),
-      MasterMessage::PrintReq(msg::PrintReq::new(
-        js::next_future_id(),
-        message,
-      )),
-    );
-  }
-}
+use compact_str::ToCompactString;
 
 /// `Rsvim.cmd.echo` API.
 pub fn echo(
@@ -36,5 +19,21 @@ pub fn echo(
 
   let state_rc = JsRuntime::state(scope);
   let state = state_rc.borrow();
-  echo_impl(&state, message.to_compact_string());
+  let mut tree = lock!(state.tree);
+  let mut contents = lock!(state.contents);
+  if tree.command_line_id().is_some() {
+    cmdline_ops::cmdline_set_message(
+      &mut tree,
+      &mut contents,
+      message.to_compact_string(),
+    );
+  } else {
+    msg::sync_send_to_master(
+      state.master_tx.clone(),
+      MasterMessage::PrintReq(msg::PrintReq::new(
+        js::next_future_id(),
+        message.to_compact_string(),
+      )),
+    );
+  }
 }
