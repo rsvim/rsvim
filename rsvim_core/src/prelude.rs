b7825a127b19f3c3ee1b1fc8868aa39e91407464
linrongbin16 <linrongbin16@outlook.com>
2025-09-05 22:25:18 +0800

    c

--- rsvim_core/src/prelude.rs
+++ rsvim_core/src/prelude.rs
@@ -9,8 +9,12 @@ pub use crate::results::*;
 pub use crate::{arc_mutex_ptr, arc_ptr, lock, rc_ptr, rc_refcell_ptr};
 pub use paste::paste;
 
-pub use foldhash::fast::RandomState;
-pub use foldhash::{HashMap, HashMapExt, HashSet, HashSetExt};
-pub use std::collections::{BTreeMap, BTreeSet};
+pub use foldhash::fast::RandomState as FandomState;
+pub use foldhash::{
+  HashMap as FashMap, HashMapExt as FashMapExt, HashSet as FashSet,
+  HashSetExt as FashSetExt,
+};
+pub use std::collections::{BTreeMap, BTreeSet /*, HashMap, HashSet */};
+// pub use std::hash::RandomState;
 
 pub use geo::{self, Point, Rect};
