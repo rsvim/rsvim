d3407ee16aec41ccd67e5e3cf84da7b6faf41086
linrongbin16 <linrongbin16@outlook.com>
2025-09-06 14:41:46 +0800

    c

--- rsvim_core/src/js.rs
+++ rsvim_core/src/js.rs
@@ -786,13 +786,7 @@ pub mod boost {
           // NOTE: Send error message to command-line.
           trace!("{exception:?}");
           let state = state_rc.borrow_mut();
-          let mut tree = lock!(state.tree);
-          let mut contents = lock!(state.contents);
-          cmdline_ops::cmdline_set_message(
-            &mut tree,
-            &mut contents,
-            exception.to_compact_string(),
-          );
+          report_js_error!(state, exception);
           continue;
         }
 
