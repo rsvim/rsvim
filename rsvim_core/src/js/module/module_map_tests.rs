450092b3f97c3227ce64480142ac71f10e406169
linrongbin16 <linrongbin16@outlook.com>
2025-09-10 16:36:17 +0800

    c

--- rsvim_core/src/js/module/module_map_tests.rs
+++ rsvim_core/src/js/module/module_map_tests.rs
@@ -266,6 +266,106 @@ mod test_static_import {
 
     Ok(())
   }
+
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn side_effect1() -> IoResult<()> {
+    test_log_init();
+
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(100))];
+    let tp = TempPathCfg::create();
+
+    let p1 = Path::new("rsvim.js");
+    let src1: &str = r#"
+  import utils from "utils";
+    "#;
+
+    let p2 = Path::new("node_modules/utils/lib/echo.js");
+    let src2: &str = r#"
+    export function echo(value) {
+        Rsvim.cmd.echo(value);
+    }
+    "#;
+
+    let p3 = Path::new("node_modules/utils/lib/calc.js");
+    let src3: &str = r#"
+    export function add(a, b) {
+        return a+b;
+    }
+    "#;
+
+    let p4 = Path::new("node_modules/utils/lib/index.js");
+    let src4: &str = r#"
+    import {add} from "./calc.js";
+    import {echo} from "./echo.js";
+
+    echo(add(4,5));
+    export default {};
+    "#;
+
+    let p5 = Path::new("node_modules/utils/package.json");
+    let src5: &str = r#"
+{
+  "exports": "./lib/index.js"
+}
+    "#;
+
+    // Prepare $RSVIM_CONFIG/rsvim.js
+    make_multi_file_configs(
+      &tp,
+      vec![(p1, src1), (p2, src2), (p3, src3), (p4, src4), (p5, src5)],
+    );
+
+    let mut event_loop =
+      make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+    // Before running
+    {
+      let contents = lock!(event_loop.contents);
+      assert!(contents.command_line_message_history().is_empty());
+    }
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      let mut contents = lock!(event_loop.contents);
+      assert_eq!(1, contents.command_line_message_history().occupied_len());
+      assert_eq!(
+        Some("9".to_compact_string()),
+        contents.command_line_message_history_mut().try_pop()
+      );
+
+      let state_rc = event_loop.js_runtime.get_state();
+      let state = state_rc.borrow();
+      let module_map = &state.module_map;
+      assert!(module_map.counter.pending.is_empty());
+      assert!(module_map.pending.is_empty());
+      assert!(module_map.counter.failed.is_empty());
+      assert_eq!(module_map.counter.resolved.len(), 0);
+      info!("module_map.counter:{:?}", module_map.counter);
+      assert_eq!(module_map.counter.evaluated.len(), 1);
+      assert_eq!(
+        module_map.counter.get_evaluated(
+          tp.xdg_config_home
+            .join("rsvim")
+            .join(p1)
+            .as_path()
+            .to_str()
+            .unwrap()
+        ),
+        1
+      );
+    }
+
+    Ok(())
+  }
 }
 
 #[cfg(test)]
