64baf8644782da46904fbc9711e86023fee4d23f
linrongbin16 <linrongbin16@outlook.com>
2025-09-10 17:36:10 +0800

    c

--- rsvim_core/src/js/module/es_module.rs
+++ rsvim_core/src/js/module/es_module.rs
@@ -300,12 +300,6 @@ impl JsFuture for EsModuleFuture {
       self.module.borrow_mut().status = ModuleStatus::Resolving;
     } else {
       self.module.borrow_mut().status = ModuleStatus::Ready;
-      // msg::sync_send_to_master(
-      //   state.master_tx.clone(),
-      //   MasterMessage::TickAgainReq(msg::TickAgainReq::new(
-      //     js::next_future_id(),
-      //   )),
-      // );
     }
   }
 }
