ca0bcc1e70418d5637ba37e9e0037ea05894bb6a
linrongbin16 <linrongbin16@outlook.com>
2025-09-10 10:02:34 +0800

    c

--- rsvim_core/src/evloop.rs
+++ rsvim_core/src/evloop.rs
@@ -729,6 +729,13 @@ impl EventLoop {
         }
       }
 
+      // Extra tick for missing js triggers
+      if self.js_runtime.has_promise_rejections()
+        || self.js_runtime.has_pending_imports()
+      {
+        self.js_runtime.tick_event_loop();
+      }
+
       // Flush logic UI to terminal, i.e. print UI to stdout
       lock!(self.tree).draw(self.canvas.clone());
       self.writer.write(&mut lock!(self.canvas))?;
@@ -767,6 +774,13 @@ impl EventLoop {
           self.process_cancellation_notify().await;
           break;
         }
+      }
+
+      // Extra tick for missing js triggers
+      if self.js_runtime.has_promise_rejections()
+        || self.js_runtime.has_pending_imports()
+      {
+        self.js_runtime.tick_event_loop();
       }
 
       // Flush logic UI to terminal, i.e. print UI to stdout
