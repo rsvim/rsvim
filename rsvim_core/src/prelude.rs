2df10b593d8c4311f49f832513507581c74951d7
linrongbin16 <linrongbin16@outlook.com>
2025-09-12 22:43:17 +0800

    c

--- rsvim_core/src/prelude.rs
+++ rsvim_core/src/prelude.rs
@@ -1,30 +1,26 @@
 //! Prelude.
 
-pub use log::debug;
-pub use log::error;
-pub use log::info;
-pub use log::trace;
-pub use log::warn;
-
-pub use crate::constant::*;
-pub use crate::coord::*;
-pub use crate::results::*;
-
 pub use crate::arc_mutex_ptr;
 pub use crate::arc_ptr;
+pub use crate::constant::*;
+pub use crate::coord::*;
 pub use crate::lock;
 pub use crate::rc_ptr;
 pub use crate::rc_refcell_ptr;
-pub use paste::paste;
-
+pub use crate::results::*;
 pub use foldhash::HashMap;
 pub use foldhash::HashMapExt;
 pub use foldhash::HashSet;
 pub use foldhash::HashSetExt;
 pub use foldhash::fast::RandomState;
-pub use std::collections::BTreeMap;
-pub use std::collections::BTreeSet;
-
 pub use geo::Point;
 pub use geo::Rect;
 pub use geo::{self};
+pub use log::debug;
+pub use log::error;
+pub use log::info;
+pub use log::trace;
+pub use log::warn;
+pub use paste::paste;
+pub use std::collections::BTreeMap;
+pub use std::collections::BTreeSet;
