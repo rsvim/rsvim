95f257a5ddbaf5997ab9e0f4d8e1aa814ce82c3d
linrongbin16 <linrongbin16@outlook.com>
2025-09-12 13:07:11 +0800

    c

--- rsvim_core/src/js/module/module_map_tests.rs
+++ rsvim_core/src/js/module/module_map_tests.rs
@@ -766,7 +766,7 @@ export function echoD(value) {
       let mut contents = lock!(event_loop.contents);
       assert_eq!(1, contents.command_line_message_history().occupied_len());
       assert_eq!(
-        Some("5".to_compact_string()),
+        Some("A:B:C:D:5".to_compact_string()),
         contents.command_line_message_history_mut().try_pop()
       );
     }
