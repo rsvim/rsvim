b1230200e7ef1f5991ca5c358a5ad0c42795f288
linrongbin16 <linrongbin16@outlook.com>
2025-09-13 08:50:49 +0800

    c

--- rsvim_core/src/ui/tree/internal.rs
+++ rsvim_core/src/ui/tree/internal.rs
@@ -4,12 +4,12 @@ pub mod inode;
 pub mod itree;
 pub mod shapes;
 
-pub use inode::*;
-pub use itree::*;
-
 #[cfg(test)]
 mod inode_tests;
 #[cfg(test)]
 mod itree_tests;
 #[cfg(test)]
 mod shapes_tests;
+
+pub use inode::*;
+pub use itree::*;
