ad16aeabd529cf8a8971e1457195aa714cab1c92
linrongbin16 <linrongbin16@outlook.com>
2025-09-13 21:29:12 +0800

    c

--- rsvim_core/src/js/pending.rs
+++ rsvim_core/src/js/pending.rs
@@ -1,18 +1,22 @@
 //! Pending futures.
 
 use crate::js::JsFutureId;
+use crate::msg;
+use crate::msg::MasterMessage;
 use crate::prelude::*;
 use std::fmt::Debug;
 use std::sync::atomic::AtomicI32;
 use std::sync::atomic::Ordering;
+use std::time::Duration;
+use tokio::sync::mpsc::Sender;
 
 fn next_timer_id() -> JsFutureId {
   static VALUE: AtomicI32 = AtomicI32::new(1);
   VALUE.fetch_add(1, Ordering::Relaxed)
 }
 
-#[derive(Default)]
 pub struct PendingFutures {
+  master_tx: Sender<MasterMessage>,
   timer_queue: HashMap<JsFutureId, Box<dyn FnMut() + 'static>>,
 }
 
@@ -32,7 +36,23 @@ impl Debug for PendingFutures {
 }
 
 impl PendingFutures {
-  pub fn timer_req(&mut self, cb: Box<dyn FnMut() + 'static>) {
+  pub fn new(master_tx: Sender<MasterMessage>) -> Self {
+    Self {
+      master_tx,
+      timer_queue: HashMap::new(),
+    }
+  }
+
+  pub fn set_timeout<F>(&mut self, delay: Duration, cb: F) -> JsFutureId
+  where
+    F: FnMut() + 'static,
+  {
     let timer_id = next_timer_id();
+    self.timer_queue.insert(timer_id, Box::new(cb));
+    msg::sync_send_to_master(
+      self.master_tx.clone(),
+      MasterMessage::TimeoutReq(msg::TimeoutReq::new(timer_id, delay)),
+    );
+    timer_id
   }
 }
