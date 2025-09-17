1c806a8c53ee8100f9e1dfaaf2df97ccb945872e
linrongbin16 <linrongbin16@outlook.com>
2025-09-17 15:25:18 +0800

    c

--- rsvim_core/src/js/loader/fs_loader_tests.rs
+++ rsvim_core/src/js/loader/fs_loader_tests.rs
@@ -66,8 +66,9 @@ export function sayHello() {
   assert_eq!(actual_module2.unwrap(), src);
 }
 
-#[test]
-fn file_path2() {
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
+async fn file_path2() {
   test_log_init();
   let tp = TempPathCfg::create();
 
@@ -107,6 +108,7 @@ export function sayHello() {
 
   // Run tests.
   let loader = FsModuleLoader::new();
+  let aloader = AsyncFsModuleLoader {};
 
   let actual = loader.resolve(Some(&base), specifier);
   assert!(actual.is_ok());
@@ -120,9 +122,13 @@ export function sayHello() {
     Path::new(&expect).normalize().unwrap()
   );
 
-  let actual_module = loader.load(&actual);
-  assert!(actual_module.is_ok());
-  assert_eq!(actual_module.unwrap(), src);
+  let actual_module1 = loader.load(&actual);
+  assert!(actual_module1.is_ok());
+  assert_eq!(actual_module1.unwrap(), src);
+
+  let actual_module2 = aloader.load(&actual).await;
+  assert!(actual_module2.is_ok());
+  assert_eq!(actual_module2.unwrap(), src);
 }
 
 #[test]
