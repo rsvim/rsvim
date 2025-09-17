6c64f8e11b903ba6689679431826e9d5c9cca5d1
linrongbin16 <linrongbin16@outlook.com>
2025-09-17 09:34:39 +0800

    c

--- dev.py
+++ dev.py
@@ -89,11 +89,16 @@ def test(name, miri, jobs):
             name = ""
         command = f"cargo +nightly miri nextest run{jobs} -F unicode_lines --no-default-features -p rsvim_core {name}"
     else:
-        log_var = os.getenv("RSVIM_LOG")
-        if isinstance(log_var, str):
-            set_env("RSVIM_LOG", log_var)
+        rsvim_log = os.getenv("RSVIM_LOG")
+        rust_log = os.getenv("RUST_LOG")
+        if isinstance(rsvim_log, str):
+            set_env("RSVIM_LOG", rsvim_log)
         else:
             set_env("RSVIM_LOG", "trace")
+        if isinstance(rust_log, str):
+            set_env("RUST_LOG", rust_log)
+        else:
+            set_env("RUST_LOG", "trace")
         set_sccache()
         set_rustflags()
         if name is None:
