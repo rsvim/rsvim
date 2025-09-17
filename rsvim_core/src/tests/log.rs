8f5831a11746ced9a4f8d27c92d5514a34ae42a3
linrongbin16 <linrongbin16@outlook.com>
2025-09-17 09:48:56 +0800

    c

--- rsvim_core/src/tests/log.rs
+++ rsvim_core/src/tests/log.rs
@@ -29,5 +29,19 @@ pub fn init() {
       .chain(std::io::stdout())
       .apply()
       .unwrap();
+
+    // tracing for oxc_resolver
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+      // .with_file(true)
+      .with_line_number(true)
+      // .with_thread_ids(true)
+      // .with_thread_names(true)
+      .with_level(true)
+      .with_ansi(false)
+      .with_env_filter(tracing_subscriber::EnvFilter::from_env("RSVIM_LOG"))
+      .with_writer(std::io::stderr)
+      .without_time()
+      .finish();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
   });
 }
