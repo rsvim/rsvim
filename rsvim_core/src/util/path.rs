1c4d0d3e0f2067373d74fb03a723affb83004389
linrongbin16 <linrongbin16@outlook.com>
2025-09-17 17:16:29 +0800

    c

--- rsvim_core/src/util/path.rs
+++ rsvim_core/src/util/path.rs
@@ -4,7 +4,6 @@ use std::ffi::OsStr;
 use std::path::Path;
 
 /// Map path to its parent, or remain unchanged if it has no parent.
-///
 pub fn parent_or_remain<S: AsRef<OsStr> + ?Sized>(s: &S) -> &Path {
   Path::new(s).parent().unwrap_or(Path::new(s))
 }
