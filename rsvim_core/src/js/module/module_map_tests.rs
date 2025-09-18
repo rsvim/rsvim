b98976b3009fb3d2180c1669fbb1d54aa7bb0af5
linrongbin16 <linrongbin16@outlook.com>
2025-09-18 11:03:30 +0800

    c

--- rsvim_core/src/js/module/module_map_tests.rs
+++ rsvim_core/src/js/module/module_map_tests.rs
@@ -13,6 +13,8 @@ use std::time::Duration;
 
 #[cfg(test)]
 mod test_static_import {
+  use assert_fs::prelude::PathChild;
+
   use super::*;
 
   #[tokio::test]
@@ -230,7 +232,7 @@ mod test_static_import {
 
   #[tokio::test]
   #[cfg_attr(miri, ignore)]
-  async fn no_side_effect5() -> IoResult<()> {
+  async fn no_side_effect4() -> IoResult<()> {
     test_log_init();
 
     let terminal_cols = 10_u16;
@@ -410,6 +412,88 @@ export function echoD(value) {
       assert_eq!(
         Some("9".to_compact_string()),
         contents.command_line_message_history_mut().try_pop()
+      );
+    }
+
+    Ok(())
+  }
+
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn import_meta_url1() -> IoResult<()> {
+    test_log_init();
+
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(1000))];
+    let tp = TempPathCfg::create();
+
+    let p1 = Path::new("rsvim.js");
+    let src1: &str = r#"
+import { echo } from './utils/a.js';
+
+const url = import.meta.url;
+echo(url);
+    "#;
+
+    let p2 = Path::new("utils/a.js");
+    let src2: &str = r#"
+export function echo(value) {
+  const url = import.meta.url;
+  Rsvim.cmd.echo(`${url}:${value}`);
+}
+    "#;
+
+    // Prepare $RSVIM_CONFIG
+    // |- rsvim.js
+    // |- utils/
+    //    |- a.js
+    //
+    make_configs(&tp, vec![(p1, src1), (p2, src2)]);
+
+    let mut event_loop =
+      make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+    // Before running
+    {
+      let contents = lock!(event_loop.contents);
+      assert!(contents.command_line_message_history().is_empty());
+    }
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      let state_rc = event_loop.js_runtime.get_state();
+      let state = state_rc.borrow();
+      info!("module_map:{:#?}", state.module_map);
+
+      let mut contents = lock!(event_loop.contents);
+      assert_eq!(1, contents.command_line_message_history().occupied_len());
+      let actual = contents.command_line_message_history_mut().try_pop();
+      assert!(actual.is_some());
+      let actual = actual.unwrap();
+      assert!(
+        actual.contains(
+          &tp
+            .xdg_config_home
+            .child("rsvim")
+            .child("rsvim.js")
+            .to_string_lossy()
+            .to_string()
+        ) && actual.contains(
+          &tp
+            .xdg_config_home
+            .child("rsvim")
+            .child("utils")
+            .child("a.js")
+            .to_string_lossy()
+            .to_string()
+        )
       );
     }
 
