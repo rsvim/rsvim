bda6ab40ac21655101f07b5a63f28e4b4c772bed
linrongbin16 <linrongbin16@outlook.com>
2025-09-19 22:11:59 +0800

    c

--- rsvim_core/src/js/module/module_map_tests.rs
+++ rsvim_core/src/js/module/module_map_tests.rs
@@ -1388,3 +1388,71 @@ export default {};
     Ok(())
   }
 }
+
+#[cfg(test)]
+mod test_require {
+  use super::*;
+
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn test1() -> IoResult<()> {
+    test_log_init();
+
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(100))];
+
+    let p1 = Path::new("rsvim.js");
+    let src1: &str = r#"
+    const util = require("./util.js");
+    util.echo(1);
+    "#;
+
+    let p2 = Path::new("util.js");
+    let src2: &str = r#"
+    export function echo(value) {
+        Rsvim.cmd.echo(value);
+    }
+    "#;
+
+    // Prepare $RSVIM_CONFIG:
+    // - rsvim.js
+    // - util.js
+    let (_tp, path_cfg) = make_configs(vec![(p1, src1), (p2, src2)]);
+
+    let mut event_loop = make_event_loop(
+      terminal_cols,
+      terminal_rows,
+      CliOptions::empty(),
+      path_cfg,
+    );
+
+    // Before running
+    {
+      let contents = lock!(event_loop.contents);
+      assert!(contents.command_line_message_history().is_empty());
+    }
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      let state_rc = event_loop.js_runtime.get_state();
+      let state = state_rc.borrow();
+      info!("module_map:{:#?}", state.module_map);
+
+      let mut contents = lock!(event_loop.contents);
+      assert_eq!(1, contents.command_line_message_history().occupied_len());
+      assert_eq!(
+        Some("1".to_compact_string()),
+        contents.command_line_message_history_mut().try_pop()
+      );
+    }
+
+    Ok(())
+  }
+}
