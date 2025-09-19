4c86d43d4dcfe38eddb324039c50d87dd4ca0902
linrongbin16 <linrongbin16@outlook.com>
2025-09-19 22:40:43 +0800

    c

--- rsvim_core/src/js/module/module_map_tests.rs
+++ rsvim_core/src/js/module/module_map_tests.rs
@@ -633,6 +633,104 @@ export function echoA(value) {
 
     Ok(())
   }
+
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn home_dir_failed2() -> IoResult<()> {
+    test_log_init();
+
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(100))];
+
+    let p1 = Path::new(".rsvim.js");
+    let src1: &str = r#"
+  import utils from "./.rsvim/utils";
+  utils.echo(utils.add(1,2));
+    "#;
+
+    let p2 = Path::new(".rsvim/utils/lib/echo.js");
+    let src2: &str = r#"
+    export function echo(value) {
+        Rsvim.cmd.echo(value);
+    }
+    "#;
+
+    let p3 = Path::new(".rsvim/utils/lib/calc.js");
+    let src3: &str = r#"
+    export function add(a, b) {
+        return a+b;
+    }
+    "#;
+
+    let p4 = Path::new(".rsvim/utils/lib/index.js");
+    let src4: &str = r#"
+    import {add} from "./calc.js";
+    import {echo} from "./echo.js";
+
+    export default {add, echo};
+    "#;
+
+    let p5 = Path::new(".rsvim/utils/package.json");
+    let src5: &str = r#"
+{
+  "exports": "./lib/index.js"
+}
+    "#;
+
+    // Prepare $RSVIM_CONFIG:
+    // |- rsvim.js
+    // |- node_modules/
+    //    |- utils/
+    //       |- package.json
+    //       |- lib/
+    //          |- index.js
+    //          |- echo.js
+    //          |- calc.js
+    //
+    let (_tp, path_cfg) = make_home_configs(vec![
+      (p1, src1),
+      (p2, src2),
+      (p3, src3),
+      (p4, src4),
+      (p5, src5),
+    ]);
+
+    let mut event_loop = make_event_loop(
+      terminal_cols,
+      terminal_rows,
+      CliOptions::empty(),
+      path_cfg,
+    );
+
+    // Before running
+    {
+      let contents = lock!(event_loop.contents);
+      assert!(contents.command_line_message_history().is_empty());
+    }
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      let state_rc = event_loop.js_runtime.get_state();
+      let state = state_rc.borrow();
+      info!("module_map:{:#?}", state.module_map);
+
+      let mut contents = lock!(event_loop.contents);
+      assert_eq!(1, contents.command_line_message_history().occupied_len());
+      assert_eq!(
+        Some("3".to_compact_string()),
+        contents.command_line_message_history_mut().try_pop()
+      );
+    }
+
+    Ok(())
+  }
 }
 
 #[cfg(test)]
