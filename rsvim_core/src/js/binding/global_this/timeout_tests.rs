95ea4c0a0002275d4e3e1e2e94068ede56d7be79
linrongbin16 <linrongbin16@outlook.com>
2025-09-21 10:21:25 +0800

    c

--- rsvim_core/src/js/binding/global_this/timeout_tests.rs
+++ rsvim_core/src/js/binding/global_this/timeout_tests.rs
@@ -258,3 +258,57 @@ async fn test_timeout4() -> IoResult<()> {
 
   Ok(())
 }
+
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
+async fn test_timeout5() -> IoResult<()> {
+  test_log_init();
+
+  let terminal_cols = 10_u16;
+  let terminal_rows = 10_u16;
+
+  let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(50))];
+  let src: &str = r#"
+  // Set timeout to update global options.
+  const timerId = setTimeout(() => {
+    Rsvim.opt.wrap = false;
+    Rsvim.opt.lineBreak = true;
+  }); // by default `delay` is 1
+"#;
+
+  // Prepare $RSVIM_CONFIG/rsvim.js
+  let (_tp, path_cfg) = make_configs(vec![(Path::new("rsvim.js"), src)]);
+
+  let mut event_loop = make_event_loop(
+    terminal_cols,
+    terminal_rows,
+    CliOptions::empty(),
+    path_cfg,
+  );
+
+  // Before evaluating javascript configs
+  {
+    use crate::defaults;
+
+    let tree = lock!(event_loop.tree);
+    let global_local_options = tree.global_local_options();
+    assert_eq!(global_local_options.wrap(), defaults::win::WRAP);
+    assert_eq!(global_local_options.line_break(), defaults::win::LINE_BREAK);
+  }
+
+  event_loop.initialize()?;
+  event_loop
+    .run_with_mock_events(MockEventReader::new(mocked_events))
+    .await?;
+  event_loop.shutdown()?;
+
+  // After timeout, it changes to new value
+  {
+    let tree = lock!(event_loop.tree);
+    let global_local_options = tree.global_local_options();
+    assert!(!global_local_options.wrap());
+    assert!(global_local_options.line_break());
+  }
+
+  Ok(())
+}
