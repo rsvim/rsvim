5afe301a84aeeeffe5b180cf0a1a7804aeb17e7f
linrongbin16 <linrongbin16@outlook.com>
2025-09-21 10:09:56 +0800

    c

--- rsvim_core/src/js/runtime/00__web.d.ts
+++ rsvim_core/src/js/runtime/00__web.d.ts
@@ -1,4 +1,6 @@
 export interface GlobalThis {
+    clearInterval(id: number): void;
     clearTimeout(id: number): void;
-    setTimeout(callback: (...args: any[]) => void, delay: number, ...args: any[]): number;
+    setInterval(callback: (...args: any[]) => void, delay?: number, ...args: any[]): number;
+    setTimeout(callback: (...args: any[]) => void, delay?: number, ...args: any[]): number;
 }
--- rsvim_core/src/js/runtime/00__web.js
+++ rsvim_core/src/js/runtime/00__web.js
@@ -2,28 +2,64 @@
     const TIMEOUT_MAX = Math.pow(2, 31) - 1;
     let nextTimerId = 1;
     const activeTimers = new Map();
-    function clearTimeout(id) {
+    function clearInterval(id) {
         if (!Number.isInteger(id)) {
-            throw new Error(`"clearTimeout" id parameter must be an integer value, but found ${id} (${typeof id})`);
+            throw new TypeError(`"clearInterval" id must be an integer, but found ${typeof id}`);
         }
         if (activeTimers.has(id)) {
-            __InternalRsvimGlobalObject.global_clear_timeout(activeTimers.get(id));
+            __InternalRsvimGlobalObject.global_clear_timer(activeTimers.get(id));
+            activeTimers.delete(id);
+        }
+    }
+    function clearTimeout(id) {
+        if (!Number.isInteger(id)) {
+            throw new TypeError(`"clearTimeout" id must be an integer, but found ${typeof id}`);
+        }
+        if (activeTimers.has(id)) {
+            __InternalRsvimGlobalObject.global_clear_timer(activeTimers.get(id));
             activeTimers.delete(id);
         }
     }
     function setTimeout(callback, delay, ...args) {
+        if (delay === undefined || delay === null) {
+            delay = 1;
+        }
+        else if (typeof delay !== "number") {
+            throw new TypeError(`"setTimeout" delay must be a number, but found ${typeof delay}`);
+        }
         delay *= 1;
         if (!(delay >= 1 && delay <= TIMEOUT_MAX)) {
             delay = 1;
         }
         if (typeof callback !== "function") {
-            throw new Error(`"setTimeout" callback parameter must be a function, but found ${callback} (${typeof callback})`);
+            throw new Error(`"setTimeout" callback must be a function, but found ${typeof callback}`);
         }
         const id = nextTimerId++;
-        const timer = __InternalRsvimGlobalObject.global_set_timeout(() => {
+        const timer = __InternalRsvimGlobalObject.global_create_timer(() => {
             callback(...args);
             activeTimers.delete(id);
-        }, delay);
+        }, delay, false);
+        activeTimers.set(id, timer);
+        return id;
+    }
+    function setInterval(callback, delay, ...args) {
+        if (delay === undefined || delay === null) {
+            delay = 1;
+        }
+        else if (typeof delay !== "number") {
+            throw new TypeError(`"setInterval" delay must be a number, but found ${typeof delay}`);
+        }
+        delay *= 1;
+        if (!(delay >= 1 && delay <= TIMEOUT_MAX)) {
+            delay = 1;
+        }
+        if (typeof callback !== "function") {
+            throw new Error(`"setTimeout" callback must be a function, but found ${typeof callback}`);
+        }
+        const id = nextTimerId++;
+        const timer = __InternalRsvimGlobalObject.global_create_timer(() => {
+            callback(...args);
+        }, delay, true);
         activeTimers.set(id, timer);
         return id;
     }
