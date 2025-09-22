e8e27fedc4d742da2307a4d587709dc08f95e6cb
linrongbin16 <linrongbin16@outlook.com>
2025-09-22 16:56:20 +0800

    c

---
+++ rsvim_core/src/js/binding/global_this/microtask.rs
@@ -0,0 +1,39 @@
+//! Microtask Queue APIs.
+
+use crate::js;
+use crate::js::JsFuture;
+use crate::js::JsRuntime;
+use crate::js::pending;
+use crate::prelude::*;
+use std::rc::Rc;
+
+fn report_error(
+  scope: &mut v8::HandleScope,
+  args: v8::FunctionCallbackArguments,
+  _: v8::ReturnValue,
+) {
+  let exception = v8::Global::new(scope, args.get(0));
+  let state_rc = JsRuntime::state(scope);
+  let mut state = state_rc.borrow_mut();
+
+  state.exceptions.capture_exception(exception);
+  drop(state);
+
+  if let Some(error) = check_exceptions(scope) {
+    report_and_exit(error);
+  }
+}
+
+// This method queues a microtask to invoke callback.
+fn queue_microtask(
+  scope: &mut v8::HandleScope,
+  args: v8::FunctionCallbackArguments,
+  _: v8::ReturnValue,
+) {
+  let callback = v8::Local::<v8::Function>::try_from(args.get(0)).unwrap();
+  let state_rc = JsRuntime::state(scope);
+  let state = state_rc.borrow();
+  let ctx = state.context.open(scope);
+
+  ctx.get_microtask_queue().enqueue_microtask(scope, callback);
+}
