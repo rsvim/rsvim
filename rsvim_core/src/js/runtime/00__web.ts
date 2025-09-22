533dd152e813804b940b4646f272fdb1abdb162e
linrongbin16 <linrongbin16@outlook.com>
2025-09-22 17:04:42 +0800

    c

--- rsvim_core/src/js/runtime/00__web.ts
+++ rsvim_core/src/js/runtime/00__web.ts
@@ -35,7 +35,7 @@ export interface GlobalThis {
   queueMicrotask(callback: () => void): void;
 
   /**
-   * Dispatch an uncaught exception. Similar to `setTimeout(() => {throw error;}, 0);`.
+   * Dispatch an uncaught exception. Similar to synchronous version of `setTimeout(() => {throw error;}, 0);`.
    */
   reportError(error: Error): void;
 
