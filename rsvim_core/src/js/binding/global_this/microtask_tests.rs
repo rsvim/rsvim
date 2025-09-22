a703c63016fe539375df1d436b93659552daba2d
linrongbin16 <linrongbin16@outlook.com>
2025-09-23 04:33:16 +0800

    c

--- rsvim_core/src/js/binding/global_this/microtask_tests.rs
+++ rsvim_core/src/js/binding/global_this/microtask_tests.rs
@@ -49,3 +49,50 @@ async fn test_queue_microtask1() -> IoResult<()> {
 
   Ok(())
 }
+
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
+async fn test_queue_microtask2() -> IoResult<()> {
+  test_log_init();
+
+  let terminal_cols = 10_u16;
+  let terminal_rows = 10_u16;
+
+  let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(50))];
+  let src: &str = r#"
+  setTimeout(() => {
+    queueMicrotask(() => {
+      Rsvim.cmd.echo(2);
+    })
+  }, 10);
+"#;
+
+  // Prepare $RSVIM_CONFIG/rsvim.js
+  let (_tp, path_cfg) = make_configs(vec![(Path::new("rsvim.js"), src)]);
+
+  let mut event_loop = make_event_loop(
+    terminal_cols,
+    terminal_rows,
+    CliOptions::empty(),
+    path_cfg,
+  );
+
+  event_loop.initialize()?;
+  event_loop
+    .run_with_mock_events(MockEventReader::new(mocked_events))
+    .await?;
+  event_loop.shutdown()?;
+
+  // After
+  {
+    let mut contents = lock!(event_loop.contents);
+    let n = contents.command_line_message_history().occupied_len();
+    assert!(n == 1);
+    let actual = contents.command_line_message_history_mut().try_pop();
+    assert!(actual.is_some());
+    let actual = actual.unwrap();
+    assert_eq!(actual, "2");
+  }
+
+  Ok(())
+}
