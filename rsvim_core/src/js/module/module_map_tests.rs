bf3aa4f16a6ca5c9b2215297ae234553a9723b5a
linrongbin16 <linrongbin16@outlook.com>
2025-09-23 09:59:45 +0800

    c

--- rsvim_core/src/js/module/module_map_tests.rs
+++ rsvim_core/src/js/module/module_map_tests.rs
@@ -649,6 +649,84 @@ export function echoD(value) {
 
   #[tokio::test]
   #[cfg_attr(miri, ignore)]
+  async fn home_dir4() -> IoResult<()> {
+    test_log_init();
+
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(100))];
+
+    let p1 = Path::new(".rsvim.js");
+    let src1: &str = r#"
+  import a from "a";
+  import b from "b";
+  a.echo(1);
+  b.echo(2);
+    "#;
+
+    let p2 = Path::new(".rsvim/node_modules/a/index.js");
+    let src2: &str = r#"
+    import echo from "util";
+    export default { echo };
+    "#;
+
+    let p3 = Path::new(".rsvim/b/index.js");
+    let src3: &str = r#"
+    import echo from "util";
+    export default { echo };
+    "#;
+
+    let p4 = Path::new(".rsvim/node_modules/util/index.js");
+    let src4: &str = r#"
+    export function echo(value) {
+        Rsvim.cmd.echo(value);
+    }
+    "#;
+
+    // Prepare $HOME:
+    // |- .rsvim.js
+    // |- .rsvim/
+    //    |- a/index.js
+    //    |- util/index.js
+    //    |- node_modules/
+    //       |- b/index.js
+    let (_tp, path_cfg) =
+      make_home_configs(vec![(p1, src1), (p2, src2), (p3, src3), (p4, src4)]);
+
+    let mut event_loop = make_event_loop(
+      terminal_cols,
+      terminal_rows,
+      CliOptions::empty(),
+      path_cfg,
+    );
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After
+    {
+      let state_rc = event_loop.js_runtime.get_state();
+      let state = state_rc.borrow();
+      info!("module_map:{:#?}", state.module_map);
+
+      let mut contents = lock!(event_loop.contents);
+      assert_eq!(2, contents.command_line_message_history().occupied_len());
+      for i in 0..2 {
+        let actual = contents.command_line_message_history_mut().try_pop();
+        assert!(actual.is_some());
+        let actual = actual.unwrap();
+        assert_eq!(actual, (i + 1).to_compact_string());
+      }
+    }
+
+    Ok(())
+  }
+
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
   async fn home_dir_failed1() -> IoResult<()> {
     test_log_init();
 
