8171a9cc9dd7645cf929fbe934f2369c89f4cb34
linrongbin16 <linrongbin16@outlook.com>
2025-09-25 13:32:17 +0800

    c

--- rsvim_core/src/buf/opt_tests.rs
+++ rsvim_core/src/buf/opt_tests.rs
@@ -4,6 +4,6 @@ use crate::defaults;
 #[test]
 fn default1() {
   let opt1 = BufferOptionsBuilder::default().build().unwrap();
-  assert_eq!(opt1.tab_stop(), defaults::buf::TAB_STOP);
-  assert_eq!(opt1.file_encoding(), defaults::buf::FILE_ENCODING);
+  assert_eq!(opt1.tab_stop(), TAB_STOP);
+  assert_eq!(opt1.file_encoding(), FILE_ENCODING);
 }
