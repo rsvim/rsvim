8ca010cb29e590096c777e55fd1408f51962ad1d
linrongbin16 <linrongbin16@outlook.com>
2025-09-25 17:05:52 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -187,7 +187,7 @@ export interface RsvimCmd {
   /**
    * Create a user EX command.
    *
-   * @param {string} name - The command name that is going to create. Only letters (both lowercase `a-z` and uppercase `A-Z`), digits (`0-9`) and underscore (`_`) are allowed to be used as a command name. And a command name must begin with a letter or underscore, digit is not allowed.
+   * @param {string} name - The command name that is going to create. Only letters (both lowercase `a-z` and uppercase `A-Z`), digits (`0-9`) and underscore (`_`) are allowed to be used as a command name. And a command name must begin with either a letter or underscore, digit is not allowed.
    *
    * @throws Throws {@link !TypeError} if name is not a `null` or `undefined` or no parameter provided.
    *
