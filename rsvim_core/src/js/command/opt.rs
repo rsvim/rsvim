f81d2fc84b924f4a134c2e7372b947cab9db78d7
linrongbin16 <linrongbin16@outlook.com>
2025-09-25 13:59:14 +0800

    c

--- rsvim_core/src/js/command/opt.rs
+++ rsvim_core/src/js/command/opt.rs
@@ -11,3 +11,24 @@ pub struct CommandOptions {
   #[builder(default = FORCE_VALUE)]
   pub force: bool,
 }
+
+impl CommandOptions {
+  fn from<'a>(
+    scope: &mut v8::HandleScope,
+    value: v8::Local<'a, v8::Object>,
+  ) -> Self {
+    let mut builder = CommandOptionsBuilder::default();
+
+    // force
+    let force_name = v8::String::new(scope, FORCE_NAME).unwrap();
+    match value.get(scope, force_name.into()) {
+      Some(force_value) => {
+        let force = force_value.to_boolean(scope).boolean_value(scope);
+        builder.force(force);
+      }
+      None => { /* do nothing */ }
+    }
+
+    builder.build().unwrap()
+  }
+}
