388b6d70d8d84a6c7068b234397d6f01ee7b9cba
linrongbin16 <linrongbin16@outlook.com>
2025-09-25 17:04:23 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -187,9 +187,9 @@ export interface RsvimCmd {
   /**
    * Create a user EX command.
    *
-   * @param {string} name - The command name that is going to create.
+   * @param {string} name - The command name that is going to create. Only letters (both lowercase `a-z` and uppercase `A-Z`), digits (`0-9`) and underscore (`_`) are allowed to be used as a command name. And a command name must begin with a letter or underscore, digit is not allowed.
    *
-   * @throws Throws {@link !TypeError} if the parameter is `null` or `undefined` or no parameter provided.
+   * @throws Throws {@link !TypeError} if name is not a `null` or `undefined` or no parameter provided.
    *
    * @example
    * ```javascript
@@ -220,6 +220,15 @@ export interface RsvimCmd {
 }
 
 class RsvimCmdImpl implements RsvimCmd {
+  create(
+    name: string,
+    callback: CreateCommandCallback,
+    attr?: CreateCommandAttributes,
+    opts?: CreateCommandOptions,
+  ): undefined | CreateCommandCallback {
+    return undefined;
+  }
+
   echo(message: any): void {
     if (message === undefined || message === null) {
       throw new TypeError(
