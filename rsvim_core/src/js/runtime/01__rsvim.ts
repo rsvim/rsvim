e7523520619daea5a129c73353367a9c9e971e93
linrongbin16 <linrongbin16@outlook.com>
2025-09-25 17:09:00 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -192,6 +192,7 @@ export interface RsvimCmd {
    * :::
    *
    * @param {string} name - The command name that is going to create. Only letters (both lowercase `a-z` and uppercase `A-Z`), digits (`0-9`) and underscore (`_`) are allowed to be used as a command name. And a command name must begin with either a letter or underscore, digit is not allowed.
+   * @param {CreateCommandCallback} callback - The backend function that implements the command logic.
    *
    * @throws Throws {@link !TypeError} if name is not a `null` or `undefined` or no parameter provided.
    *
