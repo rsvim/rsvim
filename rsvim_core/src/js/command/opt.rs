314954677fec29a3357e0e5382f7ab12d30350f5
linrongbin16 <linrongbin16@outlook.com>
2025-09-26 13:37:54 +0800

    c

--- rsvim_core/src/js/command/opt.rs
+++ rsvim_core/src/js/command/opt.rs
@@ -66,7 +66,7 @@ impl CommandOptions {
     let buffer_field = v8::String::new(scope, "buffer").unwrap();
     match self.buffer {
       Some(buf_id) => {
-        let buffer_value = v8::Integer::new(scope, self.buffer.unwrap());
+        let buffer_value = v8::Integer::new(scope, buf_id);
         obj.set(scope, buffer_field.into(), buffer_value.into());
       }
       None => {
