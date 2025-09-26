1349678153987705ea0d644840e645de7e30ce9b
linrongbin16 <linrongbin16@outlook.com>
2025-09-26 17:14:58 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -735,7 +735,7 @@ export interface RsvimRt {
    * :::
    *
    * @param {exitCode?} exitCode - The editor process exit with this exit code. This parameter can be omitted,
-   * by default it uses `0` to indicate no error happens.
+   * by default uses `0` to indicate no error happens.
    *
    * @throws Throws {@link !TypeError} if `exitCode` is neither an integer nor `undefined`.
    *
@@ -753,6 +753,9 @@ export interface RsvimRt {
 
 class RsvimRtImpl implements RsvimRt {
   exit(exitCode?: number): void {
+    if (exitCode === undefined) {
+      exitCode = 0;
+    }
     checkIsOptionalInteger(exitCode, `"Rsvim.rt.exit" exit code`);
     // @ts-ignore Ignore warning
     __InternalRsvimGlobalObject.rt_exit(exitCode);
