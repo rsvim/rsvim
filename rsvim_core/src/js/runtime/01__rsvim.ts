deaf92368af1268b46325f7d9d1b4e3198196762
linrongbin16 <linrongbin16@outlook.com>
2025-09-26 17:24:47 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -253,7 +253,7 @@ export interface RsvimCmd {
   echo(message: any): void;
 
   /**
-   * List all registered ex commands.
+   * List all registered ex commands. Note: The builtin `js` command is not listed here.
    */
 }
 
