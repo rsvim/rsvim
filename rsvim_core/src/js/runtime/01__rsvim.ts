85bd3216de71963ced1f8dff5deafa36879496b3
linrongbin16 <linrongbin16@outlook.com>
2025-09-26 14:16:04 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -174,7 +174,7 @@ export interface RsvimCmd {
    *
    * @param {string} name - The command name that is going to create. Only letters (`a-z` and `A-Z`), digits (`0-9`), underscore (`_`) and exclamation (`!`) are allowed in a command name. Command name must not begin with a digit, nor begin with a exclamation, unless it only has 1 char and is `!`.
    * @param {RsvimCmd.CommandCallback} callback - The backend logic that implements the command. It accepts an `ctx` parameter that contains all the information when user is running it. See {@link RsvimCmd.CommandCallback}.
-   * @param {RsvimCmd.CommandAttributes} attributes - The command attributes, it controls the command behavior, such as `bang`, `nargs`, etc. This parameter can be omitted, it will use the default attributes, see {@link RsvimCmd.CommandAttributes}.
+   * @param {RsvimCmd.CommandAttributes} attributes - The command attributes, it controls the command behavior, such as whether takes a `!` modifier (bang), how many arguments it takes (nargs), etc. This parameter can be omitted, it will use the default attributes, see {@link RsvimCmd.CommandAttributes}.
    * @param {RsvimCmd.CommandOptions} opts - The command options, it controls how a command is created, such as `force`, etc. This parameter can be omitted, it will use the default options, see {@link RsvimCmd.CommandOptions}.
    * @returns {undefined | {attr:RsvimCmd.CommandAttributes,opts:RsvimCmd.CommandOptions,callback:RsvimCmd.CommandCallback}} It returns `undefined` is the command is newly created, or an object with `attr`, `opts` and `callback`fields that was defined the same command name previously.
    *
