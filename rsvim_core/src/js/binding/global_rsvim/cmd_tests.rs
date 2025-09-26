f977aa7d1d2f4739e5b660b7e5b868cc6d063389
linrongbin16 <linrongbin16@outlook.com>
2025-09-26 23:08:44 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/cmd_tests.rs
+++ rsvim_core/src/js/binding/global_rsvim/cmd_tests.rs
@@ -210,3 +210,54 @@ async fn test_echo4() -> IoResult<()> {
 
   Ok(())
 }
+
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
+async fn test_create1() -> IoResult<()> {
+  test_log_init();
+
+  let terminal_cols = 10_u16;
+  let terminal_rows = 10_u16;
+  let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(50))];
+
+  let src: &str = r#"
+function write() {
+  try {
+    const bufId = Rsvim.cmd.current();
+    const n = Rsvim.buf.writeSync(bufId);
+    Rsvim.cmd.echo(`Buffer ${bufId} saved, written ${n} bytes`);
+  } catch (e) {
+    Rsvim.cmd.echo(`Failed to save buffer ${bufId}: ${e}`);
+  }
+}
+    "#;
+
+  // Prepare $RSVIM_CONFIG/rsvim.js
+  let _tp = make_configs(vec![(Path::new("rsvim.js"), src)]);
+
+  let mut event_loop =
+    make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+  // Before running
+  {
+    let contents = lock!(event_loop.contents);
+    let actual = contents.command_line_message().rope().to_string();
+    assert!(actual.is_empty());
+  }
+
+  event_loop.initialize()?;
+  event_loop
+    .run_with_mock_events(MockEventReader::new(mocked_events))
+    .await?;
+  event_loop.shutdown()?;
+
+  // After running
+  {
+    let contents = lock!(event_loop.contents);
+    let actual = contents.command_line_message().rope().to_string();
+    let actual = actual.trim();
+    assert_eq!(actual, "true");
+  }
+
+  Ok(())
+}
