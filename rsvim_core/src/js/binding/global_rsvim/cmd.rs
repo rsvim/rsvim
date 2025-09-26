5c092e7c06474d0f2c4f41a512ca77f15733cc9d
linrongbin16 <linrongbin16@outlook.com>
2025-09-26 11:27:44 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/cmd.rs
+++ rsvim_core/src/js/binding/global_rsvim/cmd.rs
@@ -62,3 +62,25 @@ pub fn create(
   let mut commands = lock!(state.commands);
   commands.insert(name.to_compact_string(), (callback, attrs, opts));
 }
+
+/// `Rsvim.cmd.list` API.
+pub fn list(
+  scope: &mut v8::HandleScope,
+  args: v8::FunctionCallbackArguments,
+  mut _rv: v8::ReturnValue,
+) {
+  debug_assert!(args.length() == 4);
+  let name = args.get(0).to_rust_string_lossy(scope);
+  let callback = v8::Local::<v8::Function>::try_from(args.get(1)).unwrap();
+  let callback = Rc::new(v8::Global::new(scope, callback));
+  let attrs = args.get(2).to_object(scope).unwrap();
+  let attrs = CommandAttributes::from_object(scope, attrs);
+  let opts = args.get(3).to_object(scope).unwrap();
+  let opts = CommandOptions::from_object(scope, opts);
+  trace!("Rsvim.cmd.create:{:?}", name);
+
+  let state_rc = JsRuntime::state(scope);
+  let state = state_rc.borrow_mut();
+  let mut commands = lock!(state.commands);
+  commands.insert(name.to_compact_string(), (callback, attrs, opts));
+}
