db8f851755a82b9af0a6ba2b0dd616720dc11733
linrongbin16 <linrongbin16@outlook.com>
2025-09-26 14:08:58 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -169,7 +169,7 @@ export interface RsvimCmd {
    * Create a ex command with a callback function.
    *
    * :::warning
-   * The only builtin command from Rsvim is the `js` command, which cannot be override.
+   * The only builtin command from Rsvim is the `js` command, it cannot be override.
    * :::
    *
    * @param {string} name - The command name that is going to create. Only letters (both lowercase `a-z` and uppercase `A-Z`), digits (`0-9`) and underscore (`_`) are allowed to be used as a command name. And a command name must begin with either a letter or underscore, digit is not allowed.
