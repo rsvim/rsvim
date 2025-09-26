8e3ec4f8d9f355985596a5b0252380feee7e53b2
linrongbin16 <linrongbin16@outlook.com>
2025-09-26 14:09:29 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -172,7 +172,7 @@ export interface RsvimCmd {
    * The only builtin command is the `js` command, it cannot be override.
    * :::
    *
-   * @param {string} name - The command name that is going to create. Only letters (both lowercase `a-z` and uppercase `A-Z`), digits (`0-9`) and underscore (`_`) are allowed to be used as a command name. And a command name must begin with either a letter or underscore, digit is not allowed.
+   * @param {string} name - The command name that is going to create. Only letters (`a-z` and `A-Z`), digits (`0-9`) and underscore (`_`) are allowed to be used as a command name. And a command name must begin with either a letter or underscore, digit is not allowed.
    * @param {RsvimCmd.CommandCallback} callback - The backend function that implements the command logic. It accepts an `ctx` parameter that contains all the information when user is running the command, such as `bang`, arguments, buffer ID, etc. See {@link RsvimCmd.CommandCallback}.
    * @param {RsvimCmd.CommandAttributes} attr - The command attributes, it controls the command related behavior, such as `bang`, `nargs`, etc. This parameter can be omitted, it will use the default attributes, see {@link RsvimCmd.CommandAttributes}.
    * @param {RsvimCmd.CommandOptions} opts - The command options, it controls how a command is created, such as `force`, etc. This parameter can be omitted, it will use the default options, see {@link RsvimCmd.CommandOptions}.
