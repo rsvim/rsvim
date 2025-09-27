c5c3ebb84496d7708eea9de28e9ab384fccf8cbc
linrongbin16 <linrongbin16@outlook.com>
2025-09-27 14:55:10 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/cmd_tests.rs
+++ rsvim_core/src/js/binding/global_rsvim/cmd_tests.rs
@@ -612,3 +612,51 @@ Rsvim.cmd.echo(`Previous command:${prev}`);
 
   Ok(())
 }
+
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
+async fn test_create_failed8() -> IoResult<()> {
+  test_log_init();
+
+  let terminal_cols = 10_u16;
+  let terminal_rows = 10_u16;
+  let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(50))];
+
+  let src: &str = r#"
+const prev = Rsvim.cmd.create("abc", () => {}, "a");
+Rsvim.cmd.echo(`Previous command:${prev}`);
+    "#;
+
+  // Prepare $RSVIM_CONFIG/rsvim.js
+  let _tp = make_configs(vec![(Path::new("rsvim.js"), src)]);
+
+  let mut event_loop =
+    make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+  event_loop.initialize()?;
+  event_loop
+    .run_with_mock_events(MockEventReader::new(mocked_events))
+    .await?;
+  event_loop.shutdown()?;
+
+  // After running
+  {
+    let mut contents = lock!(event_loop.contents);
+    let n = contents.command_line_message_history().occupied_len();
+    assert_eq!(n, 1);
+    let actual = contents.command_line_message_history_mut().try_pop();
+    info!("actual:{:?}", actual);
+    assert!(actual.is_some());
+    let actual = actual.unwrap();
+    assert!(actual.contains(
+      r####""Rsvim.cmd.create" attributes must be an object, but found"####
+    ));
+
+    let state_rc = event_loop.js_runtime.get_state();
+    let state = state_rc.borrow();
+    let commands = lock!(state.commands);
+    assert!(commands.is_empty());
+  }
+
+  Ok(())
+}
