a9372427dfba350f930411730c740866659f1c26
linrongbin16 <linrongbin16@outlook.com>
2025-09-27 14:52:24 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/cmd_tests.rs
+++ rsvim_core/src/js/binding/global_rsvim/cmd_tests.rs
@@ -575,7 +575,7 @@ async fn test_create_failed7() -> IoResult<()> {
   let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(50))];
 
   let src: &str = r#"
-const prev = Rsvim.cmd.create("abc", () => {}, {nargs:"a"});
+const prev = Rsvim.cmd.create("abc", () => {}, "a");
 Rsvim.cmd.echo(`Previous command:${prev}`);
     "#;
 
