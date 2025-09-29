9cad91ebef724d7c0fc93b4ef9902cb31223fb4a
linrongbin16 <linrongbin16@outlook.com>
2025-09-29 12:53:18 +0800

    c

--- rsvim_core/bench/snapshot.rs
+++ rsvim_core/bench/snapshot.rs
@@ -30,35 +30,51 @@ fn create_snapshot(tp: &TempConfigDir) -> Vec<u8> {
   snapshot
 }
 
-async fn with_snapshot(tp: &TempConfigDir, snapshot: Vec<u8>) -> IoResult<()> {
-  // Create js runtime with snapshot.
-  let mut event_loop = {
-    let cli_opts = CliOptions::empty();
-    let (
-      startup_moment,
-      startup_unix_epoch,
-      canvas,
-      tree,
-      state_machine,
-      buffers,
-      contents,
-      commands,
-      cancellation_token,
-      detached_tracker,
-      blocked_tracker,
-      exit_code,
-      (master_tx, master_rx),
-      (jsrt_forwarder_tx, jsrt_forwarder_rx),
-      (jsrt_tx, jsrt_rx),
-    ) = EventLoop::_internal_new(10, 10).unwrap();
+fn create_event_loop(
+  tp: &TempConfigDir,
+  snapshot: Option<Vec<u8>>,
+) -> EventLoop {
+  let cli_opts = CliOptions::empty();
+  let (
+    startup_moment,
+    startup_unix_epoch,
+    canvas,
+    tree,
+    state_machine,
+    buffers,
+    contents,
+    commands,
+    cancellation_token,
+    detached_tracker,
+    blocked_tracker,
+    exit_code,
+    (master_tx, master_rx),
+    (jsrt_forwarder_tx, jsrt_forwarder_rx),
+    (jsrt_tx, jsrt_rx),
+  ) = EventLoop::_internal_new(10, 10).unwrap();
 
-    let writer = StdoutWriterValue::dev_null();
-    let bytes: &'static [u8] = Box::leak(snapshot.into_boxed_slice());
+  let writer = StdoutWriterValue::dev_null();
 
-    // Js Runtime
-    let js_runtime = JsRuntime::new(
+  // Js Runtime
+  let js_runtime = match snapshot {
+    Some(snapshot) => {
+      let bytes: &'static [u8] = Box::leak(snapshot.into_boxed_slice());
+      JsRuntime::new(
+        JsRuntimeOptions::default(),
+        SnapshotData::new(bytes),
+        startup_moment,
+        startup_unix_epoch,
+        master_tx.clone(),
+        jsrt_rx,
+        cli_opts.clone(),
+        tree.clone(),
+        buffers.clone(),
+        contents.clone(),
+        commands,
+      )
+    }
+    None => JsRuntime::new_without_snapshot(
       JsRuntimeOptions::default(),
-      SnapshotData::new(bytes),
       startup_moment,
       startup_unix_epoch,
       master_tx.clone(),
@@ -68,31 +84,33 @@ async fn with_snapshot(tp: &TempConfigDir, snapshot: Vec<u8>) -> IoResult<()> {
       buffers.clone(),
       contents.clone(),
       commands,
-    );
-
-    EventLoop {
-      startup_moment,
-      startup_unix_epoch,
-      cli_opts,
-      canvas,
-      tree,
-      state_machine,
-      buffers,
-      contents,
-      writer,
-      cancellation_token,
-      detached_tracker,
-      blocked_tracker,
-      exit_code,
-      js_runtime,
-      master_tx,
-      master_rx,
-      jsrt_forwarder_tx,
-      jsrt_forwarder_rx,
-      jsrt_tx,
-    }
+    ),
   };
 
+  EventLoop {
+    startup_moment,
+    startup_unix_epoch,
+    cli_opts,
+    canvas,
+    tree,
+    state_machine,
+    buffers,
+    contents,
+    writer,
+    cancellation_token,
+    detached_tracker,
+    blocked_tracker,
+    exit_code,
+    js_runtime,
+    master_tx,
+    master_rx,
+    jsrt_forwarder_tx,
+    jsrt_forwarder_rx,
+    jsrt_tx,
+  }
+}
+
+fn run_event_loop(mut ev: EventLoop) -> IoResult<()> {
   // Run the event loop.
   let mocked_ops = vec![
     MockOperation::Operation(Operation::GotoCommandLineExMode),
@@ -102,15 +120,14 @@ async fn with_snapshot(tp: &TempConfigDir, snapshot: Vec<u8>) -> IoResult<()> {
     MockOperation::Operation(Operation::ConfirmExCommandAndGotoNormalMode),
   ];
 
-  event_loop.initialize()?;
-  event_loop
-    .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+  ev.initialize()?;
+  ev.run_with_mock_operations(MockOperationReader::new(mocked_ops))
     .await?;
-  event_loop.shutdown()?;
+  ev.shutdown()?;
 
   // After running
   {
-    let mut contents = lock!(event_loop.contents);
+    let mut contents = lock!(ev.contents);
     let n = contents.command_line_message_history().occupied_len();
     assert_eq!(n, 1);
 
@@ -123,128 +140,38 @@ async fn with_snapshot(tp: &TempConfigDir, snapshot: Vec<u8>) -> IoResult<()> {
   Ok(())
 }
 
-async fn with_snapshot() -> IoResult<()> {
-  test_log_init();
-
-  // Prepare $RSVIM_CONFIG/rsvim.js
-  let tp = make_configs(vec![(Path::new("rsvim.js"), "")]);
-
-  let snapshot_file = tp.xdg_data_home.child("snapshot.bin");
-
-  // Prepare snapshot data
-  {
-    let js_runtime = JsRuntimeForSnapshot::new();
-    let snapshot = js_runtime.create_snapshot();
-    let snapshot = Box::from(&snapshot);
-    let mut vec = Vec::with_capacity(snapshot.len());
-    vec.extend_from_slice(&snapshot);
-
-    info!("Write snapshot to {:?}", snapshot_file.path());
-    std::fs::write(snapshot_file.path(), vec.into_boxed_slice()).unwrap();
-  };
-
-  let bytes = std::fs::read(snapshot_file.path()).unwrap();
-
+async fn with_snapshot(tp: &TempConfigDir, snapshot: Vec<u8>) -> IoResult<()> {
   // Create js runtime with snapshot.
-  let mut event_loop = {
-    let cli_opts = CliOptions::empty();
-    let (
-      startup_moment,
-      startup_unix_epoch,
-      canvas,
-      tree,
-      state_machine,
-      buffers,
-      contents,
-      commands,
-      cancellation_token,
-      detached_tracker,
-      blocked_tracker,
-      exit_code,
-      (master_tx, master_rx),
-      (jsrt_forwarder_tx, jsrt_forwarder_rx),
-      (jsrt_tx, jsrt_rx),
-    ) = EventLoop::_internal_new(10, 10).unwrap();
+  let mut event_loop = create_event_loop(Some(snapshot));
 
-    let writer = StdoutWriterValue::dev_null();
+  run_event_loop(event_loop)
+}
 
-    let bytes: &'static [u8] = Box::leak(bytes.into_boxed_slice());
+async fn without_snapshot(tp: &TempConfigDir) -> IoResult<()> {
+  let mut event_loop = create_event_loop(None);
 
-    // Js Runtime
-    let js_runtime = JsRuntime::new(
-      JsRuntimeOptions::default(),
-      SnapshotData::new(bytes),
-      startup_moment,
-      startup_unix_epoch,
-      master_tx.clone(),
-      jsrt_rx,
-      cli_opts.clone(),
-      tree.clone(),
-      buffers.clone(),
-      contents.clone(),
-      commands,
-    );
-
-    EventLoop {
-      startup_moment,
-      startup_unix_epoch,
-      cli_opts,
-      canvas,
-      tree,
-      state_machine,
-      buffers,
-      contents,
-      writer,
-      cancellation_token,
-      detached_tracker,
-      blocked_tracker,
-      exit_code,
-      js_runtime,
-      master_tx,
-      master_rx,
-      jsrt_forwarder_tx,
-      jsrt_forwarder_rx,
-      jsrt_tx,
-    }
-  };
-
-  // Run the event loop.
-  let mocked_ops = vec![
-    MockOperation::Operation(Operation::GotoCommandLineExMode),
-    MockOperation::Operation(Operation::CursorInsert(
-      CursorInsertPayload::Text("js Rsvim.cmd.echo(1);".to_compact_string()),
-    )),
-    MockOperation::Operation(Operation::ConfirmExCommandAndGotoNormalMode),
-    MockOperation::SleepFor(Duration::from_millis(50)),
-  ];
-
-  event_loop.initialize()?;
-  event_loop
-    .run_with_mock_operations(MockOperationReader::new(mocked_ops))
-    .await?;
-  event_loop.shutdown()?;
-
-  // After running
-  {
-    let mut contents = lock!(event_loop.contents);
-    let n = contents.command_line_message_history().occupied_len();
-    assert_eq!(n, 1);
-
-    let actual = contents.command_line_message_history_mut().try_pop();
-    info!("actual:{:?}", actual);
-    assert!(actual.is_some());
-    let actual = actual.unwrap();
-    assert_eq!(actual, "1");
-  }
-
-  Ok(())
+  run_event_loop(event_loop)
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
   let tp = make_configs(vec![(Path::new("rsvim.js"), "")]);
+  let snapshot = create_snapshot(&tp);
+  let rt = tokio::runtime::Runtime::new().unwrap();
 
-  c.bench_function("with snapshot", |b| b.iter(|| fibonacci(black_box(20))));
-  c.bench_function("without snapshot", |b| b.iter(|| fibonacci(black_box(20))));
+  c.bench_function("with snapshot", |b| {
+    b.iter(|| {
+      rt.block_on(async {
+        with_snapshot(&tp, snapshot).await;
+      });
+    })
+  });
+  c.bench_function("without snapshot", |b| {
+    b.iter(|| {
+      rt.block_on(async {
+        without_snapshot(&tp, snapshot).await;
+      });
+    })
+  });
 }
 
 criterion_group!(benches, criterion_benchmark);
