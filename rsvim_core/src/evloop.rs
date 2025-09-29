2a412e9cb33ec0fe1dd34545163a3a737ae17d19
linrongbin16 <linrongbin16@outlook.com>
2025-09-29 17:32:46 +0800

    c

--- rsvim_core/src/evloop.rs
+++ rsvim_core/src/evloop.rs
@@ -753,7 +753,7 @@ impl EventLoop {
   }
 
   #[cfg(test)]
-  pub async fn _run_with_mocked_operations(
+  pub async fn run_with_mock_operations(
     &mut self,
     mut reader: MockOperationReader,
   ) -> IoResult<()> {
