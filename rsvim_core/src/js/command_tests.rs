6d7486a3c1e5d580ed6a4ff1499bc8bdded761ec
linrongbin16 <linrongbin16@outlook.com>
2025-09-29 11:36:55 +0800

    c

--- rsvim_core/src/js/command_tests.rs
+++ rsvim_core/src/js/command_tests.rs
@@ -1,10 +1,10 @@
 // use super::command::*;
 use crate::cli::CliOptions;
+use crate::evloop::mock::*;
 use crate::prelude::*;
 use crate::results::IoResult;
 use crate::state::ops::CursorInsertPayload;
 use crate::state::ops::Operation;
-use crate::tests::evloop::*;
 use crate::tests::log::init as test_log_init;
 use compact_str::ToCompactString;
 use ringbuf::traits::*;
