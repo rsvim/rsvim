0cec86443a0bb532219f950ee71d641aeac30990
linrongbin16 <linrongbin16@outlook.com>
2025-09-29 10:29:29 +0800

    c

--- rsvim_core/src/js.rs
+++ rsvim_core/src/js.rs
@@ -238,39 +238,33 @@ pub mod build {
     pub fn new() -> Self {
       init_v8_platform(true, None);
 
-      let (isolate, state) = {
-        let mut isolate = v8::Isolate::snapshot_creator(
-          None,
-          Some(v8::CreateParams::default()),
-        );
+      let mut isolate =
+        v8::Isolate::snapshot_creator(None, Some(v8::CreateParams::default()));
 
-        // NOTE: For snapshot runtime, it cannot call the
-        // `init_v8_isolate(&mut isolate)` API because it doesn't have many
-        // components such as "ModuleMap".
+      // NOTE: For snapshot runtime, it cannot call the
+      // `init_v8_isolate(&mut isolate)` API because it doesn't have many
+      // components such as "ModuleMap".
 
-        let context: v8::Global<v8::Context> = {
-          v8::scope!(scope, &mut *isolate);
-          let context = v8::Context::new(scope, Default::default());
-          v8::Global::new(scope, context)
-        };
+      let context: v8::Global<v8::Context> = {
+        v8::scope!(scope, &mut *isolate);
+        let context = v8::Context::new(scope, Default::default());
+        v8::Global::new(scope, context)
+      };
 
-        let state = {
-          v8::scope_with_context!(scope, &mut *isolate, context.clone());
+      let state = {
+        v8::scope_with_context!(scope, &mut *isolate, context.clone());
 
-          // Load, compile and evaluate all built-in modules.
-          init_builtin_modules(scope);
+        // Load, compile and evaluate all built-in modules.
+        init_builtin_modules(scope);
 
-          let state =
-            JsRuntimeStateForSnapshot::to_rc(JsRuntimeStateForSnapshot {
-              context: Some(context),
-            });
+        let state =
+          JsRuntimeStateForSnapshot::to_rc(JsRuntimeStateForSnapshot {
+            context: Some(context),
+          });
 
-          scope.set_slot(state.clone());
+        scope.set_slot(state.clone());
 
-          state
-        };
-
-        (isolate, state)
+        state
       };
 
       JsRuntimeForSnapshot {
