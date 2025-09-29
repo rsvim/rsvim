3f4abdbd9f49079f550f6f1ccbd5bbc3c46ea135
linrongbin16 <linrongbin16@outlook.com>
2025-09-29 11:36:46 +0800

    c

--- rsvim_core/src/js/module/module_map_tests.rs
+++ rsvim_core/src/js/module/module_map_tests.rs
@@ -1,8 +1,8 @@
 // use super::module_map::*;
 use crate::cli::CliOptions;
+use crate::evloop::mock::*;
 use crate::prelude::*;
 use crate::results::IoResult;
-use crate::tests::evloop::*;
 use crate::tests::log::init as test_log_init;
 use ringbuf::traits::*;
 use std::path::Path;
