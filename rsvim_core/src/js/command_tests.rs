214a2ae9c52f121e3cd7575709c84a0994a76d04
linrongbin16 <linrongbin16@outlook.com>
2025-09-29 15:15:56 +0800

    test(js): add 1 test case for invalid js scripts error (#695)

--- rsvim_core/src/js/command_tests.rs
+++ rsvim_core/src/js/command_tests.rs
@@ -93,3 +93,43 @@ async fn test_js_throw1() -> IoResult<()> {
 
   Ok(())
 }
+
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
+async fn test_js_invalid1() -> IoResult<()> {
+  test_log_init();
+
+  let terminal_cols = 10_u16;
+  let terminal_rows = 10_u16;
+  let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(30))];
+
+  let src: &str = r#"
+  asdf
+    "#;
+
+  // Prepare $RSVIM_CONFIG/rsvim.js
+  let _tp = make_configs(vec![(Path::new("rsvim.js"), src)]);
+
+  let mut event_loop =
+    make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+  event_loop.initialize()?;
+  event_loop
+    .run_with_mock_events(MockEventReader::new(mocked_events))
+    .await?;
+  event_loop.shutdown()?;
+
+  // After running
+  {
+    let mut contents = lock!(event_loop.contents);
+    let n = contents.command_line_message_history().occupied_len();
+    assert_eq!(n, 1);
+    let actual = contents.command_line_message_history_mut().try_pop();
+    info!("actual:{:?}", actual);
+    assert!(actual.is_some());
+    let actual = actual.unwrap();
+    assert!(actual.contains("Uncaught ReferenceError: asdf is not defined"));
+  }
+
+  Ok(())
+}
