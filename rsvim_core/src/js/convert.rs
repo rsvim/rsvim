45fdcf73577d88187cabd827586d899b07ffb922
linrongbin16 <linrongbin16@outlook.com>
2025-09-30 21:08:29 +0800

    c

--- rsvim_core/src/js/convert.rs
+++ rsvim_core/src/js/convert.rs
@@ -1,32 +1,44 @@
 //! Converters between rust and v8 values.
 
+use crate::prelude::*;
+
 pub trait ToV8 {
   fn to_v8<'s>(
     &self,
     scope: &mut v8::PinScope<'s, '_>,
-  ) -> v8::Local<'s, v8::Value>;
+  ) -> Option<v8::Local<'s, v8::Value>>;
 }
 
 pub trait FromV8CallbackArguments {
   fn from_v8_callback_arguments(
     scope: &mut v8::PinScope,
     args: v8::FunctionCallbackArguments,
-  ) -> Self;
+  ) -> Option<Self>;
 }
 
 pub trait FromV8 {
   fn from_v8<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     value: v8::Local<'s, v8::Value>,
-  ) -> Self;
+  ) -> Option<Self>;
 }
 
 pub fn to_v8<'s, 'b, T>(
   scope: &mut v8::PinScope<'s, 'b>,
   input: T,
-) -> v8::Local<'s, v8::Value>
+) -> Option<v8::Local<'s, v8::Value>>
 where
   T: ToV8,
 {
   input.to_v8(scope)
+}
+
+impl ToV8 for u8 {
+  fn to_v8<'s>(
+    &self,
+    scope: &mut v8::PinScope<'s, '_>,
+  ) -> Option<v8::Local<'s, v8::Value>> {
+    let v = v8::Uint32::from(self);
+    v.to_object(scope)
+  }
 }
