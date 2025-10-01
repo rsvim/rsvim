92ef6a1d8255cbd9a81db680b964e1e1dc79c29f
linrongbin16 <linrongbin16@outlook.com>
2025-10-01 15:46:43 +0800

    c

--- rsvim_core/src/js/converter_tests.rs
+++ rsvim_core/src/js/converter_tests.rs
@@ -10,18 +10,18 @@ fn test_integer1() {
   v8::scope_with_context!(scope, &mut jsrt.isolate, context);
 
   let a1 = 10_i32;
-  let obj1 = to_v8(scope, &a1);
+  let obj1 = to_v8(scope, a1);
   assert!(obj1.is_some());
   let obj1 = obj1.unwrap();
-  let val1 = from_v8::<i32>(scope, &obj1);
-  assert_eq!(val1, &a1);
+  let val1 = from_v8::<i32>(scope, obj1);
+  assert_eq!(val1, a1);
 
   let a2 = 10_u32;
-  let obj2 = to_v8(scope, &a2);
+  let obj2 = to_v8(scope, a2);
   assert!(obj2.is_some());
   let obj2 = obj2.unwrap();
-  let val2 = from_v8::<u32>(scope, &obj2);
-  assert_eq!(val2, &a2);
+  let val2 = from_v8::<u32>(scope, obj2);
+  assert_eq!(val2, a2);
 }
 
 #[test]
