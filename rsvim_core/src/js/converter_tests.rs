100c679e80014430ac91d3eea799b4f603eb5fab
linrongbin16 <linrongbin16@outlook.com>
2025-10-01 10:09:03 +0800

    c

--- rsvim_core/src/js/converter_tests.rs
+++ rsvim_core/src/js/converter_tests.rs
@@ -1,5 +1,7 @@
 use super::converter::*;
 use crate::tests::js::*;
+use compact_str::CompactString;
+use compact_str::ToCompactString;
 
 #[test]
 fn test_integer1() {
@@ -44,4 +46,19 @@ fn test_bool1() {
   let obj2 = to_v8(scope, false);
   let val2 = from_v8::<bool>(scope, obj2);
   assert!(!val2);
+}
+
+#[test]
+fn test_string1() {
+  let mut jsrt = make_js_runtime();
+  let context = jsrt.context();
+  v8::scope_with_context!(scope, &mut jsrt.isolate, context);
+
+  let obj1 = to_v8(scope, "Hello".to_string());
+  let val1 = from_v8::<String>(scope, obj1);
+  assert_eq!(val1, "Hello".to_string());
+
+  let obj2 = to_v8(scope, "World".to_compact_string());
+  let val2 = from_v8::<CompactString>(scope, obj2);
+  assert_eq!(val2, "World".to_compact_string());
 }
