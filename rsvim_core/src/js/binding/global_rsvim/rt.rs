78b69e4616ec20c464efd56b5af377eb7b7d3761
linrongbin16 <linrongbin16@outlook.com>
2025-10-01 16:32:06 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/rt.rs
+++ rsvim_core/src/js/binding/global_rsvim/rt.rs
@@ -14,10 +14,10 @@ pub fn exit<'s>(
 ) {
   debug_assert!(args.length() == 1);
   let exit_code = from_v8::<i32>(scope, args.get(0)).unwrap();
+  trace!("exit: {:?}", exit_code);
 
   let state_rc = JsRuntime::state(scope);
   let state = state_rc.borrow();
-  trace!("exit: {:?}", exit_code);
 
   msg::sync_send_to_master(
     state.master_tx.clone(),
