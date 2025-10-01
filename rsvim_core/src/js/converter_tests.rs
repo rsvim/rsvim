d08333905efc2bb532c8dc78c276a64c113dda93
linrongbin16 <linrongbin16@outlook.com>
2025-10-01 10:11:04 +0800

    c

--- rsvim_core/src/js/converter_tests.rs
+++ rsvim_core/src/js/converter_tests.rs
@@ -62,3 +62,18 @@ fn test_string1() {
   let val2 = from_v8::<CompactString>(scope, obj2);
   assert_eq!(val2, "World".to_compact_string());
 }
+
+#[test]
+fn test_string1() {
+  let mut jsrt = make_js_runtime();
+  let context = jsrt.context();
+  v8::scope_with_context!(scope, &mut jsrt.isolate, context);
+
+  let obj1 = to_v8(scope, "Hello".to_string());
+  let val1 = from_v8::<String>(scope, obj1);
+  assert_eq!(val1, "Hello".to_string());
+
+  let obj2 = to_v8(scope, "World".to_compact_string());
+  let val2 = from_v8::<CompactString>(scope, obj2);
+  assert_eq!(val2, "World".to_compact_string());
+}
