cd4a8037f48a457d8227ef5494a265aaf903503e
linrongbin16 <linrongbin16@outlook.com>
2025-10-01 16:31:58 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/rt.rs
+++ rsvim_core/src/js/binding/global_rsvim/rt.rs
@@ -1,18 +1,19 @@
 //! APIs for `Rsvim.rt` namespace.
 
 use crate::js::JsRuntime;
+use crate::js::converter::*;
 use crate::msg;
 use crate::msg::MasterMessage;
 use crate::prelude::*;
 
 /// Exit editor.
-pub fn exit(
-  scope: &mut v8::PinScope,
-  args: v8::FunctionCallbackArguments,
+pub fn exit<'s>(
+  scope: &mut v8::PinScope<'s, '_>,
+  args: v8::FunctionCallbackArguments<'s>,
   mut _rv: v8::ReturnValue,
 ) {
   debug_assert!(args.length() == 1);
-  let exit_code = args.get(0).int32_value(scope).unwrap();
+  let exit_code = from_v8::<i32>(scope, args.get(0)).unwrap();
 
   let state_rc = JsRuntime::state(scope);
   let state = state_rc.borrow();
