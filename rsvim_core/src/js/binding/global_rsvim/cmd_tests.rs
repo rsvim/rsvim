9b7be566dae2d079ad2e17069a8b2b47d71a1703
linrongbin16 <linrongbin16@outlook.com>
2025-10-01 22:42:10 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/cmd_tests.rs
+++ rsvim_core/src/js/binding/global_rsvim/cmd_tests.rs
@@ -360,9 +360,9 @@ async fn test_recreate2() -> IoResult<()> {
 
   let src: &str = r#"
 setTimeout(() => {
-  const prev1 = Rsvim.cmd.create("write", () => {Rsvim.cmd.echo(1); return 1;});
+  const prev1 = Rsvim.cmd.create("write", () => {Rsvim.cmd.echo(1); return 1;}, {alias:"w"}, {force:false});
   Rsvim.cmd.echo(`Previous-1 command:${prev1}`);
-  const prev2 = Rsvim.cmd.create("writeSync", () => {Rsvim.cmd.echo(2); return 2;});
+  const prev2 = Rsvim.cmd.create("writeSync", () => {Rsvim.cmd.echo(2); return 2;}, {alias:"w"}, {force:false});
   Rsvim.cmd.echo(`Previous-2 command:${typeof prev2}, ${prev2.callback()}`);
 });
     "#;
