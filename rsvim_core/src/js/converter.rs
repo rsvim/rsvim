e7e7961420f0599c866c1c650e659db79855674c
linrongbin16 <linrongbin16@outlook.com>
2025-10-01 22:04:03 +0800

    c

--- rsvim_core/src/js/converter.rs
+++ rsvim_core/src/js/converter.rs
@@ -14,14 +14,14 @@ pub trait FromV8CallbackArguments {
   fn from_v8_callback_arguments<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     args: v8::FunctionCallbackArguments<'s>,
-  ) -> Self
+  ) -> Self;
 }
 
 pub trait FromV8 {
   fn from_v8<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     value: v8::Local<'s, v8::Value>,
-  ) -> Self
+  ) -> Self;
 }
 
 pub fn to_v8<'s, 'b, T>(
@@ -29,7 +29,7 @@ pub fn to_v8<'s, 'b, T>(
   input: T,
 ) -> v8::Local<'s, v8::Value>
 where
-  T: ToV8 ,
+  T: ToV8,
 {
   input.to_v8(scope)
 }
@@ -125,10 +125,7 @@ where
     &self,
     scope: &mut v8::PinScope<'s, '_>,
   ) -> v8::Local<'s, v8::Value> {
-    let elements = self
-      .iter()
-      .map(|v| v.to_v8(scope))
-      .collect::<Vec<_>>();
+    let elements = self.iter().map(|v| v.to_v8(scope)).collect::<Vec<_>>();
     v8::Array::new_with_elements(scope, &elements).into()
   }
 }
@@ -138,7 +135,7 @@ impl FromV8 for u32 {
     scope: &mut v8::PinScope<'s, '_>,
     value: v8::Local<'s, v8::Value>,
   ) -> Self {
-      value.uint32_value(scope).unwrap()
+    value.uint32_value(scope).unwrap()
   }
 }
 
@@ -146,12 +143,8 @@ impl FromV8 for i32 {
   fn from_v8<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     value: v8::Local<'s, v8::Value>,
-  ) -> Option<Self> {
-    if value.is_int32() {
-      value.int32_value(scope)
-    } else {
-      None
-    }
+  ) -> Self {
+    value.int32_value(scope).unwrap()
   }
 }
 
@@ -159,12 +152,8 @@ impl FromV8 for f64 {
   fn from_v8<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     value: v8::Local<'s, v8::Value>,
-  ) -> Option<Self> {
-    if value.is_number() || value.is_number_object() {
-      value.number_value(scope)
-    } else {
-      None
-    }
+  ) -> Self {
+    value.number_value(scope).unwrap()
   }
 }
 
@@ -172,12 +161,8 @@ impl FromV8 for bool {
   fn from_v8<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     value: v8::Local<'s, v8::Value>,
-  ) -> Option<Self> {
-    if value.is_boolean() || value.is_boolean_object() {
-      Some(value.boolean_value(scope))
-    } else {
-      None
-    }
+  ) -> Self {
+    value.boolean_value(scope)
   }
 }
 
@@ -185,12 +170,8 @@ impl FromV8 for String {
   fn from_v8<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     value: v8::Local<'s, v8::Value>,
-  ) -> Option<Self> {
-    if value.is_string() || value.is_string_object() {
-      Some(value.to_rust_string_lossy(scope))
-    } else {
-      None
-    }
+  ) -> Self {
+    value.to_rust_string_lossy(scope)
   }
 }
 
@@ -198,12 +179,8 @@ impl FromV8 for CompactString {
   fn from_v8<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     value: v8::Local<'s, v8::Value>,
-  ) -> Option<Self> {
-    if value.is_string() || value.is_string_object() {
-      Some(value.to_rust_string_lossy(scope).to_compact_string())
-    } else {
-      None
-    }
+  ) -> Self {
+    value.to_rust_string_lossy(scope).to_compact_string()
   }
 }
 
@@ -214,19 +191,15 @@ where
   fn from_v8<'s>(
     scope: &mut v8::PinScope<'s, '_>,
     value: v8::Local<'s, v8::Value>,
-  ) -> Option<Self> {
-    match v8::Local::<v8::Array>::try_from(value) {
-      Ok(elements) => {
-        let n = elements.length();
-        let mut v: Vec<T> = Vec::with_capacity(n as usize);
-        for i in 0..n {
-          let e = elements.get_index(scope, i).unwrap();
-          let t = T::from_v8(scope, e).unwrap();
-          v.push(t);
-        }
-        Some(v)
-      }
-      Err(_) => None,
+  ) -> Self {
+    let elements = v8::Local::<v8::Array>::try_from(value).unwrap();
+    let n = elements.length();
+    let mut v: Vec<T> = Vec::with_capacity(n as usize);
+    for i in 0..n {
+      let e = elements.get_index(scope, i).unwrap();
+      let t = T::from_v8(scope, e);
+      v.push(t);
     }
+    v
   }
 }
