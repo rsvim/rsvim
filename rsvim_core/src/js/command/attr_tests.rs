5a1d752fd46007317f2dc16c20db02096a4f25cd
linrongbin16 <linrongbin16@outlook.com>
2025-10-01 15:37:08 +0800

    c

--- rsvim_core/src/js/command/attr_tests.rs
+++ rsvim_core/src/js/command/attr_tests.rs
@@ -1,4 +1,7 @@
 use super::attr::*;
+use crate::js::converter::*;
+use crate::tests::js::*;
+use compact_str::ToCompactString;
 use std::str::FromStr;
 
 #[test]
@@ -17,4 +20,24 @@ fn test_nargs() {
 
   assert_eq!(format!("{}", Nargs::Any), "*");
   assert_eq!(Nargs::from_str("*"), Ok(Nargs::Any));
+}
+
+#[test]
+fn test_converter1() {
+  let mut jsrt = make_js_runtime();
+  let context = jsrt.context();
+  v8::scope_with_context!(scope, &mut jsrt.isolate, context);
+
+  let a1 = CommandAttributesBuilder::default().build().unwrap();
+  let obj1 = to_v8(scope, a1.clone()).unwrap();
+  let val1 = from_v8::<CommandAttributes>(scope, obj1).unwrap();
+  assert_eq!(val1, a1);
+
+  let a2 = CommandAttributesBuilder::default()
+    .alias(Some("w".to_compact_string()))
+    .build()
+    .unwrap();
+  let obj2 = to_v8(scope, a2.clone()).unwrap();
+  let val2 = from_v8::<CommandAttributes>(scope, obj2).unwrap();
+  assert_eq!(val2, a2);
 }
