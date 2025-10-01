ddc2e7dadb9ded67d740cc5f861bf27c059515b1
linrongbin16 <linrongbin16@outlook.com>
2025-10-01 22:08:03 +0800

    c

--- rsvim_core/src/js/command/opt_tests.rs
+++ rsvim_core/src/js/command/opt_tests.rs
@@ -10,15 +10,15 @@ fn test1() {
   v8::scope_with_context!(scope, &mut jsrt.isolate, context);
 
   let a1 = CommandOptionsBuilder::default().build().unwrap();
-  let obj1 = to_v8(scope, a1.clone()).unwrap();
-  let val1 = from_v8::<CommandOptions>(scope, obj1).unwrap();
+  let obj1 = to_v8(scope, a1.clone());
+  let val1 = from_v8::<CommandOptions>(scope, obj1);
   assert_eq!(val1, a1);
 
   let a2 = CommandOptionsBuilder::default()
     .alias(Some("w".to_compact_string()))
     .build()
     .unwrap();
-  let obj2 = to_v8(scope, a2.clone()).unwrap();
-  let val2 = from_v8::<CommandOptions>(scope, obj2).unwrap();
+  let obj2 = to_v8(scope, a2.clone());
+  let val2 = from_v8::<CommandOptions>(scope, obj2);
   assert_eq!(val2, a2);
 }
