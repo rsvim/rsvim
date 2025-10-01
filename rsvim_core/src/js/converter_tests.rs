070fee4633f9ebb1660b831a0c0a97bf9b7d9f4c
linrongbin16 <linrongbin16@outlook.com>
2025-10-01 10:05:25 +0800

    c

--- rsvim_core/src/js/converter_tests.rs
+++ rsvim_core/src/js/converter_tests.rs
@@ -7,4 +7,8 @@ fn test_integer1() {
   let mut jsrt = make_js_runtime();
   let context = jsrt.context();
   v8::scope_with_context!(scope, &mut jsrt.isolate, context);
+
+  let obj1 = to_v8(scope, 10_i32);
+  let val1 = from_v8(scope, obj1);
+  assert_eq!(val1, 10);
 }
