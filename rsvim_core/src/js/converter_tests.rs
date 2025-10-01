2e3405d80e655ede8ebbabd2bb9ffdc1eb55eb4f
linrongbin16 <linrongbin16@outlook.com>
2025-10-01 15:59:00 +0800

    c

--- rsvim_core/src/js/converter_tests.rs
+++ rsvim_core/src/js/converter_tests.rs
@@ -79,7 +79,7 @@ fn test_string1() {
   v8::scope_with_context!(scope, &mut jsrt.isolate, context);
 
   let a1 = "Hello".to_string();
-  let obj1 = to_v8(scope, a1);
+  let obj1 = to_v8(scope, a1.clone());
   assert!(obj1.is_some());
   let obj1 = obj1.unwrap();
   let val1 = from_v8::<String>(scope, obj1);
