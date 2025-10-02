42ab1f14634429b08f07091cbd74f50b18911832
linrongbin16 <linrongbin16@outlook.com>
2025-10-02 15:17:39 +0800

    c

--- rsvim_core/src/js/runtime/00__web.d.ts
+++ rsvim_core/src/js/runtime/00__web.d.ts
@@ -1,3 +1,10 @@
+declare function checkNotNull(arg: any, msg: string): void;
+declare function checkIsNumber(arg: any, msg: string): void;
+declare function checkIsInteger(arg: any, msg: string): void;
+declare function checkIsBoolean(arg: any, msg: string): void;
+declare function checkIsFunction(arg: any, msg: string): void;
+declare function checkIsOptions(arg: any, options: any[], msg: string): void;
+declare function boundByIntegers(arg: any, bound: [number, number]): any;
 declare interface GlobalThis {
     clearInterval(id: number): void;
     clearTimeout(id: number): void;
