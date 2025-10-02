80f6ee8d46aa881ecab1625e302ebb48a7b0295d
linrongbin16 <linrongbin16@outlook.com>
2025-10-03 04:12:33 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/opt.rs
+++ rsvim_core/src/js/binding/global_rsvim/opt.rs
@@ -82,7 +82,7 @@ pub fn get_tab_stop(
   let buffers = lock!(buffers);
   let value = buffers.global_local_options().tab_stop();
   trace!("get_tab_stop: {:?}", value);
-  rv.set_int32(value as i32);
+  rv.set_uint32(value as u32);
 }
 
 /// Set the _tab-stop_ option.
