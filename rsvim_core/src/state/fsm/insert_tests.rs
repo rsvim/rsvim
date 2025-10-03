71e3b395f7f51dc44e380a25f1d452ae78d93363
linrongbin16 <linrongbin16@outlook.com>
2025-10-03 13:29:13 +0800

    c

--- rsvim_core/src/state/fsm/insert_tests.rs
+++ rsvim_core/src/state/fsm/insert_tests.rs
@@ -8499,31 +8499,23 @@ mod tests_delete_text {
       let expect = vec![
         "HeRSVIM!\n",
         "This is a ",
+        "quite ",
+        "simple and",
+        " small ",
+        "test lines",
+        ".\n",
         "But still ",
-        "  1. When ",
-        "  2. When ",
-        "* The extr",
-        "* The extr",
-        "",
+        "it ",
+        "contains ",
       ];
-      let expect_fills: BTreeMap<usize, usize> = vec![
-        (0, 0),
-        (1, 0),
-        (2, 0),
-        (3, 0),
-        (4, 0),
-        (5, 0),
-        (6, 0),
-        (7, 0),
-      ]
-      .into_iter()
-      .collect();
+      let expect_fills: BTreeMap<usize, usize> =
+        vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
       assert_viewport(
         buf.clone(),
         &viewport,
         &expect,
         0,
-        8,
+        3,
         &expect_fills,
         &expect_fills,
       );
@@ -8531,14 +8523,14 @@ mod tests_delete_text {
       let expect_canvas = vec![
         "HeRSVIM!  ",
         "This is a ",
+        "quite     ",
+        "simple and",
+        " small    ",
+        "test lines",
+        ".         ",
         "But still ",
-        "  1. When ",
-        "  2. When ",
-        "* The extr",
-        "* The extr",
-        "          ",
-        "          ",
-        "          ",
+        "it        ",
+        "contains  ",
       ];
       let actual_canvas =
         make_canvas(terminal_size, window_options, buf.clone(), viewport);
