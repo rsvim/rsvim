9152eb9b211c19f2a4c3723486ff1da66d9e1669
linrongbin16 <linrongbin16@outlook.com>
2025-10-05 16:29:02 +0800

    c

--- rsvim_core/src/ui/tree/internal/inode.rs
+++ rsvim_core/src/ui/tree/internal/inode.rs
@@ -325,7 +325,7 @@ pub fn next_node_id() -> TreeNodeId {
   VALUE.fetch_add(1, Ordering::Relaxed)
 }
 
-flags_impl!(Flags, u8, ENABLED, 0b0000_0001, VISIBLE, 0b0000_0010);
+flags_impl!(Flags, u8, ENABLED, 1, VISIBLE, 1 << 1);
 
 // enabled=true
 // visible=true
