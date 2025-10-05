7dd6d5a8988d15a1be92be6557ac9d598420b8cd
linrongbin16 <linrongbin16@outlook.com>
2025-10-05 13:34:55 +0800

    c

--- rsvim_core/src/util/flag.rs
+++ rsvim_core/src/util/flag.rs
@@ -2,11 +2,11 @@
 
 #[macro_export]
 macro_rules! flags_impl {
-  ($name:ident,$unsigned:ty,$($field:tt,$value:literal),*) => {
+  ($name:ident,$unsigned:ty,$($upperfield:tt,$value:literal,$lowerfield:tt),*) => {
     #[derive(Copy, Clone, PartialEq, Eq)]
     struct $name: $unsigned {
       $(
-        const $field = $value;
+        const $upperfield = $value;
       )*
     }
 
@@ -14,6 +14,24 @@ macro_rules! flags_impl {
       fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         bitflags::parser::to_writer(self, f)
       }
+    }
+
+    paste::paste! {
+    impl $name {
+    $(
+      pub fn $lowerfield(&self) -> bool {
+        self.contains($name::$upperfield)
+      }
+
+      pub fn [<set_ $lowerfield>](&mut self, value: bool) {
+        if value {
+          self.insert(&name::&upperfield);
+        } else {
+          self.remove(&name::&upperfield);
+        }
+      }
+    )*
+    }
     }
   };
 }
