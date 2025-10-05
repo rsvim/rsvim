68e1180ebbbeecdcfa8840d244a1f0656dc241c1
linrongbin16 <linrongbin16@outlook.com>
2025-10-05 15:44:01 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/cmd_tests.rs
+++ rsvim_core/src/js/binding/global_rsvim/cmd_tests.rs
@@ -339,10 +339,10 @@ setTimeout(() => {
     let (command_name, command_def) = first_command.unwrap();
     assert_eq!(command_name, "write");
     assert_eq!(command_def.name, "write");
-    assert!(!command_def.attributes.bang);
-    assert_eq!(command_def.attributes.nargs, Nargs::Zero);
-    assert!(command_def.options.force);
-    assert_eq!(command_def.options.alias, None);
+    assert!(!command_def.attributes.bang());
+    assert_eq!(command_def.attributes.nargs(), Nargs::Zero);
+    assert!(command_def.options.force());
+    assert_eq!(command_def.options.alias(), None);
   }
 
   Ok(())
