576976b07b87c8b849f1d756a7e4c281b3295123
linrongbin16 <linrongbin16@outlook.com>
2025-10-07 23:40:29 +0800

    c

--- rsvim_core/src/buf/text.rs
+++ rsvim_core/src/buf/text.rs
@@ -389,9 +389,11 @@ impl Text {
     self
       .cached_lines_width
       .borrow_mut()
-      .get_or_insert_mut(line_idx, || -> ColumnIndex {
-        ColumnIndex::with_capacity(rope_line.len_chars())
+      .get_mut_or_insert_with(&line_idx, || -> Result<ColumnIndex, ()> {
+        Ok(ColumnIndex::with_capacity(rope_line.len_chars()))
       })
+      .unwrap()
+      .unwrap()
       .last_char_until(&self.options, &rope_line, width)
   }
 
