4b6e2100412c75a70a1d5c634883f25af9ea9b45
linrongbin16 <linrongbin16@outlook.com>
2025-10-07 21:58:32 +0800

    c

--- rsvim_core/src/js/command/ctx.rs
+++ rsvim_core/src/js/command/ctx.rs
@@ -32,31 +32,6 @@ impl CommandContext {
   }
 }
 
-impl FromV8 for CommandContext {
-  fn from_v8<'s>(
-    scope: &mut v8::PinScope<'s, '_>,
-    value: v8::Local<'s, v8::Value>,
-  ) -> Self {
-    let mut builder = CommandContextBuilder::default();
-    let obj = value.to_object(scope).unwrap();
-
-    // bang
-    let bang_name = to_v8(scope, BANG);
-    if let Some(bang_value) = obj.get(scope, bang_name) {
-      builder.bang(from_v8::<bool>(scope, bang_value));
-    }
-
-    // nargs
-    let nargs_name = to_v8(scope, NARGS);
-    if let Some(nargs_value) = obj.get(scope, nargs_name) {
-      let nargs = from_v8::<CompactString>(scope, nargs_value);
-      builder.nargs(Nargs::from_str(&nargs).unwrap());
-    }
-
-    builder.build().unwrap()
-  }
-}
-
 impl ToV8 for CommandContext {
   fn to_v8<'s>(
     &self,
@@ -69,10 +44,10 @@ impl ToV8 for CommandContext {
     let bang_value = to_v8(scope, self.bang());
     obj.set(scope, bang_field, bang_value);
 
-    // nargs
-    let nargs_field = to_v8(scope, NARGS);
-    let nargs_value = to_v8(scope, self.nargs.to_compact_string());
-    obj.set(scope, nargs_field, nargs_value);
+    // args
+    let args_field = to_v8(scope, ARGS);
+    let args_value = to_v8(scope, self.args().clone());
+    obj.set(scope, args_field, args_value);
 
     obj.into()
   }
