eaac7ce8c488473de26863ad25d07d85302f1c09
linrongbin16 <linrongbin16@outlook.com>
2025-10-14 10:42:18 +0800

    c

--- rsvim_core/src/buf/text/cache.rs
+++ rsvim_core/src/buf/text/cache.rs
@@ -60,21 +60,50 @@ impl std::fmt::Display for Stats {
   }
 }
 
-// Lines width cache.
-pub struct LinesWidthCache {
-  cache: RefCell<CLruCache<usize, ColumnIndex, RandomState>>,
+fn _cached_size(canvas_size: U16Size) -> std::num::NonZeroUsize {
+  std::num::NonZeroUsize::new(canvas_size.height() as usize * 2 + 3).unwrap()
+}
+
+type LinesWidthCache = CLruCache<usize, ColumnIndex, RandomState>;
+
+// Cached lines width.
+pub struct CachedLinesWidth {
+  cache: RefCell<LinesWidthCache>,
   stats: RefCell<Stats>,
 }
 
+impl CachedLinesWidth {
+  pub fn new(canvas_size: U16Size) -> Self {
+    let cache_size = _cached_size(canvas_size);
+    Self {
+      cache: RefCell::new(CLruCache::with_hasher(
+        cache_size,
+        RandomState::default(),
+      )),
+      stats: RefCell::new(Stats::default()),
+    }
+  }
+
+  fn with<F, U>(&self, f: F) -> U
+  where
+    F: FnOnce(&mut LinesWidthCache, &mut CacheStatus) -> U,
+  {
+    f(
+      &mut self.cached_width.borrow_mut(),
+      &mut self.cached_width_stats.borrow_mut(),
+    )
+  }
+}
+
 #[derive(Hash, PartialEq, Eq, Copy, Clone)]
-struct ClonedLinesCacheKey {
+struct CachedClonedLinesKey {
   pub line_idx: usize,
   pub start_char_idx: usize,
   pub max_chars: usize,
 }
 
-// Cloned lines cache.
-pub struct ClonedLinesCache {
-  cache: RefCell<CLruCache<ClonedLinesCacheKey, Rc<String>, RandomState>>,
+// Cached cloned lines.
+pub struct CachedClonedLines {
+  cache: RefCell<CLruCache<CachedClonedLinesKey, Rc<String>, RandomState>>,
   stats: RefCell<Stats>,
 }
