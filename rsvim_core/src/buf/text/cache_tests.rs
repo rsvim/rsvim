f2465986cb8ffdbd083c2e913610b67c90715db6
linrongbin16 <linrongbin16@outlook.com>
2025-10-14 10:24:24 +0800

    c

--- rsvim_core/src/buf/text/cache_tests.rs
+++ rsvim_core/src/buf/text/cache_tests.rs
@@ -9,12 +9,12 @@ mod stats {
     assert_eq!(stats.hits(), 0);
     assert_eq!(stats.misses(), 0);
     assert_eq!(stats.total(), 0);
-    assert_eq!(stats.ratio(), 0);
+    assert_eq!(stats.ratio(), 0_f32);
     stats.hit();
     stats.miss();
     assert_eq!(stats.hits(), 1);
     assert_eq!(stats.misses(), 1);
     assert_eq!(stats.total(), 2);
-    assert_eq!(stats.total(), 2);
+    assert_eq!(stats.ratio(), 0.5_f32);
   }
 }
