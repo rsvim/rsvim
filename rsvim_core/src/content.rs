c4be683674e47f2c4790b12aecba7587edda0d7f
linrongbin16 <linrongbin16@outlook.com>
2025-10-15 10:59:32 +0800

    c

--- rsvim_core/src/content.rs
+++ rsvim_core/src/content.rs
@@ -4,7 +4,6 @@ use crate::buf::opt::BufferOptionsBuilder;
 use crate::buf::text::Text;
 use crate::prelude::*;
 use ringbuf::HeapRb;
-use ringbuf::traits::RingBuffer;
 use ropey::Rope;
 use std::fmt::Debug;
 
