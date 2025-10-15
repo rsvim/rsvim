b101444b80cdcbfeb65a087c7da018311c12f304
linrongbin16 <linrongbin16@outlook.com>
2025-10-15 14:03:45 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs.rs
@@ -15,32 +15,32 @@ use crate::state::ops::cmdline_ops;
 use compact_str::CompactString;
 use compact_str::ToCompactString;
 use ringbuf::traits::RingBuffer;
+use std::fs;
+use std::fs::File;
 use std::rc::Rc;
 
 #[cfg(not(target_family = "windows"))]
-fn to_fd(file: std::fs::File) -> usize {
+fn to_fd(file: File) -> usize {
   use std::os::fd::IntoRawFd;
   file.into_raw_fd() as usize
 }
 
 #[cfg(target_family = "windows")]
-fn to_fd(file: std::fs::File) -> usize {
+fn to_fd(file: File) -> usize {
   use std::os::windows::io::IntoRawHandle;
   file.into_raw_handle() as usize
 }
 
 #[cfg(not(target_family = "windows"))]
-fn from_fd(fd: usize) -> std::fs::File {
+fn from_fd(fd: usize) -> File {
   use std::os::fd::FromRawFd;
-  unsafe { std::fs::File::from_raw_fd(fd as std::os::fd::RawFd) }
+  unsafe { File::from_raw_fd(fd as std::os::fd::RawFd) }
 }
 
 #[cfg(target_family = "windows")]
 fn from_fd(handle: usize) -> File {
   use std::os::windows::io::FromRawHandle;
-  unsafe {
-    fs::File::from_raw_handle(handle as std::os::windows::io::RawHandle)
-  }
+  unsafe { File::from_raw_handle(handle as std::os::windows::io::RawHandle) }
 }
 
 struct FsOpenFuture {
