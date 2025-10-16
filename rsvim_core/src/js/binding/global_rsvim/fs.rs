3dc16bf6c54c396fb32f29cc0dd62b9cc458cbee
linrongbin16 <linrongbin16@outlook.com>
2025-10-16 11:05:11 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs.rs
@@ -3,25 +3,9 @@
 pub mod fs_file;
 pub mod open;
 
-use crate::js;
-use crate::js::JsFuture;
-use crate::js::JsRuntime;
-use crate::js::JsRuntimeState;
-use crate::js::JsTimerId;
-use crate::js::binding;
 use crate::js::binding::global_rsvim::fs::open::FsOpenOptions;
-use crate::js::command::def::CommandDefinition;
 use crate::js::converter::*;
-use crate::js::converter::*;
-use crate::js::pending;
 use crate::prelude::*;
-use crate::state::ops::cmdline_ops;
-use compact_str::CompactString;
-use compact_str::ToCompactString;
-use ringbuf::traits::RingBuffer;
-use std::fs;
-use std::fs::File;
-use std::rc::Rc;
 
 /// `Rsvim.fs.open` API.
 pub fn open(
