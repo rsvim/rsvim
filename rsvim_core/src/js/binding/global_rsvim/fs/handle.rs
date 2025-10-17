9896fe52d40301264e528f8cf4d882535cdf5464
linrongbin16 <linrongbin16@outlook.com>
2025-10-17 10:53:02 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs/handle.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs/handle.rs
@@ -1,37 +1,33 @@
 //! File handle.
 
+use std::fs::File;
+
 pub const FD: &str = "fd";
 
 #[cfg(not(target_family = "windows"))]
-pub fn to_fd<F>(file: F) -> usize
-where
-  F: std::os::fd::IntoRawFd,
-{
+pub fn to_fd(file: File) -> usize {
+  use std::os::fd::IntoRawFd;
   file.into_raw_fd() as usize
 }
 
 #[cfg(target_family = "windows")]
-pub fn to_fd<F>(file: F) -> usize
-where
-  F: std::os::windows::io::IntoRawHandle,
-{
+pub fn to_fd(file: File) -> usize {
+  use std::os::windows::io::IntoRawHandle;
   file.as_raw_handle() as usize
 }
 
 #[cfg(not(target_family = "windows"))]
-pub fn from_fd<F>(fd: usize) -> F
-where
-  F: std::os::fd::FromRawFd,
-{
-  unsafe { F::from_raw_fd(fd as std::os::fd::RawFd) }
+pub fn from_fd(fd: usize) -> File {
+  use std::os::fd::FromRawFd;
+  unsafe { File::from_raw_fd(fd as std::os::fd::RawFd) }
 }
 
 #[cfg(target_family = "windows")]
-pub fn from_fd<F>(handle: usize) -> F
+pub fn from_fd<F>(handle: usize) -> File
 where
-  F: std::os::windows::io::FromRawHandle,
 {
-  unsafe { F::from_raw_handle(handle as std::os::windows::io::RawHandle) }
+  use std::os::windows::io::FromRawHandle;
+  unsafe { File::from_raw_handle(handle as std::os::windows::io::RawHandle) }
 }
 
 #[derive(Debug)]
