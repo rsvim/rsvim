166dedbd80def1fe3641682cad779f6ca19ebdb3
linrongbin16 <linrongbin16@outlook.com>
2025-10-17 10:44:58 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs/open.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs/open.rs
@@ -187,6 +187,7 @@ pub fn create_fs_file_wrapper<'s>(
   // 0-index: The `file_handle`, i.e. the `std::fs::File`
   // 1-index: The `file_weak` finalizer, it helps release the `file_handle`
   file_wrapper.set_internal_field_count(2);
+
   let file_wrapper = file_wrapper.new_instance(scope).unwrap();
 
   let file_ptr = binding::set_internal_ref::<Option<File>>(
