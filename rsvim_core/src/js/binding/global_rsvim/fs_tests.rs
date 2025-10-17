b5d237514ee5b11b9c7ea23be3b16f95e2ab7990
linrongbin16 <linrongbin16@outlook.com>
2025-10-17 14:40:08 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs_tests.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs_tests.rs
@@ -16,8 +16,44 @@ async fn test_open_close1() -> IoResult<()> {
   let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(50))];
 
   let src: &str = r#"
-  const f1 = await Rsvim.fs.open("README.md");
-  f1.close();
+  const f = await Rsvim.fs.open("README.md");
+  f.close();
+    "#;
+
+  // Prepare $RSVIM_CONFIG/rsvim.js
+  let _tp = make_configs(vec![(Path::new("rsvim.js"), src)]);
+
+  let mut event_loop =
+    make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+  event_loop.initialize()?;
+  event_loop
+    .run_with_mock_events(MockEventReader::new(mocked_events))
+    .await?;
+  event_loop.shutdown()?;
+
+  // After running
+  {
+    let contents = lock!(event_loop.contents);
+    let actual = contents.command_line_message_history().is_empty();
+    assert!(actual);
+  }
+
+  Ok(())
+}
+
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
+async fn test_open_close2() -> IoResult<()> {
+  test_log_init();
+
+  let terminal_cols = 10_u16;
+  let terminal_rows = 10_u16;
+  let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(50))];
+
+  let src: &str = r#"
+  const f = Rsvim.fs.openSync("README.md");
+  f.close();
     "#;
 
   // Prepare $RSVIM_CONFIG/rsvim.js
