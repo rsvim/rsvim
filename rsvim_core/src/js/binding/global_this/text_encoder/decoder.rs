5530f60261a02b84311ef9b9fe9d125c6d6d40f0
linrongbin16 <linrongbin16@outlook.com>
2025-10-19 06:14:04 +0800

    c

--- rsvim_core/src/js/binding/global_this/text_encoder/decoder.rs
+++ rsvim_core/src/js/binding/global_this/text_encoder/decoder.rs
@@ -93,7 +93,7 @@ impl DecoderOptionsToV8 for DecoderOptions {
     let ignore_bom_value = self.ignore_bom().to_v8(scope);
     binding::set_property_to(scope, obj, IGNORE_BOM, ignore_bom_value);
 
-    obj.into()
+    obj
   }
 }
 
