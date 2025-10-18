801237515882b238b6ded036480b4fb00ce5cede
linrongbin16 <linrongbin16@outlook.com>
2025-10-18 21:33:39 +0800

    c

--- rsvim_core/src/js/binding/global_this/text_encoder.rs
+++ rsvim_core/src/js/binding/global_this/text_encoder.rs
@@ -11,6 +11,7 @@ use icu::normalizer::ComposingNormalizerBorrowed;
 use icu::normalizer::DecomposingNormalizerBorrowed;
 use std::rc::Rc;
 
+#[allow(deprecated)]
 /// `TextEncoder.encode` API.
 pub fn encode<'s>(
   scope: &mut v8::PinScope<'s, '_>,
@@ -21,12 +22,25 @@ pub fn encode<'s>(
   let payload = args.get(0).to_string(scope).unwrap();
   trace!("|encode| payload:{:?}", payload.to_rust_string_lossy(scope));
 
-  let payload = payload.write_utf8_v2());
+  let mut buf: Vec<u8> = vec![];
+  let mut processed: Vec<usize> = vec![];
 
-  let nfc = ComposingNormalizerBorrowed::new_nfc();
-  let normalized = nfc.normalize(&payload);
-  let (result, _actual_encoding, _had_unmappable) =
-    encoding_rs::UTF_8.encode(&normalized);
+  /// FIXME: Update to `write_utf8_v8` follow deno's implementation:
+  ///
+  /// https://github.com/denoland/deno/blob/v2.5.4/ext/web/08_text_encoding.js#L256
+  /// https://github.com/denoland/deno/blob/v2.5.4/ext/web/lib.rs#L367
+  let written = payload.write_utf8(
+    scope,
+    &mut buf,
+    &mut processed,
+    v8::WriteOptions::NO_NULL_TERMINATION
+      | v8::WriteOptions::REPLACE_INVALID_UTF8,
+  );
+
+  // let nfc = ComposingNormalizerBorrowed::new_nfc();
+  // let normalized = nfc.normalize(&payload);
+  // let (result, _actual_encoding, _had_unmappable) =
+  //   encoding_rs::UTF_8.encode(&normalized);
 
   let store = v8::ArrayBuffer::new_backing_store_from_vec(result.into_owned());
   let buf = v8::ArrayBuffer::with_backing_store(scope, &store.make_shared());
