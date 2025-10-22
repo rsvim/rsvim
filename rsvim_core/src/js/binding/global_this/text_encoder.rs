593692489f4d69408e1c0e7e5b90720686b08434
linrongbin16 <linrongbin16@outlook.com>
2025-10-22 15:58:55 +0800

    c

--- rsvim_core/src/js/binding/global_this/text_encoder.rs
+++ rsvim_core/src/js/binding/global_this/text_encoder.rs
@@ -76,6 +76,11 @@ pub fn encode_into<'s>(
 ) {
   debug_assert!(args.length() == 2);
   debug_assert!(is_v8_str!(args.get(0)));
+  if !args.get(1).is_array_buffer_view() {
+    binding::throw_type_error(scope, &TheErr::BufferInvalid);
+    return;
+  }
+
   let payload = args.get(0).to_string(scope).unwrap();
   trace!(
     "|encode_into| payload:{:?}",
@@ -84,11 +89,6 @@ pub fn encode_into<'s>(
 
   let (data, read, written) = encode_impl(scope, payload);
   let store = v8::ArrayBuffer::new_backing_store_from_vec(data).make_shared();
-
-  if !args.get(1).is_array_buffer_view() {
-    binding::throw_type_error(scope, &TheErr::BufferInvalid);
-    return;
-  }
 
   let output = args.get(1).cast::<v8::ArrayBufferView>();
 
@@ -209,13 +209,17 @@ pub fn decode<'s>(
 ) {
   debug_assert!(args.length() == 3);
   debug_assert!(is_v8_obj!(args.get(0)));
+  if !args.get(1).is_array_buffer_view() {
+    binding::throw_type_error(scope, &TheErr::BufferInvalid);
+    return;
+  }
+
   let decoder_wrapper = args.get(0).to_object(scope).unwrap();
   let decoder_obj = TextDecoder::from_v8(scope, decoder_wrapper);
   let decoder_handle =
     binding::get_internal_ref::<RefCell<Decoder>>(scope, decoder_wrapper, 0);
   let mut decoder_handle = decoder_handle.borrow_mut();
 
-  debug_assert!(args.get(1).is_array_buffer_view());
   let buf = args.get(1).cast::<v8::ArrayBufferView>();
   let mut storage: Vec<u8> =
     Vec::with_capacity(v8::TYPED_ARRAY_MAX_SIZE_IN_HEAP);
