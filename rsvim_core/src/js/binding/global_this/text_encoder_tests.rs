21f9f286a2b8e1490072c18f83cb44622a17fab6
linrongbin16 <linrongbin16@outlook.com>
2025-10-23 12:41:00 +0800

    c

--- rsvim_core/src/js/binding/global_this/text_encoder_tests.rs
+++ rsvim_core/src/js/binding/global_this/text_encoder_tests.rs
@@ -103,6 +103,55 @@ async fn test_encode2() -> IoResult<()> {
 
 #[tokio::test]
 #[cfg_attr(miri, ignore)]
+async fn test_encode3() -> IoResult<()> {
+  test_log_init();
+
+  let terminal_cols = 10_u16;
+  let terminal_rows = 10_u16;
+
+  let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(50))];
+  let src: &str = r#"
+  const encoder = new TextEncoder();
+  const s1 = "This is some data";
+  const s2 = "你好，世界！";
+  const buf1 = new Uint8Array(16);
+  const buf2 = new Uint8Array(17);
+  const res1 = encoder.encodeInto(s1, buf1);
+  const res2 = encoder.encodeInto(s2, buf2);
+
+  if (res1.read !== s1.length || res1.written !== 17) {
+    Rsvim.cmd.echo(`buf1 failed, res1.read:${res1.read}, res1.written:${res1.written}, s1.length:${s1.length}`);
+  }
+
+  if (res2.read !== s2.length || res2.written !== 18) {
+    Rsvim.cmd.echo(`buf2 failed, res2.read:${res2.read}, res2.written:${res2.written}, s2.length:${s2.length}`);
+  }
+"#;
+
+  // Prepare $RSVIM_CONFIG/rsvim.js
+  let _tp = make_configs(vec![(Path::new("rsvim.js"), src)]);
+
+  let mut event_loop =
+    make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+  event_loop.initialize()?;
+  event_loop
+    .run_with_mock_events(MockEventReader::new(mocked_events))
+    .await?;
+  event_loop.shutdown()?;
+
+  // After
+  {
+    let contents = lock!(event_loop.contents);
+    let actual = contents.command_line_message_history().is_empty();
+    assert!(actual);
+  }
+
+  Ok(())
+}
+
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
 async fn test_decode1() -> IoResult<()> {
   test_log_init();
 
