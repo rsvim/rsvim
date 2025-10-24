b0477e0190ff8d8e861b1ef80c696ca68a47fc7e
linrongbin16 <linrongbin16@outlook.com>
2025-10-24 09:32:50 +0800

    c

--- rsvim_core/src/js/runtime/00__web.ts
+++ rsvim_core/src/js/runtime/00__web.ts
@@ -332,7 +332,7 @@ export class TextDecoder {
    * @see {@link !TextDecoder}
    *
    * @param {(ArrayBuffer | GlobalThis.TypedArray | DataView)} input? - (Optional) Bytes array, by default is `new Uint8Array()`.
-   * @param {TextDecoderDecodeOptions} options - Decode options, by default is `{stream: false}`. When decode a stream data (e.g. read from tcp network) while reading it and cannot determine the end of bytes, should set `stream` option to `true`.
+   * @param {TextDecoderDecodeOptions} options? - (Optional) Decode options, by default is `{stream: false}`. When decode a stream data (e.g. read from tcp network) while reading it and cannot determine the end of bytes, should set `stream` option to `true`.
    * @returns {string} Decoded string text.
    * @throws Throws {@link !TypeError} if input is not a Uint8Array, or options is invalid, or the data is malformed and `fatal` option is set.
    */
@@ -352,6 +352,7 @@ export class TextDecoder {
       buffer = input.buffer as ArrayBuffer;
     }
 
+    options = options ?? { stream: false };
     checkIsObject(options, `"TextDecoder.decode" options`);
     if (!Object.hasOwn(options, "stream")) {
       options.stream = false;
