5f0c6448ac9b85bb89745f7a45bd7af4206615c2
linrongbin16 <linrongbin16@outlook.com>
2025-10-24 22:55:42 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs/close.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs/close.rs
@@ -12,12 +12,11 @@ pub fn fs_close<'s>(
   scope: &mut v8::PinScope<'s, '_>,
   file_wrapper: v8::Local<'s, v8::Object>,
 ) {
-  if let Some(fd) =
+  if let Some(file) =
     get_cppgc_handle!(scope, file_wrapper, Option<Arc<Mutex<File>>>).take()
   {
     // Note: By taking the file reference out of the option and immediately dropping
     // it will make rust to close the file.
-    let file = handle::std_from_fd(fd);
     drop(file);
   } else {
     binding::throw_exception(scope, &TheErr::FileAlreadyClosed);
