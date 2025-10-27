36fe288796aba1c205dae85fa2897214a6201dab
linrongbin16 <linrongbin16@outlook.com>
2025-10-27 10:26:33 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs_tests.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs_tests.rs
@@ -3,6 +3,7 @@ use crate::prelude::*;
 use crate::tests::evloop::*;
 use crate::tests::log::init as test_log_init;
 use assert_fs::prelude::FileTouch;
+use assert_fs::prelude::FileWriteStr;
 use ringbuf::traits::Consumer;
 use ringbuf::traits::Observer;
 use std::time::Duration;
@@ -303,6 +304,50 @@ try {{
     let actual = actual.unwrap();
     assert!(actual.contains("Failed to open file"));
     assert!(!tmpfile.exists());
+  }
+
+  Ok(())
+}
+
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
+async fn test_read_write1() -> IoResult<()> {
+  test_log_init();
+
+  let terminal_cols = 10_u16;
+  let terminal_rows = 10_u16;
+  let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(50))];
+  let tmpfile = assert_fs::NamedTempFile::new("README.md").unwrap();
+  info!("tmpfile:{:?}", tmpfile);
+  tmpfile.touch().unwrap();
+  tmpfile.write_str("Hello, World!");
+
+  let src = format!(
+    r#"
+  using f = await Rsvim.fs.open({:?});
+    "#,
+    tmpfile.path()
+  );
+  info!("src:{:?}", src);
+
+  // Prepare $RSVIM_CONFIG/rsvim.js
+  let _tp = make_configs(vec![(Path::new("rsvim.js"), &src)]);
+
+  let mut event_loop =
+    make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+  event_loop.initialize()?;
+  event_loop
+    .run_with_mock_events(MockEventReader::new(mocked_events))
+    .await?;
+  event_loop.shutdown()?;
+
+  // After running
+  {
+    let contents = lock!(event_loop.contents);
+    let actual = contents.command_line_message_history().is_empty();
+    assert!(actual);
+    assert!(tmpfile.exists());
   }
 
   Ok(())
