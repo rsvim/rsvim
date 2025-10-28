d49d38c5f4b2c8d64cfd69d437a37dddbe216026
linrongbin16 <linrongbin16@outlook.com>
2025-10-28 09:07:07 +0800

    c

--- rsvim_core/src/js/runtime/01__rsvim.ts
+++ rsvim_core/src/js/runtime/01__rsvim.ts
@@ -268,26 +268,10 @@ export class RsvimCmd {
    *
    * @example
    * ```javascript
-   * // For command that is pure sync, i.e. doesn't contain async/await:
-   * function writeSync(ctx: any): void {
+   * async function write(ctx: any): void {
    *   try {
    *     const bytes = Rsvim.buf.writeSync(bufId);
    *     Rsvim.cmd.echo(`Buffer ${bufId} has been saved, ${bytes} bytes written`);
-   *   } catch (e) {
-   *     Rsvim.cmd.echo(`Error: failed to save buffer ${bufId}, exception: ${e}`);
-   *   }
-   * }
-   * Rsvim.cmd.create("writeSync", writeSync);
-   *
-   * // For command function that contains async/await:
-   * async function write(ctx: any): void {
-   *   try {
-   *     const file = await Rsvim.fs.open("README");
-   *     const buf = new Uint8Array(100);
-   *     const n = await file.read(buf);
-   *     const msg = new TextDecoder.decode(buf);
-   *     const bytes = Rsvim.buf.writeSync(bufId);
-   *     Rsvim.cmd.echo(`Buffer ${bufId} has been saved, ${bytes} bytes written with message: ${msg}`);
    *   } catch (e) {
    *     Rsvim.cmd.echo(`Error: failed to save buffer ${bufId}, exception: ${e}`);
    *   }
