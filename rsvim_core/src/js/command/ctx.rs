d5eb358f4aeec06e10d518ca13f703332c20b302
linrongbin16 <linrongbin16@outlook.com>
2025-10-31 15:08:53 +0800

    c

--- rsvim_core/src/js/command/ctx.rs
+++ rsvim_core/src/js/command/ctx.rs
@@ -15,8 +15,8 @@ pub const CURRENT_WINDOW_ID: &str = "currentWindowId";
 /// Default command attributes.
 pub const BANG_DEFAULT: bool = false;
 pub const ARGS_DEFAULT: Vec<CompactString> = vec![];
-pub const CURRENT_BUFFER_ID_DEFAULT: Option<BufferId> = None;
-pub const CURRENT_WINDOW_ID_DEFAULT: Option<TreeNodeId> = None;
+pub const CURRENT_BUFFER_ID_DEFAULT: BufferId = -1;
+pub const CURRENT_WINDOW_ID_DEFAULT: TreeNodeId = -1;
 
 #[derive(Debug, Clone, PartialEq, Eq, derive_builder::Builder)]
 pub struct CommandContext {
@@ -28,10 +28,10 @@ pub struct CommandContext {
   pub args: Vec<CompactString>,
 
   #[builder(default = CURRENT_BUFFER_ID_DEFAULT)]
-  pub current_buffer_id: Option<BufferId>,
+  pub current_buffer_id: BufferId,
 
   #[builder(default = CURRENT_WINDOW_ID_DEFAULT)]
-  pub current_window_id: Option<BufferId>,
+  pub current_window_id: TreeNodeId,
 }
 
 impl StructToV8 for CommandContext {
@@ -41,13 +41,10 @@ impl StructToV8 for CommandContext {
   ) -> v8::Local<'s, v8::Object> {
     let obj = v8::Object::new(scope);
 
-    debug_assert!(self.current_buffer_id.is_some());
-    debug_assert!(self.current_window_id.is_some());
-
     to_v8_prop!(self, obj, scope, bang);
     to_v8_prop!(self, obj, scope, args, Vec);
-    to_v8_prop!(self, obj, scope, current_buffer_id, optional);
-    to_v8_prop!(self, obj, scope, current_window_id, optional);
+    to_v8_prop!(self, obj, scope, current_buffer_id);
+    to_v8_prop!(self, obj, scope, current_window_id);
 
     obj
   }
