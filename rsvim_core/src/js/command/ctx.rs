0bc77759e0bd444ef541901fb2545700c31b786a
linrongbin16 <linrongbin16@outlook.com>
2025-10-31 14:29:42 +0800

    c

--- rsvim_core/src/js/command/ctx.rs
+++ rsvim_core/src/js/command/ctx.rs
@@ -1,7 +1,9 @@
 //! Ex command runtime context.
 
+use crate::buf::BufferId;
 use crate::js::converter::*;
 use crate::to_v8_prop;
+use crate::ui::tree::TreeNodeId;
 use compact_str::CompactString;
 
 /// Command attribute name.
@@ -13,6 +15,8 @@ pub const CURRENT_WINDOW_ID: &str = "currentWindowId";
 /// Default command attributes.
 pub const BANG_DEFAULT: bool = false;
 pub const ARGS_DEFAULT: Vec<CompactString> = vec![];
+pub const CURRENT_BUFFER_ID_DEFAULT: Option<BufferId> = None;
+pub const CURRENT_WINDOW_ID_DEFAULT: Option<TreeNodeId> = None;
 
 #[derive(Debug, Clone, PartialEq, Eq, derive_builder::Builder)]
 pub struct CommandContext {
@@ -23,8 +27,11 @@ pub struct CommandContext {
   #[builder(default = ARGS_DEFAULT)]
   pub args: Vec<CompactString>,
 
-  #[builder(default = ARGS_DEFAULT)]
-  pub args: Vec<CompactString>,
+  #[builder(default = CURRENT_BUFFER_ID_DEFAULT)]
+  pub current_buffer_id: Option<BufferId>,
+
+  #[builder(default = CURRENT_WINDOW_ID_DEFAULT)]
+  pub current_window_id: Option<BufferId>,
 }
 
 impl StructToV8 for CommandContext {
