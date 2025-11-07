b427d28dd42daa9a2967fcf53835be969ee4892d
linrongbin16 <linrongbin16@outlook.com>
2025-11-07 17:00:46 +0800

    c

--- rsvim_core/src/ui/widget/window/content_tests.rs
+++ rsvim_core/src/ui/widget/window/content_tests.rs
@@ -218,7 +218,10 @@ mod tests_nowrap {
   fn new4() {
     test_log_init();
 
-    let terminal_size = U16Size::new(31, 20);
+    let terminal_size = U16Size {
+      width: 31,
+      height: 20,
+    };
     let buf_opts = BufferOptionsBuilder::default().build().unwrap();
     let win_opts = WindowOptionsBuilder::default().wrap(false).build().unwrap();
 
