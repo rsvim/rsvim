e546dd1857fdaf0d8c0e655461958c5cfce06df6
linrongbin16 <linrongbin16@outlook.com>
2025-11-07 16:38:14 +0800

    c

--- rsvim_core/src/coord_tests.rs
+++ rsvim_core/src/coord_tests.rs
@@ -10,8 +10,8 @@ use std::mem;
 fn cast_geo_points() {
   let p1: IPos = IPos { x: 1, y: 2 };
   let actual1 = geo_point_as!(p1, usize);
-  let actual1_x = actual1.x();
-  let actual1_y = actual1.y();
+  let actual1_x = actual1.x;
+  let actual1_y = actual1.y;
   assert_eq!(mem::size_of_val(&actual1_x), mem::size_of_val(&1_usize));
   assert_eq!(mem::size_of_val(&actual1_y), mem::size_of_val(&2_usize));
 
@@ -20,8 +20,8 @@ fn cast_geo_points() {
     y: 25_u16,
   };
   let actual2 = geo_point_as!(p2, i32);
-  let actual2_x = actual2.x();
-  let actual2_y = actual2.y();
+  let actual2_x = actual2.x;
+  let actual2_y = actual2.y;
   assert_eq!(mem::size_of_val(&actual2_x), mem::size_of_val(&15_i32));
   assert_eq!(mem::size_of_val(&actual2_y), mem::size_of_val(&25_i32));
 
@@ -30,8 +30,8 @@ fn cast_geo_points() {
     y: 88_u32,
   };
   let actual3 = geo_point_as!(p3, i16);
-  let actual3_x = actual3.x();
-  let actual3_y = actual3.y();
+  let actual3_x = actual3.x;
+  let actual3_y = actual3.y;
   assert_eq!(mem::size_of_val(&actual3_x), mem::size_of_val(&78_i16));
   assert_eq!(mem::size_of_val(&actual3_y), mem::size_of_val(&88_i16));
 }
