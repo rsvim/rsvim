c0d93c974598252e2da6c5ce1fb9c498ef928d62
linrongbin16 <linrongbin16@outlook.com>
2025-11-07 23:04:00 +0800

    c

--- rsvim_core/src/ui/tree/internal/shapes.rs
+++ rsvim_core/src/ui/tree/internal/shapes.rs
@@ -4,6 +4,7 @@
 
 use crate::geo_point_as;
 use crate::prelude::*;
+use num_traits::clamp;
 use std::cmp::max;
 use std::cmp::min;
 
@@ -24,22 +25,26 @@ pub fn make_actual_shape(
   let parent_actual_top_left_pos: U16Pos = parent_actual_shape.min();
   let parent_actual_top_left_ipos: IPos =
     geo_point_as!(parent_actual_top_left_pos, isize);
-  let parent_actual_bottom_right_pos: U16Pos = parent_actual_shape.max().into();
+  let parent_actual_bottom_right_pos: U16Pos = parent_actual_shape.max();
   let parent_actual_bottom_right_ipos: IPos =
     geo_point_as!(parent_actual_bottom_right_pos, isize);
 
-  let top_left_pos: IPos = shape.min().into();
-  let bottom_right_pos: IPos = shape.max().into();
+  let top_left_pos: IPos = shape.min();
+  let bottom_right_pos: IPos = shape.max();
 
   let actual_top_left_ipos: IPos = top_left_pos + parent_actual_top_left_ipos;
-  let actual_top_left_x = min(
-    max(actual_top_left_ipos.x, parent_actual_top_left_ipos.x),
+
+  let actual_top_left_x = clamp(
+    actual_top_left_ipos.x,
+    parent_actual_top_left_ipos.x,
     parent_actual_bottom_right_ipos.x,
   );
-  let actual_top_left_y = min(
-    max(actual_top_left_ipos.y, parent_actual_top_left_ipos.y),
+  let actual_top_left_y = clamp(
+    actual_top_left_ipos.y,
+    parent_actual_top_left_ipos.y,
     parent_actual_bottom_right_ipos.y,
   );
+
   let actual_top_left_pos: U16Pos = U16Pos {
     x: actual_top_left_x as u16,
     y: actual_top_left_y as u16,
@@ -51,12 +56,14 @@ pub fn make_actual_shape(
 
   let actual_bottom_right_ipos: IPos =
     bottom_right_pos + parent_actual_top_left_ipos;
-  let actual_bottom_right_x = min(
-    max(actual_bottom_right_ipos.x, parent_actual_top_left_ipos.x),
+  let actual_bottom_right_x = clamp(
+    actual_bottom_right_ipos.x,
+    parent_actual_top_left_ipos.x,
     parent_actual_bottom_right_ipos.x,
   );
-  let actual_bottom_right_y = min(
-    max(actual_bottom_right_ipos.y, parent_actual_top_left_ipos.y),
+  let actual_bottom_right_y = clamp(
+    actual_bottom_right_ipos.y,
+    parent_actual_top_left_ipos.y,
     parent_actual_bottom_right_ipos.y,
   );
   let actual_bottom_right_pos: U16Pos = U16Pos {
@@ -94,12 +101,14 @@ pub fn bound_size(shape: &IRect, parent_actual_shape: &U16Rect) -> IRect {
   let top_left_pos: IPos = shape.min();
 
   // Truncate shape if size is larger than parent.
-  let height = max(min(shape.height, parent_actual_shape.height as isize), 0);
-  let width = max(min(shape.width(), parent_actual_shape.width() as isize), 0);
-  IRect::new(
-    top_left_pos,
-    point!(x: top_left_pos.x() + width, y: top_left_pos.y() + height),
-  )
+  let height = clamp(shape.height, 0, parent_actual_shape.height as isize);
+  let width = clamp(shape.width, 0, parent_actual_shape.width as isize);
+  IRect {
+    left: top_left_pos.x,
+    top: top_left_pos.y,
+    right: top_left_pos.x + width,
+    bottom: top_left_pos.y + height,
+  }
 }
 
 /// Bound child position by its parent actual shape.
