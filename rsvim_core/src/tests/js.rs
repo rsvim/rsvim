20b4872a5c16f040fa4e3f417e7cebd3937d390d
linrongbin16 <linrongbin16@outlook.com>
2025-11-10 10:53:40 +0800

    c

--- rsvim_core/src/tests/js.rs
+++ rsvim_core/src/tests/js.rs
@@ -5,6 +5,7 @@ use crate::js::JsRuntime;
 use crate::js::JsRuntimeOptions;
 use crate::js::command::CommandsManager;
 use crate::prelude::*;
+use crate::size;
 use crate::ui::tree::Tree;
 use std::time::Instant;
 use std::time::SystemTime;
@@ -12,10 +13,7 @@ use std::time::UNIX_EPOCH;
 use tokio::sync::mpsc::unbounded_channel;
 
 pub fn make_js_runtime() -> JsRuntime {
-  let canvas_size = U16Size {
-    width: 10,
-    height: 10,
-  };
+  let canvas_size = size!(10, 10);
   let (master_tx, _master_rx) = unbounded_channel();
   let (_jsrt_tx, jsrt_rx) = unbounded_channel();
 
