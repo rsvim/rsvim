e2100b21e4602c61923493192051ac35b6c84ae3
linrongbin16 <linrongbin16@outlook.com>
2025-11-10 11:55:16 +0800

    c

--- rsvim_core/src/ui/tree/internal/shapes_tests.rs
+++ rsvim_core/src/ui/tree/internal/shapes_tests.rs
@@ -121,108 +121,18 @@ fn bound_shape1() {
   // test_log_init();
 
   let inputs: Vec<(IRect, U16Rect)> = vec![
-    (
-      IRect {
-        left: 0,
-        top: 0,
-        right: 7,
-        bottom: 8,
-      },
-      U16Rect {
-        left: 0,
-        top: 0,
-        right: 10,
-        bottom: 10,
-      },
-    ),
-    (
-      IRect {
-        left: 3,
-        top: 2,
-        right: 17,
-        bottom: 11,
-      },
-      U16Rect {
-        left: 0,
-        top: 0,
-        right: 10,
-        bottom: 10,
-      },
-    ),
-    (
-      IRect {
-        left: 7,
-        top: -2,
-        right: 13,
-        bottom: 8,
-      },
-      U16Rect {
-        left: 0,
-        top: 0,
-        right: 5,
-        bottom: 5,
-      },
-    ),
-    (
-      IRect {
-        left: -5,
-        top: 8,
-        right: 3,
-        bottom: 16,
-      },
-      U16Rect {
-        left: 3,
-        top: 7,
-        right: 13,
-        bottom: 17,
-      },
-    ),
-    (
-      IRect {
-        left: -5,
-        top: 17,
-        right: 1,
-        bottom: 21,
-      },
-      U16Rect {
-        left: 10,
-        top: 15,
-        right: 18,
-        bottom: 23,
-      },
-    ),
+    (rect!(0, 0, 7, 8), rect!(0, 0, 10, 10)),
+    (rect!(3, 2, 17, 11), rect!(0, 0, 10, 10)),
+    (rect!(7, -2, 13, 8), rect!(0, 0, 5, 5)),
+    (rect!(-5, 8, 3, 16), rect!(3, 7, 13, 17)),
+    (rect!(-5, 17, 1, 21), rect!(10, 15, 18, 23)),
   ];
   let expects: Vec<IRect> = vec![
-    IRect {
-      left: 0,
-      top: 0,
-      right: 7,
-      bottom: 8,
-    },
-    IRect {
-      left: 0,
-      top: 1,
-      right: 10,
-      bottom: 10,
-    },
-    IRect {
-      left: 0,
-      top: 0,
-      right: 5,
-      bottom: 5,
-    },
-    IRect {
-      left: 0,
-      top: 2,
-      right: 8,
-      bottom: 10,
-    },
-    IRect {
-      left: 0,
-      top: 4,
-      right: 6,
-      bottom: 8,
-    },
+    rect!(0, 0, 7, 8),
+    rect!(0, 1, 10, 10),
+    rect!(0, 0, 5, 5),
+    rect!(0, 2, 8, 10),
+    rect!(0, 4, 6, 8),
   ];
   for (i, p) in inputs.iter().enumerate() {
     let actual = bound_shape(&p.0, &p.1);
