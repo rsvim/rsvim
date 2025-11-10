af0b00f5c1f8356682bd14c40c685bbb056a75ea
linrongbin16 <linrongbin16@outlook.com>
2025-11-10 11:20:31 +0800

    c

--- rsvim_core/src/ui/tree/internal/shapes_tests.rs
+++ rsvim_core/src/ui/tree/internal/shapes_tests.rs
@@ -18,18 +18,9 @@ fn make_actual_shapes1() {
   for t in inputs.iter() {
     for p in 0..10 {
       for q in 0..10 {
-        let input_actual_parent_shape = U16Rect {
-          left: 0,
-          top: 0,
-          right: p as u16,
-          bottom: q as u16,
-        };
-        let expect = U16Rect {
-          left: 0,
-          top: 0,
-          right: min(t.max().x, p) as u16,
-          bottom: min(t.max().y, q) as u16,
-        };
+        let input_actual_parent_shape = rect!(0, 0, p as u16, q as u16);
+        let expect =
+          rect!(0, 0, min(t.max().x, p) as u16, min(t.max().y, q) as u16);
         let actual = make_actual_shape(t, &input_actual_parent_shape);
         info!("expect:{:?}, actual:{:?}", expect, actual);
         assert_eq!(actual, expect);
@@ -43,108 +34,18 @@ fn make_actual_shapes2() {
   // test_log_init();
 
   let inputs: Vec<(IRect, U16Rect)> = vec![
-    (
-      IRect {
-        left: 0,
-        top: 0,
-        right: 3,
-        bottom: 5,
-      },
-      U16Rect {
-        left: 0,
-        top: 0,
-        right: 7,
-        bottom: 8,
-      },
-    ),
-    (
-      IRect {
-        left: -3,
-        top: 1,
-        right: 1,
-        bottom: 5,
-      },
-      U16Rect {
-        left: 3,
-        top: 2,
-        right: 9,
-        bottom: 8,
-      },
-    ),
-    (
-      IRect {
-        left: 3,
-        top: 9,
-        right: 6,
-        bottom: 10,
-      },
-      U16Rect {
-        left: 1,
-        top: 1,
-        right: 2,
-        bottom: 2,
-      },
-    ),
-    (
-      IRect {
-        left: 0,
-        top: 0,
-        right: 0,
-        bottom: 0,
-      },
-      U16Rect {
-        left: 0,
-        top: 0,
-        right: 0,
-        bottom: 0,
-      },
-    ),
-    (
-      IRect {
-        left: 5,
-        top: 3,
-        right: 6,
-        bottom: 4,
-      },
-      U16Rect {
-        left: 0,
-        top: 0,
-        right: 5,
-        bottom: 3,
-      },
-    ),
+    (rect!(0, 0, 3, 5), rect!(0, 0, 7, 8)),
+    (rect!(-3, 1, 1, 5), rect!(3, 2, 9, 8)),
+    (rect!(3, 9, 6, 10), rect!(1, 1, 2, 2)),
+    (rect!(0, 0, 0, 0), rect!(0, 0, 0, 0)),
+    (rect!(5, 3, 6, 4), rect!(0, 0, 5, 3)),
   ];
   let expects: Vec<U16Rect> = vec![
-    U16Rect {
-      left: 0,
-      top: 0,
-      right: 3,
-      bottom: 5,
-    },
-    U16Rect {
-      left: 3,
-      top: 3,
-      right: 4,
-      bottom: 7,
-    },
-    U16Rect {
-      left: 2,
-      top: 2,
-      right: 2,
-      bottom: 2,
-    },
-    U16Rect {
-      left: 0,
-      top: 0,
-      right: 0,
-      bottom: 0,
-    },
-    U16Rect {
-      left: 5,
-      top: 3,
-      right: 5,
-      bottom: 3,
-    },
+    rect!(0, 0, 3, 5),
+    rect!(3, 3, 4, 7),
+    rect!(2, 2, 2, 2),
+    rect!(0, 0, 0, 0),
+    rect!(5, 3, 5, 3),
   ];
   for (i, p) in inputs.iter().enumerate() {
     let actual = make_actual_shape(&p.0, &p.1);
