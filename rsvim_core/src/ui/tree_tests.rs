2550cc759d3840ffd8f986009142f5ed7ccc8c2b
linrongbin16 <linrongbin16@outlook.com>
2025-11-17 17:59:38 +0800

    c

--- rsvim_core/src/ui/tree_tests.rs
+++ rsvim_core/src/ui/tree_tests.rs
@@ -3,7 +3,7 @@ use crate::prelude::*;
 
 #[test]
 fn new() {
-  let terminal_size = U16Size::new(18, 10);
+  let terminal_size = size!(18, 10);
   let tree = Tree::new(terminal_size);
   assert!(tree.is_empty());
   assert!(tree.len() == 1);
