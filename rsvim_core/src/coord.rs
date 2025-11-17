ffdcbad5552522a7080e3642125fcc10f0807145
linrongbin16 <linrongbin16@outlook.com>
2025-11-17 16:34:47 +0800

    c

--- rsvim_core/src/coord.rs
+++ rsvim_core/src/coord.rs
@@ -42,86 +42,23 @@
 //! This is also compatible with the coordinates used in the
 //! [crossterm](https://docs.rs/crossterm/latest/crossterm/index.html) library.
 
-pub type Point = taffy::geometry::Point;
-use geo::Rect;
-
-// Positions {
-
-/// Position that uses [`isize`] as internal type.
+// Point
+pub type Point<T> = taffy::geometry::Point<T>;
 pub type IPos = Point<isize>;
-
-/// Position that uses [`usize`] as internal type.
 pub type UPos = Point<usize>;
-
-/// Position that uses [`u16`] as internal type. NOTE: This is especially for terminal devices.
 pub type U16Pos = Point<u16>;
 
-// Positions }
-
-// Rectangles {
-
-/// Rectangle that uses [`isize`] as internal type.
+// Rectangle
+pub type Rect<T> = taffy::geometry::Rect<T>;
 pub type IRect = Rect<isize>;
-
-/// Rectangle that uses [`usize`] as internal type.
 pub type URect = Rect<usize>;
-
-/// Rectangle that uses [`u16`] as internal type. NOTE: This is especially for terminal devices.
 pub type U16Rect = Rect<u16>;
 
-// Rectangles }
-
-// Size {
-
-#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Hash)]
-/// Generic rectangle size.
-pub struct Size<T: geo::CoordNum> {
-  width: T,
-  height: T,
-}
-
-impl<T> Size<T>
-where
-  T: geo::CoordNum,
-{
-  /// Make size from width(columns) and height(rows).
-  ///
-  /// NOTE: Width/columns is Y-axis, height/rows is X-axis.
-  pub fn new(width: T, height: T) -> Self {
-    Size { width, height }
-  }
-
-  /// Get width(columns).
-  pub fn width(&self) -> T {
-    self.width
-  }
-
-  /// Get height(rows).
-  pub fn height(&self) -> T {
-    self.height
-  }
-}
-
-impl<T> From<Rect<T>> for Size<T>
-where
-  T: geo::CoordNum,
-{
-  /// Make size from [`Rect`].
-  fn from(rect: Rect<T>) -> Size<T> {
-    Size::new(rect.width() as T, rect.height() as T)
-  }
-}
-
-/// Size that uses [`isize`] as internal type.
+// Size
+pub type Size<T> = taffy::geometry::Size<T>;
 pub type ISize = Size<isize>;
-
-/// Size that uses [`usize`] as internal type.
 pub type USize = Size<usize>;
-
-/// Size that uses [`u16`] as internal type. NOTE: This is especially for terminal devices.
 pub type U16Size = Size<u16>;
-
-// Size }
 
 /// Convert the generic type `T` inside `geo::Point<T>` to another type `U`.
 #[macro_export]
