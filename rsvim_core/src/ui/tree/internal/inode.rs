275831217e6086a4e96e876603780b2dbaf8493f
linrongbin16 <linrongbin16@outlook.com>
2025-11-18 11:52:52 +0800

    c

--- rsvim_core/src/ui/tree/internal/inode.rs
+++ rsvim_core/src/ui/tree/internal/inode.rs
@@ -2,6 +2,7 @@
 
 use crate::flags_impl;
 use crate::prelude::*;
+use crate::ui::tree::TaffyTreeWk;
 use std::fmt::Debug;
 use std::sync::atomic::AtomicI32;
 use std::sync::atomic::Ordering;
@@ -14,29 +15,7 @@ pub trait Inodeable: Sized + Clone + Debug {
 
   fn loid(&self) -> LayoutNodeId;
 
-  fn depth(&self) -> usize;
-
-  fn set_depth(&mut self, depth: usize);
-
-  fn zindex(&self) -> usize;
-
-  fn set_zindex(&mut self, zindex: usize);
-
-  fn shape(&self) -> &IRect;
-
-  fn set_shape(&mut self, shape: &IRect);
-
-  fn actual_shape(&self) -> &U16Rect;
-
-  fn set_actual_shape(&mut self, actual_shape: &U16Rect);
-
-  fn enabled(&self) -> bool;
-
-  fn set_enabled(&mut self, enabled: bool);
-
-  fn visible(&self) -> bool;
-
-  fn set_visible(&mut self, visible: bool);
+  fn lotree(&self) -> TaffyTreeWk;
 }
 
 /// Generate getter/setter for `Inode`.
