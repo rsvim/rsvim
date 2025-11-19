4fe67eceaf0c4dbd4b109013ac84674597a18554
linrongbin16 <linrongbin16@outlook.com>
2025-11-19 10:17:35 +0800

    c

--- rsvim_core/src/ui/tree/internal/itree.rs
+++ rsvim_core/src/ui/tree/internal/itree.rs
@@ -270,7 +270,10 @@ where
 #[derive(Debug)]
 /// The level-order iterator of the tree, start from tree root.
 ///
-/// For each node, it first visits the node itself, then visits all its children.
+/// It starts from tree root node, pushes the root node into a queue. Then it
+/// always visits the node popped from the queue, and pushes all the children
+/// nodes into the queue. Repeat this operation until the queue is empty.
+///
 /// For all the children under the same parent, it visits from lower z-index to higher, thus the higher z-index ones will cover those lower ones.
 /// This also follows the order when rendering the widget tree to terminal device.
 pub struct ItreeIter<'a, T>
