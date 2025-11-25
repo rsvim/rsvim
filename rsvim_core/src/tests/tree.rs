b14a8c55ee20bb70fe8629642f3da9ab7448c748
linrongbin16 <linrongbin16@outlook.com>
2025-11-25 13:06:44 +0800

    c

--- rsvim_core/src/tests/tree.rs
+++ rsvim_core/src/tests/tree.rs
@@ -37,27 +37,13 @@ pub fn make_tree_with_buffers(
     },
     ..Default::default()
   };
-  let cursor_style = Style {
-    position: taffy::Position::Absolute,
-    size: taffy::Size {
-      width: taffy::Dimension::from_length(1_u16),
-      height: taffy::Dimension::from_length(1_u16),
-    },
-    inset: taffy::Rect {
-      left: taffy::LengthPercentageAuto::from_length(0_u16),
-      top: taffy::LengthPercentageAuto::from_length(0_u16),
-      right: taffy::LengthPercentageAuto::AUTO,
-      bottom: taffy::LengthPercentageAuto::AUTO,
-    },
-    ..Default::default()
-  };
 
   let (_, buf) = buffers.first_key_value().unwrap();
   let window_id = tree
     .insert_new_window(
       tree_root_id,
       window_style,
-      tree.global_local_options(),
+      *tree.global_local_options(),
       Arc::downgrade(buf),
     )
     .unwrap();
@@ -84,7 +70,6 @@ pub fn make_tree_with_buffers_cmdline(
   let mut tree = lock!(tree_arc);
   tree.set_global_local_options(&window_local_opts);
   let tree_root_id = tree.root_id();
-  let tree_root_loid = tree.root_loid();
 
   let window_style = Style {
     size: taffy::Size {
@@ -100,88 +85,26 @@ pub fn make_tree_with_buffers_cmdline(
     },
     ..Default::default()
   };
-  let cursor_style = Style {
-    size: taffy::Size {
-      width: taffy::Dimension::from_length(1_u16),
-      height: taffy::Dimension::from_length(1_u16),
-    },
-    padding: taffy::Rect {
-      left: taffy::LengthPercentage::from_length(0_u16),
-      top: taffy::LengthPercentage::from_length(0_u16),
-      right: taffy::LengthPercentage::calc(
-        taffy::style::CompactLength::auto().calc_value(),
-      ),
-      bottom: taffy::LengthPercentage::calc(
-        taffy::style::CompactLength::auto().calc_value(),
-      ),
-    },
-    ..Default::default()
-  };
 
-  let (
-    window_loid,
-    window_shape,
-    cmdline_loid,
-    cmdline_shape,
-    cursor_loid,
-    cursor_shape,
-  ) = {
-    let lotree = tree.lotree();
-    let mut lo = lotree.borrow_mut();
-    let window_loid = lo.new_leaf(window_style).unwrap();
-    let cmdline_loid = lo.new_leaf(cmdline_style).unwrap();
-    let cursor_loid = lo.new_leaf(cursor_style).unwrap();
-    lo.add_child(tree_root_loid, window_loid).unwrap();
-    lo.add_child(tree_root_loid, cmdline_loid).unwrap();
-    lo.compute_layout(tree_root_loid, taffy::Size::MAX_CONTENT)
-      .unwrap();
-    let window_layout = lo.layout(window_loid).unwrap();
-    let cmdline_layout = lo.layout(cmdline_loid).unwrap();
-    let window_shape = rect_from_layout!(window_layout, u16);
-    let cmdline_shape = rect_from_layout!(cmdline_layout, u16);
-    let cursor_shape = rect!(0, 0, 1, 1); // dummy shape
-    (
-      window_loid,
-      window_shape,
-      cmdline_loid,
-      cmdline_shape,
-      cursor_loid,
-      cursor_shape,
-    )
-  };
-
-  // window
-  let mut window = {
-    let (_, buf) = buffers.first_key_value().unwrap();
-    Window::new(
-      tree.lotree(),
-      window_loid,
-      window_shape,
-      tree.global_local_options(),
+  let (_, buf) = buffers.first_key_value().unwrap();
+  let window_id = tree
+    .insert_new_window(
+      tree_root_id,
+      window_style,
+      *tree.global_local_options(),
       Arc::downgrade(buf),
     )
-    .unwrap()
-  };
-  let window_id = window.id();
-
-  // cursor
-  let cursor = Cursor::default(cursor_loid, cursor_shape);
-  window.insert_cursor(cursor);
-
-  tree._insert(tree_root_id, TreeNode::Window(window));
+    .unwrap();
+  let window_content_id = tree.window(window_id).unwrap().content_id();
+  let cursor_id = tree.insert_new_default_cursor(window_content_id).unwrap();
   tree.set_current_window_id(Some(window_id));
 
-  // command-line
-  let cmdline_shape = rect!(
-    0,
-    canvas_size.height().saturating_sub(1) as isize,
-    canvas_size.width() as isize,
-    canvas_size.height() as isize
+  let cmdline_id = tree.insert_new_cmdline(
+    tree_root_id,
+    cmdline_style,
+    IndicatorSymbol::Empty,
+    Arc::downgrade(&text_contents),
   );
-  let cmdline = CommandLine::new(cmdline_shape, Arc::downgrade(&text_contents));
-  let _cmdline_id = cmdline.id();
-
-  tree.bounded_insert(tree_root_id, TreeNode::CommandLine(cmdline));
 
   tree_arc.clone()
 }
