56eb41fc16185e44052279d3e373f0c7addce0e1
linrongbin16 <linrongbin16@outlook.com>
2025-11-26 16:55:45 +0800

    c

--- rsvim_core/src/ui/tree/internal.rs
+++ rsvim_core/src/ui/tree/internal.rs
@@ -1,17 +1,11 @@
 //! Internal tree structure.
 
-pub mod dummy;
-pub mod inode;
 pub mod itree;
 pub mod shapes;
 
-#[cfg(test)]
-mod inode_tests;
 #[cfg(test)]
 mod itree_tests;
 #[cfg(test)]
 mod shapes_tests;
 
-pub use dummy::*;
-pub use inode::*;
 pub use itree::*;
