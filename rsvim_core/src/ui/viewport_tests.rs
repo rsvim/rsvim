6453a2b4ed32bb71c5fc72a10becae01f5a46f11
linrongbin16 <linrongbin16@outlook.com>
2025-11-26 14:49:44 +0800

    c

--- rsvim_core/src/ui/viewport_tests.rs
+++ rsvim_core/src/ui/viewport_tests.rs
@@ -9741,7 +9741,7 @@ mod tests_search_anchor_upward_wrap_linebreak {
       ];
 
       let actual =
-        search_up_viewport(window.clone(), buf.clone(), 6, 295, 6, 317);
+        search_up_viewport(&mut tree, window_id, buf.clone(), 6, 295, 6, 317);
 
       let expect_start_fills: BTreeMap<usize, usize> =
         vec![(6, 0)].into_iter().collect();
@@ -9764,7 +9764,7 @@ mod tests_search_anchor_upward_wrap_linebreak {
         vec!["and\t", "word-wrap\t", "options\t", "are\tnot", "\tset.\n"];
 
       let actual =
-        search_up_viewport(window.clone(), buf.clone(), 5, 60, 5, 87);
+        search_up_viewport(&mut tree, window_id, buf.clone(), 5, 60, 5, 87);
 
       let expect_start_fills: BTreeMap<usize, usize> =
         vec![(5, 6)].into_iter().collect();
