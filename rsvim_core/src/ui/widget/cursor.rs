b3f19db2925d0112e5ea345954b747693b6273d6
linrongbin16 <linrongbin16@outlook.com>
2025-12-01 13:13:44 +0800

    c

--- rsvim_core/src/ui/widget/cursor.rs
+++ rsvim_core/src/ui/widget/cursor.rs
@@ -77,7 +77,7 @@ inode_impl!(Cursor);
 
 impl Widgetable for Cursor {
   fn draw(&self, canvas: &mut Canvas) {
-    let actual_shape = self.base.borrow().actual_shape(self.id).unwrap();
+    let actual_shape = self.actual_shape();
     let pos: U16Pos = actual_shape.min().into();
     // trace!(
     //   "draw, actual shape:{:?}, top-left pos:{:?}",
