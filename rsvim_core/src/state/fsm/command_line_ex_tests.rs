50b7676308fb925abab582392c594641f7d9e9eb
linrongbin16 <linrongbin16@outlook.com>
2025-12-04 11:50:59 +0800

    c

--- rsvim_core/src/state/fsm/command_line_ex_tests.rs
+++ rsvim_core/src/state/fsm/command_line_ex_tests.rs
@@ -1,7 +1,6 @@
 #![allow(unused_imports, dead_code, unused_variables)]
 
 use super::command_line_ex::*;
-use crate::tests::viewport::make_canvas_from_tree;
 use crate::buf::BufferArc;
 use crate::buf::BuffersManagerArc;
 use crate::buf::opt::BufferOptions;
@@ -23,6 +22,8 @@ use crate::tests::buf::make_buffers_manager;
 use crate::tests::log::init as test_log_init;
 use crate::tests::tree::make_tree_with_buffers;
 use crate::tests::tree::make_tree_with_buffers_cmdline;
+use crate::tests::viewport::assert_viewport;
+use crate::tests::viewport::make_canvas_from_tree;
 use crate::ui::canvas::Canvas;
 use crate::ui::canvas::CanvasArc;
 use crate::ui::tree::*;
@@ -146,8 +147,6 @@ pub fn make_tree_with_cmdline(
     lines,
   )
 }
-
-
 
 pub fn assert_canvas(actual: &Canvas, expect: &[&str]) {
   let actual = actual
