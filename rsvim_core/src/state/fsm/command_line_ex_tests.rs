02dd967dba85ebe285d2edbf727693c001741c08
linrongbin16 <linrongbin16@outlook.com>
2025-12-04 12:49:00 +0800

    c

--- rsvim_core/src/state/fsm/command_line_ex_tests.rs
+++ rsvim_core/src/state/fsm/command_line_ex_tests.rs
@@ -19,6 +19,9 @@ use crate::state::ops::Operation;
 use crate::state::ops::cursor_ops;
 use crate::tests::buf::make_buffer_from_lines;
 use crate::tests::buf::make_buffers_manager;
+use crate::tests::fsm::make_default_fsm_context;
+use crate::tests::fsm::make_default_fsm_context_with_cmdline;
+use crate::tests::fsm::make_fsm_context_with_cmdline;
 use crate::tests::log::init as test_log_init;
 use crate::tests::tree::make_tree_with_buffers;
 use crate::tests::tree::make_tree_with_buffers_cmdline;
@@ -47,108 +50,6 @@ use tokio::sync::mpsc::UnboundedReceiver;
 use tokio::sync::mpsc::UnboundedSender;
 use tokio::sync::mpsc::unbounded_channel;
 
-pub fn make_tree(
-  terminal_size: U16Size,
-  window_local_opts: WindowOptions,
-  lines: Vec<&str>,
-) -> (
-  Event,
-  TreeArc,
-  BuffersManagerArc,
-  BufferArc,
-  TextContentsArc,
-  StateDataAccess,
-) {
-  let buf_opts = BufferOptionsBuilder::default().build().unwrap();
-  let buf = make_buffer_from_lines(terminal_size, buf_opts, lines);
-  let bufs = make_buffers_manager(buf_opts, vec![buf.clone()]);
-  let tree =
-    make_tree_with_buffers(terminal_size, window_local_opts, bufs.clone());
-  let contents = TextContents::to_arc(TextContents::new(terminal_size));
-
-  let key_event = KeyEvent::new_with_kind(
-    KeyCode::Char('a'),
-    KeyModifiers::empty(),
-    KeyEventKind::Press,
-  );
-  let event = Event::Key(key_event);
-
-  let (jsrt_forwarder_tx, _jsrt_forwarder_rx) = unbounded_channel();
-  let (master_tx, _master_rx) = unbounded_channel();
-  let data_access = StateDataAccess::new(
-    tree.clone(),
-    bufs.clone(),
-    contents.clone(),
-    master_tx,
-    jsrt_forwarder_tx,
-  );
-
-  (event, tree, bufs, buf, contents, data_access)
-}
-
-pub fn make_tree_with_cmdline_and_buffer_options(
-  terminal_size: U16Size,
-  buffer_local_opts: BufferOptions,
-  window_local_opts: WindowOptions,
-  lines: Vec<&str>,
-) -> (
-  Event,
-  TreeArc,
-  BuffersManagerArc,
-  BufferArc,
-  TextContentsArc,
-  StateDataAccess,
-) {
-  let buf = make_buffer_from_lines(terminal_size, buffer_local_opts, lines);
-  let bufs = make_buffers_manager(buffer_local_opts, vec![buf.clone()]);
-  let contents = TextContents::to_arc(TextContents::new(terminal_size));
-  let tree = make_tree_with_buffers_cmdline(
-    terminal_size,
-    window_local_opts,
-    bufs.clone(),
-    contents.clone(),
-  );
-
-  let key_event = KeyEvent::new_with_kind(
-    KeyCode::Char('a'),
-    KeyModifiers::empty(),
-    KeyEventKind::Press,
-  );
-  let event = Event::Key(key_event);
-  let (jsrt_forwarder_tx, _jsrt_forwarder_rx) = unbounded_channel();
-  let (master_tx, _master_rx) = unbounded_channel();
-  let data_access = StateDataAccess::new(
-    tree.clone(),
-    bufs.clone(),
-    contents.clone(),
-    master_tx,
-    jsrt_forwarder_tx,
-  );
-
-  (event, tree, bufs, buf, contents, data_access)
-}
-
-pub fn make_tree_with_cmdline(
-  terminal_size: U16Size,
-  window_local_opts: WindowOptions,
-  lines: Vec<&str>,
-) -> (
-  Event,
-  TreeArc,
-  BuffersManagerArc,
-  BufferArc,
-  TextContentsArc,
-  StateDataAccess,
-) {
-  let buf_opts = BufferOptionsBuilder::default().build().unwrap();
-  make_tree_with_cmdline_and_buffer_options(
-    terminal_size,
-    buf_opts,
-    window_local_opts,
-    lines,
-  )
-}
-
 #[cfg(test)]
 mod tests_goto_normal_mode {
   use super::*;
@@ -162,7 +63,11 @@ mod tests_goto_normal_mode {
       WindowOptionsBuilder::default().wrap(false).build().unwrap();
     let lines = vec![];
     let (event, tree, bufs, _buf, contents, data_access) =
-      make_tree_with_cmdline(terminal_size, window_options, lines);
+      make_default_fsm_context_with_cmdline(
+        terminal_size,
+        window_options,
+        lines,
+      );
 
     let prev_cursor_viewport = lock!(tree.clone())
       .current_window()
@@ -315,7 +220,7 @@ mod tests_goto_normal_mode {
       WindowOptionsBuilder::default().wrap(false).build().unwrap();
     let lines = vec![];
     let (event, tree, bufs, _buf, contents, data_access) =
-      make_tree_with_cmdline_and_buffer_options(
+      make_fsm_context_with_cmdline(
         terminal_size,
         buf_opts,
         window_options,
@@ -474,7 +379,11 @@ mod tests_confirm_ex_command_and_goto_normal_mode {
       WindowOptionsBuilder::default().wrap(false).build().unwrap();
     let lines = vec![];
     let (event, tree, bufs, _buf, contents, data_access) =
-      make_tree_with_cmdline(terminal_size, window_options, lines);
+      make_default_fsm_context_with_cmdline(
+        terminal_size,
+        window_options,
+        lines,
+      );
 
     let prev_cursor_viewport = lock!(tree.clone())
       .current_window()
