60f1a08083e0a344bed04836061870852b7f26b4
linrongbin16 <linrongbin16@outlook.com>
2025-12-07 09:58:05 +0800

    c

--- rsvim_core/src/tests/viewport.rs
+++ rsvim_core/src/tests/viewport.rs
@@ -1,4 +1,5 @@
 #![allow(unused_imports)]
+#![allow(clippy::too_many_arguments)]
 
 use crate::buf::BufferArc;
 use crate::prelude::*;
@@ -85,5 +86,123 @@ pub fn assert_canvas(actual: &Canvas, expect: &[&str]) {
     info!("i-{}, actual[{}]:{:?}, expect[{}]:{:?}", i, i, a, i, e);
     assert_eq!(e.len(), a.len());
     assert_eq!(e, a);
+  }
+}
+
+pub fn assert_viewport(
+  buffer: BufferArc,
+  actual: &Viewport,
+  expect: &Vec<&str>,
+  expect_start_line: usize,
+  expect_end_line: usize,
+  expect_start_fills: &BTreeMap<usize, usize>,
+  expect_end_fills: &BTreeMap<usize, usize>,
+) {
+  info!(
+    "actual start_line/end_line:{:?}/{:?}",
+    actual.start_line_idx(),
+    actual.end_line_idx()
+  );
+  info!(
+    "expect start_line/end_line:{:?}/{:?}",
+    expect_start_line, expect_end_line
+  );
+  for (k, v) in actual.lines().iter() {
+    info!("actual line[{:?}]: {:?}", k, v);
+  }
+  for (i, e) in expect.iter().enumerate() {
+    info!("expect line[{}]:{:?}", i, e);
+  }
+  assert_eq!(expect_start_fills.len(), expect_end_fills.len());
+  for (k, start_v) in expect_start_fills.iter() {
+    let end_v = expect_end_fills.get(k).unwrap();
+    info!(
+      "expect start_fills/end_fills line[{}]:{:?}/{:?}",
+      k, start_v, end_v
+    );
+  }
+
+  assert_eq!(actual.start_line_idx(), expect_start_line);
+  assert_eq!(actual.end_line_idx(), expect_end_line);
+  if actual.lines().is_empty() {
+    assert!(actual.end_line_idx() <= actual.start_line_idx());
+  } else {
+    let (first_line_idx, _first_line_viewport) =
+      actual.lines().first().unwrap();
+    let (last_line_idx, _last_line_viewport) = actual.lines().last().unwrap();
+    assert_eq!(*first_line_idx, actual.start_line_idx());
+    assert_eq!(*last_line_idx, actual.end_line_idx() - 1);
+  }
+  assert_eq!(
+    actual.end_line_idx() - actual.start_line_idx(),
+    actual.lines().len()
+  );
+
+  let buffer = lock!(buffer);
+  let buflines = buffer.text().rope().lines_at(actual.start_line_idx());
+  let total_lines = expect_end_line - expect_start_line;
+
+  for (l, line) in buflines.enumerate() {
+    if l >= total_lines {
+      break;
+    }
+    let actual_line_idx = l + expect_start_line;
+    let line_viewport = actual.lines().get(&actual_line_idx).unwrap();
+
+    info!(
+      "l-{:?}, actual_line_idx:{}, line_viewport:{:?}",
+      actual.start_line_idx() + l,
+      actual_line_idx,
+      line_viewport
+    );
+    info!(
+      "l-{:?},start_filled_cols (expect/actual):{:?}/{}, end_filled_cols (expect/actual):{:?}/{}",
+      actual.start_line_idx() + l,
+      expect_start_fills.get(&actual_line_idx),
+      line_viewport.start_filled_cols(),
+      expect_end_fills.get(&actual_line_idx),
+      line_viewport.end_filled_cols()
+    );
+    assert_eq!(
+      line_viewport.start_filled_cols(),
+      *expect_start_fills.get(&actual_line_idx).unwrap()
+    );
+    assert_eq!(
+      line_viewport.end_filled_cols(),
+      *expect_end_fills.get(&actual_line_idx).unwrap()
+    );
+
+    let rows = &line_viewport.rows();
+    for (r, row) in rows.iter() {
+      info!("row-index-{:?}, row:{:?}", r, row);
+
+      if r > rows.first().unwrap().0 {
+        let prev_r = r - 1;
+        let prev_row = rows.get(&prev_r).unwrap();
+        info!(
+          "row-{:?}, current[{}]:{:?}, previous[{}]:{:?}",
+          r, r, row, prev_r, prev_row
+        );
+      }
+      if r < rows.last().unwrap().0 {
+        let next_r = r + 1;
+        let next_row = rows.get(&next_r).unwrap();
+        info!(
+          "row-{:?}, current[{}]:{:?}, next[{}]:{:?}",
+          r, r, row, next_r, next_row
+        );
+      }
+
+      let mut payload = String::new();
+      for c_idx in row.start_char_idx()..row.end_char_idx() {
+        let c = line.get_char(c_idx).unwrap();
+        payload.push(c);
+      }
+      info!(
+        "row-{:?}, payload actual:{:?}, expect:{:?}",
+        r, payload, expect[*r as usize]
+      );
+      assert_eq!(payload, expect[*r as usize]);
+    }
   }
 }
