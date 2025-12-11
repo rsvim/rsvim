fc19b6fd1489837c4318806d24831f9c2464ecb1
linrongbin16 <linrongbin16@outlook.com>
2025-12-11 18:05:48 +0800

    c

--- rsvim_core/src/ui/tree/internal/itree.rs
+++ rsvim_core/src/ui/tree/internal/itree.rs
@@ -5,7 +5,7 @@ use crate::ui::tree::TreeNodeId;
 use crate::ui::tree::internal::Inodeable;
 use crate::ui::tree::internal::inode::next_node_id;
 use crate::ui::tree::internal::shapes;
-use compact_str::CompactStringExt;
+use itertools::Itertools;
 use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::fmt::Debug;
