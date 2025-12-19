1ebd2397a677d953205abe23c8177b0ba815c72e
linrongbin16 <linrongbin16@outlook.com>
2025-12-19 22:37:53 +0800

    c

--- rsvim_core/src/ui/tree/internal/itree.rs
+++ rsvim_core/src/ui/tree/internal/itree.rs
@@ -935,7 +935,6 @@ where
 
   /// Same with [`add_child`](Itree::add_child) method, with default values for
   /// below parameters:
-  ///
   /// - zindex: 0
   /// - enabled: true
   /// - policy: Truncate
