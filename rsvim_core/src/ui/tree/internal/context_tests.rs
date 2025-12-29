98060e589b31d2f48ac943af21e60d079f9fa41d
linrongbin16 <linrongbin16@outlook.com>
2025-12-29 15:41:16 +0800

    c

--- rsvim_core/src/ui/tree/internal/context_tests.rs
+++ rsvim_core/src/ui/tree/internal/context_tests.rs
@@ -128,8 +128,8 @@ fn insert1() {
   assert_eq!(ctx.children(nid1).unwrap().len(), 2);
   assert_eq!(ctx.children(nid2).unwrap().len(), 2);
   assert_eq!(ctx.children(nid3).unwrap().len(), 1);
-  assert_eq!(tree.children_ids(nid4).len(), 0);
-  assert_eq!(tree.children_ids(nid5).len(), 0);
+  assert_eq!(ctx.children(nid4).unwrap().len(), 0);
+  assert_eq!(ctx.children(nid5).unwrap().len(), 0);
   assert_eq!(tree.children_ids(nid6).len(), 0);
 
   let contains_child = |parent_id: TreeNodeId, child_id: TreeNodeId| -> bool {
