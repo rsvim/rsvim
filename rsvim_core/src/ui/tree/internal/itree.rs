74f0fd66c626d738b5b3a94d09de928331f28b98
linrongbin16 <linrongbin16@outlook.com>
2025-12-30 14:35:26 +0800

    c

--- rsvim_core/src/ui/tree/internal/itree.rs
+++ rsvim_core/src/ui/tree/internal/itree.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+use crate::ui::tree::internal::context::*;
 use crate::ui::tree::internal::inode::*;
 use itertools::Itertools;
 use std::collections::VecDeque;
@@ -15,12 +16,128 @@ pub struct Itree<T>
 where
   T: Inodeable,
 {
-  // Nodes collection, maps from node ID to its node struct.
-  nodes: FoldMap<TreeNodeId, T>,
+  // The tree context.
+  context: TreeContextRc,
 
-  // Maps parent and children edges. The parent edge weight is negative,
-  // children edges are positive. The edge weight of each child is increased
-  // with the order when they are inserted, i.e. the first child has the lowest
-  // edge weight, the last child has the highest edge weight.
-  relationships: RefCell<Relationships>,
+  // Node collections.
+  nodes: FoldMap<TreeNodeId, T>,
 }
+
+impl<T> Itree<T>
+where
+  T: Inodeable,
+{
+  pub fn new() -> Self {
+    Self {
+      context: TreeContext::to_rc(TreeContext::new()),
+      nodes: FoldMap::new(),
+    }
+  }
+
+  fn _internal_check(&self) {
+    debug_assert_eq!(self.context.borrow().len(), self.nodes.len());
+  }
+
+  pub fn len(&self) -> usize {
+    self._internal_check();
+    self.nodes.len()
+  }
+
+  pub fn is_empty(&self) -> bool {
+    self._internal_check();
+    self.nodes.is_empty()
+  }
+
+  pub fn context(&self) -> TreeContextRc {
+    self.context.clone()
+  }
+
+  pub fn root_id(&self) -> TreeNodeId {
+    self._internal_check();
+    self.context.borrow().root()
+  }
+
+  pub fn parent_id(&self, id: TreeNodeId) -> Option<TreeNodeId> {
+    self._internal_check();
+    self.context.borrow().parent(id)
+  }
+
+  pub fn children_ids(&self, id: TreeNodeId) -> Vec<TreeNodeId> {
+    self._internal_check();
+    self.context.borrow().children(id).unwrap_or_default()
+  }
+
+  /// Get node by its `id`.
+  pub fn node(&self, id: TreeNodeId) -> Option<&T> {
+    self._internal_check();
+    self.nodes.get(&id)
+  }
+
+  /// Get mutable node by its `id`.
+  pub fn node_mut(&mut self, id: TreeNodeId) -> Option<&mut T> {
+    self._internal_check();
+    self.nodes.get_mut(&id)
+  }
+
+  pub fn iter(&self) -> ItreeIter<'_> {
+    ItreeIter::new(self, Some(self.context.borrow().root()))
+  }
+}
+
+// ItreeIter {
+#[derive(Debug)]
+pub struct ItreeIter<'a, T>
+where
+  T: Inodeable,
+{
+  tree: &'a Itree<T>,
+  que: VecDeque<TreeNodeId>,
+}
+
+impl<'a, T> Iterator for ItreeIter<'a, T>
+where
+  T: Inodeable,
+{
+  type Item = &'a T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if let Some(id) = self.que.pop_front() {
+      // Visit all children nodes under a parent node by following Z-index,
+      // from higher to lower.
+      let children_ids_sorted_by_zindex = {
+        let ctx = self.tree.context();
+        let ctx = ctx.borrow();
+        ctx
+          .children(id)
+          .unwrap_or_default()
+          .iter()
+          .sorted_by_key(|i| ctx.zindex(**i).unwrap())
+          .rev()
+          .copied()
+          .collect_vec()
+      };
+      for child_id in children_ids_sorted_by_zindex {
+        if self.tree.node(child_id).is_some() {
+          self.que.push_back(child_id);
+        }
+      }
+      self.tree.node(id)
+    } else {
+      None
+    }
+  }
+}
+
+impl<'a, T> ItreeIter<'a, T>
+where
+  T: Inodeable,
+{
+  pub fn new(tree: &'a Itree<T>, start_id: Option<TreeNodeId>) -> Self {
+    let mut que = VecDeque::new();
+    if let Some(id) = start_id {
+      que.push_back(id);
+    }
+    Self { tree, que }
+  }
+}
+// ItreeIter }
