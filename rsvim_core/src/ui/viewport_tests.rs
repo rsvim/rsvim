88f5eadf11f7bf82ac5bd6e1ffc5e7e2906d5025
linrongbin16 <linrongbin16@outlook.com>
2025-12-30 17:45:07 +0800

    c

--- rsvim_core/src/ui/viewport_tests.rs
+++ rsvim_core/src/ui/viewport_tests.rs
@@ -1030,8 +1030,10 @@ mod tests_view_nowrap_startcol {
       "",
     ];
 
-    let mut window = make_window(terminal_size, buf.clone(), &win_opts);
-    let actual = update_viewport(buf.clone(), &mut window, 0, 60);
+    let (mut tree, window_id) =
+      make_window(terminal_size, buf.clone(), &win_opts);
+    let actual =
+      update_viewport(buf.clone(), tree.window_mut(window_id), 0, 60);
     let expect_fills: BTreeMap<usize, usize> = vec![
       (0, 0),
       (1, 0),
