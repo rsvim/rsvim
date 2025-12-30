5cf95b3e19a4be969aaf46687f7f9c5c367b0d57
linrongbin16 <linrongbin16@outlook.com>
2025-12-30 14:26:03 +0800

    c

--- rsvim_core/src/ui/tree_tests.rs
+++ rsvim_core/src/ui/tree_tests.rs
@@ -22,13 +22,13 @@ impl TestValue {
 #[test]
 fn new() {
   let terminal_size = size!(18, 10);
-  let tree = Tree::new(terminal_size);
+  let tree = Tree::new(terminal_size).unwrap();
   assert!(tree.is_empty());
   assert!(tree.len() == 1);
 }
 
 #[test]
-fn move_by1() {
+fn raw_move_position_by1() {
   // test_log_init();
 
   let s1 = rect!(0, 0, 20, 20);
@@ -93,6 +93,79 @@ fn move_by1() {
     let x = m.0;
     let y = m.1;
     tree.move_by(nid3, x, y);
+    let actual = *tree.node(nid3).unwrap().shape();
+    let expect = expects[i];
+    info!("i:{:?}, actual:{:?}, expect:{:?}", i, actual, expect);
+    assert!(actual == expect);
+  }
+}
+
+#[test]
+fn raw_move_position_to1() {
+  test_log_init();
+
+  let s1 = rect!(0, 0, 20, 20);
+  let n1 = TestValue::new(1, s1);
+  let nid1 = n1.id();
+
+  let s2 = rect!(0, 0, 20, 20);
+  let n2 = TestValue::new(2, s2);
+  let nid2 = n2.id();
+
+  let s3 = rect!(0, 0, 1, 1);
+  let n3 = TestValue::new(3, s3);
+  let nid3 = n3.id();
+
+  /*
+   * The tree looks like:
+   * ```
+   *           n1
+   *         /
+   *        n2
+   *       /
+   *      n3
+   * ```
+   */
+  let mut tree = Itree::new();
+  tree.new_root(n1);
+  tree.new_with_parent(nid1, n2);
+  tree.new_with_parent(nid2, n3);
+
+  let n1 = tree.node(nid1).unwrap();
+  let n2 = tree.node(nid2).unwrap();
+  let n3 = tree.node(nid3).unwrap();
+  print_node!(n1, "n1");
+  print_node!(n2, "n2");
+  print_node!(n3, "n3");
+
+  // n3 Move: (x, y)
+  let moves: Vec<(isize, isize)> = vec![
+    (-10, -4),
+    (2, -7),
+    (1, 90),
+    (-70, 41),
+    (23, -4),
+    (49, -121),
+    (8, 3),
+    (-10, -7),
+    (6, 8),
+  ];
+  let expects: Vec<IRect> = vec![
+    rect!(-10, -4, -9, -3),
+    rect!(2, -7, 3, -6),
+    rect!(1, 90, 2, 91),
+    rect!(-70, 41, -69, 42),
+    rect!(23, -4, 24, -3),
+    rect!(49, -121, 50, -120),
+    rect!(8, 3, 9, 4),
+    rect!(-10, -7, -9, -6),
+    rect!(6, 8, 7, 9),
+  ];
+
+  for (i, m) in moves.iter().enumerate() {
+    let x = m.0;
+    let y = m.1;
+    tree.move_to(nid3, x, y);
     let actual = *tree.node(nid3).unwrap().shape();
     let expect = expects[i];
     info!("i:{:?}, actual:{:?}, expect:{:?}", i, actual, expect);
@@ -166,79 +239,6 @@ fn bounded_move_by1() {
     let x = m.0;
     let y = m.1;
     tree.bounded_move_by(nid3, x, y);
-    let actual = *tree.node(nid3).unwrap().shape();
-    let expect = expects[i];
-    info!("i:{:?}, actual:{:?}, expect:{:?}", i, actual, expect);
-    assert!(actual == expect);
-  }
-}
-
-#[test]
-fn move_to1() {
-  test_log_init();
-
-  let s1 = rect!(0, 0, 20, 20);
-  let n1 = TestValue::new(1, s1);
-  let nid1 = n1.id();
-
-  let s2 = rect!(0, 0, 20, 20);
-  let n2 = TestValue::new(2, s2);
-  let nid2 = n2.id();
-
-  let s3 = rect!(0, 0, 1, 1);
-  let n3 = TestValue::new(3, s3);
-  let nid3 = n3.id();
-
-  /*
-   * The tree looks like:
-   * ```
-   *           n1
-   *         /
-   *        n2
-   *       /
-   *      n3
-   * ```
-   */
-  let mut tree = Itree::new();
-  tree.new_root(n1);
-  tree.new_with_parent(nid1, n2);
-  tree.new_with_parent(nid2, n3);
-
-  let n1 = tree.node(nid1).unwrap();
-  let n2 = tree.node(nid2).unwrap();
-  let n3 = tree.node(nid3).unwrap();
-  print_node!(n1, "n1");
-  print_node!(n2, "n2");
-  print_node!(n3, "n3");
-
-  // n3 Move: (x, y)
-  let moves: Vec<(isize, isize)> = vec![
-    (-10, -4),
-    (2, -7),
-    (1, 90),
-    (-70, 41),
-    (23, -4),
-    (49, -121),
-    (8, 3),
-    (-10, -7),
-    (6, 8),
-  ];
-  let expects: Vec<IRect> = vec![
-    rect!(-10, -4, -9, -3),
-    rect!(2, -7, 3, -6),
-    rect!(1, 90, 2, 91),
-    rect!(-70, 41, -69, 42),
-    rect!(23, -4, 24, -3),
-    rect!(49, -121, 50, -120),
-    rect!(8, 3, 9, 4),
-    rect!(-10, -7, -9, -6),
-    rect!(6, 8, 7, 9),
-  ];
-
-  for (i, m) in moves.iter().enumerate() {
-    let x = m.0;
-    let y = m.1;
-    tree.move_to(nid3, x, y);
     let actual = *tree.node(nid3).unwrap().shape();
     let expect = expects[i];
     info!("i:{:?}, actual:{:?}, expect:{:?}", i, actual, expect);
