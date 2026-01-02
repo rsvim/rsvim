e0297c25a76be6055a12cdad924a61b6fca8b51f
linrongbin16 <linrongbin16@outlook.com>
2026-01-02 23:44:32 +0800

    c

--- rsvim_core/src/state/ops/cmdline_ops.rs
+++ rsvim_core/src/state/ops/cmdline_ops.rs
@@ -4,6 +4,7 @@ use crate::content::TextContents;
 use crate::prelude::*;
 use crate::ui::tree::Inodeable;
 use crate::ui::tree::Tree;
+use crate::ui::tree::TreeNode;
 use crate::ui::viewport::CursorViewport;
 use crate::ui::viewport::Viewport;
 use compact_str::ToCompactString;
@@ -15,20 +16,27 @@ fn set_message(
   text_contents: &mut TextContents,
   payload: String,
 ) {
-  debug_assert!(tree.cmdline().is_some());
+  debug_assert!(tree.cmdline_id().is_some());
 
   let message_text = text_contents.command_line_message_mut();
   message_text.clear();
   message_text.insert_at(0, 0, payload.to_compact_string());
 
-  let cmdline = tree.cmdline_mut().unwrap();
-  let opts = *cmdline.options();
-  let actual_size = cmdline.message().actual_shape().size();
+  let (cmdline_opts, cmdline_message_id) = {
+    let cmdline = tree.cmdline();
+    (*cmdline.options(), cmdline.message_id())
+  };
+  let actual_size = match tree.node(cmdline_message_id).unwrap() {
+    TreeNode::CmdlineMessage(message) => message.actual_shape().size(),
+    _ => unreachable!(),
+  };
 
   let new_message_viewport =
-    Viewport::to_arc(Viewport::view(&opts, message_text, &actual_size, 0, 0));
+    Viewport::view(&cmdline_opts, message_text, &actual_size, 0, 0);
 
-  cmdline.set_message_viewport(new_message_viewport);
+  tree
+    .cmdline_mut()
+    .set_message_viewport(new_message_viewport);
 }
 
 pub fn cmdline_flush_pending_message(
