8c2345cbe5818162325ff3f0f92d33c8e9a4fb96
linrongbin16 <linrongbin16@outlook.com>
2026-01-02 23:40:50 +0800

    c

--- rsvim_core/src/state/fsm/cmdline_ex.rs
+++ rsvim_core/src/state/fsm/cmdline_ex.rs
@@ -137,8 +137,7 @@ impl CmdlineExStateful {
     let cmdline_id = tree.cmdline_id().unwrap();
     let current_window_id = tree.current_window_id().unwrap();
 
-    let _old_widget_id =
-      cursor_ops::cursor_jump(&mut tree, current_window_id).unwrap();
+    let _old_widget_id = cursor_ops::cursor_jump(&mut tree, current_window_id);
     debug_assert_eq!(_old_widget_id, Some(cmdline_id));
 
     let cursor_viewport = tree.editable_cursor_viewport(current_window_id);
