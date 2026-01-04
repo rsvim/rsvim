1f47f40d9a0c707966b0864448aa6d7c3d90e7be
linrongbin16 <linrongbin16@outlook.com>
2026-01-04 13:18:56 +0800

    c

--- rsvim_core/src/state/fsm.rs
+++ rsvim_core/src/state/fsm.rs
@@ -1,8 +1,8 @@
 //! Finite-state machine collections.
 
-pub mod cmdline_backward;
-pub mod cmdline_ex;
-pub mod cmdline_forward;
+pub mod command_line_ex;
+pub mod command_line_search_backward;
+pub mod command_line_search_forward;
 pub mod insert;
 pub mod normal;
 pub mod operator_pending;
@@ -11,15 +11,15 @@ pub mod terminal;
 pub mod visual;
 
 #[cfg(test)]
-mod cmdline_ex_tests;
+mod command_line_ex_tests;
 #[cfg(test)]
 mod insert_tests;
 #[cfg(test)]
 mod normal_tests;
 
-pub use cmdline_backward::CmdlineBackwardStateful;
-pub use cmdline_ex::CmdlineExStateful;
-pub use cmdline_forward::CmdlineForwardStateful;
+pub use command_line_ex::CmdlineExStateful;
+pub use command_line_search_backward::CmdlineBackwardStateful;
+pub use command_line_search_forward::CmdlineForwardStateful;
 pub use insert::InsertStateful;
 pub use normal::NormalStateful;
 pub use operator_pending::OperatorPendingStateful;
