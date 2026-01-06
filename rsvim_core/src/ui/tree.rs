94cb506cfa0b80de5f6f6fef4567da1125fbef86
linrongbin16 <linrongbin16@outlook.com>
2026-01-06 15:03:13 +0800

    c

--- rsvim_core/src/ui/tree.rs
+++ rsvim_core/src/ui/tree.rs
@@ -607,6 +607,7 @@ impl Tree {
         ..Default::default()
       };
       let message_style = Style {
+        display: taffy::Display::Flex,
         size: taffy::Size {
           width: taffy::Dimension::from_percent(1.0),
           height: taffy::Dimension::from_percent(1.0),
