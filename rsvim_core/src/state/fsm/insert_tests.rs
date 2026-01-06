ed7b53cc0ffde98c1ce8f1f5fb82dc4f961149f0
linrongbin16 <linrongbin16@outlook.com>
2026-01-06 11:07:59 +0800

    c

--- rsvim_core/src/state/fsm/insert_tests.rs
+++ rsvim_core/src/state/fsm/insert_tests.rs
@@ -4651,7 +4651,7 @@ mod tests_insert_text {
         "4th.      ",
       ];
       let actual_canvas =
-        make_canvas(terminal_size, window_options, buf.clone());
+        make_canvas(terminal_size, window_options, buf.clone(), viewport);
       assert_canvas(&actual_canvas, &expect_canvas);
     }
 
@@ -4697,7 +4697,7 @@ mod tests_insert_text {
         "4th.      ",
       ];
       let actual_canvas =
-        make_canvas(terminal_size, window_options, buf.clone());
+        make_canvas(terminal_size, window_options, buf.clone(), viewport);
       assert_canvas(&actual_canvas, &expect_canvas);
     }
 
@@ -4746,7 +4746,7 @@ mod tests_insert_text {
         "4th.      ",
       ];
       let actual_canvas =
-        make_canvas(terminal_size, window_options, buf.clone());
+        make_canvas(terminal_size, window_options, buf.clone(), viewport);
       assert_canvas(&actual_canvas, &expect_canvas);
     }
 
