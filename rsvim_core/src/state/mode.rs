ba29a87327c1c3462589a89f9026ac9d00ec8302
linrongbin16 <linrongbin16@outlook.com>
2026-01-07 17:02:04 +0800

    c

--- rsvim_core/src/state/mode.rs
+++ rsvim_core/src/state/mode.rs
@@ -19,7 +19,7 @@ pub const C: &str = "c";
 pub const COMMAND_LINE_SEARCH_FORWARD: &str = "command-line-search-forward";
 pub const CMDLINE_SEARCH_FORWARD: &str = "cmdline-search-forward";
 pub const COMMAND_LINE_SEARCH_BACKWARD: &str = "command-line-search-backward";
-pub const CMDLINE_LINE_SEARCH_BACKWARD: &str = "cmdline-search-backward";
+pub const CMDLINE_SEARCH_BACKWARD: &str = "cmdline-search-backward";
 
 #[derive(
   Debug,
