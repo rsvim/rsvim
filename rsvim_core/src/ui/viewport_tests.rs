861db2fa042c9d2e69b8c3656403f89660347a96
linrongbin16 <linrongbin16@outlook.com>
2026-01-08 13:10:22 +0800

    c

--- rsvim_core/src/ui/viewport_tests.rs
+++ rsvim_core/src/ui/viewport_tests.rs
@@ -510,28 +510,22 @@ mod tests_view_nowrap {
 
   #[test]
   fn new7() {
-    test_log_init();
-
-    let terminal_size = size!(20, 20);
-    let buf_opts = BufferOptionsBuilder::default().build().unwrap();
-    let win_opts = nowrap();
-
-    let buf = make_buffer_from_lines(terminal_size, buf_opts, vec![]);
-    let expect = vec![""];
-
-    let (tree, window_id) = make_window(terminal_size, buf.clone(), win_opts);
-    let actual = tree.window(window_id).unwrap().viewport();
+    let buffer_lines = vec![];
+    let expect_lines = vec![""];
     let expect_fills: BTreeMap<usize, usize> =
       vec![(0, 0)].into_iter().collect();
-    assert_viewport(
-      lock!(buf).text(),
-      &actual,
-      &expect,
-      0,
-      1,
-      &expect_fills,
-      &expect_fills,
-    );
+
+    run_lines(Arguments {
+      terminal_size: size!(20, 20),
+      buffer_opts: BufferOptionsBuilder::default().build().unwrap(),
+      window_opts: nowrap(),
+      buffer_lines,
+      expect_lines,
+      expect_start_line: 0,
+      expect_end_line: 1,
+      expect_start_fills: expect_fills.clone(),
+      expect_end_fills: expect_fills.clone(),
+    });
   }
 
   #[test]
