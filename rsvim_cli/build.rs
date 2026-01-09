26625c68c3e52b2913de17f6c7ae14bc818d8b45
linrongbin16 <linrongbin16@outlook.com>
2026-01-09 17:47:38 +0800

    c

--- rsvim_cli/build.rs
+++ rsvim_cli/build.rs
@@ -3,15 +3,15 @@ use rsvim_core::js::JsRuntimeForSnapshot;
 use rsvim_core::js::v8_version;
 use std::path::Path;
 
-pub const LOG_LEVEL: &str = "cargo:info=[RSVIM]";
-// pub const LOG_LEVEL: &str = "cargo:warning=[RSVIM]";
+pub const LOG: &str = "[RSVIM]";
+// pub const LOG: &str = "cargo:warning=[RSVIM]";
 
 fn version() {
   let profile = std::env::var("PROFILE").unwrap_or("debug".to_string());
   let opt_level = std::env::var("OPT_LEVEL").unwrap_or("0".to_string());
   let debug = std::env::var("DEBUG").unwrap_or("0".to_string());
   println!(
-    "{LOG_LEVEL} Raw profile:{:?}, opt_level:{:?}, debug:{:?}",
+    "{LOG} Raw profile:{:?}, opt_level:{:?}, debug:{:?}",
     profile, opt_level, debug
   );
 
@@ -23,14 +23,14 @@ fn version() {
     && (opt_level == "s" || opt_level == "z")
     && debug != "true";
   if is_release_profile {
-    println!("{LOG_LEVEL} Resolved profile:release");
+    println!("{LOG} Resolved profile:release");
   } else {
     let profile = if profile == "release" {
       "nightly".to_string()
     } else {
       profile
     };
-    println!("{LOG_LEVEL} Resolved profile:{:?}", profile);
+    println!("{LOG} Resolved profile:{:?}", profile);
     let maybe_git_commit = match Repository::open(&workspace_dir) {
       Ok(repo) => {
         let head = repo.head().unwrap();
@@ -38,16 +38,16 @@ fn version() {
         let commit = repo.find_commit(oid).unwrap();
         let id = commit.id();
         let id = id.to_string();
-        println!("{LOG_LEVEL} Git id:{:?}", id);
+        println!("{LOG} Git id:{:?}", id);
         format!("+{}", &id[0..8])
       }
       Err(e) => {
-        println!("{LOG_LEVEL} Git error:{:?}", e);
+        println!("{LOG} Git error:{:?}", e);
         "".to_string()
       }
     };
     println!(
-      "{LOG_LEVEL} Resolved version:{:?}, profile:{:?}, git_commit:{:?}",
+      "{LOG} Resolved version:{:?}, profile:{:?}, git_commit:{:?}",
       version, profile, maybe_git_commit
     );
     version = format!("{}+{}{}", version, profile, maybe_git_commit)
@@ -59,16 +59,16 @@ fn version() {
       Ok(parsed_manifest) => {
         let deps = &parsed_manifest["workspace"]["dependencies"];
         let parser = deps["swc_ecma_parser"].as_str();
-        println!("{LOG_LEVEL} Swc version, swc_ecma_parser:{:?}", parser);
+        println!("{LOG} Swc version, swc_ecma_parser:{:?}", parser);
         format!(", swc_ecma_parser {}", parser.unwrap())
       }
       Err(e) => {
-        println!("{LOG_LEVEL} Parse Cargo.toml error:{:?}", e);
+        println!("{LOG} Parse Cargo.toml error:{:?}", e);
         "".to_string()
       }
     },
     Err(e) => {
-      println!("{LOG_LEVEL} Read Cargo.toml error:{:?}", e);
+      println!("{LOG} Read Cargo.toml error:{:?}", e);
       "".to_string()
     }
   };
@@ -80,17 +80,14 @@ fn version() {
 
   let output_path =
     Path::new(env!("CARGO_MANIFEST_DIR")).join("RSVIM_VERSION.TXT");
-  println!(
-    "{LOG_LEVEL} Writing version into {:?}...",
-    output_path.as_path()
-  );
+  println!("{LOG} Writing version into {:?}...", output_path.as_path());
 
   std::fs::write(output_path.as_path(), version.as_bytes()).unwrap();
 }
 
 fn snapshot() {
   let js_runtime = JsRuntimeForSnapshot::new();
-  println!("{LOG_LEVEL} Build snapshot for rsvim cli...");
+  println!("{LOG} Build snapshot for rsvim cli...");
   let snapshot = js_runtime.create_snapshot();
   let snapshot = Box::from(&snapshot);
   let mut vec = Vec::with_capacity(snapshot.len());
@@ -98,10 +95,7 @@ fn snapshot() {
 
   let output_path =
     Path::new(env!("CARGO_MANIFEST_DIR")).join("RSVIM_SNAPSHOT.BIN");
-  println!(
-    "{LOG_LEVEL} Writing snapshot into {:?}...",
-    output_path.as_path()
-  );
+  println!("{LOG} Writing snapshot into {:?}...", output_path.as_path());
   std::fs::write(output_path.as_path(), vec.into_boxed_slice()).unwrap();
 }
 
