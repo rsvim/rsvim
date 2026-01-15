65191c46e951442a5c6de27426bc8a51f50bca72
linrongbin16 <linrongbin16@outlook.com>
2026-01-15 14:42:02 +0800

    c

--- rsvim_core/src/util/range_tree_map.rs
+++ rsvim_core/src/util/range_tree_map.rs
@@ -13,7 +13,7 @@ pub enum IsOverlappedResult {
 }
 
 #[derive(Clone, Default, Debug)]
-/// RangeTree is a specialized BTreeMap, which uses [`Range`] as its key. A
+/// RangeTreeMap is a specialized BTreeMap, which uses [`Range`] as its key. A
 /// range can be split into two or three new ranges if any insertion overlaps,
 /// new value will override old value on the same range. A position only
 /// belongs to one range.
