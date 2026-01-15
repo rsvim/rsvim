c424889c26e8ade769dc82d03f9205e8327a12ac
linrongbin16 <linrongbin16@outlook.com>
2026-01-15 15:05:15 +0800

    c

--- rsvim_core/src/util/range_tree_map_tests.rs
+++ rsvim_core/src/util/range_tree_map_tests.rs
@@ -26,20 +26,12 @@ fn test2() {
   test_log_init();
 
   let mut tree: RangeTreeMap<usize, i32> = RangeTreeMap::new();
-  tree.insert(15..25, 2);
-  println!("插入 key(start:15, end:25) = value(2)");
-  // tree.print_all();
-  println!();
-
-  // 重置并测试用例 2
-  println!("=== 测试用例 2 ===");
-  let mut tree2 = RangeTreeMap::new();
-  tree2.insert(10..20, 1);
-  tree2.insert(15..25, 2);
-  tree2.insert(11..13, 3);
-  println!("依次插入:");
-  println!("  key(start:10, end:20) = value(1)");
-  println!("  key(start:15, end:25) = value(2)");
-  println!("  key(start:11, end:13) = value(3)");
-  // tree2.print_all();
+  tree.insert(15..25, 1);
+  info!("tree-1:{:?}", tree);
+  tree.insert(10..20, 2);
+  info!("tree-2:{:?}", tree);
+  tree.insert(15..25, 3);
+  info!("tree-3:{:?}", tree);
+  tree.insert(11..13, 4);
+  info!("tree-4:{:?}", tree);
 }
