c8fc9d1dad44ac2a49d6dcedfc4874335bc97a89
linrongbin16 <linrongbin16@outlook.com>
2026-01-20 15:09:35 +0800

    c

--- rsvim_core/src/buf/change_tests.rs
+++ rsvim_core/src/buf/change_tests.rs
@@ -1,6 +1,15 @@
 use super::change::*;
+use compact_str::CompactString;
+use compact_str::ToCompactString;
 
 #[test]
 fn insert1() {
   let mut change_manager = ChangeManager::new();
+  let payload = "Hello, World!";
+  for (i, c) in payload.chars().enumerate() {
+    change_manager.save(Operation::Insert(Insert {
+      char_idx: i,
+      payload: c.to_string().to_compact_string(),
+    }));
+  }
 }
