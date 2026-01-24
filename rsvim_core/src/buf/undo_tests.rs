3b7ae62c0968e224e3e344031fc2615023929b54
linrongbin16 <linrongbin16@outlook.com>
2026-01-24 22:03:33 +0800

    c

--- rsvim_core/src/buf/undo_tests.rs
+++ rsvim_core/src/buf/undo_tests.rs
@@ -346,7 +346,15 @@ fn delete2() {
   let actual = undo_manager.current();
   assert_eq!(actual.records().len(), 2);
 
-  assert_insert(&undo_manager, 0, 0, payload1);
+  assert_insert(
+    &undo_manager,
+    0,
+    Insert {
+      payload: payload1.to_compact_string(),
+      char_idx_before: 0,
+      char_idx_after: payload1.chars().count(),
+    },
+  );
   assert_delete(&undo_manager, 1, 8, "orld!");
 
   undo_manager.commit();
