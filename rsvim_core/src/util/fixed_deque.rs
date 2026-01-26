cbe43d889a07e335ac348e5811353c8704886b9f
linrongbin16 <linrongbin16@outlook.com>
2026-01-26 18:05:45 +0800

    c

--- rsvim_core/src/util/fixed_deque.rs
+++ rsvim_core/src/util/fixed_deque.rs
@@ -5,14 +5,22 @@ use std::collections::VecDeque;
 
 pub struct FixedDeque<T> {
   dq: VecDeque<T>,
-  size: usize,
+  max_size: usize,
 }
 
 impl<T> FixedDeque<T> {
   pub fn new(size: usize) -> Self {
     Self {
       dq: VecDeque::with_capacity(size),
-      size,
+      max_size: size,
     }
+  }
+
+  pub fn push_back_overwrite(&mut self, value: T) {
+    while self.dq.len() > self.max_size && !self.dq.is_empty() {
+      self.dq.pop_front();
+    }
+
+    self.dq.push_back(value);
   }
 }
