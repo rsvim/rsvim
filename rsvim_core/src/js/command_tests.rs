3e6bb067e2bbfb0ec8d19f65457e1732267572fc
linrongbin16 <linrongbin16@outlook.com>
2026-01-27 18:10:22 +0800

    c

--- rsvim_core/src/js/command_tests.rs
+++ rsvim_core/src/js/command_tests.rs
@@ -45,7 +45,7 @@ async fn test_js_echo1() -> IoResult<()> {
     let n = contents.cmdline_message_history().len();
     assert_eq!(n, 1);
 
-    let actual = contents.cmdline_message_history_mut().try_pop();
+    let actual = contents.cmdline_message_history_mut().pop_front();
     info!("actual:{:?}", actual);
     assert!(actual.is_some());
     let actual = actual.unwrap();
