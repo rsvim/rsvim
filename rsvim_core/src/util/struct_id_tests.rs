0b8f478f40d923210756e96430dc96e7d6372a31
linrongbin16 <linrongbin16@outlook.com>
2026-01-28 17:52:28 +0800

    c

--- rsvim_core/src/util/struct_id_tests.rs
+++ rsvim_core/src/util/struct_id_tests.rs
@@ -4,17 +4,17 @@ use crate::struct_id_impl;
 use crate::tests::log::init as test_log_init;
 use std::sync::atomic::AtomicU8;
 
-struct_id_impl!(TestId, u8);
-next_incremental_id_impl!(next_test_id, TestId, AtomicU8, u8, 1);
+struct_id_impl!(Test1Id, u8);
+next_incremental_id_impl!(next_test1_id, Test1Id, AtomicU8, u8, 1);
 
 #[test]
-fn next_test_id1() {
+fn test_next_test1_id() {
   test_log_init();
 
   let mut miss_count = 0;
-  let mut last_id: Option<TestId> = None;
+  let mut last_id: Option<Test1Id> = None;
   for i in 0..1000 {
-    let id = next_test_id();
+    let id = next_test1_id();
     info!(
       "i:{:?},id:{:?},last_id:{:?},miss_count:{:?}",
       i, id, last_id, miss_count
@@ -23,6 +23,36 @@ fn next_test_id1() {
       assert!(last_id.value() >= 1);
       if last_id.value() == u8::MAX {
         assert_eq!(id.value(), 1);
+      } else {
+        assert_eq!(last_id.value() + 1, id.value());
+      }
+    } else {
+      miss_count += 1;
+      assert!(miss_count <= 1);
+    }
+    last_id = Some(id);
+  }
+}
+
+struct_id_impl!(Test2Id, u8);
+next_incremental_id_impl!(next_test2_id, Test2Id, AtomicU8, u8, 100);
+
+#[test]
+fn test_next_test2_id() {
+  test_log_init();
+
+  let mut miss_count = 0;
+  let mut last_id: Option<Test2Id> = None;
+  for i in 0..1000 {
+    let id = next_test2_id();
+    info!(
+      "i:{:?},id:{:?},last_id:{:?},miss_count:{:?}",
+      i, id, last_id, miss_count
+    );
+    if let Some(last_id) = last_id {
+      assert!(last_id.value() >= 100);
+      if last_id.value() == u8::MAX {
+        assert_eq!(id.value(), 100);
       } else {
         assert_eq!(last_id.value() + 1, id.value());
       }
