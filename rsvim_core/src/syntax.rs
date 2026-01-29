19e5892585d8116bf91befc3aff71951deb802e6
linrongbin16 <linrongbin16@outlook.com>
2026-01-29 16:36:19 +0800

    c

--- rsvim_core/src/syntax.rs
+++ rsvim_core/src/syntax.rs
@@ -2,10 +2,15 @@
 
 pub mod lang;
 
+use crate::prelude::*;
+pub use lang::*;
+use tree_sitter::Language;
 use tree_sitter::Parser;
 
 pub struct Syntax {
   parser: Parser,
 }
 
-pub struct SyntaxManager {}
+pub struct SyntaxManager {
+  languages: FoldMap<LanguageName, Language>,
+}
--- rsvim_core/src/syntax/lang.rs
+++ rsvim_core/src/syntax/lang.rs
@@ -1,6 +1,16 @@
 //! Language
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub enum Lang {
+#[derive(
+  Debug,
+  Copy,
+  Clone,
+  PartialEq,
+  Eq,
+  Hash,
+  strum_macros::Display,
+  strum_macros::EnumString,
+)]
+pub enum LanguageName {
+  #[strum(serialize = "rust")]
   Rust,
 }
