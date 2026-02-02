7c1763109370b17923daa72fd8807e74ae2a66a8
linrongbin16 <linrongbin16@outlook.com>
2026-02-02 09:15:49 +0800

    c

--- rsvim_core/src/syntax_tests.rs
+++ rsvim_core/src/syntax_tests.rs
@@ -3,5 +3,5 @@ use super::syntax::*;
 #[test]
 fn init1() {
   let mut syn_mgr = SyntaxManager::new();
-  syn_mgr.insert_file_ext(lang_id, ext);
+  syn_mgr.insert_file_ext(LanguageId::Rust, "rs");
 }
