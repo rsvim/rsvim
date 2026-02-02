6a2264762bc1f3b7b6b5671ae2bb464624f81c41
linrongbin16 <linrongbin16@outlook.com>
2026-02-02 09:47:30 +0800

    c

--- rsvim_core/src/syntax_tests.rs
+++ rsvim_core/src/syntax_tests.rs
@@ -1,7 +1,11 @@
 use super::syntax::*;
+use compact_str::ToCompactString;
 
 #[test]
 fn init1() {
   let mut syn_mgr = SyntaxManager::new();
-  syn_mgr.insert_lang_id_and_file_ext(LanguageId::Rust, "rs");
+  syn_mgr.insert_lang_id_and_file_ext(
+    LanguageId::from("rust".to_compact_string()),
+    "rs",
+  );
 }
