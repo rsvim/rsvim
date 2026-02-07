8aa79b71c7a0847e3c6798ecc64da322af487c24
linrongbin16 <linrongbin16@outlook.com>
2026-02-07 22:16:08 +0800

    cc

--- rsvim_core/src/buf/text.rs
+++ rsvim_core/src/buf/text.rs
@@ -821,16 +821,15 @@ impl Text {
     char_idx: usize,
     n: isize,
   ) -> Option<(usize, usize)> {
-    let to_delete_range = self.get_removable_char_range(line_idx, char_idx, n);
-    if to_delete_range.is_none() || to_delete_range.as_ref().unwrap().is_empty()
-    {
+    let delete_range = self.get_removable_char_range(line_idx, char_idx, n);
+    if delete_range.is_none() || delete_range.as_ref().unwrap().is_empty() {
       return None;
     }
-    let to_delete_range = to_delete_range.unwrap();
+    let delete_range = delete_range.unwrap();
 
-    self.rope_mut().remove(to_delete_range.clone());
+    self.rope_mut().remove(delete_range.clone());
 
-    let absolute_char_idx_after_deleted = to_delete_range.start;
+    let absolute_char_idx_after_deleted = delete_range.start;
     let absolute_char_idx_after_deleted =
       std::cmp::min(absolute_char_idx_after_deleted, self.rope.len_chars());
     let (line_idx_after_deleted, char_idx_after_deleted) =
