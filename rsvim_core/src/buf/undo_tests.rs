b86b456b30186ef04add641bab9a28604c205af2
linrongbin16 <linrongbin16@outlook.com>
2026-02-09 21:34:03 +0800

    c

--- rsvim_core/src/buf/undo_tests.rs
+++ rsvim_core/src/buf/undo_tests.rs
@@ -807,4 +807,73 @@ mod tests_buffer_editing {
 
     Ok(())
   }
+
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn insert2() -> IoResult<()> {
+    test_log_init();
+
+    let src: &str = r#""#;
+
+    // Prepare $RSVIM_CONFIG/rsvim.js
+    let _tp = make_configs(vec![(Path::new("rsvim.js"), src)]);
+
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![
+      MockOperation::Operation(state_ops::Operation::GotoInsertMode(
+        state_ops::GotoInsertModeVariant::Keep,
+      )),
+      MockOperation::Operation(state_ops::Operation::CursorInsert(
+        state_ops::CursorInsertPayload::Text("Hello".to_compact_string()),
+      )),
+      MockOperation::Operation(state_ops::Operation::GotoNormalMode),
+      MockOperation::Operation(state_ops::Operation::GotoInsertMode(
+        state_ops::GotoInsertModeVariant::Keep,
+      )),
+      MockOperation::Operation(state_ops::Operation::CursorInsert(
+        state_ops::CursorInsertPayload::Text(", ".to_compact_string()),
+      )),
+      MockOperation::Operation(state_ops::Operation::GotoNormalMode),
+      MockOperation::Operation(state_ops::Operation::GotoInsertMode(
+        state_ops::GotoInsertModeVariant::Keep,
+      )),
+      MockOperation::Operation(state_ops::Operation::CursorInsert(
+        state_ops::CursorInsertPayload::Text("World".to_compact_string()),
+      )),
+      MockOperation::Operation(state_ops::Operation::GotoNormalMode),
+      MockOperation::Operation(state_ops::Operation::CursorMoveTo((5, 0))),
+      MockOperation::SleepFor(Duration::from_millis(30)),
+    ];
+
+    let mut event_loop =
+      make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      let buf = lock!(event_loop.buffers)
+        .first_key_value()
+        .unwrap()
+        .1
+        .clone();
+      let mut buf = lock!(buf);
+      let buf_eol = buf.options().end_of_line();
+      let after_payload = buf.text().rope().to_string();
+      assert_eq!(after_payload, format!("Hello, World{}", buf_eol));
+      let max_commits = buf.undo().undo_stack().len();
+      debug_assert_eq!(max_commits, 1);
+      let mut revert_rope = buf.text().rope().clone();
+      buf.undo_mut().undo(0, &mut revert_rope).unwrap();
+      let before_payload = revert_rope.to_string();
+      assert_eq!(before_payload, format!("{}", buf_eol));
+    }
+
+    Ok(())
+  }
 }
