4ecf8c5ae0a867edeb0c4b26ee07611e9fb5556c
linrongbin16 <linrongbin16@outlook.com>
2026-02-09 19:43:07 +0800

    c

--- rsvim_core/src/buf_tests.rs
+++ rsvim_core/src/buf_tests.rs
@@ -1,15 +1,4 @@
 use super::buf::*;
-use crate::cli::CliOptions;
-use crate::cli::SpecialCliOptions;
-use crate::prelude::*;
-use crate::state::ops::CursorInsertPayload;
-use crate::state::ops::GotoInsertModeVariant;
-use crate::state::ops::Operation;
-use crate::tests::evloop::*;
-use crate::tests::log::init as test_log_init;
-use compact_str::ToCompactString;
-use regex::Regex;
-use std::time::Duration;
 
 #[test]
 fn next_buffer_id1() {
