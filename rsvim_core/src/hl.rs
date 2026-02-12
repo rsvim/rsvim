543d030dbb3b7a073c5b5101875b75d1d980f42f
linrongbin16 <linrongbin16@outlook.com>
2026-02-12 11:33:27 +0800

    c

--- rsvim_core/src/hl.rs
+++ rsvim_core/src/hl.rs
@@ -6,9 +6,13 @@ use compact_str::CompactString;
 use crossterm::style::Attributes;
 use crossterm::style::Color;
 
+structural_id_impl!(str, HighlightId);
+
 #[derive(Debug, Clone, Eq, PartialEq)]
 /// Highlight style, including colors and attributes.
 pub struct Style {
+  pub id: HighlightId,
+
   /// Foreground color.
   pub fg: Color,
 
@@ -19,9 +23,9 @@ pub struct Style {
   pub attr: Attributes,
 }
 
-structural_id_impl!(str, HighlightId);
-
-pub struct Highlight {}
+pub struct Highlight {
+  styles: FoldMap<HighlightId, Style>,
+}
 
 arc_mutex_ptr!(Highlight);
 
