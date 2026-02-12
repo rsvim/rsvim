494f75655f097996e2f3bfc7920e7ce32189b9b3
linrongbin16 <linrongbin16@outlook.com>
2026-02-12 10:54:36 +0800

    c

--- rsvim_core/src/hl/style.rs
+++ rsvim_core/src/hl/style.rs
@@ -1,3 +1,15 @@
-//! Style, color and attribute.
+//! Style, including colors and attributes.
 
-pub struct Style {}
+use crossterm::style::Attributes;
+use crossterm::style::Color;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+/// Highlight style, including colors and attributes.
+pub struct Style {
+  // Foreground color.
+  fg: Color,
+  // Background color.
+  bg: Color,
+  // Attributes: underline, bold, italic, etc.
+  attrs: Attributes,
+}
