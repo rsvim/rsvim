267c823340ae5d6478bc018029c8baf61a382390
linrongbin16 <linrongbin16@outlook.com>
2026-03-04 16:37:54 +0800

    c

--- rsvim_core/src/syntax_tests.rs
+++ rsvim_core/src/syntax_tests.rs
@@ -471,4 +471,100 @@ mod tests_buffer_editing {
 
     Ok(())
   }
+
+  #[tokio::test]
+  #[cfg_attr(miri, ignore)]
+  async fn rust_err3() -> IoResult<()> {
+    test_log_init();
+
+    let src: &str = r#""#;
+
+    // Prepare $RSVIM_CONFIG/rsvim.js
+    let _tp = make_configs(vec![(Path::new("rsvim.js"), src)]);
+
+    let terminal_cols = 10_u16;
+    let terminal_rows = 10_u16;
+    let mocked_ops = vec![
+      MockOperation::Operation(state_ops::Operation::GotoInsertMode(
+        state_ops::GotoInsertModeVariant::Keep,
+      )),
+      MockOperation::Operation(state_ops::Operation::CursorInsert(
+        state_ops::CursorInsertPayload::Text(
+          "use std::sync::Arc;".to_compact_string(),
+        ),
+      )),
+      MockOperation::Operation(state_ops::Operation::CursorInsert(
+        state_ops::CursorInsertPayload::Eol,
+      )),
+      MockOperation::Operation(state_ops::Operation::CursorInsert(
+        state_ops::CursorInsertPayload::Text("fn main() {".to_compact_string()),
+      )),
+      MockOperation::Operation(state_ops::Operation::CursorInsert(
+        state_ops::CursorInsertPayload::Eol,
+      )),
+      MockOperation::Operation(state_ops::Operation::CursorInsert(
+        state_ops::CursorInsertPayload::Text(
+          "  println!(\"Hello World!\"".to_compact_string(),
+        ),
+      )),
+      MockOperation::Operation(state_ops::Operation::CursorInsert(
+        state_ops::CursorInsertPayload::Eol,
+      )),
+      MockOperation::Operation(state_ops::Operation::CursorInsert(
+        state_ops::CursorInsertPayload::Text("}".to_compact_string()),
+      )),
+      MockOperation::Operation(state_ops::Operation::GotoNormalMode),
+      MockOperation::SleepFor(Duration::from_millis(2000)),
+    ];
+
+    let mut event_loop = make_event_loop(
+      terminal_cols,
+      terminal_rows,
+      CliOptions::new(
+        SpecialCliOptions::empty(),
+        vec![Path::new("test3.rs").to_path_buf()],
+        false,
+      ),
+    );
+
+    event_loop.initialize()?;
+    event_loop
+      .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+      .await?;
+    event_loop.shutdown()?;
+
+    // After running
+    {
+      let buf = lock!(event_loop.buffer_manager)
+        .first_key_value()
+        .unwrap()
+        .1
+        .clone();
+      let buf = lock!(buf);
+      let buf_eol = buf.options().end_of_line();
+      let payload = buf.text().rope().to_string();
+      assert_eq!(
+        format!(
+          "use std::sync::Arc;{buf_eol}fn main() {{{buf_eol}  println!(\"Hello, World\"{buf_eol}}}{buf_eol}"
+        ),
+        payload
+      );
+      let buf_editing_version = buf.editing_version();
+      let syn_editing_version =
+        buf.syntax().as_ref().unwrap().editing_version();
+      assert_eq!(buf_editing_version, syn_editing_version);
+      let syn_tree = buf.syntax().as_ref().unwrap().tree();
+      assert!(syn_tree.as_ref().is_some());
+      info!(
+        "syn tree:{:?}",
+        syn_tree.as_ref().unwrap().root_node().to_string()
+      );
+      assert_eq!(
+        syn_tree.as_ref().unwrap().root_node().to_string(),
+        "(source_file)"
+      );
+    }
+
+    Ok(())
+  }
 }
