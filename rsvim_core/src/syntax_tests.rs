9cda8179f41391404bf5313de35ee368a3ea26e8
linrongbin16 <linrongbin16@outlook.com>
2026-03-10 12:04:28 +0800

    c

--- rsvim_core/src/syntax_tests.rs
+++ rsvim_core/src/syntax_tests.rs
@@ -692,7 +692,7 @@ mod tests_buffer_scrolling {
     let mocked_ops = vec![
       MockOperation::SleepFor(Duration::from_millis(1000)),
       MockOperation::Operation(state_ops::Operation::CursorMoveTo((0, 11))),
-      MockOperation::SleepFor(Duration::from_millis(500)),
+      MockOperation::SleepFor(Duration::from_millis(1000)),
     ];
 
     let tmpfile = NamedTempFile::new("rust1.rs").unwrap();
