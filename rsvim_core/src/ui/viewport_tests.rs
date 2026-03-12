a9a460e59d1595c1f1b61ccec5e77b07a40b3240
linrongbin16 <linrongbin16@outlook.com>
2026-03-12 17:02:37 +0800

    c

--- rsvim_core/src/ui/viewport_tests.rs
+++ rsvim_core/src/ui/viewport_tests.rs
@@ -2194,26 +2194,24 @@ mod tests_view_wrap_nolinebreak_eol {
     );
     let expect = vec![
       "AAAAAAAAAA",
+      "\r\n",
       "1st.\r\n",
       "BBBBBBBBBB",
       "CCCCCCCCCC",
       "\r\n",
-      "3rd.\r\n",
       // "4th.\r\n",
     ];
 
     let (tree, window_id) = make_window(terminal_size, buf.clone(), win_opts);
     let actual = tree.window(window_id).unwrap().viewport();
     let expect_fills: BTreeMap<usize, usize> =
-      vec![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)]
-        .into_iter()
-        .collect();
+      vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
     assert_viewport(
       lock!(buf).text(),
       &actual,
       &expect,
       0,
-      5,
+      3,
       &expect_fills,
       &expect_fills,
     );
@@ -2244,25 +2242,23 @@ mod tests_view_wrap_nolinebreak_eol {
     );
     let expect = vec![
       "AAAAAAAAAA",
+      "\r\n",
       "1st.\r\n",
       "BBBBBBBBBB",
       "CCCCCCCCCC",
-      "3rd.\r\n",
-      "4th.\r\n",
+      "\r\n",
     ];
 
     let (tree, window_id) = make_window(terminal_size, buf.clone(), win_opts);
     let actual = tree.window(window_id).unwrap().viewport();
     let expect_fills: BTreeMap<usize, usize> =
-      vec![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)]
-        .into_iter()
-        .collect();
+      vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
     assert_viewport(
       lock!(buf).text(),
       &actual,
       &expect,
       0,
-      5,
+      3,
       &expect_fills,
       &expect_fills,
     );
