6b374b55530217bec7636f24fb59305016fce940
linrongbin16 <linrongbin16@outlook.com>
2026-03-12 17:06:31 +0800

    c

--- rsvim_core/src/ui/viewport_tests.rs
+++ rsvim_core/src/ui/viewport_tests.rs
@@ -2388,7 +2388,7 @@ mod tests_view_wrap_nolinebreak_eol {
       "1st.\r\n",
       "BBBBBBBBBB",
       "CCCCCCCCCC",
-      "\r",
+      "\r\n",
       "3rd.\r\n",
       "4th.\r\n",
       // "5th.\r\n",
@@ -2435,23 +2435,22 @@ mod tests_view_wrap_nolinebreak_eol {
       "1st.\r\n",
       "BBBBBBBBBB",
       "CCCCCCCCCC",
+      "\r\n",
       "3rd.\r\n",
       "4th.\r\n",
-      "5th.\r\n",
+      // "5th.\r\n",
     ];
 
     let (tree, window_id) = make_window(terminal_size, buf.clone(), win_opts);
     let actual = tree.window(window_id).unwrap().viewport();
     let expect_fills: BTreeMap<usize, usize> =
-      vec![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)]
-        .into_iter()
-        .collect();
+      vec![(0, 0), (1, 0), (2, 0), (3, 0)].into_iter().collect();
     assert_viewport(
       lock!(buf).text(),
       &actual,
       &expect,
       0,
-      5,
+      4,
       &expect_fills,
       &expect_fills,
     );
