1de4f22365103a248990106ffaec810e89abe788
linrongbin16 <linrongbin16@outlook.com>
2026-03-12 17:44:22 +0800

    c

--- rsvim_core/src/ui/viewport_tests.rs
+++ rsvim_core/src/ui/viewport_tests.rs
@@ -7241,21 +7241,22 @@ mod tests_search_anchor_downward_wrap_nolinebreak_eol {
         "1st.\r\n",
         "AAAAAAAAAA",
         "BBBBBBBBBB",
+        "\r\n",
         "3rd.\r\n",
-        "4th.\r\n",
+        // "4th.\r\n",
       ];
 
       let actual = tree.window(window_id).unwrap().viewport();
       let expect_start_fills: BTreeMap<usize, usize> =
-        vec![(0, 0), (1, 0), (2, 0), (3, 0)].into_iter().collect();
+        vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
       let expect_end_fills: BTreeMap<usize, usize> =
-        vec![(0, 0), (1, 0), (2, 0), (3, 0)].into_iter().collect();
+        vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
       assert_viewport(
         lock!(buf).text(),
         &actual,
         &expect,
         0,
-        4,
+        3,
         &expect_start_fills,
         &expect_end_fills,
       );
@@ -7267,23 +7268,24 @@ mod tests_search_anchor_downward_wrap_nolinebreak_eol {
         "1st.\r\n",
         "AAAAAAAAAA",
         "BBBBBBBBBB",
+        "\r\n",
         "3rd.\r\n",
-        "4th.\r\n",
+        // "4th.\r\n",
       ];
 
       let actual =
         search_down_viewport(&mut tree, window_id, buf.clone(), 1, 20, 0, 0);
 
       let expect_start_fills: BTreeMap<usize, usize> =
-        vec![(0, 0), (1, 0), (2, 0), (3, 0)].into_iter().collect();
+        vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
       let expect_end_fills: BTreeMap<usize, usize> =
-        vec![(0, 0), (1, 0), (2, 0), (3, 0)].into_iter().collect();
+        vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
       assert_viewport(
         lock!(buf).text(),
         &actual,
         &expect,
         0,
-        4,
+        3,
         &expect_start_fills,
         &expect_end_fills,
       );
@@ -7318,20 +7320,26 @@ mod tests_search_anchor_downward_wrap_nolinebreak_eol {
 
     // Initialize
     {
-      let expect =
-        vec!["1st.\r", "AAAAAAAAAA", "BBBBBBBBBB", "3rd.\r", "4th.\r"];
+      let expect = vec![
+        "1st.\r",
+        "AAAAAAAAAA",
+        "BBBBBBBBBB",
+        "\r",
+        "3rd.\r",
+        // "4th.\r"
+      ];
 
       let actual = tree.window(window_id).unwrap().viewport();
       let expect_start_fills: BTreeMap<usize, usize> =
-        vec![(0, 0), (1, 0), (2, 0), (3, 0)].into_iter().collect();
+        vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
       let expect_end_fills: BTreeMap<usize, usize> =
-        vec![(0, 0), (1, 0), (2, 0), (3, 0)].into_iter().collect();
+        vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
       assert_viewport(
         lock!(buf).text(),
         &actual,
         &expect,
         0,
-        4,
+        3,
         &expect_start_fills,
         &expect_end_fills,
       );
@@ -7339,22 +7347,28 @@ mod tests_search_anchor_downward_wrap_nolinebreak_eol {
 
     // Search-1
     {
-      let expect =
-        vec!["1st.\r", "AAAAAAAAAA", "BBBBBBBBBB", "3rd.\r", "4th.\r"];
+      let expect = vec![
+        "1st.\r",
+        "AAAAAAAAAA",
+        "BBBBBBBBBB",
+        "\r",
+        "3rd.\r",
+        // "4th.\r"
+      ];
 
       let actual =
         search_down_viewport(&mut tree, window_id, buf.clone(), 1, 20, 0, 0);
 
       let expect_start_fills: BTreeMap<usize, usize> =
-        vec![(0, 0), (1, 0), (2, 0), (3, 0)].into_iter().collect();
+        vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
       let expect_end_fills: BTreeMap<usize, usize> =
-        vec![(0, 0), (1, 0), (2, 0), (3, 0)].into_iter().collect();
+        vec![(0, 0), (1, 0), (2, 0)].into_iter().collect();
       assert_viewport(
         lock!(buf).text(),
         &actual,
         &expect,
         0,
-        4,
+        3,
         &expect_start_fills,
         &expect_end_fills,
       );
