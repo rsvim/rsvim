501e6b9d297ba1b2e851310959d57834337dbc5b
linrongbin16 <linrongbin16@outlook.com>
2026-03-26 21:46:01 +0800

    c

--- rsvim_core/src/buf/text.rs
+++ rsvim_core/src/buf/text.rs
@@ -50,19 +50,19 @@ impl Text {
   }
 }
 
-// #[cfg(debug_assertions)]
-// impl Drop for Text {
-//   fn drop(&mut self) {
-//     let cached_width = self.cached_width.borrow();
-//     if cached_width.stats().total() > 0 {
-//       trace!("|drop| cached_width {}", cached_width.stats());
-//     }
-//     let cached_lines = self.cached_lines.borrow();
-//     if cached_lines.stats().total() > 0 {
-//       trace!("|drop| cached_lines {}", cached_lines.stats());
-//     }
-//   }
-// }
+#[cfg(test)]
+impl Drop for Text {
+  fn drop(&mut self) {
+    let cached_width = self.cached_width.borrow();
+    if cached_width.stats().total() > 0 {
+      trace!("|drop| cached_width {}", cached_width.stats());
+    }
+    let cached_lines = self.cached_lines.borrow();
+    if cached_lines.stats().total() > 0 {
+      trace!("|drop| cached_lines {}", cached_lines.stats());
+    }
+  }
+}
 
 // Unicode {
 impl Text {
