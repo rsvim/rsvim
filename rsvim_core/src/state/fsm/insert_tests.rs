765348e2b8464f10d2d49ac594ec7ac316ef0f82
linrongbin16 <linrongbin16@outlook.com>
2026-03-27 10:08:12 +0800

    c

--- rsvim_core/src/state/fsm/insert_tests.rs
+++ rsvim_core/src/state/fsm/insert_tests.rs
@@ -11834,26 +11834,17 @@ mod tests_delete_text_nofixeol {
         "  2. When ",
         "* The extr",
         "* The extr",
-        "",
       ];
-      let expect_fills: BTreeMap<usize, usize> = vec![
-        (0, 0),
-        (1, 0),
-        (2, 0),
-        (3, 0),
-        (4, 0),
-        (5, 0),
-        (6, 0),
-        (7, 0),
-      ]
-      .into_iter()
-      .collect();
+      let expect_fills: BTreeMap<usize, usize> =
+        vec![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0)]
+          .into_iter()
+          .collect();
       assert_viewport(
         lock!(buf).text(),
         &viewport,
         &expect,
         0,
-        8,
+        7,
         &expect_fills,
         &expect_fills,
       );
@@ -11895,26 +11886,17 @@ mod tests_delete_text_nofixeol {
         "  2. When ",
         "* The extr",
         "* The extr",
-        "",
       ];
-      let expect_fills: BTreeMap<usize, usize> = vec![
-        (0, 0),
-        (1, 0),
-        (2, 0),
-        (3, 0),
-        (4, 0),
-        (5, 0),
-        (6, 0),
-        (7, 0),
-      ]
-      .into_iter()
-      .collect();
+      let expect_fills: BTreeMap<usize, usize> =
+        vec![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0)]
+          .into_iter()
+          .collect();
       assert_viewport(
         lock!(buf).text(),
         &viewport,
         &expect,
         0,
-        8,
+        7,
         &expect_fills,
         &expect_fills,
       );
