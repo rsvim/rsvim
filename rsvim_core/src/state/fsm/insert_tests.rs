9eeda385c793e370611daf39104e2004251f3e3d
linrongbin16 <linrongbin16@outlook.com>
2026-03-27 10:54:39 +0800

    c

--- rsvim_core/src/state/fsm/insert_tests.rs
+++ rsvim_core/src/state/fsm/insert_tests.rs
@@ -6842,26 +6842,17 @@ mod tests_insert_text_nofixeol {
         " line is t",
         "tra parts ",
         "tra parts ",
-        "",
       ];
-      let expect_fills: BTreeMap<usize, usize> = vec![
-        (0, 0),
-        (1, 0),
-        (2, 0),
-        (3, 0),
-        (4, 0),
-        (5, 0),
-        (6, 0),
-        (7, 0),
-      ]
-      .into_iter()
-      .collect();
+      let expect_fills: BTreeMap<usize, usize> =
+        vec![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0)]
+          .into_iter()
+          .collect();
       assert_viewport(
         lock!(buf).text(),
         &viewport,
         &expect,
         0,
-        8,
+        7,
         &expect_fills,
         &expect_fills,
       );
