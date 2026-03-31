11746284bedd573aae82d1e44a3ac9d0c56697e8
linrongbin16 <linrongbin16@outlook.com>
2026-03-31 11:52:47 +0800

    c

--- rsvim_core/benches/viewport_search.rs
+++ rsvim_core/benches/viewport_search.rs
@@ -3,6 +3,7 @@
 //! 1. `benches/bigfiles/MIMRT1176_cm7.h`
 //! 2. `dcn_3_2_0_sh_mask.h`
 
+use criterion::BenchmarkId;
 use criterion::Criterion;
 use criterion::criterion_group;
 use criterion::criterion_main;
@@ -32,6 +33,60 @@ const FILENAME1: &str =
 const FILENAME2: &str =
   "../../../tests_and_benchmarks/benches/bigfiles/MIMXRT1176_cm7.h";
 const REPEAT: usize = 1000;
+
+fn make_buffer(
+  filename: &str,
+  buffer_opts: BufferOptions,
+  canvas_size: U16Size,
+) -> BufferArc {
+  let text = std::fs::read_to_string(filename).unwrap();
+  let rop = Rope::from_str(&text);
+  let buffer = Buffer::_new(
+    buffer_opts,
+    canvas_size,
+    rop,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+  );
+  Buffer::to_arc(buffer)
+}
+
+fn make_tree(
+  canvas_size: &U16Size,
+  window_opts: WindowOptions,
+  buffer: &BufferArc,
+) -> (Tree, NodeId) {
+  let tree_style = Style {
+    size: taffy::Size {
+      width: taffy::prelude::length(canvas_size.width()),
+      height: taffy::prelude::length(canvas_size.height()),
+    },
+    ..Default::default()
+  };
+  let mut tree = Tree::new(tree_style).unwrap();
+  tree.set_global_local_options(window_opts);
+  let window_style = Style {
+    size: taffy::Size {
+      height: taffy::prelude::percent(1.0),
+      width: taffy::prelude::percent(1.0),
+    },
+    ..Default::default()
+  };
+  let window_id = tree
+    .new_window_with_parent(
+      tree.root_id(),
+      window_style,
+      *tree.global_local_options(),
+      Arc::downgrade(buffer),
+    )
+    .unwrap();
+  (tree, window_id)
+}
 
 fn make(
   width: u16,
@@ -84,115 +139,78 @@ fn make(
   (buffer, tree, window_id)
 }
 
-fn bench_search_nowrap_bigterm1(c: &mut Criterion) {
+fn bench_search_nowrap(c: &mut Criterion) {
   let buffer_opts = BufferOptionsBuilder::default().build().unwrap();
   let window_opts =
     WindowOptionsBuilder::default().wrap(false).build().unwrap();
-  let (buffer, mut tree, window_id) = make(
-    BIG_TERM_WIDTH,
-    BIG_TERM_HEIGHT,
-    buffer_opts,
-    window_opts,
-    FILENAME1,
-  );
 
-  let buffer = lock!(buffer);
-  for _i in 0..REPEAT {
-    c.bench_function(
-      "Viewport::search wrap=false w/h=200/50 dcn_3_2_0_sh_mask.h",
-      |b| {
-        b.iter(|| {
-          let target_cursor_line = fastrand::usize(..);
-          let target_cursor_char = fastrand::usize(..);
+  let mut g = c.benchmark_group("bench_search_nowrap");
 
-          let old_viewport = tree.window(window_id).unwrap().viewport();
-          let old_cursor_viewport =
-            tree.window(window_id).unwrap().cursor_viewport();
-          let (start_line, start_column) = old_viewport.search(
-            black_box(&old_cursor_viewport),
-            black_box(&window_opts),
-            black_box(buffer.text()),
-            black_box(&tree.window(window_id).unwrap().actual_shape().size()),
-            black_box(target_cursor_line),
-            black_box(target_cursor_char),
-          );
-          let new_viewport = Viewport::view(
-            &window_opts,
-            buffer.text(),
-            &tree.window(window_id).unwrap().actual_shape().size(),
-            start_line,
-            start_column,
-          );
-          let new_cursor_viewport =
-            CursorViewport::to_arc(CursorViewport::from_position(
-              &new_viewport,
-              buffer.text(),
-              &tree.window(window_id).unwrap().actual_shape().size(),
-              target_cursor_line,
-              target_cursor_char,
-            ));
-          tree.set_editable_cursor_viewport(window_id, new_cursor_viewport);
-          tree.set_editable_viewport(window_id, Viewport::to_arc(new_viewport));
-        })
-      },
-    );
+  let run_bench = |canvas_width_param: &u16,
+                   canvas_height_param: &u16,
+                   filename_param: &str| {
+    let canvas_size = size!(*canvas_width_param, *canvas_height_param);
+    let buffer = make_buffer(filename_param, buffer_opts, canvas_size);
+    let (mut tree, window_id) = make_tree(&canvas_size, window_opts, &buffer);
+    let buffer = lock!(buffer);
+    for _i in 0..REPEAT {
+      let target_cursor_line = fastrand::usize(..);
+      let target_cursor_char = fastrand::usize(..);
+
+      let old_viewport = tree.window(window_id).unwrap().viewport();
+      let old_cursor_viewport =
+        tree.window(window_id).unwrap().cursor_viewport();
+      let (start_line, start_column) = old_viewport.search(
+        black_box(&old_cursor_viewport),
+        black_box(&window_opts),
+        black_box(buffer.text()),
+        black_box(&tree.window(window_id).unwrap().actual_shape().size()),
+        black_box(target_cursor_line),
+        black_box(target_cursor_char),
+      );
+      let new_viewport = Viewport::view(
+        &window_opts,
+        buffer.text(),
+        &tree.window(window_id).unwrap().actual_shape().size(),
+        start_line,
+        start_column,
+      );
+      let new_cursor_viewport =
+        CursorViewport::to_arc(CursorViewport::from_position(
+          &new_viewport,
+          buffer.text(),
+          &tree.window(window_id).unwrap().actual_shape().size(),
+          target_cursor_line,
+          target_cursor_char,
+        ));
+      tree.set_editable_cursor_viewport(window_id, new_cursor_viewport);
+      tree.set_editable_viewport(window_id, Viewport::to_arc(new_viewport));
+    }
+  };
+
+  for canvas_width in [BIG_TERM_WIDTH, SMALL_TERM_WIDTH] {
+    for canvas_height in [BIG_TERM_HEIGHT, SMALL_TERM_HEIGHT] {
+      for filename in [FILENAME1, FILENAME2] {
+        let benchmark_id_param = format!(
+          "width/height={}/{} file={}",
+          canvas_width, canvas_height, filename
+        );
+        let benchmark_id =
+          BenchmarkId::new("Viewport::search wrap=false", &benchmark_id_param);
+        let params = (canvas_width, canvas_height, filename);
+        g.bench_with_input(
+          benchmark_id,
+          &params,
+          |b, (canvas_width_param, canvas_height_param, filename_param)| {
+            b.iter(|| {
+              run_bench(canvas_width_param, canvas_height_param, filename_param)
+            })
+          },
+        );
+      }
+    }
   }
 }
 
-fn bench_search_nowrap_bigterm2(c: &mut Criterion) {
-  let buffer_opts = BufferOptionsBuilder::default().build().unwrap();
-  let window_opts =
-    WindowOptionsBuilder::default().wrap(false).build().unwrap();
-  let (buffer, mut tree, window_id) = make(
-    BIG_TERM_WIDTH,
-    BIG_TERM_HEIGHT,
-    buffer_opts,
-    window_opts,
-    FILENAME2,
-  );
-
-  let buffer = lock!(buffer);
-  for _i in 0..REPEAT {
-    c.bench_function(
-      "Viewport::search wrap=false w/h=200/50 MIMXRT1176_cm7.h",
-      |b| {
-        b.iter(|| {
-          let target_cursor_line = fastrand::usize(..);
-          let target_cursor_char = fastrand::usize(..);
-
-          let old_viewport = tree.window(window_id).unwrap().viewport();
-          let old_cursor_viewport =
-            tree.window(window_id).unwrap().cursor_viewport();
-          let (start_line, start_column) = old_viewport.search(
-            black_box(&old_cursor_viewport),
-            black_box(&window_opts),
-            black_box(buffer.text()),
-            black_box(&tree.window(window_id).unwrap().actual_shape().size()),
-            black_box(target_cursor_line),
-            black_box(target_cursor_char),
-          );
-          let new_viewport = Viewport::view(
-            &window_opts,
-            buffer.text(),
-            &tree.window(window_id).unwrap().actual_shape().size(),
-            start_line,
-            start_column,
-          );
-          let new_cursor_viewport =
-            CursorViewport::to_arc(CursorViewport::from_position(
-              &new_viewport,
-              buffer.text(),
-              &tree.window(window_id).unwrap().actual_shape().size(),
-              target_cursor_line,
-              target_cursor_char,
-            ));
-          tree.set_editable_cursor_viewport(window_id, new_cursor_viewport);
-          tree.set_editable_viewport(window_id, Viewport::to_arc(new_viewport));
-        })
-      },
-    );
-  }
-}
-
-criterion_group!(benches, bench_search_nowrap_bigterm1);
+criterion_group!(benches, bench_search_nowrap);
 criterion_main!(benches);
