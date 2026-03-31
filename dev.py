7c325d6a4e1c77c7d95ffcb46057330e4994bfd6
linrongbin16 <linrongbin16@outlook.com>
2026-03-31 12:00:30 +0800

    c

--- dev.py
+++ dev.py
@@ -281,6 +281,31 @@ class Build(Cmd):
         run(cmd)
 
 
+# bench
+class Bench(Cmd):
+    def __init__(self, subparsers) -> None:
+        self._name = "bench"
+
+        self.bench_parser = subparsers.add_parser(
+            self._name,
+            help="cargo bench",
+        )
+        self.bench_parser.add_argument("name", nargs=1)
+
+    def name(self) -> str:
+        return self._name
+
+    def alias(self) -> Optional[str]:
+        return None
+
+    def run(self, args) -> None:
+        sccache()
+        rustflags()
+        name = args.name
+        cmd = f"cargo bench -p rsvim_core --bench {name}"
+        run(cmd)
+
+
 # fmt/f
 class Format(Cmd):
     def __init__(self, subparsers) -> None:
