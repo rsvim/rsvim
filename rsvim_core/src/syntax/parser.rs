1ddd4831feca4691b427f83d6c9aebfdd8d00823
linrongbin16 <linrongbin16@outlook.com>
2026-04-08 14:42:07 +0800

    c

--- rsvim_core/src/syntax/parser.rs
+++ rsvim_core/src/syntax/parser.rs
@@ -15,69 +15,9 @@ pub type SyntaxLoaderArc = Arc<Mutex<Loader>>;
 pub type SyntaxLoaderWk = Weak<Mutex<Loader>>;
 pub type SyntaxLoaderMutexGuard<'a> = MutexGuard<'a, Loader>;
 
-pub struct LanguageNameCache {
-  // tree-sitter grammar.json path to its name
-  path2name: FoldMap<PathBuf, CompactString>,
-  // tree-sitter grammar.json name to its path
-  name2path: FoldMap<CompactString, PathBuf>,
-}
-
-impl LanguageNameCache {
-  pub fn new() -> Self {
-    Self {
-      path2name: FoldMap::new(),
-      name2path: FoldMap::new(),
-    }
-  }
-
-  pub fn parse_language_name_from_grammar_json(
-    grammar_json_path: &Path,
-  ) -> TheResult<CompactString> {
-    let err = || {
-      TheErr::TreesitterParserNotFound(
-        grammar_json_path.to_string_lossy().to_compact_string(),
-      )
-    };
-    let grammar_json_text =
-      std::fs::read_to_string(grammar_json_path).map_err(|_e| err())?;
-    let grammar_json_data: serde_json::Value =
-      serde_json::from_str(&grammar_json_text).map_err(|_e| err())?;
-    match grammar_json_data.get("name") {
-      Some(name_value) => match name_value.as_str() {
-        Some(name) => Ok(name.to_compact_string()),
-        None => Err(err()),
-      },
-      None => Err(err()),
-    }
-  }
-
-  pub fn get_language_name(
-    &mut self,
-    src_path: &Path,
-  ) -> TheResult<CompactString> {
-    let grammar_json_path = src_path.join("grammar.json");
-    if !self.grammarpaths2names.contains_key(&grammar_json_path) {
-      let name = self.get_grammar_json_name(grammar_json_path.as_path())?;
-      self
-        .grammarpaths2names
-        .insert(grammar_json_path.clone(), name.clone());
-      self
-        .names2grammarpaths
-        .insert(name, grammar_json_path.clone());
-    }
-    let name = self.grammarpaths2names.get(&grammar_json_path).unwrap();
-    Ok(name.to_compact_string())
-  }
-}
-
 pub struct SyntaxParserLoader {
   // tree-sitter loader
   loader: Loader,
-
-  // tree-sitter parser grammar.json paths to its names
-  grammarpaths2names: FoldMap<PathBuf, CompactString>,
-  // tree-sitter parser grammar.json names to its paths
-  names2grammarpaths: FoldMap<CompactString, PathBuf>,
 
   // tree-sitter parsers
   parsers: FoldMap<CompactString, Language>,
@@ -95,16 +35,15 @@ impl SyntaxParserLoader {
     Self {
       // loader: Arc::new(Mutex::new(Loader::new().unwrap())),
       loader: Loader::new().unwrap(),
-      grammarpaths2names: FoldMap::new(),
-      names2grammarpaths: FoldMap::new(),
       parsers: FoldMap::new(),
     }
   }
 
-  pub fn get_treesitter_parser_grammar_json_name(
-    &self,
-    grammar_json_path: &Path,
+  pub fn get_language_name_from_grammar_json(
+    src_path: &Path,
   ) -> TheResult<CompactString> {
+    let grammar_json_path = src_path.join("grammar.json");
+    let grammar_json_path = grammar_json_path.as_path();
     let err = || {
       TheErr::TreesitterParserNotFound(
         grammar_json_path.to_string_lossy().to_compact_string(),
@@ -123,25 +62,6 @@ impl SyntaxParserLoader {
     }
   }
 
-  pub fn get_treesitter_parser_name(
-    &mut self,
-    src_path: &Path,
-  ) -> TheResult<CompactString> {
-    let grammar_json_path = src_path.join("grammar.json");
-    if !self.grammarpaths2names.contains_key(&grammar_json_path) {
-      let name = self
-        .get_treesitter_parser_grammar_json_name(grammar_json_path.as_path())?;
-      self
-        .grammarpaths2names
-        .insert(grammar_json_path.clone(), name.clone());
-      self
-        .names2grammarpaths
-        .insert(name, grammar_json_path.clone());
-    }
-    let name = self.grammarpaths2names.get(&grammar_json_path).unwrap();
-    Ok(name.to_compact_string())
-  }
-
   /// Load the tree-sitter parser (`Language`) FFI dynamic library.
   pub fn load_treesitter_parser(
     &mut self,
@@ -149,7 +69,7 @@ impl SyntaxParserLoader {
   ) -> TheResult<&Language> {
     let src_path = opts.grammar_path.join("src");
     let src_path = src_path.as_path();
-    let lang_name = self.get_treesitter_parser_name(src_path)?;
+    let lang_name = Self::get_language_name_from_grammar_json(src_path)?;
     if !self.parsers.contains_key(&lang_name) {
       let compile_cfg = CompileConfig::new(src_path, None, None);
       match self.loader.load_language_at_path(compile_cfg) {
