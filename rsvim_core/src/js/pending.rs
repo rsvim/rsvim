ec58dba53c315ea5231e73fbe4145ab2a5674fec
linrongbin16 <linrongbin16@outlook.com>
2026-04-10 22:02:04 +0800

    c

--- rsvim_core/src/js/pending.rs
+++ rsvim_core/src/js/pending.rs
@@ -105,3 +105,19 @@ pub fn create_fs_write(
     MasterMessage::FsWriteReq(chan::FsWriteReq { task_id, fd, buf }),
   );
 }
+
+pub fn create_syn_grammar_load(
+  state: &mut JsRuntimeState,
+  task_id: TaskId,
+  grammar_path: &Path,
+  cb: TaskCallback,
+) {
+  state.pending_tasks.insert(task_id, cb);
+  chan::send_to_master(
+    state.master_tx.clone(),
+    MasterMessage::LoadTreesitterGrammarReq(chan::LoadTreesitterGrammarReq {
+      task_id,
+      grammar_path: grammar_path.to_path_buf(),
+    }),
+  );
+}
