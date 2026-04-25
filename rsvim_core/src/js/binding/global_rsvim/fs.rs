09c779175952c85939bcbc01542d451258822f51
linrongbin16 <linrongbin16@outlook.com>
2026-04-26 00:16:29 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs.rs
@@ -249,3 +249,75 @@ pub fn write_sync<'s>(
     Err(e) => binding::throw_exception(scope, &e),
   }
 }
+
+/// `Rsvim.fs.readFile` API.
+pub fn read_file<'s>(
+  scope: &mut v8::PinScope<'s, '_>,
+  args: v8::FunctionCallbackArguments<'s>,
+  mut rv: v8::ReturnValue,
+) {
+  debug_assert!(args.length() == 2);
+  debug_assert!(args.get(0).is_object());
+  let file_wrapper = args.get(0).to_object(scope).unwrap();
+  debug_assert!(args.get(1).is_array_buffer());
+  let buf = args.get(1).cast::<v8::ArrayBuffer>();
+  trace!("RsvimFs.read: {:?}, {:?}", file_wrapper, buf);
+
+  let promise_resolver = v8::PromiseResolver::new(scope).unwrap();
+  let promise = promise_resolver.get_promise(scope);
+
+  let state_rc = JsRuntime::state(scope);
+  let read_cb = {
+    let promise = v8::Global::new(scope, promise_resolver);
+    let state_rc = state_rc.clone();
+    let buffer_store = buf.get_backing_store().clone();
+    move |maybe_result: Option<TheResult<Vec<u8>>>| {
+      let fut = FsReadFuture {
+        promise: promise.clone(),
+        buffer_store: buffer_store.clone(),
+        maybe_result,
+      };
+      let mut state = state_rc.borrow_mut();
+      state.pending_futures.push(Box::new(fut));
+    }
+  };
+
+  let fd = get_cppgc_handle!(scope, file_wrapper, Option<usize>).unwrap();
+  let mut state = state_rc.borrow_mut();
+  let task_id = js::TaskId::next();
+  pending::create_fs_read(
+    &mut state,
+    task_id,
+    fd,
+    buf.byte_length(),
+    Box::new(read_cb),
+  );
+
+  rv.set(promise.into());
+}
+
+/// `Rsvim.fs.readFileSync` API.
+pub fn read_file_sync<'s>(
+  scope: &mut v8::PinScope<'s, '_>,
+  args: v8::FunctionCallbackArguments<'s>,
+  mut rv: v8::ReturnValue,
+) {
+  debug_assert!(args.length() == 2);
+  debug_assert!(args.get(0).is_object());
+  let file_wrapper = args.get(0).to_object(scope).unwrap();
+  debug_assert!(args.get(1).is_array_buffer());
+  let buf = args.get(1).cast::<v8::ArrayBuffer>();
+  trace!("RsvimFs.readSync: {:?}, {:?}", file_wrapper, buf);
+
+  let fd = get_cppgc_handle!(scope, file_wrapper, Option<usize>).unwrap();
+  match fs_read(fd, buf.byte_length()) {
+    Ok(data) => {
+      let buffer_store = buf.get_backing_store();
+      for (i, b) in data.iter().enumerate() {
+        buffer_store[i].set(*b);
+      }
+      rv.set_int32(data.len() as i32);
+    }
+    Err(e) => binding::throw_exception(scope, &e),
+  }
+}
