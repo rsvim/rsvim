d02b93c8ade8f3250a36e3dfcee4b8abad2dadad
linrongbin16 <linrongbin16@outlook.com>
2026-04-28 16:26:14 +0800

    c

--- rsvim_cli/build.rs
+++ rsvim_cli/build.rs
@@ -94,16 +94,30 @@ fn version() {
 }
 
 fn snapshot() {
-  let js_runtime = JsRuntimeForSnapshot::new();
-  let snapshot = js_runtime.create_snapshot();
-  let snapshot = Box::from(&snapshot);
-  let mut vec = Vec::with_capacity(snapshot.len());
-  vec.extend_from_slice(&snapshot);
+  let snapshot = {
+    let js_runtime = JsRuntimeForSnapshot::new();
+    let snapshot = js_runtime.create_snapshot();
+    let snapshot = Box::from(&snapshot);
+    let snapshot_len = snapshot.len();
+    println!("{LOG} Snapshot is {snapshot_len} bytes before compress...");
+    let mut vec = Vec::with_capacity(snapshot.len());
+    vec.extend((snapshot.len() as u32).to_le_bytes());
+    let max_compress_level: i32 = *zstd::compression_level_range().end();
+    println!("{LOG} Compress snapshot with zstd-level={max_compress_level}...");
+    vec.extend_from_slice(
+      &zstd::bulk::compress(&snapshot, max_compress_level)
+        .expect("Failed to compress snapshot with zstd"),
+    );
+    let snapshot = vec.into_boxed_slice();
+    let snapshot_len = snapshot.len();
+    println!("{LOG} Snapshot is {snapshot_len} bytes after compress...");
+    snapshot
+  };
 
   let output_path =
     Path::new(env!("CARGO_MANIFEST_DIR")).join("RSVIM_SNAPSHOT.BIN");
   println!("{LOG} Writing snapshot into {:?}...", output_path.as_path());
-  std::fs::write(output_path.as_path(), vec.into_boxed_slice()).unwrap();
+  std::fs::write(output_path.as_path(), &snapshot).unwrap();
 }
 
 fn main() {
