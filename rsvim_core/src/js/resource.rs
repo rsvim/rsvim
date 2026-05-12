e9c61277c409898da93cfbbb9150611bd002a407
linrongbin16 <linrongbin16@outlook.com>
2026-05-12 16:55:44 +0800

    c

--- rsvim_core/src/js/resource.rs
+++ rsvim_core/src/js/resource.rs
@@ -23,7 +23,7 @@ pub struct ResourceTable {
   resources: BTreeMap<ResourceId, Resource>,
 }
 
-rc_refcell_ptr!(ResourceTable);
+arc_mutex_ptr!(ResourceTable);
 
 pub type ResourceTableKeys<'a> =
   std::collections::btree_map::Keys<'a, ResourceId, Resource>;
