12072b235ec332108e067a246921d304bbfad351
linrongbin16 <linrongbin16@outlook.com>
2026-05-13 14:39:48 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs.rs
@@ -206,8 +206,9 @@ pub fn write<'s>(
   mut rv: v8::ReturnValue,
 ) {
   debug_assert!(args.length() == 2);
-  debug_assert!(args.get(0).is_object());
-  let file_wrapper = args.get(0).to_object(scope).unwrap();
+  debug_assert!(is_v8_int!(args.get(0)));
+  let file_rid = i32::from_v8(scope, args.get(0).to_integer(scope).unwrap());
+  let file_rid = ResourceId::from(file_rid);
   debug_assert!(args.get(1).is_array_buffer());
   let buf = args.get(1).cast::<v8::ArrayBuffer>();
   let buf = buf
@@ -215,7 +216,7 @@ pub fn write<'s>(
     .iter()
     .map(|b| b.get())
     .collect_vec();
-  trace!("RsvimFs.write: {:?}, {:?}", file_wrapper, buf);
+  trace!("RsvimFs.write: {:?}, {:?}", file_rid, buf);
 
   let promise_resolver = v8::PromiseResolver::new(scope).unwrap();
   let promise = promise_resolver.get_promise(scope);
@@ -234,10 +235,15 @@ pub fn write<'s>(
     }
   };
 
-  let fd = get_cppgc_handle!(scope, file_wrapper, Option<usize>).unwrap();
   let mut state = state_rc.borrow_mut();
   let task_id = js::TaskId::next();
-  pending::create_fs_write(&mut state, task_id, fd, buf, Box::new(write_cb));
+  pending::create_fs_write(
+    &mut state,
+    task_id,
+    file_rid,
+    buf,
+    Box::new(write_cb),
+  );
 
   rv.set(promise.into());
 }
