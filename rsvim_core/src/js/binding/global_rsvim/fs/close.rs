8ae8bfce69748609a4bcf8e56f824fede21677b7
linrongbin16 <linrongbin16@outlook.com>
2026-05-13 14:41:37 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs/close.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs/close.rs
@@ -4,7 +4,7 @@ use crate::js::resource::ResourceId;
 use crate::js::resource::ResourceTableArc;
 use crate::prelude::*;
 
-pub fn fs_close<'s>(resource_table: ResourceTableArc, rid: ResourceId) {
+pub fn fs_close(resource_table: ResourceTableArc, rid: ResourceId) {
   let mut resource_table = lock!(resource_table);
   let mut handle = resource_table.remove(&rid);
   debug_assert!(handle.is_some());
