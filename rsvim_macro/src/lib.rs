7bcbb5cc5ea743d99c8a07a487595f632741bda6
linrongbin16 <linrongbin16@outlook.com>
2026-05-15 17:21:09 +0800

    c

--- rsvim_macro/src/lib.rs
+++ rsvim_macro/src/lib.rs
@@ -1,1 +1,13 @@
 //! The macros for RSVIM text editor core.
+
+use proc_macro::TokenStream;
+use syn::DeriveInput;
+use syn::parse_macro_input;
+
+#[proc_macro_derive(Builder)]
+/// For `js::converter`
+pub fn to_v8_obj(input: TokenStream) -> TokenStream {
+  let input = parse_macro_input!(input as DeriveInput);
+  println!("to_v8_obj:{:?}", input);
+  TokenStream::default()
+}
