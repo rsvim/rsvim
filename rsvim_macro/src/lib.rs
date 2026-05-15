c437b558b38fc4dae97f518407545d4abfab92ca
linrongbin16 <linrongbin16@outlook.com>
2026-05-15 17:45:14 +0800

    c

--- rsvim_macro/src/lib.rs
+++ rsvim_macro/src/lib.rs
@@ -5,9 +5,15 @@ use syn::DeriveInput;
 use syn::parse_macro_input;
 
 #[proc_macro_derive(ToV8)]
-/// For `js::converter`
+/// Serialize rust struct to v8 js object. A js object is like a key-value map
+/// that contains multiple data fields. In most use cases, we only need 1 layer
+/// map, i.e. all the values are not nested js objects, they are simply plain
+/// data values and array with plain values.
 pub fn to_v8(input: TokenStream) -> TokenStream {
   let input = parse_macro_input!(input as DeriveInput);
   println!("to_v8:{:?}", input);
+  for attr in input.attrs {
+    println!("to_v8 attr:{:?}", attr);
+  }
   TokenStream::default()
 }
