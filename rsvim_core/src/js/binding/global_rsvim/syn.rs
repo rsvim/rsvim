f65edfcbba33cb8930037e3671fa0caf56c1ac44
linrongbin16 <linrongbin16@outlook.com>
2026-05-20 12:58:21 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/syn.rs
+++ rsvim_core/src/js/binding/global_rsvim/syn.rs
@@ -66,10 +66,7 @@ pub fn load_treesitter_parser<'s>(
   mut rv: v8::ReturnValue,
 ) {
   debug_assert!(args.length() == 1);
-  let options = SynLoadTreeSitterParserOptions::from_v8(
-    scope,
-    args.get(0).to_object(scope).unwrap(),
-  );
+  let options = SynLoadTreeSitterParserOptions::from_v8(scope, args.get(0));
   trace!("Rsvim.syn.loadParser:{:?}", options);
 
   let promise_resolver = v8::PromiseResolver::new(scope).unwrap();
