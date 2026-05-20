9be1399b604882137a876e61fb8f1eed5f9372c9
linrongbin16 <linrongbin16@outlook.com>
2026-05-20 13:35:16 +0800

    c

--- rsvim_core/src/js/converter.rs
+++ rsvim_core/src/js/converter.rs
@@ -11,15 +11,15 @@ pub trait U32ToV8 {
   fn to_v8<'s>(
     &self,
     scope: &mut v8::PinScope<'s, '_>,
-  ) -> v8::Local<'s, v8::Integer>;
+  ) -> v8::Local<'s, v8::Value>;
 }
 
 impl U32ToV8 for u32 {
   fn to_v8<'s>(
     &self,
     scope: &mut v8::PinScope<'s, '_>,
-  ) -> v8::Local<'s, v8::Integer> {
-    v8::Integer::new_from_unsigned(scope, *self)
+  ) -> v8::Local<'s, v8::Value> {
+    v8::Integer::new_from_unsigned(scope, *self).into()
   }
 }
 
@@ -48,15 +48,15 @@ pub trait I32ToV8 {
   fn to_v8<'s>(
     &self,
     scope: &mut v8::PinScope<'s, '_>,
-  ) -> v8::Local<'s, v8::Integer>;
+  ) -> v8::Local<'s, v8::Value>;
 }
 
 impl I32ToV8 for i32 {
   fn to_v8<'s>(
     &self,
     scope: &mut v8::PinScope<'s, '_>,
-  ) -> v8::Local<'s, v8::Integer> {
-    v8::Integer::new(scope, *self)
+  ) -> v8::Local<'s, v8::Value> {
+    v8::Integer::new(scope, *self).into()
   }
 }
 
@@ -81,15 +81,15 @@ pub trait NodeIdToV8 {
   fn to_v8<'s>(
     &self,
     scope: &mut v8::PinScope<'s, '_>,
-  ) -> v8::Local<'s, v8::Integer>;
+  ) -> v8::Local<'s, v8::Value>;
 }
 
 impl NodeIdToV8 for NodeId {
   fn to_v8<'s>(
     &self,
     scope: &mut v8::PinScope<'s, '_>,
-  ) -> v8::Local<'s, v8::Integer> {
-    v8::Integer::new(scope, Into::<i32>::into(*self))
+  ) -> v8::Local<'s, v8::Value> {
+    v8::Integer::new(scope, Into::<i32>::into(*self)).into()
   }
 }
 
