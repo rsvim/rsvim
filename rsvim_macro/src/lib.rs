918e54376db5a7556872c408981f12fec74d18d2
linrongbin16 <linrongbin16@outlook.com>
2026-05-21 13:12:26 +0800

    c

--- rsvim_macro/src/lib.rs
+++ rsvim_macro/src/lib.rs
@@ -134,3 +134,129 @@ pub fn to_v8(input: TokenStream) -> TokenStream {
 
   }.into()
 }
+
+#[proc_macro_derive(FromV8)]
+/// Convert js object to rust struct.
+pub fn from_v8(input: TokenStream) -> TokenStream {
+  let input = parse_macro_input!(input as DeriveInput);
+
+  let struct_ident = input.ident;
+  let struct_fields = match input.data {
+    syn::Data::Struct(struct_data) => match struct_data.fields {
+      syn::Fields::Named(named_field) => named_field.named,
+      _ => unreachable!("Failed to derive ToV8 macro on non-named field!"),
+    },
+    _ => unreachable!("Failed to derive ToV8 macro on non-struct data!"),
+  };
+
+  let is_option = |field_type: &syn::Type| match field_type {
+    syn::Type::Path(p) => match p.path.segments.last() {
+      Some(seg) => seg.ident == "Option",
+      None => false,
+    },
+    _ => false,
+  };
+
+  let is_vec = |field_type: &syn::Type| match field_type {
+    syn::Type::Path(p) => match p.path.segments.last() {
+      Some(seg) => seg.ident == "Vec",
+      None => false,
+    },
+    _ => false,
+  };
+
+  let fields = struct_fields
+    .iter()
+    .filter(|n| !is_option(&n.ty) && !is_vec(&n.ty))
+    .map(|n| n.ident.clone().unwrap())
+    .collect::<Vec<_>>();
+  let uppercases = struct_fields
+    .iter()
+    .filter(|n| !is_option(&n.ty) && !is_vec(&n.ty))
+    .map(|n| n.ident.clone().unwrap())
+    .map(|i| format_ident!("{}", i.to_string().to_uppercase()))
+    .collect::<Vec<_>>();
+  let values = struct_fields
+    .iter()
+    .filter(|n| !is_option(&n.ty) && !is_vec(&n.ty))
+    .map(|n| n.ident.clone().unwrap())
+    .map(|i| format_ident!("{}_value", i))
+    .collect::<Vec<_>>();
+
+  let optional_fields = struct_fields
+    .iter()
+    .filter(|n| is_option(&n.ty) && !is_vec(&n.ty))
+    .map(|n| n.ident.clone().unwrap())
+    .collect::<Vec<_>>();
+  let optional_uppercases = struct_fields
+    .iter()
+    .filter(|n| is_option(&n.ty) && !is_vec(&n.ty))
+    .map(|n| n.ident.clone().unwrap())
+    .map(|i| format_ident!("{}", i.to_string().to_uppercase()))
+    .collect::<Vec<_>>();
+  let optional_values = struct_fields
+    .iter()
+    .filter(|n| is_option(&n.ty) && !is_vec(&n.ty))
+    .map(|n| n.ident.clone().unwrap())
+    .map(|i| format_ident!("{}_value", i))
+    .collect::<Vec<_>>();
+
+  let vec_fields = struct_fields
+    .iter()
+    .filter(|n| !is_option(&n.ty) && is_vec(&n.ty))
+    .map(|n| n.ident.clone().unwrap())
+    .collect::<Vec<_>>();
+  let vec_uppercases = struct_fields
+    .iter()
+    .filter(|n| !is_option(&n.ty) && is_vec(&n.ty))
+    .map(|n| n.ident.clone().unwrap())
+    .map(|i| format_ident!("{}", i.to_string().to_uppercase()))
+    .collect::<Vec<_>>();
+  let vec_values = struct_fields
+    .iter()
+    .filter(|n| !is_option(&n.ty) && is_vec(&n.ty))
+    .map(|n| n.ident.clone().unwrap())
+    .map(|i| format_ident!("{}_value", i))
+    .collect::<Vec<_>>();
+
+  quote! {
+
+  impl crate::js::converter::ToV8 for #struct_ident {
+    fn to_v8<'s>(
+      &self,
+      scope: &mut v8::PinScope<'s, '_>,
+    ) -> v8::Local<'s, v8::Value> {
+      use crate::js::binding;
+
+      let obj = v8::Object::new(scope);
+
+      #(
+      {
+        let #values = self.#fields.to_v8(scope);
+        binding::set_property_to(scope, obj, #uppercases, #values);
+      }
+      )*
+
+      #(
+      {
+        if let Some(#optional_fields) = &self.#optional_fields {
+          let #optional_values = #optional_fields.to_v8(scope);
+          binding::set_property_to(scope, obj, #optional_uppercases, #optional_values);
+        }
+      }
+      )*
+
+      #(
+      {
+        let #vec_values = self.#vec_fields.to_v8(scope, |scope, i| i.to_v8(scope));
+        binding::set_property_to(scope, obj, #vec_uppercases, #vec_values);
+      }
+      )*
+
+
+      obj.into()
+    }
+  }
+
+  }.into()
+}
