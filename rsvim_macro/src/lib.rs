2be73d84db032d79d33089d12b94bfe2bf62904b
linrongbin16 <linrongbin16@outlook.com>
2026-05-21 15:01:25 +0800

    c

--- rsvim_macro/src/lib.rs
+++ rsvim_macro/src/lib.rs
@@ -7,10 +7,11 @@ use syn::DeriveInput;
 use syn::parse_macro_input;
 
 #[proc_macro_derive(ToV8)]
-/// Convert rust struct to js object. A js object is like a key-value map
-/// that contains multiple data fields. In most use cases, we only need 1 layer
-/// map, i.e. all the values are not nested js objects, they are simply plain
-/// data values or array with plain values.
+/// Convert rust struct to js object.
+/// A js object is like a key-value map that contains multiple data fields. In
+/// most use cases, we only need 1 layer map, i.e. all the values are not
+/// nested js objects, they are simply plain data values or array with plain
+/// values.
 pub fn to_v8(input: TokenStream) -> TokenStream {
   let input = parse_macro_input!(input as DeriveInput);
 
