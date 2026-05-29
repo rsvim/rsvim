451ebc9991e653eaf07b4bf9e268e94acfa32d13
linrongbin16 <linrongbin16@outlook.com>
2026-05-29 15:49:25 +0800

    c

---
+++ rsvim_core/src/js/binding/global_rsvim/proc_tests.rs
@@ -0,0 +1,43 @@
+use crate::cli::CliOptions;
+use crate::prelude::*;
+use crate::tests::evloop::*;
+use crate::tests::log::init as test_log_init;
+use std::time::Duration;
+
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
+async fn test_new_command1() -> IoResult<()> {
+  test_log_init();
+
+  let terminal_cols = 10_u16;
+  let terminal_rows = 10_u16;
+  let mocked_ops = vec![MockOperation::SleepFor(Duration::from_millis(30))];
+
+  let src: &str = r#"
+  setTimeout(() => {
+    Rsvim.rt.exit();
+  }, 1);
+    "#;
+
+  // Prepare $RSVIM_CONFIG/rsvim.js
+  let _tp = make_configs(vec![(Path::new("rsvim.js"), src)]);
+
+  let mut event_loop =
+    make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+  // Before running
+  {}
+
+  event_loop.initialize()?;
+  let exit_code = event_loop
+    .run_with_mock_operations(MockOperationReader::new(mocked_ops))
+    .await?;
+  event_loop.shutdown()?;
+
+  // After running
+  {
+    assert_eq!(exit_code, 0);
+  }
+
+  Ok(())
+}
