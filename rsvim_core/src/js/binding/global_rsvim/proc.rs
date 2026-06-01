07469ee325251efa9e061ce1266c0a7f30f668e2
linrongbin16 <linrongbin16@outlook.com>
2026-06-01 17:35:08 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/proc.rs
+++ rsvim_core/src/js/binding/global_rsvim/proc.rs
@@ -8,6 +8,7 @@ use crate::is_v8_str;
 use crate::js::JsRuntime;
 use crate::js::converter::*;
 use crate::prelude::*;
+use proc_command::ProcCommandOptions;
 
 /// The `spawn` method in `Rsvim.proc.Command` class.
 pub fn spawn<'s>(
@@ -18,13 +19,16 @@ pub fn spawn<'s>(
   debug_assert!(args.length() == 2);
   debug_assert!(is_v8_str!(args.get(0)));
   let exec_path = args.get(0).to_rust_string_lossy(scope);
-  trace!("spawn exec_path: {:?}", exec_path);
+  let options = ProcCommandOptions::from_v8(scope, args.get(1));
+  trace!("spawn exec_path: {:?}, options: {:?}", exec_path, options);
 
   let state_rc = JsRuntime::state(scope);
   let state = state_rc.borrow();
 
   chan::send_to_master(
     state.master_tx.clone(),
-    MasterMessage::ExitReq(chan::ExitReq { exit_code }),
+    MasterMessage::SpawnChildProcessReq(chan::SpawnChildProcessReq {
+      exit_code,
+    }),
   );
 }
