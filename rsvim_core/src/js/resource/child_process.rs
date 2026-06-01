2f6957963821b67d5bf212c6a5650702960a8580
linrongbin16 <linrongbin16@outlook.com>
2026-06-01 17:25:43 +0800

    c

--- rsvim_core/src/js/resource/child_process.rs
+++ rsvim_core/src/js/resource/child_process.rs
@@ -2,38 +2,9 @@
 
 use crate::js::resource::ResourceId;
 use crate::js::resource::Resourcify;
-use crate::prelude::*;
 use std::process::Child;
-use std::process::Command;
 use std::sync::Arc;
 use std::sync::Mutex;
-
-#[derive_where::derive_where(Debug)]
-#[derive(Clone)]
-pub struct ChildProcessResource {
-  id: ResourceId,
-  #[derive_where(skip)]
-  data: Arc<Mutex<Command>>,
-}
-
-impl ChildProcessResource {
-  pub fn new(data: Command) -> Self {
-    Self {
-      id: ResourceId::next(),
-      data: Arc::new(Mutex::new(data)),
-    }
-  }
-
-  pub fn data(&self) -> Arc<Mutex<Command>> {
-    self.data.clone()
-  }
-}
-
-impl Resourcify for ChildProcessResource {
-  fn id(&self) -> ResourceId {
-    self.id
-  }
-}
 
 #[derive_where::derive_where(Debug)]
 #[derive(Clone)]
