c1c29d18b8957a7a5debc0d3ec3899655202cf1b
linrongbin16 <linrongbin16@outlook.com>
2026-06-01 12:05:08 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/proc/proc_command_opt.rs
+++ rsvim_core/src/js/binding/global_rsvim/proc/proc_command_opt.rs
@@ -1,8 +1,52 @@
 //! Child-process command options.
 
+use crate::js::converter::*;
 use crate::prelude::*;
 use compact_str::CompactString;
-use compact_str::ToCompactString;
+use std::str::FromStr;
+
+#[derive(
+  Debug,
+  Copy,
+  Clone,
+  PartialEq,
+  Eq,
+  PartialOrd,
+  Ord,
+  Hash,
+  strum_macros::Display,
+  strum_macros::EnumString,
+)]
+pub enum Stdio {
+  #[strum(serialize = "null")]
+  Null,
+
+  #[strum(serialize = "piped")]
+  Piped,
+
+  #[strum(serialize = "inherit")]
+  Inherit,
+}
+
+impl FromV8 for Stdio {
+  fn from_v8<'s>(
+    scope: &mut v8::PinScope<'s, '_>,
+    value: v8::Local<'s, v8::Value>,
+  ) -> Self {
+    debug_assert!(value.is_string() || value.is_string_object());
+    let result = value.to_string(scope).unwrap().to_rust_string_lossy(scope);
+    Stdio::from_str(&result).unwrap()
+  }
+}
+
+impl ToV8 for Stdio {
+  fn to_v8<'s>(
+    &self,
+    scope: &mut v8::PinScope<'s, '_>,
+  ) -> v8::Local<'s, v8::Value> {
+    self.to_string().to_v8(scope)
+  }
+}
 
 #[derive(
   Debug,
@@ -26,12 +70,12 @@ pub struct ProcCommandOptions {
   #[builder(default = FoldMap::new())]
   pub env: FoldMap<CompactString, CompactString>,
 
-  #[builder(default = "null".to_compact_string())]
-  pub stdin: CompactString,
+  #[builder(default = Stdio::Null)]
+  pub stdin: Stdio,
 
-  #[builder(default = "piped".to_compact_string())]
-  pub stdout: CompactString,
+  #[builder(default = Stdio::Piped)]
+  pub stdout: Stdio,
 
-  #[builder(default = "piped".to_compact_string())]
-  pub stderr: CompactString,
+  #[builder(default = Stdio::Piped)]
+  pub stderr: Stdio,
 }
