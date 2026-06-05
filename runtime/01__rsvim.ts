b5b6e3bab1def5d8e32c5612719088cdf5de6436
linrongbin16 <linrongbin16@outlook.com>
2026-06-05 16:54:59 +0800

    chore(js): rename methods api (#1159)

--- runtime/01__rsvim.ts
+++ runtime/01__rsvim.ts
@@ -1549,9 +1549,7 @@ export namespace RsvimProc {
     async text(): Promise<string> {
       const payload =
         // @ts-ignore Ignore warning
-        await __InternalRsvimGlobalObject.proc_read_text_from_child_process_stdio(
-          this.#rid,
-        );
+        await __InternalRsvimGlobalObject.proc_read_text_from_child(this.#rid);
       return payload;
     }
   }
