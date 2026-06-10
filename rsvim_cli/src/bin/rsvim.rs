ddef228a420e1c9907d34fba7994d7d34319acae
linrongbin16 <linrongbin16@outlook.com>
2026-06-10 13:05:12 +0800

    c

--- rsvim_cli/src/bin/rsvim.rs
+++ rsvim_cli/src/bin/rsvim.rs
@@ -24,8 +24,16 @@ use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
 use tokio::time::Instant;
 
-const RSVIM_SNAPSHOT: &[u8] =
-  include_bytes!(concat!(env!("OUT_DIR"), "/RSVIM_SNAPSHOT.BIN"));
+static RSVIM_SNAPSHOT: LazyLock<Box<[u8]>> = LazyLock::new(|| {
+  static COMPRESSED_BYTES: &[u8] =
+    include_bytes!(concat!(env!("OUT_DIR"), "/RSVIM_SNAPSHOT.BIN"));
+  zstd::bulk::decompress(
+    &COMPRESSED_BYTES[4..],
+    u32::from_le_bytes(COMPRESSED_BYTES[0..4].try_into().unwrap()) as usize,
+  )
+  .unwrap()
+  .into_boxed_slice()
+});
 
 static RSVIM_VERSION: LazyLock<String> = LazyLock::new(|| {
   let version_tags =
@@ -111,7 +119,7 @@ fn main() -> IoResult<()> {
       startup_moment,
       startup_unix_epoch,
       cli_opts,
-      SnapshotData::new(RSVIM_SNAPSHOT),
+      SnapshotData::new(&RSVIM_SNAPSHOT),
     )?;
 
     // Initialize.
