e4ba95fd2be0ca0afb9785a2fffe770058b96c69
linrongbin16 <linrongbin16@outlook.com>
2026-06-11 12:48:07 +0800

    c

--- runtime/01__rsvim.ts
+++ runtime/01__rsvim.ts
@@ -794,6 +794,103 @@ export namespace RsvimFs {
   }
 
   /**
+   * Create symbolic link from a file path.
+   *
+   * On Windows platforms, you should specify the 3rd parameter "options" with below 3 options:
+   *
+   * - `"file"` or `"dir"`: Pointing to [CreateSymbolicLinkW](https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-createsymboliclinkw) function.
+   * - `"junction"`: NTFS v5+ features roughly equivalent to Unix directory symbolic links.
+   *
+   * @param {string} oldpath - Original file path.
+   * @param {string} newpath - New symbolic link that pointing to the original file.
+   * @param {RsvimFs.SymlinkOptions} options - (Optional) Only used on Windows platforms, will be ignored on Unix platforms. By default is `"junction"`.
+   * @returns {Promise<void>} It resolves to nothing.
+   *
+   * @throws Throws {@link !TypeError} if any parameter is invalid. Or throws {@link Error} if failed to create symbolic link from the file.
+   *
+   * @example
+   * ```javascript
+   * try {
+   *   // Linux
+   *   await Rsvim.fs.symlink("README.md", "linked-README.md");
+   *   // Windows
+   *   await Rsvim.fs.symlink("README.md", "linked-README.md", "junction");
+   *   Rsvim.cmd.echo(`Created symbolic link "linked-README.md" pointing to "README.md"`);
+   * } catch (e) {
+   *   Rsvim.cmd.echo(`Failed to create symbolic link pointing to "README.md": ${e}`);
+   * }
+   * ```
+   */
+  export async function symlink(
+    oldpath: string,
+    newpath: string,
+    options?: RsvimFs.SymlinkOptions,
+  ): Promise<void> {
+    checkIsString(oldpath, `"Rsvim.fs.symlink" oldpath`);
+    checkIsString(newpath, `"Rsvim.fs.symlink" newpath`);
+
+    options = options ?? "junction";
+    if (!isNull(options)) {
+      checkIsOptions(
+        options,
+        ["file", "dir", "junction"],
+        `"Rsvim.fs.symlink" options`,
+      );
+    }
+
+    // @ts-ignore Ignore warning
+    return await __InternalRsvimGlobalObject.fs_symlink(
+      oldpath,
+      newpath,
+      options,
+    );
+  }
+
+  /**
+   * Sync version of `symlink`.
+   *
+   * @param {string} oldpath - Original file path.
+   * @param {string} newpath - New symbolic link that pointing to the original file.
+   * @param {RsvimFs.SymlinkOptions} options - (Optional) Only used on Windows platforms, will be ignored on Unix platforms. By default is `"junction"`.
+   * @returns {Promise<void>} It resolves to nothing.
+   *
+   * @throws Throws {@link !TypeError} if any parameter is invalid. Or throws {@link Error} if failed to create symbolic link from the file.
+   *
+   * @example
+   * ```javascript
+   * try {
+   *   // Linux
+   *   Rsvim.fs.symlinkSync("README.md", "linked-README.md");
+   *   // Windows
+   *   Rsvim.fs.symlinkSync("README.md", "linked-README.md", "junction");
+   *   Rsvim.cmd.echo(`Created symbolic link "linked-README.md" pointing to "README.md"`);
+   * } catch (e) {
+   *   Rsvim.cmd.echo(`Failed to create symbolic link pointing to "README.md": ${e}`);
+   * }
+   * ```
+   */
+  export function symlinkSync(
+    oldpath: string,
+    newpath: string,
+    options?: RsvimFs.SymlinkOptions,
+  ): void {
+    checkIsString(oldpath, `"Rsvim.fs.symlink" oldpath`);
+    checkIsString(newpath, `"Rsvim.fs.symlink" newpath`);
+
+    options = options ?? "junction";
+    if (!isNull(options)) {
+      checkIsOptions(
+        options,
+        ["file", "dir", "junction"],
+        `"Rsvim.fs.symlink" options`,
+      );
+    }
+
+    // @ts-ignore Ignore warning
+    __InternalRsvimGlobalObject.fs_symlink_sync(oldpath, newpath, options);
+  }
+
+  /**
    * Open options.
    *
    * :::tip
@@ -1258,6 +1355,12 @@ export namespace RsvimFs {
      */
     blocks?: number;
   };
+
+  /**
+   * @see {@link RsvimFs.symlink}
+   * @inline
+   * */
+  export type SymlinkOptions = "file" | "dir" | "junction";
 }
 
 export namespace RsvimOpt {
