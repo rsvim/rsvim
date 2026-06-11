748face819e63def2637c29dc71e1ac54ab97584
linrongbin16 <linrongbin16@outlook.com>
2026-06-11 17:39:06 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs_tests.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs_tests.rs
@@ -878,6 +878,7 @@ async fn test_fs_stat3() -> IoResult<()> {
 }
 
 #[tokio::test]
+#[cfg(target_family = "unix")]
 #[cfg_attr(miri, ignore)]
 async fn test_fs_symlink1() -> IoResult<()> {
   test_log_init();
@@ -941,6 +942,7 @@ async fn test_fs_symlink1() -> IoResult<()> {
 }
 
 #[tokio::test]
+#[cfg(target_family = "unix")]
 #[cfg_attr(miri, ignore)]
 async fn test_fs_symlink2() -> IoResult<()> {
   test_log_init();
@@ -998,6 +1000,63 @@ async fn test_fs_symlink2() -> IoResult<()> {
 
     let actual = contents.message_history_mut().pop().unwrap();
     assert_eq!(actual, "Hello, World!");
+  }
+
+  Ok(())
+}
+
+#[tokio::test]
+#[cfg(target_family = "windows")]
+#[cfg_attr(miri, ignore)]
+async fn test_fs_symlink3() -> IoResult<()> {
+  test_log_init();
+
+  let terminal_cols = 10_u16;
+  let terminal_rows = 10_u16;
+  let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(30000))];
+  let tmp = assert_fs::TempDir::new().unwrap();
+  let original = tmp.child("hello.txt");
+  original.touch().unwrap();
+  original.write_str("Hello, World!").unwrap();
+  let link1 = tmp.child("link1.txt");
+  let link2 = tmp.child("link2.txt");
+
+  let src = format!(
+    r#"
+  await Rsvim.fs.symlink({:?}, {:?});
+  Rsvim.fs.symlinkSync({:?}, {:?});
+  Rsvim.rt.exit();
+    "#,
+    original.path(),
+    link1.path(),
+    original.path(),
+    link2.path(),
+  );
+  info!("src:{:?}", src);
+
+  // Prepare $RSVIM_CONFIG/rsvim.js
+  let _tp = make_configs(vec![(Path::new("rsvim.js"), &src)]);
+
+  let mut event_loop =
+    make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+  event_loop.initialize()?;
+  event_loop
+    .run_with_mock_events(MockEventReader::new(mocked_events))
+    .await?;
+  event_loop.shutdown()?;
+
+  // After running
+  {
+    let mut contents = lock!(event_loop.cmdline_text);
+    let n = contents.message_history().len();
+    assert_eq!(n, 0);
+
+    // let actual = contents.message_history_mut().pop().unwrap();
+    // assert_eq!(actual, "Hello, World!");
+    //
+    // let actual = contents.message_history_mut().pop().unwrap();
+    // assert_eq!(actual, "Hello, World!");
   }
 
   Ok(())
